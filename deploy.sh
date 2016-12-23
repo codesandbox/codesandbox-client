afea6b82baa1d5d40ce8200bba5148b8b9ba52c6
Ives van Hoorne <i.vanhoorne@catawiki.nl>
2016-12-24 00:08:15 +0100

    Update ssh address

--- deploy.sh
+++ deploy.sh
@@ -13,10 +13,10 @@ echo "Tarring build files"
 tar -C .deliver -zcvf .deliver/code_sandbox_client.tar.gz www
 
 echo "Cleaning remote tar"
-ssh nginx@codesandbox.io "rm -f code_sandbox_client.tar.gz"
+ssh nginx@ssh.codesandbox.io "rm -f code_sandbox_client.tar.gz"
 
 echo "Moving tar to server"
-scp .deliver/code_sandbox_client.tar.gz nginx@codesandbox.io:./
+scp .deliver/code_sandbox_client.tar.gz nginx@ssh.codesandbox.io:./
 
 echo "Untarring www files"
-ssh nginx@codesandbox.io "tar -xzf ./code_sandbox_client.tar.gz"
+ssh nginx@ssh.codesandbox.io "tar -xzf ./code_sandbox_client.tar.gz"
