67a0f0a1273c6fc36704cdfa5a4fa23133d9b717
Ives van Hoorne <i.vanhoorne@catawiki.nl>
2017-01-15 18:50:51 +0100

    Version input styling and dot handling

--- src/app/pages/SandboxView/Editor/Workspace/Versions/index.js
+++ src/app/pages/SandboxView/Editor/Workspace/Versions/index.js
@@ -15,6 +15,7 @@ import { versionsBySandboxSelector } from '../../../../../store/entities/version
 import EntryContainer from '../EntryContainer';
 import WorkspaceInputContainer from '../WorkspaceInputContainer';
 import Button from '../../../../../components/buttons/Button';
+import Relative from '../../../../../components/Relative';
 
 type Props = {
   sandbox: Sandbox;
@@ -44,6 +45,23 @@ const Icon = styled.span`
   vertical-align: middle;
 `;
 
+const Inputs = styled.div`
+  input {
+    margin: 0;
+    text-align: center;
+    box-sizing: border-box;
+    height: 100%;
+    width: 100%;
+  }
+`;
+
+const Dot = styled.div`
+  position: absolute;
+  color: ${props => props.theme.white};
+  right: 0;
+  bottom: 0.4rem;
+`;
+
 const mapDispatchToProps = dispatch => ({
   versionActions: bindActionCreators(versionEntity.actions, dispatch),
 });
@@ -53,8 +71,59 @@ const mapStateToProps = (state, props: Props) => ({
 class Versions extends React.PureComponent {
   props: Props;
 
+  major: ?HTMLInputElement;
+  minor: ?HTMLInputElement;
+  patch: ?HTMLInputElement;
+
+  state = {
+    major: '',
+    minor: '',
+    patch: '',
+  };
+
+  handleMajorKey = (e: KeyboardEvent) => {
+    if (e.keyCode === 190) {
+      // dot
+      if (this.minor != null) this.minor.focus();
+    }
+  };
+
+  handleMinorKey = (e: KeyboardEvent) => {
+    if (e.keyCode === 190) {
+      // dot
+      if (this.patch != null) this.patch.focus();
+    } else if (e.keyCode === 8) {
+      // backspace
+      const { minor } = this.state;
+      if (minor === '' && this.major != null) {
+        // Prevent backspace in previous field
+        this.major.focus();
+        e.preventDefault();
+      }
+    }
+  };
+
+  handlePatchKey = (e: KeyboardEvent) => {
+    if (e.keyCode === 8) {
+      // backspace
+      const { patch } = this.state;
+      if (patch === '' && this.minor != null) {
+        // Prevent backspace in previous field
+        this.minor.focus();
+        e.preventDefault();
+      }
+    }
+  }
+
+  isValid = (n: string) => n === '' || /^[0-9]+$/.test(n);
+
+  setMajor = e => this.isValid(e.target.value) && this.setState({ major: e.target.value });
+  setMinor = e => this.isValid(e.target.value) && this.setState({ minor: e.target.value });
+  setPatch = e => this.isValid(e.target.value) && this.setState({ patch: e.target.value });
+
   render() {
     const { versions } = this.props;
+    const { major, minor, patch } = this.state;
     return (
       <div>
         <WorkspaceTitle>Versions</WorkspaceTitle>
@@ -63,12 +132,40 @@ class Versions extends React.PureComponent {
           for others to use as a dependency.
         </Description>
 
-        <WorkspaceInputContainer>
-          <input placeholder="0" style={{ textAlign: 'center', flex: 1 }} />
-          <input placeholder="0" style={{ textAlign: 'center', flex: 1 }} />
-          <input placeholder="0" style={{ textAlign: 'center', flex: 1 }} />
-          <Button small style={{ flex: 5, marginLeft: '0.25rem' }}>Publish</Button>
-        </WorkspaceInputContainer>
+        <Inputs>
+          <WorkspaceInputContainer>
+            <Relative style={{ flex: 1 }}>
+              <input
+                placeholder="0"
+                ref={(e) => { this.major = e; }}
+                value={major}
+                onChange={this.setMajor}
+                onKeyDown={this.handleMajorKey}
+              />
+              <Dot>.</Dot>
+            </Relative>
+            <Relative style={{ flex: 1 }}>
+              <input
+                placeholder="0"
+                ref={(e) => { this.minor = e; }}
+                value={minor}
+                onChange={this.setMinor}
+                onKeyDown={this.handleMinorKey}
+              />
+              <Dot>.</Dot>
+            </Relative>
+            <Relative style={{ flex: 1 }}>
+              <input
+                placeholder="0"
+                ref={(e) => { this.patch = e; }}
+                value={patch}
+                onChange={this.setPatch}
+                onKeyDown={this.handlePatchKey}
+              />
+            </Relative>
+            <Button small style={{ flex: 4, marginLeft: '0.25rem' }}>Publish</Button>
+          </WorkspaceInputContainer>
+        </Inputs>
 
         {versions.map(v => (
           <EntryContainer key={v.version}>
