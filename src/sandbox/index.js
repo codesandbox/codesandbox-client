ed7c5f9071518e6d248f46e0fb5a4029cd466d22
Ives van Hoorne <i.vanhoorne@catawiki.nl>
2017-03-19 15:58:04 +0100

    Support for replacestate

--- src/sandbox/index.js
+++ src/sandbox/index.js
@@ -6,6 +6,7 @@ import {
   evalBoilerplates,
   findBoilerplate,
 } from './boilerplates';
+import { search } from 'babel-standalone';
 
 let fetching = false;
 let url = null;
@@ -100,7 +101,29 @@ function setupHistoryListeners() {
     return pushState.apply(window.history, arguments);
   };
 
+  const replaceState = window.history.replaceState;
+  window.history.replaceState = function(state) {
+    if (typeof history.onpushstate === 'function') {
+      window.history.onpushstate({ state });
+    }
+    // ... whatever else you want to do
+    // maybe call onhashchange e.handler
+    return replaceState.apply(window.history, arguments);
+  };
+
   history.onpushstate = e => {
+    setTimeout(() => {
+      window.parent.postMessage(
+        {
+          type: 'urlchange',
+          url: document.location.pathname + location.search,
+        },
+        '*'
+      );
+    });
+  };
+
+  history.onreplacestate = e => {
     setTimeout(() => {
       window.parent.postMessage(
         {
