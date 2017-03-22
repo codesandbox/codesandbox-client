89ec086c83046dba720451e476000dd4b91f5af0
Ives van Hoorne <i.vanhoorne@catawiki.nl>
2017-03-22 20:33:12 +0100

    Add tooltip to prettify button

--- src/app/pages/SandboxView/Sandbox/Editor/Content/subviews/CodeEditor/Header.js
+++ src/app/pages/SandboxView/Sandbox/Editor/Content/subviews/CodeEditor/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import Tooltip from 'app/components/Tooltip';
 
 import Button from '../../../../../../../components/buttons/Button';
 
@@ -22,9 +23,10 @@ const Path = styled.span`
 `;
 
 const Buttons = styled.div`
+  display: flex;
   button {
     width: 6rem;
-    margin-left: 1rem;
+    margin: 0.5rem;
   }
 `;
 
@@ -43,9 +45,11 @@ export default ({ path, title, saveComponent, prettify }: Props) => (
     </div>
 
     <Buttons>
-      <Button onClick={prettify} small>
-        Prettify
-      </Button>
+      <Tooltip bottom offset={-25} message="Made possible by Prettier">
+        <Button onClick={prettify} small>
+          Prettify
+        </Button>
+      </Tooltip>
       <Button disabled={!saveComponent} onClick={saveComponent} small>
         Save
       </Button>
