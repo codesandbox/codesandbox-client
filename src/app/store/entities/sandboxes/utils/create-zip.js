13d5ffa0efdc3236219b5dbb45084e5882578133
Ives van Hoorne <i.vanhoorne@catawiki.nl>
2017-05-19 10:02:27 +0200

    Update create-react-app

--- src/app/store/entities/sandboxes/utils/create-zip.js
+++ src/app/store/entities/sandboxes/utils/create-zip.js
@@ -6,6 +6,9 @@ import type { Sandbox, Module, Directory } from 'common/types';
 
 // $FlowIssue
 import favicon from '!base64-loader!./favicon.ico'; // eslint-disable-line
+import gitignore from '!raw-loader!./.gitignore';
+import manifest from '!raw-loader!./manifest.json';
+import serviceWorker from '!raw-loader!./createServiceWorker';
 // $FlowIssue
 import README from './README.md';
 
@@ -41,10 +44,16 @@ const getHTML = (modules, resources) =>
 <html lang="en">
   <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="theme-color" content="#000000">
+    <!--
+      manifest.json provides metadata used when your web app is added to the
+      homescreen on Android. See https://developers.google.com/web/fundamentals/engage-and-retain/web-app-manifest/
+    -->
+    <link rel="manifest" href="%PUBLIC_URL%/manifest.json">
     <link rel="shortcut icon" href="%PUBLIC_URL%/favicon.ico">
     <!--
-      Notice the use of %PUBLIC_URL% in the tag above.
+      Notice the use of %PUBLIC_URL% in the tags above.
       It will be replaced with the URL of the \`public\` folder during the build.
       Only files inside the \`public\` folder can be referenced from the HTML.
 
@@ -57,8 +66,19 @@ const getHTML = (modules, resources) =>
   </head>
   <body>
     ${getIndexHtmlBody(modules)}
+    <!--
+      This HTML file is a template.
+      If you open it directly in the browser, you will see an empty page.
+
+      You can add webfonts, meta tags, or analytics to this file.
+      The build step will place the bundled scripts into the <body> tag.
+
+      To begin the development, run \`npm start\` or \`yarn start\`.
+      To create a production bundle, use \`npm run build\` or \`yarn build\`.
+    -->
   </body>
-</html>`;
+</html>
+`;
 
 function slugify(text) {
   const a = 'àáäâèéëêìíïîòóöôùúüûñçßÿœæŕśńṕẃǵǹḿǘẍźḧ·/_,:;';
@@ -90,7 +110,7 @@ function createPackageJSON(sandbox: Sandbox) {
       version,
       dependencies: sandbox.npmDependencies,
       devDependencies: {
-        'react-scripts': '0.9.5',
+        'react-scripts': '1.0.0',
       },
       scripts: {
         start: 'react-scripts start',
@@ -129,6 +149,7 @@ export default (async function createZip(
   const zip = new JSZip();
 
   const src = zip.folder('src');
+  src.file('createServiceWorker.js', serviceWorker);
   modules
     .filter(x => x.directoryShortid == null)
     .filter(x => x.title !== 'index.html') // This will be included in the body
@@ -145,6 +166,7 @@ export default (async function createZip(
   });
 
   publicFolder.file('index.html', getHTML(modules, sandbox.externalResources));
+  publicFolder.file('manifest.json', manifest);
 
   if (
     !modules.find(x => x.directoryShortid == null && x.title === 'README.md')
@@ -152,8 +174,9 @@ export default (async function createZip(
     zip.file('README.md', README);
   }
   zip.file('package.json', createPackageJSON(sandbox));
+  zip.file('.gitignore', gitignore);
 
   const file = await zip.generateAsync({ type: 'blob' });
 
-  saveAs(file, `${sandbox.title || sandbox.id}.zip`);
+  saveAs(file, `${slugify(sandbox.title || sandbox.id)}.zip`);
 });
