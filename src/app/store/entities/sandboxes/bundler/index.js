f9bb4d891d9bd7f84eb17986ff06c343ede2666f
Ives van Hoorne <i.vanhoorne@catawiki.nl>
2017-06-09 12:05:05 +0200

    Update to new packager

--- src/app/store/entities/sandboxes/bundler/index.js
+++ src/app/store/entities/sandboxes/bundler/index.js
@@ -9,7 +9,7 @@ import logError from '../../../../utils/error';
 
 const debug = _debug('cs:app:packager');
 
-export const PACKAGER_URL = 'https://cdn.jsdelivr.net/webpack/v4';
+export const PACKAGER_URL = 'https://cdn.jsdelivr.net/webpack/v5';
 
 const RETRY_COUNT = 10;
 
