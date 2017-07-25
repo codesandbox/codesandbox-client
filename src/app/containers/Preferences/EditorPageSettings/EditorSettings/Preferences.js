ca8cf5d046f354f6e995d71da44a12c9d987fc04
Ade Viankakrisna Fadlil <viankakrisna@gmail.com>
2017-07-25 23:27:38 +0700

    remove editors from preferences label

--- src/app/containers/Preferences/EditorPageSettings/EditorSettings/Preferences.js
+++ src/app/containers/Preferences/EditorPageSettings/EditorSettings/Preferences.js
@@ -65,20 +65,20 @@ const Preferences = ({ preferences, preferencesActions }: Props) => {
         />
         <Rule />
         <PaddedPreference
-          title="Editor font size"
+          title="Font size"
           type="number"
           {...bindValue('fontSize')}
         />
         <Rule />
         <PaddedPreference
-          title="Editor font family"
+          title="Font family"
           type="string"
           placeholder="Source Code Pro"
           {...bindValue('fontFamily')}
         />
         <Rule />
         <PaddedPreference
-          title="Editor line height"
+          title="Line height"
           type="number"
           placeholder="1.15"
           step="0.05"
