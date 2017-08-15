0f4db8b8f7a9473c32fce56d6b344b1e4c3d04b9
Ives van Hoorne <i.vanhoorne@catawiki.nl>
2017-08-15 22:45:51 +0200

    Fix fallback whitelist

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -329,9 +329,7 @@ if (__PROD__) {
       minify: true,
       // For unknown URLs, fallback to the index page
       navigateFallback: publicPath + 'app.html',
-      // Ignores URLs starting from /__ (useful for Firebase):
-      // https://github.com/facebookincubator/create-react-app/issues/2237#issuecomment-302693219
-      navigateFallbackWhitelist: [/^(?!\/__).*/],
+      navigateFallbackWhitelist: [/^\/s\//],
       // Don't precache sourcemaps (they're large) and build asset manifest:
       staticFileGlobsIgnorePatterns: [/\.map$/, /asset-manifest\.json$/],
       maximumFileSizeToCacheInBytes: 5242880,
