57ca5abd9fa7d36fe2e61e599cedd4be7c0dec97
Ives van Hoorne <i.vanhoorne@catawiki.nl>
2017-08-21 00:15:23 +0200

    Fix tests

--- src/app/utils/test/render.js
+++ src/app/utils/test/render.js
@@ -1,7 +1,17 @@
+import React from 'react';
 import renderer from 'react-test-renderer';
 import 'jest-styled-components';
 
+import { ThemeProvider } from 'styled-components';
+import theme from 'common/theme';
+
 export default Component => {
-  const tree = renderer.create(Component).toJSON();
+  const tree = renderer
+    .create(
+      <ThemeProvider theme={theme}>
+        {Component}
+      </ThemeProvider>,
+    )
+    .toJSON();
   expect(tree).toMatchSnapshot();
 };
