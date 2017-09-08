470339e2716b5396e48f0aca630645922ae68a05
Ives van Hoorne <i.vanhoorne@catawiki.nl>
2017-09-08 21:59:21 +0200

    Create .vue files with default template

--- src/app/store/entities/sandboxes/modules/entity.js
+++ src/app/store/entities/sandboxes/modules/entity.js
@@ -1,13 +1,37 @@
 // @flow
 import { schema } from 'normalizr';
 
+const getDefaultCode = module => {
+  if (module.title.endsWith('.vue')) {
+    return `<template>
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
+`;
+  }
+
+  return '';
+};
+
 export default new schema.Entity(
   'modules',
   {},
   {
-    processStrategy: module => ({
-      ...module,
-      errors: [],
-    }),
+    processStrategy: module => {
+      const newCode = getDefaultCode(module);
+
+      const isNotSynced = newCode !== '';
+      return {
+        ...module,
+        errors: [],
+        code: module.code || newCode,
+        isNotSynced: module.code ? false : isNotSynced,
+      };
+    },
   }
 );
