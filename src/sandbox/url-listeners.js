efb1a60a25e95b63168c8cdf38fd0dfd3303fdd2
Bogdan Luca <luca.bogdan@gmail.com>
2017-09-14 01:38:55 +0300

    [WIP] Sandbox preview: replace window.history with our own emulated history, when inside the editor.

--- src/sandbox/url-listeners.js
+++ src/sandbox/url-listeners.js
@@ -1,4 +1,4 @@
-import { dispatch } from 'codesandbox-api';
+import { dispatch, isStandalone } from 'codesandbox-api';
 
 function sendUrlChange(url: string) {
   dispatch({
@@ -7,40 +7,129 @@ function sendUrlChange(url: string) {
   });
 }
 
+/* eslint-disable no-console */
+
+const origHistoryProto = window.history.__proto__; // eslint-disable-line no-proto
+const historyList = [];
+let historyPosition = -1;
+let disableNextHashChange = false;
+
+function pushHistory(url, state) {
+  if (historyPosition === -1 || historyList[historyPosition].url !== url) {
+    historyPosition += 1;
+    historyList.length = historyPosition + 1;
+    historyList[historyPosition] = { url, state };
+  }
+}
+
+function pathWithHash(location) {
+  return `${location.pathname}${location.hash}`;
+}
+
 export default function setupHistoryListeners() {
-  const pushState = window.history.pushState;
-  window.history.pushState = function(state) {
-    if (typeof history.onpushstate === 'function') {
-      window.history.onpushstate({ state });
-    }
-    // ... whatever else you want to do
-    // maybe call onhashchange e.handler
-    return pushState.apply(window.history, arguments);
-  };
+  if (!isStandalone) {
+    const historyProto = {
+      get length() {
+        console.log('get length()');
+        return historyList.length;
+      },
 
-  const replaceState = window.history.replaceState;
-  window.history.replaceState = function(state) {
-    if (typeof history.onpushstate === 'function') {
-      window.history.onpushstate({ state });
-    }
-    // ... whatever else you want to do
-    // maybe call onhashchange e.handler
-    return replaceState.apply(window.history, arguments);
-  };
+      get state() {
+        console.log('get state()');
+        return historyList[historyPosition].state;
+      },
 
-  history.onpushstate = e => {
+      get historyList() {
+        return { position: historyPosition, list: historyList };
+      },
+
+      go(delta) {
+        console.log(`go(${delta})`);
+        const newPos = historyPosition + delta;
+        if (newPos >= 0 && newPos <= historyList.length - 1) {
+          historyPosition = newPos;
+          const { url, state } = historyList[historyPosition];
+          const oldURL = document.location.href;
+          origHistoryProto.replaceState.call(window.history, state, '', url);
+          const newURL = document.location.href;
+          if (newURL.indexOf('#') === -1) {
+            window.dispatchEvent(new PopStateEvent('popstate', { state }));
+          } else {
+            disableNextHashChange = true;
+            window.dispatchEvent(
+              new HashChangeEvent('hashchange', { oldURL, newURL })
+            );
+          }
+        }
+      },
+
+      back() {
+        console.log('back()');
+        window.history.go(-1);
+      },
+
+      forward() {
+        console.log('forward()');
+        window.history.go(1);
+      },
+
+      pushState(state, title, url) {
+        console.log(`pushState("${state}", "${title}", "${url}")`);
+        origHistoryProto.replaceState.call(window.history, state, title, url);
+        pushHistory(url, state);
+        sendUrlChange(document.location.href);
+      },
+
+      replaceState(state, title, url) {
+        console.log(`replaceState("${state}", "${title}", "${url}")`);
+        origHistoryProto.replaceState.call(window.history, state, title, url);
+        historyList[historyPosition] = { state, url };
+        sendUrlChange(document.location.href);
+      },
+    };
+
+    window.addEventListener('hashchange', () => {
+      if (!disableNextHashChange) {
+        const url = `${document.location.pathname}${document.location.hash}`;
+        pushHistory(url, null);
+        sendUrlChange(document.location.href);
+      } else {
+        disableNextHashChange = false;
+      }
+    });
+
+    document.addEventListener(
+      'click',
+      ev => {
+        const el = ev.target;
+        if (el.nodeName === 'A' && el.href.indexOf('#') !== -1) {
+          const url = el.href;
+          const oldURL = document.location.href;
+          origHistoryProto.replaceState.call(window.history, null, '', url);
+          const newURL = document.location.href;
+          if (oldURL !== newURL) {
+            disableNextHashChange = true;
+            window.dispatchEvent(
+              new HashChangeEvent('hashchange', { oldURL, newURL })
+            );
+            pushHistory(pathWithHash(document.location), null);
+            sendUrlChange(document.location.href);
+          } else {
+            console.log('same url');
+          }
+          ev.preventDefault();
+          ev.stopPropagation();
+        }
+      },
+      true
+    );
+
+    pushHistory(pathWithHash(document.location), null);
+
     setTimeout(() => {
       sendUrlChange(document.location.href);
     });
-  };
 
-  history.onreplacestate = e => {
-    setTimeout(() => {
-      sendUrlChange(document.location.href);
-    });
-  };
-
-  setTimeout(() => {
-    sendUrlChange(document.location.href);
-  });
+    window.history.__proto__ = historyProto; // eslint-disable-line no-proto
+  }
 }
