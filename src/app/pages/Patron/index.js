973d794dbccb32bf3ff41292b94cb6004d3c6db7
Ives van Hoorne <i.vanhoorne@catawiki.nl>
2017-09-15 16:44:10 +0200

    Put Stripe back on window

--- src/app/pages/Patron/index.js
+++ src/app/pages/Patron/index.js
@@ -17,56 +17,27 @@ const Content = styled.div`
   color: white;
 `;
 
-type State = {
-  stripeLoaded: boolean,
-};
+export default function Patron() {
+  document.title = 'Patron - CodeSandbox';
+  return (
+    <MaxWidth>
+      <Margin vertical={1.5} horizontal={1.5}>
+        <Navigation title="Become a Patron" />
+        <Content>
+          <MaxWidth width={1024}>
+            <Title>Become a CodeSandbox Patron!</Title>
+            <SubTitle>
+              You can support us by paying a monthly amount of your choice.
+              <br />
+              The money goes to all expenses of CodeSandbox.
+            </SubTitle>
 
-export default class Patron extends React.PureComponent<void, State> {
-  constructor() {
-    super();
-    const stripeLoaded = Boolean(window.Stripe);
-
-    if (!stripeLoaded) {
-      const script = document.createElement('script');
-      script.setAttribute('src', 'https://js.stripe.com/v3/');
-      script.async = false;
-      document.head.appendChild(script);
-
-      script.onload = () => {
-        this.setState({ stripeLoaded: true });
-      };
-    }
-
-    this.state = {
-      stripeLoaded,
-    };
-  }
-
-  render() {
-    document.title = 'Patron - CodeSandbox';
-
-    if (!this.state.stripeLoaded) return null;
-
-    return (
-      <MaxWidth>
-        <Margin vertical={1.5} horizontal={1.5}>
-          <Navigation title="Become a Patron" />
-          <Content>
-            <MaxWidth width={1024}>
-              <Title>Become a CodeSandbox Patron!</Title>
-              <SubTitle>
-                You can support us by paying a monthly amount of your choice.
-                <br />
-                The money goes to all expenses of CodeSandbox.
-              </SubTitle>
-
-              <Centered horizontal>
-                <PricingModal />
-              </Centered>
-            </MaxWidth>
-          </Content>
-        </Margin>
-      </MaxWidth>
-    );
-  }
+            <Centered horizontal>
+              <PricingModal />
+            </Centered>
+          </MaxWidth>
+        </Content>
+      </Margin>
+    </MaxWidth>
+  );
 }
