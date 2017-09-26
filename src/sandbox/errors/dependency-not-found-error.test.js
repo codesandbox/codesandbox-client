d85300fe56aa60639dc3ad527e14c12ac7b744e4
Ives van Hoorne <i.vanhoorne@catawiki.nl>
2017-09-26 02:23:16 +0200

    Fix tests

--- src/sandbox/errors/dependency-not-found-error.test.js
+++ src/sandbox/errors/dependency-not-found-error.test.js
@@ -3,23 +3,23 @@ import DependencyError from './dependency-not-found-error';
 test('it handles normal deps correctly', () => {
   const error = new DependencyError('redux-form');
 
-  expect(error.payload).toMatchSnapshot();
+  expect(error.message).toMatchSnapshot();
 });
 
 test('it parses dependency names with slashes correctly', () => {
   const error = new DependencyError('redux-form/immutable');
 
-  expect(error.payload).toMatchSnapshot();
+  expect(error.message).toMatchSnapshot();
 });
 
 test('it parses dependency names with multiple slashes correctly', () => {
   const error = new DependencyError('redux-form/immutable/koekjes');
 
-  expect(error.payload).toMatchSnapshot();
+  expect(error.message).toMatchSnapshot();
 });
 
 test('it parses scoped packages', () => {
   const error = new DependencyError('@vx/group');
 
-  expect(error.payload).toMatchSnapshot();
+  expect(error.message).toMatchSnapshot();
 });
