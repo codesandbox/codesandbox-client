f9194315abe9367b686090397e805748a2ec6985
Frank Tan <tansongyang@gmail.com>
2017-10-21 15:34:30 -0400

    Add click handler to hits

--- src/app/pages/Sandbox/Editor/Workspace/Dependencies/DependencyHit.js
+++ src/app/pages/Sandbox/Editor/Workspace/Dependencies/DependencyHit.js
@@ -4,10 +4,22 @@ import { Highlight } from 'react-instantsearch/dom';
 type Props = {
   hit: Object,
 };
+
 export default function DependencyHit({ hit }: Props) {
   return (
     <div>
       <Highlight attributeName="name" hit={hit} />
     </div>
   );
+}
+
+export function hitComponent(makeOnClick) {
+  return ({ hit }: { hit: Object }) => {
+    const onClick = makeOnClick(hit);
+    return (
+      <div role="button" tabIndex={0} onClick={onClick}>
+        <DependencyHit hit={hit} />
+      </div>
+    );
+  };
 }
--- src/app/pages/Sandbox/Editor/Workspace/Dependencies/SearchDependencies.js
+++ src/app/pages/Sandbox/Editor/Workspace/Dependencies/SearchDependencies.js
@@ -3,24 +3,24 @@ import { InstantSearch, SearchBox, Hits } from 'react-instantsearch/dom';
 
 import 'app/pages/Search/Search.css';
 
-import DependencyHit from './DependencyHit';
+import { hitComponent } from './DependencyHit';
 
 type State = {
-  searchState: Object,
+  selectedHit: Object,
 };
 
-const initialState = {
-  searchState: {},
+const initialState: State = {
+  selectedHit: null,
 };
 
 export default class SearchDependencies extends React.PureComponent {
-  state: State;
-
   state = initialState;
 
-  onSearchStateChange = searchState => {
-    this.setState({ searchState });
+  makeHitOnClick = hit => () => {
+    console.log(hit); // eslint-disable-line
+    this.setState({ selectedHit: hit });
   };
+  hitComponent = hitComponent(this.makeHitOnClick);
 
   render() {
     // Copied from https://github.com/yarnpkg/website/blob/956150946634b1e6ae8c3aebd3fd269744180738/scripts/sitemaps.js
@@ -30,11 +30,9 @@ export default class SearchDependencies extends React.PureComponent {
         appId="OFCNCOG2CU"
         apiKey="f54e21fa3a2a0160595bb058179bfb1e"
         indexName="npm-search"
-        searchState={this.state.searchState}
-        onSearchStateChange={this.onSearchStateChange}
       >
         <SearchBox />
-        <Hits hitComponent={DependencyHit} />
+        <Hits hitComponent={this.hitComponent} />
       </InstantSearch>
     );
   }
