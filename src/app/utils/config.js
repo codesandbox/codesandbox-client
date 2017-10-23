d52fe6ee570d35258633824db831ece580a26a09
Ives van Hoorne <i.vanhoorne@catawiki.nl>
2017-10-23 13:50:42 +0200

    Use production search on local server

--- src/app/utils/config.js
+++ src/app/utils/config.js
@@ -1,7 +1,9 @@
 export const ALGOLIA_API_KEY = '7675b9e87ed7dd5bbf18f3e5310f92d6';
 export const ALGOLIA_APPLICATION_ID = 'ZACZHDBO7S';
 export const ALGOLIA_DEFAULT_INDEX =
-  process.env.NODE_ENV === 'production' ? 'prod_sandboxes' : 'dev_sandboxes';
+  process.env.NODE_ENV === 'production' || process.env.LOCAL_SERVER
+    ? 'prod_sandboxes'
+    : 'dev_sandboxes';
 
 export const STRIPE_API_KEY =
   process.env.NODE_ENV === 'production'
