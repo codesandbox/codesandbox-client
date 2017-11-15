ba63cb02f917dd1902585a318be2b7027f275fa4
Ives van Hoorne <i.vanhoorne@catawiki.nl>
2017-11-15 19:29:29 +0100

    Update media queries

--- src/homepage/src/utils/media.js
+++ src/homepage/src/utils/media.js
@@ -2,7 +2,7 @@ import { css } from 'styled-components';
 
 export default {
   tablet: (...args) => css`
-    @media (max-width: 1280px) {
+    @media (max-width: 1279px) {
       ${css(...args)};
     }
   `,
