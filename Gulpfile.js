5112aa803d32d9806cb6294c92e94934dbc0f831
Ives van Hoorne <i.vanhoorne@catawiki.nl>
2017-11-15 18:43:16 +0100

    Fix build script

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,36 +1,12 @@
 const gulp = require('gulp');
-const postcss = require('gulp-postcss');
-const autoprefixer = require('autoprefixer');
-const cssnano = require('cssnano');
-const rev = require('gulp-rev');
-const replace = require('gulp-replace');
 const runSequence = require('run-sequence');
 
 const config = require('./config/paths');
 
-gulp.task('css', function() {
+gulp.task('homepage', function() {
   return gulp
-    .src('src/homepage/**/*.css')
-    .pipe(postcss([autoprefixer, cssnano]))
-    .pipe(rev())
-    .pipe(gulp.dest(`${config.appBuild}/`))
-    .pipe(rev.manifest())
-    .pipe(gulp.dest(`${config.appBuild}/`));
-});
-
-gulp.task('javascript', function() {
-  return gulp
-    .src('src/homepage/**/*.js')
-    .pipe(gulp.dest(`${config.appBuild}/`));
-});
-
-gulp.task('html', function() {
-  const manifest = require(`${config.appBuild}/rev-manifest.json`);
-
-  return gulp
-    .src('src/homepage/index.html')
-    .pipe(replace('static/css/main.css', manifest['static/css/main.css']))
-    .pipe(gulp.dest(`${config.appBuild}/`));
+    .src('src/homepage/www/*.*')
+    .pipe(gulp.dest(`${config.appBuild}/homepage`));
 });
 
 gulp.task('static', function() {
@@ -40,7 +16,7 @@ gulp.task('static', function() {
 });
 
 gulp.task('build', function(cb) {
-  return runSequence(['css', 'javascript', 'static'], 'html', cb);
+  return runSequence(['homepage', 'static'], cb);
 });
 
 gulp.task('default', ['build']);
