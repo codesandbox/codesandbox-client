bef4ab57c2a94df3539e8103a61debaf6b05c393
Ives van Hoorne <i.vanhoorne@catawiki.nl>
2017-12-14 00:56:34 +0100

    Another change to pageQuery

--- packages/homepage/src/templates/docs.js
+++ packages/homepage/src/templates/docs.js
@@ -221,7 +221,7 @@ export default ({ data }) => {
   );
 };
 
-export const query = graphql`
+export const pageQuery = graphql`
   query Docs($slug: String!) {
     allMarkdownRemark(
       filter: { id: { regex: "/docs/" } }
