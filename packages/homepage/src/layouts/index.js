064f1b6ab2f159691b825c8dd55f2d3f0668fdc3
Ives van Hoorne <i.vanhoorne@catawiki.nl>
2017-12-14 04:12:29 +0100

    Make layout a class

--- packages/homepage/src/layouts/index.js
+++ packages/homepage/src/layouts/index.js
@@ -8,16 +8,22 @@ import '../css/global.css';
 import Navigation from '../components/Navigation';
 import Footer from '../components/Footer';
 
-const TemplateWrapper = ({ children }) => (
-  <ThemeProvider theme={theme}>
-    <div>
-      <div style={{ position: 'absolute', left: 0, right: 0, zIndex: 20 }}>
-        <Navigation />
-      </div>
-      <div style={{ maxWidth: '100vw', overflowX: 'hidden' }}>{children()}</div>
-      <Footer />
-    </div>
-  </ThemeProvider>
-);
-
-export default TemplateWrapper;
+// eslint-disable-next-line
+export default class TemplateWrapper extends React.Component {
+  render() {
+    const { children } = this.props;
+    return (
+      <ThemeProvider theme={theme}>
+        <div>
+          <div style={{ position: 'absolute', left: 0, right: 0, zIndex: 20 }}>
+            <Navigation />
+          </div>
+          <div style={{ maxWidth: '100vw', overflowX: 'hidden' }}>
+            {children()}
+          </div>
+          <Footer />
+        </div>
+      </ThemeProvider>
+    );
+  }
+}
