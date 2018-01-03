9a2844ab2d3aed5a52ecbd037fa225456b84a897
Ives van Hoorne <i.vanhoorne@catawiki.nl>
2018-01-03 22:36:03 +0100

    Use CodeSponsor as advertisement provider

--- packages/app/src/app/pages/Sandbox/Editor/Workspace/Advertisement/index.js
+++ packages/app/src/app/pages/Sandbox/Editor/Workspace/Advertisement/index.js
@@ -1,56 +1,7 @@
 import React from 'react';
-import styled from 'styled-components';
 
-const Container = styled.div`
-  padding: 1rem;
-  border-top: 1px solid rgba(0, 0, 0, 0.3);
+import CodeSponsor from './CodeSponsor';
 
-  .carbon-text {
-    text-decoration: none !important;
-    color: rgba(255, 255, 255, 0.9) !important;
-  }
-
-  .carbon-wrap {
-    font-size: 0.875rem;
-
-    img {
-      display: block;
-      margin-bottom: 0.5rem;
-    }
-  }
-
-  .carbon-poweredby {
-    display: block;
-    margin-top: 0.5rem;
-
-    font-size: 0.75rem;
-    text-decoration: none !important;
-    color: rgba(255, 255, 255, 0.6) !important;
-  }
-`;
-
-export default class Advertisement extends React.PureComponent {
-  componentDidMount() {
-    requestAnimationFrame(() => {
-      const script = document.createElement('script');
-      script.setAttribute(
-        'src',
-        '//cdn.carbonads.com/carbon.js?zoneid=1673&serve=C6AILKT&placement=codesandboxio'
-      );
-      script.async = 'true';
-      document.head.appendChild(script);
-    });
-  }
-
-  render() {
-    return (
-      <Container>
-        <script
-          id="_carbonads_js"
-          src="//cdn.carbonads.com/carbon.js?zoneid=1673&serve=C6AILKT&placement=codesandboxio"
-          async="true"
-        />
-      </Container>
-    );
-  }
-}
+export default () => (
+  <CodeSponsor token="4c558242-bb75-49a4-9b6f-5b393bcc78ed" theme="dark" />
+);
