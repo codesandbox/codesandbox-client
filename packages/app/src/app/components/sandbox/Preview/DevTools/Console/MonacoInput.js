4aae1f5b797b2fa0c6253554cce6f675a88a275a
Xiaoyi Chen <cxychina@gmail.com>
2018-01-10 01:39:40 -0800

    style and theme fixes for monaco, next up adjust input height

--- packages/app/src/app/components/sandbox/Preview/DevTools/Console/MonacoInput.js
+++ packages/app/src/app/components/sandbox/Preview/DevTools/Console/MonacoInput.js
@@ -1,10 +1,12 @@
 // Multi-line input
 
 import React from 'react';
+
+import Color from 'color';
 import styled from 'styled-components';
 
 import ChevronRight from 'react-icons/lib/md/chevron-right';
-import theme from 'common/theme';
+import theme from 'common/theme'; // TODO: what's this theme for?
 
 import { IconContainer } from './styles';
 
@@ -29,7 +31,6 @@ const InputWrapper = styled.div`
   position: relative;
   height: 1.5rem;
   width: 100%;
-  background-color: ${props => props.theme.background.darken(0.3)};
   /* border: none; */
   /* outline: none; */
   color: rgba(255, 255, 255, 0.8);
@@ -45,8 +46,13 @@ const monacoOptions = {
   folding: false,
   selectOnLineNumbers: false,
   selectionHighlight: false,
+
+  cursorStyle: 'line-thin',
+
   scrollbar: {
+    useShadows: false,
     horizontal: 'hidden',
+    verticalScrollbarSize: 9, // 0.5rem TODO: add this to resize scrollbar
   },
   lineDecorationsWidth: 0,
   scrollBeyondLastLine: false,
@@ -57,12 +63,19 @@ const monacoOptions = {
     enabled: false,
   },
 
+  contextmenu: false,
+
   // codesandbox configs (copied form editor)
   // ariaLabel: title, // ConsoleInput?
   // formatOnPaste: true, // check what others do
   // lineHeight: (preferences.lineHeight || 1.15) * preferences.fontSize,
+
+  // TODO: remove InputWrapper styles
+  fontFamily: 'Menlo, monospace',
+  fontSize: 13,
 };
 
+// Monaco.js
 // {
 //       selectOnLineNumbers: true,
 //       fontSize: preferences.fontSize,
@@ -107,11 +120,63 @@ function noop() {
   return Promise.resolve();
 }
 
-export default class ConsoleInput extends React.PureComponent<Props> {
+class ConsoleInput extends React.PureComponent<Props> {
   state = {
     command: '',
     commandHistory: [],
     commandCursor: -1,
+  };
+
+  editorWillMount = monaco => {
+    // console.log(this.props.theme);
+    // console.log(this.props.theme.background);
+    // console.log(this.props.theme.background.darken(0.3)());
+    // monaco.editor.defineTheme('CodeSandbox', {
+    //   base: 'vs-dark', // can also be vs-dark or hc-black
+    //   inherit: true, // can also be false to completely replace the builtin rules
+    //   rules: [
+    //     { token: 'comment', foreground: '626466' },
+    //     { token: 'keyword', foreground: '6CAEDD' },
+    //     { token: 'identifier', foreground: 'fac863' },
+    //   ],
+    //   colors: {
+    //     'editor.background': '#1a1d1e'/*this.props.theme.background.darken(0.3)()*/,
+    //   },
+    // });
+    // console.log(theme);
+    // console.dir(Color);
+    // console.log(new Color(theme.background.darken(0.3)()));
+
+    const backgroundColor = new Color(
+      theme.background.darken(0.3)()
+    ).hexString();
+    monaco.editor.defineTheme('CodeSandbox-DevTools-Input', {
+      base: 'vs-dark',
+      inherit: true,
+      rules: [
+        { token: 'comment', foreground: '626466' },
+        { token: 'keyword', foreground: '6CAEDD' },
+        { token: 'identifier', foreground: 'fac863' },
+      ],
+      colors: {
+        'editor.background': backgroundColor, // '#1a1d1e'
+        // 'editor.background': '#0000ff',
+        // 'editor.background': 'yellow',
+      },
+    });
+  };
+
+  editorDidMount = async (editor, monaco) => {
+    console.log('editor:', editor);
+
+    // on Monaco editor scroll change, adjust the height
+    editor.onDidScrollChange(e => {
+      if (!e.scrollHeightChanged) {
+        return;
+      }
+      e.scrollHeight;
+      console.log(e);
+    });
   };
 
   render() {
@@ -124,10 +189,10 @@ export default class ConsoleInput extends React.PureComponent<Props> {
           <MonacoEditor
             width="100%"
             height="100%"
-            theme="CodeSandbox"
+            theme="CodeSandbox-DevTools-Input"
             options={monacoOptions}
-            // editorDidMount={this.configureEditor}
-            // editorWillMount={this.editorWillMount}
+            editorDidMount={this.editorDidMount}
+            editorWillMount={this.editorWillMount}
             openReference={noop}
           />
         </InputWrapper>
@@ -136,3 +201,5 @@ export default class ConsoleInput extends React.PureComponent<Props> {
     // return <div>hey</div>;
   }
 }
+
+export default ConsoleInput;
