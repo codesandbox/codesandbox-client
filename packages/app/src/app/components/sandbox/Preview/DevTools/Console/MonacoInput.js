d2a7bbae8c45e9f28e5ee2ce81ee51f2468eb621
Xiaoyi Chen <cxychina@gmail.com>
2018-01-14 04:35:46 -0800

    use css to style background color of prompt instead of theme; add resize handling

--- packages/app/src/app/components/sandbox/Preview/DevTools/Console/MonacoInput.js
+++ packages/app/src/app/components/sandbox/Preview/DevTools/Console/MonacoInput.js
@@ -1,7 +1,6 @@
 /* @flow */
 import React from 'react';
 
-import Color from 'color';
 import styled from 'styled-components';
 
 import ChevronRight from 'react-icons/lib/md/chevron-right';
@@ -27,6 +26,10 @@ const Container = styled.div`
   background-color: ${props => props.theme.background.darken(0.3)};
   display: flex;
   align-items: flex-start;
+
+  .monaco-editor-background {
+    background-color: ${props => props.theme.background.darken(0.3)};
+  }
 `;
 
 type Props = {
@@ -74,6 +77,8 @@ function noop() {
 }
 
 class ConsoleInput extends React.PureComponent<Props> {
+  sizeProbeInterval: number;
+
   state = {
     command: '',
     commandHistory: [],
@@ -82,25 +87,13 @@ class ConsoleInput extends React.PureComponent<Props> {
     editorHeight: CONSOLE_INPUT_LINE_HEIGHT,
   };
 
-  editorWillMount = monaco => {
-    const backgroundColor = new Color(
-      theme.background.darken(0.3)()
-    ).hexString();
-    monaco.editor.defineTheme('CodeSandbox-DevTools-Input', {
-      base: 'vs-dark',
-      inherit: true,
-      rules: [
-        { token: 'comment', foreground: '626466' },
-        { token: 'keyword', foreground: '6CAEDD' },
-        { token: 'identifier', foreground: 'fac863' },
-      ],
-      colors: {
-        'editor.background': backgroundColor,
-      },
-    });
+  resizeEditor = () => {
+    this.editor.layout();
   };
 
   editorDidMount = async (editor, monaco) => {
+    this.editor = editor;
+
     editor.onDidChangeModelContent(() => {
       const lineCount = editor.getModel().getLineCount();
       this.setState({
@@ -109,7 +102,7 @@ class ConsoleInput extends React.PureComponent<Props> {
           lineCount * CONSOLE_INPUT_LINE_HEIGHT
         ),
       });
-      editor.layout();
+      this.resizeEditor();
     });
 
     editor.onKeyDown(event => {
@@ -117,12 +110,12 @@ class ConsoleInput extends React.PureComponent<Props> {
       const { evaluateConsole } = this.props;
 
       if (e.keyCode === 13) {
+        // Enter
         if (e.shiftKey) {
           return;
         }
         e.preventDefault();
         e.stopPropagation();
-        // Enter
         const command = editor.getModel().getValue();
         evaluateConsole(command);
         editor.setValue('');
@@ -131,6 +124,7 @@ class ConsoleInput extends React.PureComponent<Props> {
           commandHistory: [command, ...this.state.commandHistory],
         });
       } else if (e.keyCode === 38) {
+        // Up arrow
         const lineNumber = editor.getPosition().lineNumber;
         if (lineNumber !== 1) {
           return;
@@ -140,12 +134,12 @@ class ConsoleInput extends React.PureComponent<Props> {
           this.state.commandCursor + 1,
           this.state.commandHistory.length - 1
         );
-        // Up arrow
         editor.setValue(this.state.commandHistory[newCursor] || '');
         this.setState({
           commandCursor: newCursor,
         });
       } else if (e.keyCode === 40) {
+        // Down arrow
         const lineNumber = editor.getPosition().lineNumber;
         const lineCount = editor.getModel().getLineCount();
         if (lineNumber !== lineCount) {
@@ -154,16 +148,23 @@ class ConsoleInput extends React.PureComponent<Props> {
 
         const newCursor = Math.max(this.state.commandCursor - 1, -1);
         editor.setValue(this.state.commandHistory[newCursor] || '');
-        // Down arrow
         this.setState({
           commandCursor: newCursor,
         });
       }
     });
+
+    window.addEventListener('resize', this.resizeEditor);
+    this.sizeProbeInterval = setInterval(this.resizeEditor.bind(this), 3000);
   };
 
-  // componentWillUnmount() {
-  // }
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.resizeEditor);
+    clearTimeout(this.sizeProbeInterval);
+    if (this.editor) {
+      this.editor.dispose();
+    }
+  }
 
   render() {
     return (
@@ -175,10 +176,8 @@ class ConsoleInput extends React.PureComponent<Props> {
           <MonacoEditor
             width="100%"
             height={`calc(100% - ${CONSOLE_INPUT_PADDING}px)`}
-            theme="CodeSandbox-DevTools-Input"
             options={monacoOptions}
             editorDidMount={this.editorDidMount}
-            editorWillMount={this.editorWillMount}
             openReference={noop}
           />
         </InputWrapper>
