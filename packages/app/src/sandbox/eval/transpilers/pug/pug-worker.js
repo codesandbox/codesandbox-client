0ddd1999627226ffee3e5452005684942beb3bf2
Zephraph <zephraph@gmail.com>
2018-01-16 22:50:00 -0500

    Update worker to use browserified-pug

--- packages/app/src/sandbox/eval/transpilers/pug/pug-worker.js
+++ packages/app/src/sandbox/eval/transpilers/pug/pug-worker.js
@@ -1,33 +1,28 @@
-// @flow
 import { buildWorkerError } from '../utils/worker-error-handler';
 
-self.importScripts(['/static/js/pug-2.0.0-rc.4.js']);
+self.importScripts(['https://unpkg.com/browserified-pug']);
 
 self.postMessage('ready');
 
-declare var pug: {
-  compile: (
-    code: string,
-    { filename: string },
-    (err, css: string) => void
-  ) => void,
-};
 
 self.addEventListener('message', event => {
   const { code, path } = event.data;
 
   // register a custom importer callback
-  pug.render(code, { filename: path }, (err, css) => {
+  pug.render(code, { filename: path }, (err, html) => {
+
     if (err) {
       return self.postMessage({
         type: 'error',
-        error: buildWorkerError(err),
+        error: buildWorkerError(err)
       });
     }
 
     return self.postMessage({
       type: 'compiled',
-      transpiledCode: css,
+      transpiledCode: html,
     });
+
   });
+
 });
