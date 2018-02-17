a74f61f839a5af7af584110c583bb2f241065cbb
Christian Alfoni <christianalfoni@gmail.com>
2018-02-17 19:08:00 +0100

    Fixed bug related to changedModuleIds

--- packages/app/src/app/pages/Sandbox/Editor/Workspace/Files/index.js
+++ packages/app/src/app/pages/Sandbox/Editor/Workspace/Files/index.js
@@ -7,57 +7,53 @@ import WorkspaceItem from '../WorkspaceItem';
 import EditIcons from './DirectoryEntry/Entry/EditIcons';
 
 class Files extends React.Component {
-  createModule = () => {
-    // INCREDIBLY BAD PRACTICE! TODO: FIX THIS
-    this.directory.onCreateModuleClick();
-  };
+    createModule = () => {
+        // INCREDIBLY BAD PRACTICE! TODO: FIX THIS
+        this.directory.onCreateModuleClick();
+    };
 
-  createDirectory = () => {
-    // INCREDIBLY BAD PRACTICE! TODO: FIX THIS
-    this.directory.onCreateDirectoryClick();
-  };
+    createDirectory = () => {
+        // INCREDIBLY BAD PRACTICE! TODO: FIX THIS
+        this.directory.onCreateDirectoryClick();
+    };
 
-  render() {
-    const store = this.props.store;
-    const sandbox = store.editor.currentSandbox;
+    render() {
+        const store = this.props.store;
+        const sandbox = store.editor.currentSandbox;
 
-    const openedModulesByUsers = {};
+        const openedModulesByUsers = {};
 
-    // if (store.live.isLive) {
-    //   store.live.usersMetadata.forEach(user => {
-    //     openedModulesByUsers[user.currentModuleShortid] =
-    //       openedModulesByUsers[user.currentModuleShortid] || [];
-    //     openedModulesByUsers[user.currentModuleShortid].push(user.color);
-    //   });
-    // }
+        // if (store.live.isLive) {
+        //   store.live.usersMetadata.forEach(user => {
+        //     openedModulesByUsers[user.currentModuleShortid] =
+        //       openedModulesByUsers[user.currentModuleShortid] || [];
+        //     openedModulesByUsers[user.currentModuleShortid].push(user.color);
+        //   });
+        // }
 
-    return (
-      <WorkspaceItem
-        defaultOpen
-        keepState
-        title="Files"
-        actions={
-          <EditIcons
-            hovering
-            onCreateFile={this.createModule}
-            onCreateDirectory={this.createDirectory}
-          />
-        }
-      >
-        <DirectoryEntry
-          root
-          innerRef={el => {
-            this.directory = el;
-          }}
-          title={sandbox.title || 'Project'}
-          openedModulesByUsers={openedModulesByUsers}
-          depth={-1}
-          id={null}
-          shortid={null}
-        />
-      </WorkspaceItem>
-    );
-  }
+        return (
+            <WorkspaceItem
+                defaultOpen
+                keepState
+                title="Files"
+                actions={
+                    <EditIcons hovering onCreateFile={this.createModule} onCreateDirectory={this.createDirectory} />
+                }
+            >
+                <DirectoryEntry
+                    root
+                    innerRef={(el) => {
+                        this.directory = el;
+                    }}
+                    title={sandbox.title || 'Project'}
+                    openedModulesByUsers={openedModulesByUsers}
+                    depth={-1}
+                    id={null}
+                    shortid={null}
+                />
+            </WorkspaceItem>
+        );
+    }
 }
 
 export default inject('signals', 'store')(observer(Files));
