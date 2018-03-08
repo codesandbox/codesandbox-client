5b794e3e64490ea1fb3dc0af779d996b4ae4a30c
Ives van Hoorne <i.vanhoorne@catawiki.nl>
2018-03-08 14:46:26 +0100

    Improve builds

--- packages/react-sandpack/src/react-sandpack.ts
+++ packages/react-sandpack/src/react-sandpack.ts
@@ -1,4 +1,21 @@
-import Preview from './components/Preview/index';
-import OpenInCodeSandbox from './components/OpenInCodeSandbox/index';
+import BrowserPreview from './components/BrowserPreview';
+import CodeMirror from './components/CodeEditor/CodeMirror';
+import FileExplorer from './components/FileExplorer';
+import Navigator from './components/Navigator';
+import OpenInCodeSandbox from './components/OpenInCodeSandbox';
+import Preview from './components/Preview';
+import SandpackConsumer from './components/SandpackConsumer';
+import SandpackProvider from './components/SandpackProvider';
+import TranspiledCodeView from './components/TranspiledCodeView';
 
-export { Preview, OpenInCodeSandbox };
+export {
+  BrowserPreview,
+  CodeMirror,
+  FileExplorer,
+  Navigator,
+  OpenInCodeSandbox,
+  Preview,
+  SandpackConsumer,
+  SandpackProvider,
+  TranspiledCodeView,
+};
