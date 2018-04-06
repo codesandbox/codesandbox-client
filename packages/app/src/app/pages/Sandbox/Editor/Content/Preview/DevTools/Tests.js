0fd53aeb26214408ee7c12a020a88f5ce401d729
Valentin Hervieu <ValentinH@users.noreply.github.com>
2018-04-06 23:51:52 +0200

    Add the "Clear console" action for the console preview panel (#697)

--- packages/app/src/app/pages/Sandbox/Editor/Content/Preview/DevTools/Tests.js
+++ packages/app/src/app/pages/Sandbox/Editor/Content/Preview/DevTools/Tests.js
@@ -1,15 +1,28 @@
 import React from 'react';
 import Tests from 'app/components/Preview/DevTools/Tests';
 
+import VerticalAlign from 'app/src/app/components/Preview/Navigator/VerticalAlign';
+import HorizontalAlign from 'app/src/app/components/Preview/Navigator/HorizontalAlign';
+
 import Navigator from './Navigator';
 
-export default ({ alignRight, alignBottom }) => (
-  <div>
-    <Navigator
-      alignRight={alignRight}
-      alignBottom={alignBottom}
-      title="Tests"
-    />
-    <Tests.Content standalone />
-  </div>
-);
+export default ({ alignRight, alignBottom }) => {
+  const actions = [
+    {
+      title: 'Align To Bottom',
+      onClick: alignBottom,
+      Icon: HorizontalAlign,
+    },
+    {
+      title: 'Align To Right',
+      onClick: alignRight,
+      Icon: VerticalAlign,
+    },
+  ];
+  return (
+    <div>
+      <Navigator title="Tests" actions={actions} />
+      <Tests.Content standalone />
+    </div>
+  );
+};
