1a7bfa0fc94c44c4ed0e4ac0011bea4b5beaf50b
Ives van Hoorne <Ives.v.h@gmail.com>
2018-04-13 11:52:05 +0200

    Comment

--- packages/app/src/app/store/modules/preferences/index.js
+++ packages/app/src/app/store/modules/preferences/index.js
@@ -39,6 +39,9 @@ export default Module({
       trailingComma: 'none',
       useTabs: false,
       vimMode: false,
+      // Windows has problems with calculating characters widths when ligatures
+      // are disabled, however there is a weird character when you have 'fi' in
+      // Menlo. So a temporary fix is to only enable this for Windows.
       enableLigatures: navigator.platform.indexOf('Win') > -1,
     },
     isLoadingPaymentDetails: true,
