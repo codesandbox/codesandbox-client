766c9da5a1739f8082c423ebab9ae2f8f5e60402
Ives van Hoorne <Ives.v.h@gmail.com>
2018-04-29 10:22:32 -0700

    Revert "Make raw-loader load the contents of binary modules (#770)"
    This reverts commit 6795fdd288a8c0f4f1fee487723ce6ab425a7df1.

--- packages/app/src/sandbox/eval/transpilers/raw/index.js
+++ packages/app/src/sandbox/eval/transpilers/raw/index.js
@@ -6,18 +6,11 @@ class RawTranspiler extends Transpiler {
     super('raw-loader');
   }
 
-  async doTranspilation(code: string) {
-    // code is a URL, this is probably a binary module, load its contents
-    if (code.substr(0, 4) === 'http') {
-      const res = await fetch(code);
-      const text = await res.text();
-      return {
-        transpiledCode: `module.exports = ${JSON.stringify(text)};`,
-      };
-    }
-    return {
-      transpiledCode: `module.exports = ${JSON.stringify(code)};`,
-    };
+  doTranspilation(code: string) {
+    return Promise.resolve({
+      transpiledCode: `
+      module.exports = ${JSON.stringify(code)};`,
+    });
   }
 }
 
