93051805951791c1a8e7f7ae5fd6366c8368bcbe
Nick Nisi <nick@nisi.org>
2018-05-08 09:38:54 -0500

    add support for dojo theming (#804)
    For dojo themes to work properly a special " _key" property should exist
    on the CSS module JS file that's value is the package name plus the name
    of the css file.
    
    This commit makes the following changes:
    
    - add a custom styles transpiler for dojo that adds the " _key" property
    - Update the dojo preset to use the new styles transpiler
    - split out the `classesToDefinition` method into its own utility module and import it into the existing and dojo style transpilers

---
+++ packages/app/src/sandbox/eval/presets/dojo/transpilers/style.js
@@ -0,0 +1,31 @@
+// @flow
+import { dispatch } from 'codesandbox-api';
+import { StyleTranspiler } from '../../../transpilers/style';
+import { type LoaderContext } from '../../../transpiled-module';
+import insertCss from '../../../transpilers/style/utils/insert-css';
+import toDefinition from '../../../transpilers/style/utils/to-definition';
+import getModules from '../../../transpilers/style/get-modules';
+
+const getStyleId = id => id + '-css';
+
+class DojoStyleTranspiler extends StyleTranspiler {
+  async doTranspilation(code: string, loaderContext: LoaderContext) {
+    const id = getStyleId(loaderContext._module.getId());
+    const { path } = loaderContext;
+    const { code: packageJson } = loaderContext.getModules().find(module => module.path === '/package.json');
+    const { name: packageName } = JSON.parse(packageJson);
+    const [, baseName ] = /\/([^/.]*)[^/]*$/.exec(path);
+    const key = `${packageName}/${baseName}`;
+    const { css, exportTokens } = await getModules(code, loaderContext);
+    let result = insertCss(id, css);
+    result += `\nmodule.exports=${JSON.stringify({ ' _key': key, ...exportTokens  })};`
+    dispatch({ type: 'add-extra-lib', path, code: toDefinition(exportTokens) });
+    return { transpiledCode: result };
+  }
+}
+
+const transpiler = new DojoStyleTranspiler();
+
+export { DojoStyleTranspiler as StyleTranspiler };
+
+export default transpiler;
