341660c8d2d49acb5b7bd029fa592f2f14a7f558
Ives van Hoorne <Ives.v.h@gmail.com>
2018-05-08 22:44:03 +0200

    Fix resolving multiple require statements in one line

--- packages/app/src/sandbox/eval/transpilers/babel/worker/simple-get-require-statements.js
+++ packages/app/src/sandbox/eval/transpilers/babel/worker/simple-get-require-statements.js
@@ -1,3 +1,6 @@
+const lineRegex = /require\(['|"|`]([^"|'|`]*)['|"|`]\)|require\((.*)\)/g;
+const partRegex = /require\(['|"|`]([^"|'|`]*)['|"|`]\)|require\((.*)\)/;
+
 /**
  * This is the regex version of getting all require statements, it makes the assumption
  * that the file is commonjs and only has `require()` statements.
@@ -5,25 +8,31 @@
 export default function getRequireStatements(code: string) {
   const results = [];
   code.split('\n').forEach(line => {
-    const regex = /require\(['|"|`]([^"|'|`]*)['|"|`]\)|require\((.*)\)/;
-    const match = line.match(regex);
+    const matches = line.match(lineRegex);
+    if (matches) {
+      matches.forEach(codePart => {
+        const match = codePart.match(partRegex);
 
-    if (match) {
-      if (match[1]) {
-        if (!results.find(r => r.type === 'direct' && r.path === match[1])) {
-          results.push({
-            type: 'direct',
-            path: match[1],
-          });
+        if (match) {
+          if (match[1]) {
+            if (
+              !results.find(r => r.type === 'direct' && r.path === match[1])
+            ) {
+              results.push({
+                type: 'direct',
+                path: match[1],
+              });
+            }
+          } else if (match[2]) {
+            if (!results.find(r => r.type === 'glob' && r.path === match[2])) {
+              results.push({
+                type: 'glob',
+                path: match[2],
+              });
+            }
+          }
         }
-      } else if (match[2]) {
-        if (!results.find(r => r.type === 'glob' && r.path === match[2])) {
-          results.push({
-            type: 'glob',
-            path: match[2],
-          });
-        }
-      }
+      });
     }
   });
 
