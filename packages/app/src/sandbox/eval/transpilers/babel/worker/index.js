fabcc7eeea315225eb4a997098001d6f60af7257
Bogdan Luca <luca.bogdan@gmail.com>
2018-05-11 14:49:06 +0300

    Babel worker: add missing Promise polyfill.

--- packages/app/src/sandbox/eval/transpilers/babel/worker/index.js
+++ packages/app/src/sandbox/eval/transpilers/babel/worker/index.js
@@ -1,6 +1,7 @@
 require('core-js/fn/string/starts-with');
 require('core-js/fn/string/ends-with');
 require('core-js/fn/array/find');
+require('core-js/fn/promise');
 
 self.importScripts(
   `${process.env.CODESANDBOX_HOST}/static/browserfs/browserfs.min.js`
