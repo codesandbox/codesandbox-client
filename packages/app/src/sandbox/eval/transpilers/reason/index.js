2112a83a0453cc24f17441a6ae1bd84a5965b858
Ives van Hoorne <Ives.v.h@gmail.com>
2018-05-20 14:41:49 +0200

    Proper support for multiple dependencies

--- packages/app/src/sandbox/eval/transpilers/reason/index.js
+++ packages/app/src/sandbox/eval/transpilers/reason/index.js
@@ -2,10 +2,14 @@
 import ReasonWorker from 'worker-loader?publicPath=/&name=reason-transpiler.[hash:8].worker.js!./worker';
 
 import { basename } from 'path';
-import { sortBy } from 'lodash';
 
 import Transpiler from '../';
 import { type LoaderContext } from '../../transpiled-module';
+import type { Module } from '../../entities/module';
+
+type ReasonModule = Module & {
+  moduleName: string,
+};
 
 function addScript(src) {
   return new Promise(resolve => {
@@ -17,6 +21,41 @@ function addScript(src) {
       resolve();
     };
   });
+}
+
+const IGNORED_DEPENDENCIES = ['ReactDOMRe', 'ReasonReact'];
+
+function getModuleName(path: string) {
+  const moduleParts = basename(path).split('.');
+  moduleParts.pop();
+
+  const unCapitalizedModuleName = moduleParts.join('.');
+  return (
+    unCapitalizedModuleName[0].toUpperCase() + unCapitalizedModuleName.slice(1)
+  );
+}
+
+function getDependencyList(
+  modules: Array<ReasonModule>,
+  list: Set<ReasonModule>,
+  module: ReasonModule
+) {
+  const deps = window.ocaml
+    .reason_list_dependencies(module.code)
+    .filter(x => IGNORED_DEPENDENCIES.indexOf(x) === -1)
+    .filter(x => !list.has(x));
+
+  deps.shift(); // Remove the first 0 value
+
+  deps.forEach(dep => {
+    const foundModule = modules.find(x => x.moduleName === dep);
+
+    if (foundModule) {
+      getDependencyList(modules, list, foundModule);
+    }
+  });
+
+  list.add(module);
 }
 
 class ReasonTranspiler extends Transpiler {
@@ -32,37 +71,36 @@ class ReasonTranspiler extends Transpiler {
   ): Promise<{ transpiledCode: string }> {
     if (!window.ocaml) {
       await addScript(
-        'https://cdn.rawgit.com/jaredly/docre/70303ec3/static/bs-3.0.0.js'
+        'https://cdn.rawgit.com/jaredly/docre/73fb3c05/static/bs-3.0.0.js'
       );
       await addScript('https://reason.surge.sh/bucklescript-deps.js');
     }
 
-    const modulesToAdd = [
-      ...sortBy(
-        loaderContext
-          .getModules()
-          .filter(x => x.path !== loaderContext.path)
-          .filter(x => x.path.endsWith('.re')),
-        'path'
-      ),
-      loaderContext._module.module,
-    ];
+    const reasonModules = loaderContext
+      .getModules()
+      .filter(x => x.path.endsWith('.re'))
+      .map(x => ({
+        ...x,
+        moduleName: getModuleName(x.path),
+      }));
 
-    modulesToAdd
-      .filter(x => x.path !== loaderContext._module.module.path)
-      .forEach(m => {
+    const mainReasonModule: ReasonModule = reasonModules.find(
+      m => m.path === loaderContext._module.module.path
+    );
+
+    const modulesToAdd: Set<ReasonModule> = new Set();
+
+    getDependencyList(reasonModules, modulesToAdd, mainReasonModule);
+
+    modulesToAdd.forEach(m => {
+      if (m.path !== loaderContext._module.module.path) {
         loaderContext.addTranspilationDependency(m.path, {});
-      });
+      }
+    });
 
-    const newCode = modulesToAdd
+    const newCode = Array.from(modulesToAdd)
       .map(x => {
-        const moduleParts = basename(x.path).split('.');
-        moduleParts.pop();
-
-        const unCapitalizedModuleName = moduleParts.join('.');
-        const moduleName =
-          unCapitalizedModuleName[0].toUpperCase() +
-          unCapitalizedModuleName.slice(1);
+        const moduleName = x.moduleName;
 
         return `module ${moduleName} = {
 #1 ${moduleName}
