92fb5f4adfa68388bfbd70c579562bf1838ec982
Ives van Hoorne <Ives.v.h@gmail.com>
2018-05-21 17:42:31 +0200

    Fix sandbox with index.html with no <head>

--- packages/app/src/sandbox/compile.js
+++ packages/app/src/sandbox/compile.js
@@ -45,8 +45,11 @@ export function getHTMLParts(html: string) {
   if (html.includes('<body>')) {
     const bodyMatcher = /<body>([\s\S]*)<\/body>/m;
     const headMatcher = /<head>([\s\S]*)<\/head>/m;
-    const body = html.match(bodyMatcher)[1];
-    const head = html.match(headMatcher)[1];
+
+    const headMatch = html.match(headMatcher);
+    const bodyMatch = html.match(bodyMatcher);
+    const head = headMatch && headMatch[1] ? headMatch[1] : '';
+    const body = bodyMatch && bodyMatch[1] ? bodyMatch[1] : '';
 
     return { body, head };
   }
