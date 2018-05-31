3efc1530d0f2a72855138bd73e2e96880a2eb33a
Ives van Hoorne <Ives.v.h@gmail.com>
2018-05-31 16:52:32 +0200

    Move to CodeFund

--- packages/app/src/app/pages/Sandbox/Editor/Workspace/Advertisement/CodeSponsor.js
+++ packages/app/src/app/pages/Sandbox/Editor/Workspace/Advertisement/CodeSponsor.js
@@ -1,29 +1,8 @@
 import React from 'react';
-import './CodeSponsor.css';
 
-type Props = {
-  token: string,
-  theme: 'dark' | 'light',
-};
-
-export default ({ token, theme }: Props) => {
-  const pixelHref = `https://codesponsor.io/t/l/${token}/pixel.png`;
-  const linkHref = `https://codesponsor.io/t/c/${token}/`;
-
-  return (
-    <div className={theme === 'dark' ? 'cs__wrapper dark' : 'cs__wrapper'}>
-      <div className="cs__header">Proudly sponsored by</div>
-      <a href={linkHref} className="cs__blurb" target="_blank" rel="noopener">
-        <strong>Rollbar</strong>{' '}
-        <span>
-          Real-time error monitoring, alerting, and analytics for software
-          developers{' '}
-          <span aria-label="rocket" role="img">
-            🚀
-          </span>
-        </span>
-      </a>
-      <img alt="CodeSponsor" className="cs__pixel" src={pixelHref} />
-    </div>
-  );
-};
+export default () => (
+  <div>
+    <script src="https://codefund.io/scripts/1fc4e04d-9622-4844-81f9-7522c7d19ac7/embed.js?theme=codesandbox" />
+    <div id="codefund_ad" />
+  </div>
+);
