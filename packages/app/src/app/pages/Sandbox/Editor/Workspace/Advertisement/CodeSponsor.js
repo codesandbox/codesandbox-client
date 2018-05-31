9159fa41568a2cae93674b874f65bcfe1a197835
Ives van Hoorne <Ives.v.h@gmail.com>
2018-05-31 21:03:56 +0200

    Add script tag with plain HTML

--- packages/app/src/app/pages/Sandbox/Editor/Workspace/Advertisement/CodeSponsor.js
+++ packages/app/src/app/pages/Sandbox/Editor/Workspace/Advertisement/CodeSponsor.js
@@ -1,8 +1,27 @@
 import React from 'react';
 
-export default () => (
-  <div>
-    <script src="https://codefund.io/scripts/1fc4e04d-9622-4844-81f9-7522c7d19ac7/embed.js?theme=codesandbox" />
-    <div id="codefund_ad" />
-  </div>
-);
+const CODEFUND_LINK =
+  'https://codefund.io/scripts/1fc4e04d-9622-4844-81f9-7522c7d19ac7/embed.js?theme=codesandbox';
+let loaded = false;
+
+export default class CodeFund extends React.PureComponent {
+  componentDidMount() {
+    if (!loaded) {
+      loaded = true;
+
+      const script = document.createElement('script');
+      script.setAttribute('src', CODEFUND_LINK);
+      script.async = 'true';
+      script.setAttribute('id', 'external-js');
+      document.head.appendChild(script);
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <div id="codefund_ad" />
+      </div>
+    );
+  }
+}
