032cf9190bf84beadfbcdfceec5f7f97f0748396
Ives van Hoorne <Ives.v.h@gmail.com>
2018-06-03 17:46:18 +0200

    Use old webpack uglify settings

--- packages/app/config/webpack.prod.js
+++ packages/app/config/webpack.prod.js
@@ -29,38 +29,22 @@ module.exports = merge(commonConfig, {
   optimization: {
     minimizer: [
       new UglifyJSPlugin({
+        cache: true,
+        parallel: true,
+        sourceMap: true,
         uglifyOptions: {
-          parse: {
-            // we want uglify-js to parse ecma 8 code. However, we don't want it
-            // to apply any minfication steps that turns valid ecma 5 code
-            // into invalid ecma 5 code. This is why the 'compress' and 'output'
-            // sections only apply transformations that are ecma 5 safe
-            // https://github.com/facebook/create-react-app/pull/4234
-            ecma: 8,
-          },
           compress: {
-            ecma: 5,
-            warnings: false,
-            // Disabled because of an issue with Uglify breaking seemingly valid code:
-            // https://github.com/facebook/create-react-app/issues/2376
-            // Pending further investigation:
-            // https://github.com/mishoo/UglifyJS2/issues/2011
-            comparisons: false,
+            // inline is buggy as of uglify-es 3.3.7
+            // https://github.com/mishoo/UglifyJS2/issues/2842
+            inline: 1,
           },
           mangle: {
             safari10: true,
           },
           output: {
-            ecma: 5,
             comments: false,
-            // Turned on because emoji and regex is not minified properly using default
-            // https://github.com/facebook/create-react-app/issues/2488
-            ascii_only: true,
           },
         },
-        parallel: true,
-        cache: false,
-        sourceMap: true,
       }),
     ],
     concatenateModules: true, //ModuleConcatenationPlugin
