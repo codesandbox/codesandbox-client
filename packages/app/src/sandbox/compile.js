3cc69091b0d05eb4fb30b270c2f4ba2ccbecaa4a
Ives van Hoorne <Ives.v.h@gmail.com>
2018-06-04 15:05:03 +0200

    Fix height returning for embed

--- packages/app/src/sandbox/compile.js
+++ packages/app/src/sandbox/compile.js
@@ -280,7 +280,7 @@ function getDocumentHeight() {
   const body = document.body;
   const html = document.documentElement;
 
-  const height = Math.max(
+  return Math.max(
     body.scrollHeight,
     body.offsetHeight,
     html.clientHeight,
