41a904d63b91f2ef01245430b249b8ece33d99c2
Sara Vieira <hey@iamsaravieira.com>
2018-07-09 23:05:27 +0100

    delete default theme

--- packages/app/src/app/components/CodeEditor/Monaco/defaultStyles.js
+++
@@ -1,8 +0,0 @@
-import { injectGlobal } from 'styled-components';
-
-export default injectGlobal`
-body {
-
-.monaco-editor .view-overlays .current-line { border:  none;}
-
-}`;
--- packages/app/src/app/components/CodeEditor/Monaco/elements.js
+++ packages/app/src/app/components/CodeEditor/Monaco/elements.js
@@ -1,8 +1,6 @@
 import styled from 'styled-components';
 import theme from 'common/theme';
 
-import './defaultStyles';
-
 export const Container = styled.div`
   width: 100%;
   height: 100%;
