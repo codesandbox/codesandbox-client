7e33029d99bb38846c79abe252e61b2ad5263a74
Sara Vieira <hey@iamsaravieira.com>
2018-07-09 23:06:32 +0100

    Hi (#952)
    <!--
    Please make sure you are familiar with and follow the instructions in the
    contributing guidelines (found in the CONTRIBUTING.md file).
    
    Please fill out the information below to expedite the review and (hopefully)
    merge of your pull request!
    -->
    
    <!-- Is it a Bug fix, feature, docs update, ... -->
    **What kind of change does this PR introduce?**
    
    <!-- You can also link to an open issue here -->
    **What is the current behavior?**
    
    <!-- if this is a feature change -->
    **What is the new behavior?**
    
    
    <!-- Have you done all of these things?  -->
    **Checklist**:
    <!-- add "N/A" to the end of each line that's irrelevant to your changes -->
    <!-- to check an item, place an "x" in the box like so: "- [x] Documentation" -->
    - [ ] Documentation
    - [ ] Tests
    - [ ] Ready to be merged <!-- In your opinion, is this ready to be merged as soon as it's reviewed? -->
    - [ ] Added myself to contributors table <!-- this is optional, see the contributing guidelines for instructions -->
    
    <!-- feel free to add additional comments -->
    
    <!-- Thank you for contributing! -->

--- packages/app/src/app/components/CodeEditor/Monaco/define-theme.js
+++ packages/app/src/app/components/CodeEditor/Monaco/define-theme.js
@@ -1,14 +1,62 @@
 let isThemeDefined = false;
+
+const cleanHex = hex => hex.slice(1).slice(0, 6);
+
+const getTheme = (url: String) => {
+  const theme = {};
+
+  const { tokenColors = [], colors = {} } = theme;
+  const rules = tokenColors
+    .filter(t => t.settings && t.scope && t.settings.foreground)
+    .reduce((acc, token) => {
+      const settings = {
+        foreground: cleanHex(token.settings.foreground),
+        background: token.settings.background
+          ? cleanHex(token.settings.background)
+          : null,
+        fontStyle: token.settings.fontStyle || null,
+      };
+
+      if (Array.isArray(token.scope)) {
+        token.scope.map(s =>
+          acc.push({
+            token: s,
+            ...settings,
+          })
+        );
+      } else {
+        acc.push({
+          token: token.scope,
+          ...settings,
+        });
+      }
+
+      return acc;
+    }, []);
+
+  const newColors = colors;
+  Object.keys(colors).forEach(c => {
+    if (newColors[c]) return c;
+
+    delete newColors[c];
+
+    return c;
+  });
+
+  return {
+    colors: newColors,
+    rules,
+    type: theme.type,
+  };
+};
+
 const defineTheme = monaco => {
   if (!isThemeDefined) {
     monaco.editor.defineTheme('CodeSandbox', {
-      base: 'vs-dark', // can also be vs-dark or hc-black
-      inherit: true, // can also be false to completely replace the builtin rules
-      rules: [
-        { token: 'comment', foreground: '626466' },
-        { token: 'keyword', foreground: '6CAEDD' },
-        { token: 'identifier', foreground: 'fac863' },
-      ],
+      base: getTheme().type ? `vs-${getTheme().type}` : 'vs',
+      inherit: true,
+      colors: getTheme().colors,
+      rules: getTheme().rules,
     });
     isThemeDefined = true;
   }
