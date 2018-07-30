7894e5c1aaa4e9d4b0f5d6be6bc22ec677caac5e
Diego Lameira <diegojgl@gmail.com>
2018-07-30 01:00:29 -0300

    Update tsconfig.prod.ts with the ConfigurationFile

--- packages/common/templates/configuration/tsconfig/prod.ts
+++ packages/common/templates/configuration/tsconfig/prod.ts
@@ -1,3 +1,42 @@
-{
-  "extends": "./tsconfig.json"
-}
+// @flow
+import type { ConfigurationFile } from '../types';
+
+const config: ConfigurationFile = {
+  title: 'tsconfig.prod.json',
+  type: 'typescript',
+  description: 'Configuration for how TypeScript transpiles.',
+  moreInfoUrl: 'http://www.typescriptlang.org/docs/handbook/tsconfig-json.html',
+
+  getDefaultCode: (template: string) => {
+    if (template === 'create-react-app-typescript') {
+      return JSON.stringify(
+        {
+          extends: './tsconfig.json'
+        },
+        null,
+        2
+      );
+    }
+
+    if (template === 'parcel') {
+      return JSON.stringify({
+        extends: './tsconfig.json'
+      });
+    }
+
+    return JSON.stringify(
+      {
+        extends: './tsconfig.json'
+      },
+      null,
+      2
+    );
+  },
+
+  schema:
+    'https://raw.githubusercontent.com/SchemaStore/schemastore/master/src/schemas/json/tsconfig.json',
+
+  partialSupportDisclaimer: `Only \`compilerOptions\` field is supported.`,
+};
+
+export default config;
