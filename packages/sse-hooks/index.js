496cce2649cd1a7ff72bfc463fa7c90ba8511ead
Ives van Hoorne <Ives.v.h@gmail.com>
2018-09-19 18:58:35 +0200

    Add __puppeteer__ hook to sse

--- packages/sse-hooks/index.js
+++ packages/sse-hooks/index.js
@@ -5,3 +5,9 @@ import { dispatch } from 'codesandbox-api';
 hookConsole();
 setupHistoryListeners();
 dispatch({ type: 'initialized' });
+
+setTimeout(() => {
+  if (typeof window.__puppeteer__ === 'function') {
+    window.__puppeteer__('done');
+  }
+}, 1000);
