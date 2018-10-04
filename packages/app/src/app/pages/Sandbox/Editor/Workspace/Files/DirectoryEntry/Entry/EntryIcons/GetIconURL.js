8eb4ec7e934f7fbfe8a7af44501aadb7cee83a8e
Sara Vieira <hey@iamsaravieira.com>
2018-10-04 13:44:02 +0200

    clean code

--- packages/app/src/app/pages/Sandbox/Editor/Workspace/Files/DirectoryEntry/Entry/EntryIcons/GetIconURL.js
+++ packages/app/src/app/pages/Sandbox/Editor/Workspace/Files/DirectoryEntry/Entry/EntryIcons/GetIconURL.js
@@ -1,24 +1,9 @@
-import jsSvg from 'common/components/icons/js.svg';
-import tsSvg from 'common/components/icons/ts.svg';
-import cssSvg from 'common/components/icons/css.svg';
-import reactSvg from 'common/components/icons/react.svg';
 import folderSvg from 'common/components/icons/folder.svg';
 import folderOpenSvg from 'common/components/icons/folder-open.svg';
-import jsonSvg from 'common/components/icons/json.svg';
-import reasonSvg from 'common/components/icons/reason.svg';
-import yarnSvg from 'common/components/icons/yarn.svg';
-import markdownSvg from 'common/components/icons/markdown.svg';
 import faviconSvg from 'common/components/icons/favicon.svg';
-import htmlSvg from 'common/components/icons/html.svg';
-import npmSvg from 'common/components/icons/npm.svg';
-import vueSvg from 'common/components/icons/vue.svg';
 import fileSvg from 'common/components/icons/file.svg';
-import svgSvg from 'common/components/icons/svg.svg';
 import imageSvg from 'common/components/icons/image.svg';
-import prettierSvg from 'common/components/icons/prettier.svg';
 import codesandboxSvg from 'common/components/icons/codesandbox.svg';
-import babelSvg from 'common/components/icons/babel.svg';
-import sassSvg from 'common/components/icons/sass.svg';
 
 function imageExists(url) {
   return new Promise((resolve, reject) => {
@@ -32,24 +17,9 @@ function imageExists(url) {
 const icons = {
   directory: folderSvg,
   'directory-open': folderOpenSvg,
-  react: reactSvg,
-  css: cssSvg,
-  json: jsonSvg,
-  yarn: yarnSvg,
-  markdown: markdownSvg,
   favicon: faviconSvg,
-  html: htmlSvg,
-  npm: npmSvg,
-  vue: vueSvg,
-  js: jsSvg,
-  typescript: tsSvg,
-  svg: svgSvg,
   image: imageSvg,
-  prettier: prettierSvg,
   codesandbox: codesandboxSvg,
-  babel: babelSvg,
-  sass: sassSvg,
-  reason: reasonSvg,
 };
 
 async function getIconURL(type) {
--- packages/app/src/app/utils/get-type.js
+++ packages/app/src/app/utils/get-type.js
@@ -1,60 +1,72 @@
 /* @flow */
 import isImage from 'common/utils/is-image';
 
-const mdRegex = /\.md$/;
-const yamlRegex = /\.yml$/;
 const svgRegex = /\.svg$/;
-const jsxRegex = /\.jsx$/;
-const tsRegex = /\.tsx?$/;
-const jsRegex = /\.js$/;
-const reasonRegex = /\.re$/;
-const sassRegex = /\.scss$/;
+
+const specialCasesMap = {
+  'favicon.ico': 'favicon',
+  'yarn.lock': 'yarn',
+  'package.json': 'npm',
+  'sandbox.config.json': 'codesandbox',
+  'readme.md': 'readme',
+  'contributing.md': 'contributing',
+  'tsconfig.json': 'typescript',
+  'tslint.json': 'typescript',
+  dockerfile: 'docker',
+};
+
+const regexCasesMap = {
+  markdown: /\.md$/,
+  yaml: /\.yml$/,
+  react: /\.jsx$/,
+  reason: /\.re$/,
+  sass: /\.scss$/,
+  javascript: /\.js$/,
+  typescript: /\.tsx?$/,
+  console: /\.sh$/,
+  // STARTS WITH
+  git: /^.git/i,
+  flow: /^.flow/i,
+};
+
+const getKeyByValue = (object, value) =>
+  Object.keys(object).find(key => object[key] === value);
 
 export function getMode(title: string = '') {
+  // Remove Ignore
   const removeIgnoreTitle = title.split('ignore')[0].toLowerCase();
 
-  // EXCEPTIONS
+  const titleArr = removeIgnoreTitle.split('.');
 
-  if (removeIgnoreTitle === 'favicon.ico') return 'favicon';
-  if (removeIgnoreTitle === 'yarn.lock') return 'yarn';
-  if (removeIgnoreTitle === '.travis.yml') return 'travis';
-  if (removeIgnoreTitle === 'package.json') return 'npm';
-  if (removeIgnoreTitle === 'sandbox.config.json') return 'codesandbox';
-  if (removeIgnoreTitle === 'readme.md') return 'readme';
-  if (removeIgnoreTitle === 'contributing.md') return 'contributing';
+  // RemoveTitle
+  if (removeIgnoreTitle.endsWith('rc')) {
+    return titleArr.join('').split('rc')[0];
+  }
+
+  // Name Bases
+  const keys = Object.keys(specialCasesMap);
+  if (keys.includes(removeIgnoreTitle))
+    return specialCasesMap[removeIgnoreTitle];
 
   // TEST BASED
-  if (mdRegex.test(removeIgnoreTitle)) return 'markdown';
-  if (yamlRegex.test(removeIgnoreTitle)) return 'yaml';
-  if (jsxRegex.test(removeIgnoreTitle)) return 'react';
-  if (reasonRegex.test(removeIgnoreTitle)) return 'reason';
-  if (sassRegex.test(removeIgnoreTitle)) return 'sass';
+  const regexValues = Object.values(regexCasesMap);
+  const match = regexValues.find(value =>
+    new RegExp(value).test(removeIgnoreTitle)
+  );
+
+  if (match) return getKeyByValue(regexCasesMap, match);
+
+  // Include tests
   if (!removeIgnoreTitle.includes('.')) return 'raw';
-  if (removeIgnoreTitle.startsWith('.git')) return 'git';
-  if (removeIgnoreTitle.startsWith('.flow')) return 'flow';
   if (
     removeIgnoreTitle.endsWith('.module.ts') ||
     removeIgnoreTitle.endsWith('.component.ts')
   )
     return 'angular';
   if (removeIgnoreTitle.includes('webpack')) return 'webpack';
-  if (jsRegex.test(removeIgnoreTitle)) return 'javascript';
-  if (
-    tsRegex.test(title) ||
-    title === 'tsconfig.json' ||
-    title === 'tslint.json'
-  ) {
-    return 'typescript';
-  }
 
   if (isImage(removeIgnoreTitle) && !svgRegex.test(removeIgnoreTitle)) {
     return 'image';
-  }
-
-  const titleArr = removeIgnoreTitle.split('.');
-
-  if (removeIgnoreTitle.endsWith('rc')) {
-    return titleArr.join('').split('rc')[0];
   }
 
   return titleArr[titleArr.length - 1];
