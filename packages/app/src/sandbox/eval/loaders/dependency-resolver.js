9ce2f9ee7c3836812ec89f2ea20df5661b3d9d04
Ives van Hoorne <Ives.v.h@gmail.com>
2018-10-09 11:03:01 +0200

    Fix generators
    Fixes #1169

--- packages/app/src/sandbox/eval/loaders/dependency-resolver.js
+++ packages/app/src/sandbox/eval/loaders/dependency-resolver.js
@@ -1,6 +1,18 @@
 import DependencyNotFoundError from 'sandbox-hooks/errors/dependency-not-found-error';
 
 /**
+ * We don't have fancy resolving for @babel/runtime because of optimalization,
+ * so we try to alter it the best way we can.
+ */
+function alterBabelRuntimeDependencyPath(path: string) {
+  if (path === '@babel/runtime/regenerator') {
+    return '@babel/runtime/regenerator/index';
+  }
+
+  return path;
+}
+
+/**
  * Converts a dependency string to an actual dependency
  *
  * @param {string} dependencyPath
@@ -10,7 +22,9 @@ import DependencyNotFoundError from 'sandbox-hooks/errors/dependency-not-found-e
 export default function getDependency(dependencyPath: string) {
   if (dependencyPath.startsWith('@babel/runtime/')) {
     // We have a precomputed bundle for this, since it's small anyway
-    return require('./@babel-runtime.no-webpack')(dependencyPath);
+    return require('./@babel-runtime.no-webpack')(
+      alterBabelRuntimeDependencyPath(dependencyPath)
+    );
   }
 
   if (dependencyPath === 'codesandbox-api') {
