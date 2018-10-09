1665b64be149e98711b17bd51019c8b9805ef5a5
Ives van Hoorne <Ives.v.h@gmail.com>
2018-10-09 11:44:49 +0200

    Remove optimization for @babel/runtime

--- packages/app/src/sandbox/eval/loaders/dependency-resolver.js
+++ packages/app/src/sandbox/eval/loaders/dependency-resolver.js
@@ -1,18 +1,6 @@
 import DependencyNotFoundError from 'sandbox-hooks/errors/dependency-not-found-error';
 
 /**
- * We don't have fancy resolving for @babel/runtime because of optimalization,
- * so we try to alter it the best way we can.
- */
-function alterBabelRuntimeDependencyPath(path: string) {
-  if (path === '@babel/runtime/regenerator') {
-    return '@babel/runtime/regenerator/index';
-  }
-
-  return path;
-}
-
-/**
  * Converts a dependency string to an actual dependency
  *
  * @param {string} dependencyPath
@@ -20,13 +8,6 @@ function alterBabelRuntimeDependencyPath(path: string) {
  * @returns
  */
 export default function getDependency(dependencyPath: string) {
-  if (dependencyPath.startsWith('@babel/runtime/')) {
-    // We have a precomputed bundle for this, since it's small anyway
-    return require('./@babel-runtime.no-webpack')(
-      alterBabelRuntimeDependencyPath(dependencyPath)
-    );
-  }
-
   if (dependencyPath === 'codesandbox-api') {
     return require('codesandbox-api');
   }
--- packages/app/src/sandbox/eval/transpilers/babel/index.js
+++ packages/app/src/sandbox/eval/transpilers/babel/index.js
@@ -7,6 +7,7 @@ import getBabelConfig from './babel-parser';
 import WorkerTranspiler from '../worker-transpiler';
 import { type LoaderContext } from '../../transpiled-module';
 import type { default as Manager } from '../../manager';
+import { isBabel7 } from '../../utils/is-babel-7';
 
 import delay from '../../../utils/delay';
 
@@ -71,14 +72,18 @@ class BabelTranspiler extends WorkerTranspiler {
 
       const loaderOptions = loaderContext.options || {};
 
+      const dependencies =
+        configs.package &&
+        configs.package.parsed &&
+        configs.package.parsed.dependencies;
+
+      const devDependencies =
+        configs.package &&
+        configs.package.parsed &&
+        configs.package.parsed.devDependencies;
+
       const isV7 =
-        loaderContext.options.isV7 ||
-        !!(
-          configs.package &&
-          configs.package.parsed &&
-          configs.package.parsed.devDependencies &&
-          configs.package.parsed.devDependencies['@vue/cli-plugin-babel']
-        );
+        loaderContext.options.isV7 || isBabel7(dependencies, devDependencies);
 
       const babelConfig = getBabelConfig(
         foundConfig || loaderOptions.config,
