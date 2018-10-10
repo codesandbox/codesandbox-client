ea48b8e4b4135c400600d8da74a3b797da9b296a
Sara Vieira <hey@iamsaravieira.com>
2018-10-10 14:03:21 +0200

    make search responsive (#1185)

--- packages/app/src/app/pages/Search/Filters/Filter/index.js
+++ packages/app/src/app/pages/Search/Filters/Filter/index.js
@@ -1,21 +1,31 @@
-import React from 'react';
-
+import React, { Component } from 'react';
+import Down from 'react-icons/lib/md/expand-more';
+import Up from 'react-icons/lib/md/expand-less';
 import { RefinementList } from 'react-instantsearch/dom';
 
-import { Container, Title } from './elements';
+import { Container, Title, Button } from './elements';
 
-function Filter({ title, attributeName, operator, noSearch }) {
-  return (
-    <Container>
-      <Title>{title}</Title>
-      <RefinementList
-        withSearchBox={!noSearch}
-        showMore={!noSearch}
-        operator={operator}
-        attributeName={attributeName}
-      />
-    </Container>
-  );
+class Filter extends Component {
+  state = { open: false };
+
+  toggle = () => this.setState(({ open }) => ({ open: !open }));
+  render() {
+    const { title, attributeName, operator, noSearch } = this.props;
+    const { open } = this.state;
+    return (
+      <Container open={open}>
+        <Title>
+          <span>{title}</span>
+          <Button onClick={this.toggle}>{open ? <Up /> : <Down />}</Button>
+        </Title>
+        <RefinementList
+          withSearchBox={!noSearch}
+          showMore={!noSearch}
+          operator={operator}
+          attributeName={attributeName}
+        />
+      </Container>
+    );
+  }
 }
-
 export default Filter;
