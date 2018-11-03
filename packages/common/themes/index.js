1e836608df8f56d4ee6f956f925cb19a6ee862cb
Sai Ram Kunala <sairam@kunala.com>
2018-11-03 18:56:26 +0530

    move all rawgit.com to serve from jsdelivr.net Reason: rawgit.com is getting shutdown

--- packages/common/themes/index.js
+++ packages/common/themes/index.js
@@ -10,50 +10,50 @@ export default [
     name: 'Night Owl',
     id: 'nightOwl',
     url:
-      'https://cdn.rawgit.com/sdras/night-owl-vscode-theme/ff58e0d3/themes/Night%20Owl-color-theme.json',
+      'https://cdn.jsdelivr.net/gh/sdras/night-owl-vscode-theme@ff58e0d3967ef968492ce8c9809239f0511b9b70/themes/Night%20Owl-color-theme.json',
   },
   {
     name: 'Night Owl (No Italics)',
     id: 'nightOwlNoItalics',
     url:
-      'https://cdn.rawgit.com/sdras/night-owl-vscode-theme/ff58e0d3/themes/Night%20Owl-color-theme-noitalic.json',
+      'https://cdn.jsdelivr.net/gh/sdras/night-owl-vscode-theme@ff58e0d3967ef968492ce8c9809239f0511b9b70/themes/Night%20Owl-color-theme-noitalic.json',
   },
   {
     name: 'Atom Dark',
     id: 'atomDark',
     type: 'dark',
     url:
-      'https://cdn.rawgit.com/akamud/vscode-theme-onedark/11ef4839/themes/OneDark.json',
+      'https://cdn.jsdelivr.net/gh/akamud/vscode-theme-onedark@11ef483921495ea6c086f56149dfa0ca6225ae53/themes/OneDark.json',
   },
   {
     name: 'Cobalt 2',
     id: 'cobalt2',
     url:
-      'https://cdn.rawgit.com/wesbos/cobalt2-vscode/master/theme/cobalt2.json',
+      'https://cdn.jsdelivr.net/gh/wesbos/cobalt2-vscode/theme/cobalt2.json',
   },
   {
     name: 'Palenight',
     id: 'palenight',
     url:
-      'https://cdn.rawgit.com/whizkydee/vscode-material-palenight-theme/master/themes/palenight.json',
+      'https://cdn.jsdelivr.net/gh/whizkydee/vscode-material-palenight-theme/themes/palenight.json',
   },
   {
     name: 'Palenight Italic',
     id: 'palenightItalic',
     url:
-      'https://cdn.rawgit.com/whizkydee/vscode-material-palenight-theme/master/themes/palenight-italic.json',
+      'https://cdn.jsdelivr.net/gh/whizkydee/vscode-material-palenight-theme/themes/palenight-italic.json',
   },
   {
     name: 'Shades of Purple',
     id: 'shadesOfPurple',
     url:
-      'https://cdn.rawgit.com/ahmadawais/shades-of-purple-vscode/983101b2e9f57638fbc1bd5c551f5c84e561756e/themes/shades-of-purple-color-theme.json',
+      'https://cdn.jsdelivr.net/gh/ahmadawais/shades-of-purple-vscode@983101b2e9f57638fbc1bd5c551f5c84e561756e/themes/shades-of-purple-color-theme.json',
   },
   {
     name: 'Lucy',
     id: 'lucy',
     url:
-      'https://cdn.rawgit.com/juliettepretot/lucy-vscode/f27d8363/dist/color-theme.json',
+      'https://cdn.jsdelivr.net/gh/juliettepretot/lucy-vscode@f27d836399d906bc7f7af5fd3950126240be969c/dist/color-theme.json',
   },
   {
     name: 'High Contrast',
@@ -69,12 +69,12 @@ export default [
     name: 'Atom Light',
     id: 'atomLight',
     url:
-      'https://cdn.rawgit.com/akamud/vscode-theme-onelight/master/themes/OneLight.json',
+      'https://cdn.jsdelivr.net/gh/akamud/vscode-theme-onelight/themes/OneLight.json',
   },
   {
     name: 'Solarized Light',
     id: 'solarizedLight',
     url:
-      'https://raw.githubusercontent.com/Microsoft/vscode/d2b6bbb46fbdf535e2c96b3e00ac56ac1d427a69/extensions/theme-solarized-light/themes/solarized-light-color-theme.json',
+      'https://cdn.jsdelivr.net/gh/Microsoft/vscode@d2b6bbb46fbdf535e2c96b3e00ac56ac1d427a69/extensions/theme-solarized-light/themes/solarized-light-color-theme.json',
   },
 ];
