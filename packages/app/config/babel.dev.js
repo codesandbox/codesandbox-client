3d7845113aea260b1433fe2055837bc42f78b220
Ives van Hoorne <Ives.v.h@gmail.com>
2018-11-03 22:01:21 +0100

    Fix integration tests

--- packages/app/config/babel.dev.js
+++ packages/app/config/babel.dev.js
@@ -7,23 +7,23 @@ module.exports = {
   presets: [
     // Latest stable ECMAScript features
     require.resolve('@babel/preset-flow'),
-    // [
-    //   require.resolve('@babel/preset-env'),
-    //   {
-    //     targets: {
-    //       chrome: 67,
-    //       // We currently minify with uglify
-    //       // Remove after https://github.com/mishoo/UglifyJS2/issues/448
-    //     },
-    //     // Disable polyfill transforms
-    //     useBuiltIns: false,
-    //     modules: false,
-    //     forceAllTransforms: true,
-    //   },
-    // ],
+    process.env.NODE_ENV === 'test' && [
+      require.resolve('@babel/preset-env'),
+      {
+        targets: {
+          chrome: 67,
+          // We currently minify with uglify
+          // Remove after https://github.com/mishoo/UglifyJS2/issues/448
+        },
+        // Disable polyfill transforms
+        useBuiltIns: false,
+        modules: false,
+        forceAllTransforms: true,
+      },
+    ],
     // JSX, Flow
     require.resolve('@babel/preset-react'),
-  ],
+  ].filter(Boolean),
   plugins: [
     require.resolve('@babel/plugin-transform-template-literals'),
     require.resolve('@babel/plugin-transform-destructuring'),
