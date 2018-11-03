ea0bae15cf3f5d36a3294fbd21401d98932faebd
Ives van Hoorne <Ives.v.h@gmail.com>
2018-11-03 16:04:11 +0100

    New vscode bump

--- packages/app/config/webpack.common.js
+++ packages/app/config/webpack.common.js
@@ -445,7 +445,7 @@ module.exports = {
       [
         {
           from: '../../standalone-packages/vscode-editor/release/min/vs',
-          to: 'public/vscode4/vs',
+          to: 'public/vscode5/vs',
           force: true,
         },
         {
