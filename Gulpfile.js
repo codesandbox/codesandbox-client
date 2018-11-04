e67fa24333e0588c871365b2b6a3fd75349764a2
Ives van Hoorne <Ives.v.h@gmail.com>
2018-11-04 02:55:18 +0100

    Bump vscode version

--- Gulpfile.js
+++ Gulpfile.js
@@ -45,7 +45,7 @@ gulp.task('old-browserfs', () =>
 gulp.task('old-vscode', () =>
   gulp
     .src('standalone-packages/vscode-editor/release/min/vs/**/*')
-    .pipe(gulp.dest('public/vscode4/vs'))
+    .pipe(gulp.dest('public/vscode5/vs'))
 );
 
 gulp.task('statics', () =>
