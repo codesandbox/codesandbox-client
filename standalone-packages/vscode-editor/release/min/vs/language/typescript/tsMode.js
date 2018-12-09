3535a73f590762fac669534c9c6d98a7a82ac517
Ives van Hoorne <ives.v.h@gmail.com>
2018-12-09 22:13:13 +0100

    Update type fetching of VSCode

--- standalone-packages/vscode-editor/release/min/vs/language/typescript/tsMode.js
+++ standalone-packages/vscode-editor/release/min/vs/language/typescript/tsMode.js
@@ -1,6 +1,6 @@
 /*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
- * monaco-typescript version: 3.2.0(3717a2f38876a6e289dd25c7f611bdc38c00ca1f)
+ * monaco-typescript version: 3.2.0(5ab1ef89572ce60e2a5c3bc2f3f6eb0da3160398)
  * Released under the MIT license
  * https://github.com/Microsoft/monaco-typescript/blob/master/LICENSE.md
  *-----------------------------------------------------------------------------*/
