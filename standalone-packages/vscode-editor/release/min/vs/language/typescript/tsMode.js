4ffe38232b85a60ae740b567a0d042a040820801
Ives van Hoorne <ives.v.h@gmail.com>
2018-12-10 14:35:45 +0100

    Update compiled vscode

--- standalone-packages/vscode-editor/release/min/vs/language/typescript/tsMode.js
+++ standalone-packages/vscode-editor/release/min/vs/language/typescript/tsMode.js
@@ -1,6 +1,6 @@
 /*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
- * monaco-typescript version: 3.2.0(5ab1ef89572ce60e2a5c3bc2f3f6eb0da3160398)
+ * monaco-typescript version: 3.2.0(34dd754e23a35b1d6b72c820f773edf5e14bbe14)
  * Released under the MIT license
  * https://github.com/Microsoft/monaco-typescript/blob/master/LICENSE.md
  *-----------------------------------------------------------------------------*/
