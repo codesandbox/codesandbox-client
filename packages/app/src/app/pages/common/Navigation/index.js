397b6b5f527eefdfbffdc5a1d5f7604e18dcca07
Ives van Hoorne <ives.v.h@gmail.com>
2018-12-16 19:42:42 +0100

    Fix lint

--- packages/app/src/app/pages/common/Navigation/index.js
+++ packages/app/src/app/pages/common/Navigation/index.js
@@ -2,17 +2,15 @@ import * as React from 'react';
 import { inject, observer } from 'mobx-react';
 import { Link } from 'react-router-dom';
 import Media from 'react-media';
-import { patronUrl, dashboardUrl, searchUrl } from 'common/utils/url-generator';
+import { patronUrl, searchUrl } from 'common/utils/url-generator';
 
 import SearchIcon from 'react-icons/lib/go/search';
 import PlusIcon from 'react-icons/lib/go/plus';
 import BellIcon from 'react-icons/lib/md/notifications';
-import BookIcon from 'react-icons/lib/md/library-books';
 import Row from 'common/components/flex/Row';
 import Tooltip from 'common/components/Tooltip';
 import PatronBadge from '-!svg-react-loader!common/utils/badges/svg/patron-4.svg'; // eslint-disable-line import/no-webpack-loader-syntax
 import HeaderSearchBar from 'app/components/HeaderSearchBar';
-import InfoIcon from 'app/pages/Sandbox/Editor/Navigation/InfoIcon';
 import OverlayComponent from 'app/components/Overlay';
 import Notifications from './Notifications';
 
