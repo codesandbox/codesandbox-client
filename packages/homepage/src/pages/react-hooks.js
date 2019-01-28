811ed743bcb6652affabfa4bc686fec6acd08b9c
Ives van Hoorne <ives.v.h@gmail.com>
2019-01-28 20:45:34 +0100

    Add picked modal

--- packages/homepage/src/pages/react-hooks.js
+++ packages/homepage/src/pages/react-hooks.js
@@ -1,31 +1,62 @@
 import React from 'react';
 import { graphql } from 'gatsby';
 
-import { protocolAndHost, sandboxUrl } from 'common/utils/url-generator';
 import TitleAndMetaTags from '../components/TitleAndMetaTags';
 import PageContainer from '../components/PageContainer';
 import { Heading2 } from '../components/headings';
 import Layout from '../components/layout';
+import SandboxModal from '../screens/explore/SandboxModal';
 
 import WideSandbox from '../screens/explore/WideSandbox';
 import { Container, Sandboxes } from './_explore.elements';
 
 export default class extends React.PureComponent {
   state = {
-    starters: this.props.data.allAirtable.edges.map(s => ({
+    sandboxes: this.props.data.allAirtable.edges.map(s => ({
       ...s.node.data,
       title: s.node.data.title,
     })),
   };
 
-  loadSandboxes = async id => {
-    const s = await fetch(`${protocolAndHost()}/api/v1/sandboxes/${id}`);
-    const json = await s.json();
-    return json.data;
+  openSandbox = index => {
+    const sandbox = this.state.sandboxes[index];
+    const { id, title, description } = sandbox;
+
+    this.setState({
+      selectedSandbox: {
+        id,
+        title,
+        description,
+        screenshotUrl: sandbox.screenshot_url,
+      },
+    });
   };
 
+  openPreviousSandbox = currentIndex => () => {
+    this.openSandbox(currentIndex - 1);
+  };
+
+  openNextSandbox = currentIndex => () => {
+    this.openSandbox(currentIndex + 1);
+  };
+
+  selectSandbox = ({ id, title, description, screenshotUrl }) => {
+    this.setState({
+      selectedSandbox: { id, title, description, screenshotUrl },
+    });
+  };
+
+  getCurrentIndex = () =>
+    this.state.selectedSandbox
+      ? this.state.sandboxes.findIndex(
+          s => this.state.selectedSandbox.id === s.id
+        )
+      : -1;
+
   render() {
-    const { starters } = this.state;
+    const { sandboxes, selectedSandbox } = this.state;
+
+    const currentIndex = this.getCurrentIndex();
 
     return (
       <Layout>
@@ -34,18 +65,34 @@ export default class extends React.PureComponent {
             description="A showcase of amazing uses for React Hooks!"
             title="React Hooks - CodeSandbox"
           />
+
+          <SandboxModal
+            onClose={this.closeModal}
+            sandboxId={selectedSandbox && selectedSandbox.id}
+            screenshotUrl={selectedSandbox && selectedSandbox.screenshotUrl}
+            title={selectedSandbox && selectedSandbox.title}
+            description={selectedSandbox && selectedSandbox.description}
+            openPreviousSandbox={
+              currentIndex > 0 &&
+              currentIndex !== -1 &&
+              this.openPreviousSandbox(currentIndex)
+            }
+            openNextSandbox={
+              currentIndex < this.state.sandboxes.length - 1 &&
+              currentIndex !== -1 &&
+              this.openNextSandbox(currentIndex)
+            }
+          />
+
           <PageContainer as="main" width={1440}>
             <Heading2 style={{ margin: '3rem 0' }}>
               React Hooks Sandboxes
             </Heading2>
             <Sandboxes>
-              {starters.map(sandbox => (
+              {sandboxes.map(sandbox => (
                 <WideSandbox
                   key={sandbox.id}
-                  pickSandbox={({ id }) => {
-                    const url = sandboxUrl({ id });
-                    return window.open(url, '_blank');
-                  }}
+                  pickSandbox={this.selectSandbox}
                   sandbox={sandbox}
                 />
               ))}
