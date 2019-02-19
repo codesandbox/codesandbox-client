2d859f0cafc50a847c083bc87064ac9e9d83fe26
Sara Vieira <hey@iamsaravieira.com>
2019-02-19 14:45:53 +0100

    try again

--- packages/homepage/src/pages/legal/privacy.js
+++ packages/homepage/src/pages/legal/privacy.js
@@ -1,25 +1,28 @@
 import React from 'react';
-import { graphql } from 'gatsby';
+import { StaticQuery, graphql } from 'gatsby';
 import { Content } from './_elements';
 import Navigation from './_navigation';
 import Layout from '../../components/layout';
 import TitleAndMetaTags from '../../components/TitleAndMetaTags';
 import PageContainer from '../../components/PageContainer';
 
-export default ({ data: { markdownRemark } }) => (
+export default () => (
   <Layout>
     <TitleAndMetaTags title="CodeSandbox - Privacy Policy" />
     <PageContainer width={1024}>
       <Navigation />
-      <Content dangerouslySetInnerHTML={{ __html: markdownRemark.html }} />
+      <StaticQuery
+        query={graphql`
+          query {
+            markdownRemark(id: { eq: "61d88176-8b26-5ec7-86db-c64d70676530" }) {
+              html
+            }
+          }
+        `}
+        render={({ markdownRemark }) => (
+          <Content dangerouslySetInnerHTML={{ __html: markdownRemark.html }} />
+        )}
+      />
     </PageContainer>
   </Layout>
 );
-
-export const pageQuery = graphql`
-  query Privacy {
-    markdownRemark(id: { eq: "61d88176-8b26-5ec7-86db-c64d70676530" }) {
-      html
-    }
-  }
-`;
--- packages/homepage/src/pages/legal/terms.js
+++ packages/homepage/src/pages/legal/terms.js
@@ -1,25 +1,28 @@
 import React from 'react';
-import { graphql } from 'gatsby';
+import { graphql, StaticQuery } from 'gatsby';
 import { Content } from './_elements';
 import Navigation from './_navigation';
 import Layout from '../../components/layout';
 import TitleAndMetaTags from '../../components/TitleAndMetaTags';
 import PageContainer from '../../components/PageContainer';
 
-export default ({ data: { markdownRemark } }) => (
+export default () => (
   <Layout>
     <TitleAndMetaTags title="CodeSandbox - Terms and Conditions" />
     <PageContainer width={1024}>
       <Navigation />
-      <Content dangerouslySetInnerHTML={{ __html: markdownRemark.html }} />
+      <StaticQuery
+        query={graphql`
+          query {
+            markdownRemark(id: { eq: "c591e19c-94a0-5b0b-be4a-b42142aeffe7" }) {
+              html
+            }
+          }
+        `}
+        render={({ markdownRemark }) => (
+          <Content dangerouslySetInnerHTML={{ __html: markdownRemark.html }} />
+        )}
+      />
     </PageContainer>
   </Layout>
 );
-
-export const pageQuery = graphql`
-  query Terms {
-    markdownRemark(id: { eq: "c591e19c-94a0-5b0b-be4a-b42142aeffe7" }) {
-      html
-    }
-  }
-`;
