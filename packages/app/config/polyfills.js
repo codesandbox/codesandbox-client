5b6f9892e55cb7368ca4853782c055c4f23b7971
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2019-03-20 08:41:28 +0100

    Fix "Can't call set on undefined"
    Map.prototype.set returns undefined in IE11.
    Officiial React docs recommend the same solution

--- packages/app/config/polyfills.js
+++ packages/app/config/polyfills.js
@@ -33,3 +33,5 @@ require('core-js/fn/array/find');
 require('core-js/fn/array/from');
 require('core-js/fn/object/assign');
 require('core-js/fn/symbol');
+require('core-js/fn/map');
+require('core-js/fn/set');
