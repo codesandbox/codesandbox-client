c4a40b1a496ea1eae3169214f9ab86c13d6bdfa6
Ives van Hoorne <ives.v.h@gmail.com>
2019-03-22 14:23:38 +0100

    New VSCode

--- packages/app/config/webpack.common.js
+++ packages/app/config/webpack.common.js
@@ -465,7 +465,7 @@ module.exports = {
       [
         {
           from: '../../standalone-packages/vscode-editor/release/min/vs',
-          to: 'public/vscode14/vs',
+          to: 'public/vscode15/vs',
           force: true,
         },
         {
