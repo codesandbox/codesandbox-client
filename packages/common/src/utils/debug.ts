5ea3b3c904281cfbd74064d15e1a28d4ab4a59d4
Ives van Hoorne <ives.v.h@gmail.com>
2019-03-29 22:17:15 +0000

    Disable Raven logging

--- packages/common/src/utils/debug.ts
+++ packages/common/src/utils/debug.ts
@@ -26,7 +26,7 @@ const getDebugger: () => (key: string) => (...message: any[]) => void = () => {
     const global = getGlobal() as any;
     // Return a debugger that will log to sentry
     return (key: string) => (message: string) => {
-      if (typeof global.Raven === 'object') {
+      if (false || typeof global.Raven === 'object') {
         try {
           global.Raven.captureBreadcrumb({
             message: `${key} - ${message}`,
