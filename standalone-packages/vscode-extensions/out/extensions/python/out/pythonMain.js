a4abec5ce5d385cff3406ffdc054458771a66ce7
Sara Vieira <hey@iamsaravieira.com>
2019-03-29 16:26:39 +0000

    Update pythonMain.js

--- standalone-packages/vscode-extensions/out/extensions/python/out/pythonMain.js
+++ standalone-packages/vscode-extensions/out/extensions/python/out/pythonMain.js
@@ -1,12 +1,10 @@
-
-
+"use strict";
 /*---------------------------------------------------------------------------------------------
  *  Copyright (c) Microsoft Corporation. All rights reserved.
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 Object.defineProperty(exports, "__esModule", { value: true });
 const vscode_1 = require("vscode");
-
 function activate(_context) {
     vscode_1.languages.setLanguageConfiguration('python', {
         onEnterRules: [
@@ -18,4 +16,4 @@ function activate(_context) {
     });
 }
 exports.activate = activate;
-// # sourceMappingURL=https://ticino.blob.core.windows.net/sourcemaps/05f146c7a8f7f78e80261aa3b2a2e642586f9eb3/extensions/python/out/pythonMain.js.map
+//# sourceMappingURL=https://ticino.blob.core.windows.net/sourcemaps/05f146c7a8f7f78e80261aa3b2a2e642586f9eb3/extensions/python/out/pythonMain.js.map
