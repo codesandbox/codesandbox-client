60ae833933436066aa1bea26aaba9cf0b42a06c9
Neville Mehta <nevmehta@ebay.com>
2019-04-07 00:38:33 -0700

    marko is a client template

--- packages/common/src/templates/marko.ts
+++ packages/common/src/templates/marko.ts
@@ -8,7 +8,6 @@ export default new Template(
   'github/nm123github/marko-codesandbox',
   decorateSelector(() => '#f5ac00'),
   {
-    isServer: true,
     showOnHomePage: true,
     main: false,
     netlify: false
