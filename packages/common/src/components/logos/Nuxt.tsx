30f1fcb61c4de0b1c12629267d9a600c89fb8508
Sébastien Chopin <seb@orion.sh>
2019-04-08 18:12:52 +0200

    fix(nuxt): Update logo (#1743)

--- packages/common/src/components/logos/Nuxt.tsx
+++ packages/common/src/components/logos/Nuxt.tsx
@@ -5,29 +5,13 @@ export default ({ width = 32, height = 32, className }) => (
     width={width}
     height={height}
     className={className}
-    viewBox="0 0 223 158"
+    viewBox="0 0 452 342"
     xmlns="http://www.w3.org/2000/svg"
   >
-    <g id="Page-1" fill="none" fillRule="evenodd">
-      <g id="nuxt" fillRule="nonzero">
-        <g id="Group">
-          <polyline
-            id="Shape"
-            fill="#41B883"
-            points="70.6465795 158 36.7703219 158 0.157042254 158 91.7126761 0 183.26831 158 147.103722 158 113.227465 158"
-          />
-          <polyline
-            id="Shape"
-            fill="#328170"
-            points="174.272032 158 222.842958 158 143.424447 21.4332386 64.6789738 158 113.249899 158"
-          />
-          <polyline
-            id="Shape"
-            fill="#35495E"
-            points="145.421127 158 181.204326 158 122.694869 57.4096591 64.6789738 158 100.462173 158"
-          />
-        </g>
-      </g>
+    <g fill="none" fill-rule="evenodd">
+      <path d="M139 330l-1-2c-2-4-2-8-1-13H29L189 31l67 121 22-16-67-121c-1-2-9-14-22-14-6 0-15 2-22 15L5 303c-1 3-8 16-2 27 4 6 10 12 24 12h136c-14 0-21-6-24-12z" fill="#00C58E"/>
+      <path d="M447 304L317 70c-2-2-9-15-22-15-6 0-15 3-22 15l-17 28v54l39-67 129 230h-49a23 23 0 0 1-2 14l-1 1c-6 11-21 12-23 12h76c3 0 17-1 24-12 3-5 5-14-2-26z" fill="#108775"/>
+      <path d="M376 330v-1l1-2c1-4 2-8 1-12l-4-12-102-178-15-27h-1l-15 27-102 178-4 12a24 24 0 0 0 2 15c4 6 10 12 24 12h190c3 0 18-1 25-12zM256 152l93 163H163l93-163z" fill="#FFF" fill-rule="nonzero"/>
     </g>
   </svg>
 );
