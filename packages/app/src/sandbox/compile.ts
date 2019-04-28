8f148a69346a3a4e778d7dd7a4cd2c660b7fb78f
Ives van Hoorne <ives.v.h@gmail.com>
2019-04-28 14:56:02 +0200

    Move check to devDependencies

--- packages/app/src/sandbox/compile.ts
+++ packages/app/src/sandbox/compile.ts
@@ -245,19 +245,23 @@ function getDependencies(parsedPackage, templateDefinition, configurations) {
       return; // is replaced
     }
 
-    if (usedDep === '@vue/babel-preset-app' && d[dep].startsWith('^3')) {
-      // Native modules got added in 3.7.0, we need to hardcode to latest
-      // working version of the babel plugin as a fix. https://twitter.com/notphanan/status/1122475053633941509
-      returnedDependencies[usedDep] = '3.6.0';
-      return;
-    }
-
     returnedDependencies[usedDep] = d[dep];
   });
 
   Object.keys(devDependencies).forEach(dep => {
     const usedDep = DEPENDENCY_ALIASES[dep] || dep;
+
     if (foundWhitelistedDevDependencies.indexOf(usedDep) > -1) {
+      if (
+        usedDep === '@vue/babel-preset-app' &&
+        devDependencies[dep].startsWith('^3')
+      ) {
+        // Native modules got added in 3.7.0, we need to hardcode to latest
+        // working version of the babel plugin as a fix. https://twitter.com/notphanan/status/1122475053633941509
+        returnedDependencies[usedDep] = '3.6.0';
+        return;
+      }
+
       returnedDependencies[usedDep] = devDependencies[dep];
     }
   });
