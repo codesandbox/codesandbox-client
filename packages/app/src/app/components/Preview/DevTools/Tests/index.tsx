b75e9969baa73478b05f49e7d8da85b08945f5ad
Ives van Hoorne <ives.v.h@gmail.com>
2019-04-28 22:01:35 +0200

    Improve scrolling of tests view

--- packages/app/src/app/components/Preview/DevTools/Tests/index.tsx
+++ packages/app/src/app/components/Preview/DevTools/Tests/index.tsx
@@ -501,7 +501,13 @@ class Tests extends React.Component<DevToolProps, State> {
     const TSplitPane = SplitPane as any;
     return (
       <Container>
-        <TSplitPane split="horizontal" defaultSize={'50%'}>
+        <TSplitPane
+          split="horizontal"
+          defaultSize={'50%'}
+          pane2Style={{
+            height: '100%',
+          }}
+        >
           <TestContainer>
             <TestSummary
               running={this.state.running}
