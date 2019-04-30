6422f696f806b1e590081ae8418da6908703ab5b
Sara Vieira <hey@iamsaravieira.com>
2019-04-30 15:01:59 +0200

    Add theme support to tests tab (#1793)
    * add themes to the tests tab
    
    * clea clors
    
    * clean more colors
    
    * fix arrows
    
    * fix other arrow
    
    * Use jsdom instead of window in jest test (#1812)
    
    * Hacky implementation of jsdom inside jest tests
    
    * Update path to jsdom
    
    * Properly set globals according to jest defaults
    
    * Properly set global to jsdom window
    
    * Make globals dynamic
    
    * Improve scrolling of tests view
    
    * Proper fix for the styling of tests
    
    * Convert jest-lite to typescript
    
    * Fix scrolling for the full test details
    
    * Deduplicate Tests container
    
    * Properly mark errors as jest errors, clear them on new start
    
    * Move clear error to less spammy place
    
    * Fix error loading later on in the editor
    
    * add bg

--- packages/app/src/app/components/Preview/DevTools/Tests/TestDetails/TestBlock/TestName/elements.ts
+++ packages/app/src/app/components/Preview/DevTools/Tests/TestDetails/TestBlock/TestName/elements.ts
@@ -1,13 +1,13 @@
 import styled from 'styled-components';
 
 export const Block = styled.div<{ last: boolean }>`
-  transition: 0.3s ease color;
   display: flex;
   padding: 0.5rem 0.4rem;
   padding-left: 0.5rem;
   position: relative;
   margin-right: ${props => (props.last ? 0 : 12)}px;
-  color: rgba(255, 255, 255, 0.5);
+  color: ${props =>
+    props.theme.light ? 'rgba(0, 0, 0, 0.5)' : 'rgba(255, 255, 255, 0.5)'};
 
   white-space: nowrap;
 
@@ -15,28 +15,25 @@ export const Block = styled.div<{ last: boolean }>`
     content: '';
     position: absolute;
     margin: auto;
-    top: 5px;
+    top: 13px;
     right: -10px;
-    width: 24px;
-    height: 24px;
+    width: 8px;
+    height: 8px;
     transform: rotate(45deg);
-    -webkit-transform: rotate(45deg);
-    -moz-transform: rotate(45deg);
-    -o-transform: rotate(45deg);
-    -ms-transform: rotate(45deg);
-    border-right: 1px solid rgba(0, 0, 0, ${props => (props.last ? 0.3 : 0.4)});
-    border-top: 1px solid rgba(0, 0, 0, ${props => (props.last ? 0.3 : 0.4)});
-    background-color: #181b1d;
+    border-right: 1px solid ${props =>
+    props.theme.light ? `rgba(0, 0, 0, ${(props.last ? 0.3 : 0.4)})` : `rgba(255, 255, 255, ${(props.last ? 0.3 : 0.4)})`};
+    border-top:  1px solid ${props =>
+    props.theme.light ? `rgba(0, 0, 0, ${(props.last ? 0.3 : 0.4)})` : `rgba(255, 255, 255, ${(props.last ? 0.3 : 0.4)})`};
+    background-color:  rgba(0, 0, 0, 0.01);
     z-index: 1;
   }
 `;
 
 export const TestName = styled.div`
-  transition: 0.3s ease background-color;
   padding: 0.4rem;
   padding-left: 20px;
-  background-color: ${props => props.theme.background2};
-  color: rgba(255, 255, 255, 0.8);
+  color: ${props =>
+    props.theme.light ? 'rgba(0, 0, 0, 0.8)' : 'rgba(255, 255, 255, 0.8)'};
   flex: auto;
   white-space: nowrap;
 `;
--- packages/app/src/app/components/Preview/DevTools/Tests/TestElement/elements.ts
+++ packages/app/src/app/components/Preview/DevTools/Tests/TestElement/elements.ts
@@ -6,26 +6,27 @@ export const Actions = styled.div`
   opacity: 0;
   font-size: 1.125rem;
 
-  color: white;
+  color: ${props =>
+    props.theme.light ? 'rgb(0, 0, 0)' : 'rgb(255, 255, 255)'};
 
   svg {
     margin-left: 0.5rem;
-    transition: 0.3s ease color;
-
-    color: rgba(255, 255, 255, 0.6);
+    transition: 0.3s ease opacity;
+    opacity: 0.6;
+    color: ${props => props.theme['button.hoverBackground']};
 
     &:hover {
-      color: rgba(255, 255, 255, 1);
+      opacity: 1;
     }
   }
 `;
 
 export const TestName = styled.div`
-  transition: 0.3s ease background-color;
   padding: 0.25rem;
   padding-left: 20px;
-  background-color: ${props => props.theme.background2};
-  color: rgba(255, 255, 255, 0.8);
+  background-color: ${props => props.theme['sideBar.background']};
+  color: ${props =>
+    props.theme.light ? 'rgba(0, 0, 0, 0.8)' : 'rgba(255, 255, 255, 0.8)'};
   flex: auto;
   white-space: nowrap;
 `;
@@ -36,43 +37,37 @@ export const Test = styled.div<{ status: Status }>`
   padding-left: 1rem;
   cursor: pointer;
 
-  background-color: #181b1d;
-
   ${props =>
     props.status === 'idle' &&
     css`
-      color: rgba(255, 255, 255, 0.4);
+      color: ${props =>
+        props.theme.light ? 'rgba(0, 0, 0, 0.4)' : 'rgba(255, 255, 255, 0.4)'};
     `};
 `;
 
 export const Block = styled.div<{ last: boolean }>`
-  transition: 0.3s ease color;
   display: flex;
   padding: 0.25rem 0.4rem;
   padding-left: 0.5rem;
   position: relative;
   margin-right: ${props => (props.last ? 0 : 12)}px;
-  color: rgba(255, 255, 255, 0.5);
+  color: ${props =>
+    props.theme.light ? 'rgba(0, 0, 0, 0.5)' : 'rgba(255, 255, 255, 0.5)'};
 
   white-space: nowrap;
 
   &::after {
     content: '';
     position: absolute;
-    margin: auto;
-    top: 3px;
+    top: 8px;
     right: -10px;
-    width: 17px;
-    height: 17px;
+    width: 10px;
+    height: 10px;
     transform: rotate(45deg);
-    -webkit-transform: rotate(45deg);
-    -moz-transform: rotate(45deg);
-    -o-transform: rotate(45deg);
-    -ms-transform: rotate(45deg);
-    border-right: 2px solid rgba(0, 0, 0, 0.2);
-    border-top: 2px solid rgba(0, 0, 0, 0.2);
-    background-color: #181b1d;
-    z-index: 1;
+    border-right: 1px solid ${props =>
+    props.theme.light ? `rgba(0, 0, 0, ${(props.last ? 0.3 : 0.4)})` : `rgba(255, 255, 255, ${(props.last ? 0.3 : 0.4)})`};
+    border-top:  1px solid ${props =>
+    props.theme.light ? `rgba(0, 0, 0, ${(props.last ? 0.3 : 0.4)})` : `rgba(255, 255, 255, ${(props.last ? 0.3 : 0.4)})`};
   }
 `;
 
@@ -86,18 +81,21 @@ export const FileData = styled.div`
 `;
 
 export const Path = styled.span`
-  color: rgba(255, 255, 255, 0.6);
+  color: ${props =>
+    props.theme.light ? 'rgba(0, 0, 0, 0.6)' : 'rgba(255, 255, 255, 0.6)'};
 `;
 
 export const FileName = styled.span`
-  color: rgba(255, 255, 255, 0.8);
+  color: ${props =>
+    props.theme.light ? 'rgba(0, 0, 0, 0.8)' : 'rgba(255, 255, 255, 0.8)'};
 
   flex: 1;
 `;
 
 export const Tests = styled.div`
   font-weight: 400;
-  color: rgba(255, 255, 255, 0.7);
+  color: ${props =>
+    props.theme.light ? 'rgba(0, 0, 0, 0.7)' : 'rgba(255, 255, 255, 0.7)'};
   overflow-x: auto;
 
   &::-webkit-scrollbar {
@@ -113,7 +111,10 @@ export const Container = styled.div<{ selected: boolean }>`
   border-left: 2px solid transparent;
 
   &:hover {
-    background-color: rgba(0, 0, 0, 0.2);
+    background-color: ${props =>
+    !props.theme.light ? 'rgba(0, 0, 0, 0.2)' : 'rgba(255, 255, 255, 0.2)'};
+    color: ${props =>
+    props.theme.light ? 'rgba(0, 0, 0, 1)' : 'rgba(255, 255, 255, 1)'};
     border-left-color: ${props => props.theme.secondary.clearer(0.5)};
 
     ${Actions} {
@@ -122,11 +123,11 @@ export const Container = styled.div<{ selected: boolean }>`
 
     ${Test} {
       ${TestName} {
-        background-color: rgba(0, 0, 0, 0.05);
       }
 
       ${Block} {
-        color: rgba(255, 255, 255, 0.8);
+        color: ${props =>
+    props.theme.light ? 'rgba(0, 0, 0, 1)' : 'rgba(255, 255, 255, 1)'};
       }
     }
   }
@@ -135,21 +136,33 @@ export const Container = styled.div<{ selected: boolean }>`
     props.selected &&
     css`
       border-left-color: ${props.theme.secondary};
-      background-color: rgba(0, 0, 0, 0.3);
+      background-color: ${props =>
+        !props.theme.light ? 'rgba(0, 0, 0, 0.2)' : 'rgba(255, 255, 255, 0.2)'};
+      color: ${props =>
+        props.theme.light ? 'rgba(0, 0, 0, 1)' : 'rgba(255, 255, 255, 1)'};
 
       ${Test} {
         ${TestName} {
-          background-color: rgba(0, 0, 0, 0.05);
+          background-color: ${props =>
+        !props.theme.light
+          ? 'rgba(0, 0, 0, 0.2)'
+          : 'rgba(255, 255, 255, 0.2)'};
+          color: ${props =>
+        props.theme.light ? 'rgba(0, 0, 0, 1)' : 'rgba(255, 255, 255, 1)'};
         }
 
         ${Block} {
-          color: rgba(255, 255, 255, 0.8);
+          background-color: ${props =>
+        !props.theme.light
+          ? 'rgba(0, 0, 0, 0.2)'
+          : 'rgba(255, 255, 255, 0.2)'};
+          color: ${props =>
+        props.theme.light ? 'rgba(0, 0, 0, 1)' : 'rgba(255, 255, 255, 1)'};
         }
       }
 
       &:hover {
         border-left-color: ${props.theme.secondary};
-        background-color: rgba(0, 0, 0, 0.3);
       }
     `};
 `;
