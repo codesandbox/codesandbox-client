1a5e7e984ec778b8a93864133284efa8acc45f5c
Ives van Hoorne <ives.v.h@gmail.com>
2019-05-01 16:26:10 +0200

    Fix scrolling of embed

--- packages/app/src/embed/components/Sidebar/elements.js
+++ packages/app/src/embed/components/Sidebar/elements.js
@@ -1,10 +1,14 @@
 // @flow
 import styled from 'styled-components';
+import { isIOS } from '@codesandbox/common/lib/utils/platform';
 
 export const Container = styled.div`
   flex: 250px;
   width: 250px;
-  height: 100%;
+  /* There is a bug with ios that causes the sidebar to be longer than the preview, when you then
+   * scroll the preview it scrolls the editor down (page is longer). If I set this to 100% scrolling
+   * is broken in Chrome though. That's why we have this check */
+  height: ${isIOS ? '100%' : '100vh'};
   color: rgba(255, 255, 255, 0.8);
   z-index: 10;
   background-color: ${props => props.theme.sidebar};
