c58a680892c1dd81b4dc6d493306d19cbbc86b77
Ives van Hoorne <ives.v.h@gmail.com>
2019-05-02 12:23:26 +0200

    Redo snapshot generating logic

--- generate-test-screenshots.sh
+++ generate-test-screenshots.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-docker run --rm --name test-container -v $(pwd):/home/circleci/codesandbox-client -w /home/circleci/codesandbox-client -d -t codesandbox/node-puppeteer yarn start:test && \
-sleep 50 && docker exec -it test-container yarn test:integrations && \
-docker stop test-container
+docker run --rm --name test-container -v $(pwd):/home/circleci/codesandbox-client -w /home/circleci/codesandbox-client -t codesandbox/node-puppeteer node packages/app/integration-tests/tests/generate-sandboxes.js
