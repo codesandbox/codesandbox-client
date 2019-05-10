3ad92b3015b3fb6287c3795efb12e0f5c2e7f459
gabeeden <14077091+gabeeden@users.noreply.github.com>
2019-05-10 23:08:42 +1200

    Export sandboxes from dashboard (#1878)
    * exportSandboxes, getSandbox funcs & menu options
    
    * move export above trash and add separator

--- packages/app/src/app/pages/Dashboard/Content/SandboxCard/index.tsx
+++ packages/app/src/app/pages/Dashboard/Content/SandboxCard/index.tsx
@@ -45,6 +45,7 @@ type Props = {
   ) => void;
   selectedCount: number;
   deleteSandboxes: () => void;
+  exportSandboxes: () => void;
   permanentlyDeleteSandboxes: () => void;
   collectionPath: string; // eslint-disable-line react/no-unused-prop-types
   collectionTeamId: string | undefined;
@@ -210,13 +211,22 @@ class SandboxItem extends React.PureComponent<Props, State> {
         ...items,
         [
           {
+            title: `Export ${selectedCount} Sandboxes`,
+            action: () => {
+              this.props.exportSandboxes()
+              return true;
+            },
+          },
+        ],
+        [
+          {
             title: `Move ${selectedCount} Sandboxes To Trash`,
             action: () => {
               this.props.deleteSandboxes();
               return true;
             },
             color: theme.red.darken(0.2)(),
-          },
+          }
         ],
       ];
     }
@@ -229,7 +239,7 @@ class SandboxItem extends React.PureComponent<Props, State> {
             if (this.props.collectionTeamId) {
               history.push(
                 `/dashboard/teams/${this.props.collectionTeamId}/sandboxes${
-                  this.props.collectionPath
+                this.props.collectionPath
                 }`
               );
             } else {
@@ -250,31 +260,38 @@ class SandboxItem extends React.PureComponent<Props, State> {
             return true;
           },
         },
+        {
+          title: 'Export Sandbox',
+          action: () => {
+            this.props.exportSandboxes();
+            return true;
+          },
+        },
       ],
       this.props.isPatron &&
-        [
-          this.props.privacy !== 0 && {
-            title: `Make Sandbox Public`,
-            action: () => {
-              this.props.setSandboxesPrivacy(0);
-              return true;
-            },
+      [
+        this.props.privacy !== 0 && {
+          title: `Make Sandbox Public`,
+          action: () => {
+            this.props.setSandboxesPrivacy(0);
+            return true;
           },
-          this.props.privacy !== 1 && {
-            title: `Make Sandbox Unlisted`,
-            action: () => {
-              this.props.setSandboxesPrivacy(1);
-              return true;
-            },
+        },
+        this.props.privacy !== 1 && {
+          title: `Make Sandbox Unlisted`,
+          action: () => {
+            this.props.setSandboxesPrivacy(1);
+            return true;
           },
-          this.props.privacy !== 2 && {
-            title: `Make Sandbox Private`,
-            action: () => {
-              this.props.setSandboxesPrivacy(2);
-              return true;
-            },
+        },
+        this.props.privacy !== 2 && {
+          title: `Make Sandbox Private`,
+          action: () => {
+            this.props.setSandboxesPrivacy(2);
+            return true;
           },
-        ].filter(Boolean),
+        },
+      ].filter(Boolean),
       [
         {
           title: `Rename Sandbox`,
@@ -533,10 +550,10 @@ class SandboxItem extends React.PureComponent<Props, State> {
                           }}
                         </Mutation>
                       ) : (
-                        <SandboxTitle>
-                          {title} {this.getPrivacyIcon()}
-                        </SandboxTitle>
-                      )}
+                          <SandboxTitle>
+                            {title} {this.getPrivacyIcon()}
+                          </SandboxTitle>
+                        )}
                     </div>
                     <SandboxDetails>{details}</SandboxDetails>
                   </div>
