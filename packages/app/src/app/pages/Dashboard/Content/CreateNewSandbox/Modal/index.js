73a3f66594b2106fe0fa16679cf34e89b807458b
Sara Vieira <hey@iamsaravieira.com>
2019-05-16 10:09:57 +0200

    add hooooookd fam

--- packages/app/src/app/pages/Dashboard/Content/CreateNewSandbox/Modal/index.js
+++ packages/app/src/app/pages/Dashboard/Content/CreateNewSandbox/Modal/index.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 
 import track from '@codesandbox/common/lib/utils/analytics';
 import Template from '@codesandbox/common/lib/components/Template';
@@ -20,83 +20,72 @@ import {
 } from './elements';
 import availableTemplates from './availableTemplates';
 
-export default class Modal extends React.PureComponent {
-  state = {
-    selectedTab: 0,
-  };
+export default ({ forking = false, closing = false, createSandbox }) => {
+  const [selectedTab, setSelectedTab] = useState(0);
 
-  selectTemplate = template => {
+  const selectTemplate = template => {
     track('New Sandbox Modal - Select Template', { template });
-    this.props.createSandbox(template);
+    createSandbox(template);
   };
 
-  render() {
-    const { forking = false, closing = false } = this.props;
+  return (
+    <Container
+      closing={closing}
+      forking={forking}
+      onMouseDown={e => e.preventDefault()}
+    >
+      <TabContainer forking={forking} closing={closing}>
+        {availableTemplates.map(({ name }, i) => (
+          <Button
+            key={name}
+            selected={selectedTab === i}
+            onClick={() => setSelectedTab(i)}
+          >
+            {name}
+          </Button>
+        ))}
+      </TabContainer>
 
-    const { selectedTab } = this.state;
-
-    return (
-      <Container
-        closing={closing}
-        forking={forking}
-        onMouseDown={e => e.preventDefault()}
-      >
-        <TabContainer forking={forking} closing={closing}>
-          {availableTemplates.map(({ name }, i) => (
-            <Button
-              key={name}
-              selected={selectedTab === i}
-              onClick={() => this.setState({ selectedTab: i })}
-            >
-              {name}
-            </Button>
-          ))}
-        </TabContainer>
-
-        <InnerContainer forking={forking} closing={closing}>
-          {availableTemplates.map((tab, i) => (
-            <Tab key={tab.name} visible={selectedTab === i}>
-              <Templates>
-                {tab.templates &&
-                  tab.templates.map(template => (
-                    <Template
-                      key={template.name}
-                      template={template}
-                      selectTemplate={this.selectTemplate}
-                    />
-                  ))}
-                {tab.types &&
-                  tab.types.map(type => (
-                    <Fragment key={type.name}>
-                      <Title>{type.name}</Title>
-                      {type.templates.map(template => (
-                        <Template
-                          key={template.name}
-                          template={template}
-                          selectTemplate={this.selectTemplate}
-                        />
-                      ))}
-                    </Fragment>
-                  ))}
-              </Templates>
-            </Tab>
-          ))}
-          <ImportChoices>
-            <ImportChoice href="/s/github" target="_blank">
-              <GithubLogo /> Import from GitHub
-            </ImportChoice>
-            <ImportChoice
-              href="/docs/importing#export-with-cli"
-              target="_blank"
-            >
-              <TerminalIcon /> Export with CLI
-            </ImportChoice>
-            <ImportChoice href="/docs/importing#define-api" target="_blank">
-              <UploadIcon /> Create with API
-            </ImportChoice>
-          </ImportChoices>
-        </InnerContainer>
-      </Container>
-    );
-  }
-}
+      <InnerContainer forking={forking} closing={closing}>
+        {availableTemplates.map((tab, i) => (
+          <Tab key={tab.name} visible={selectedTab === i}>
+            <Templates>
+              {tab.templates &&
+                tab.templates.map(template => (
+                  <Template
+                    key={template.name}
+                    template={template}
+                    selectTemplate={selectTemplate}
+                  />
+                ))}
+              {tab.types &&
+                tab.types.map(type => (
+                  <Fragment key={type.name}>
+                    <Title>{type.name}</Title>
+                    {type.templates.map(template => (
+                      <Template
+                        key={template.name}
+                        template={template}
+                        selectTemplate={selectTemplate}
+                      />
+                    ))}
+                  </Fragment>
+                ))}
+            </Templates>
+          </Tab>
+        ))}
+        <ImportChoices>
+          <ImportChoice href="/s/github" target="_blank">
+            <GithubLogo /> Import from GitHub
+          </ImportChoice>
+          <ImportChoice href="/docs/importing#export-with-cli" target="_blank">
+            <TerminalIcon /> Export with CLI
+          </ImportChoice>
+          <ImportChoice href="/docs/importing#define-api" target="_blank">
+            <UploadIcon /> Create with API
+          </ImportChoice>
+        </ImportChoices>
+      </InnerContainer>
+    </Container>
+  );
+};
