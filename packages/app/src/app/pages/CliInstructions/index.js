12389c490fce7c0e4bb36f763d779cdc905c7f52
Michaël De Boey <info@michaeldeboey.be>
2019-05-22 00:17:26 +0200

    Refactor CliInstructions to functional component with hooks

--- packages/app/src/app/pages/CliInstructions/index.js
+++ packages/app/src/app/pages/CliInstructions/index.js
@@ -1,44 +1,44 @@
-import React from 'react';
-
-import { inject } from 'mobx-react';
-
-import Navigation from 'app/pages/common/Navigation';
-import Title from 'app/components/Title';
-import SubTitle from 'app/components/SubTitle';
 import MaxWidth from '@codesandbox/common/lib/components/flex/MaxWidth';
 import Margin from '@codesandbox/common/lib/components/spacing/Margin';
+import { inject } from 'mobx-react';
+import React, { useEffect } from 'react';
+
+import Navigation from 'app/pages/common/Navigation';
+import SubTitle from 'app/components/SubTitle';
+import Title from 'app/components/Title';
 
 import { Container, Content, Code } from './elements';
 
-class CliInstructions extends React.PureComponent {
-  componentDidMount() {
-    this.props.signals.cliInstructionsMounted();
-  }
+const CLIInstructions = ({ signals: { cliInstructionsMounted } }) => {
+  useEffect(() => {
+    cliInstructionsMounted();
+  }, [cliInstructionsMounted]);
 
-  render() {
-    return (
-      <MaxWidth>
-        <Margin vertical={1.5} horizontal={1.5}>
-          <Container>
-            <Navigation title="CLI Import" />
-            <Content vertical>
-              <Title>Import from CLI</Title>
-              <SubTitle>
-                1. Install the CLI <Code>npm i -g codesandbox</Code>
-              </SubTitle>
-              <SubTitle>
-                2. Go to your project <Code>cd path-of-your-project</Code>
-              </SubTitle>
-              <SubTitle>
-                3. Deploy your project to CodeSandbox{' '}
-                <Code>codesandbox ./</Code>
-              </SubTitle>
-            </Content>
-          </Container>
-        </Margin>
-      </MaxWidth>
-    );
-  }
-}
+  return (
+    <MaxWidth>
+      <Margin vertical={1.5} horizontal={1.5}>
+        <Container>
+          <Navigation title="CLI Import" />
 
-export default inject(['signals'])(CliInstructions);
+          <Content vertical>
+            <Title>Import from CLI</Title>
+
+            <SubTitle>
+              1. Install the CLI <Code>npm i -g codesandbox</Code>
+            </SubTitle>
+
+            <SubTitle>
+              2. Go to your project <Code>cd path-of-your-project</Code>
+            </SubTitle>
+
+            <SubTitle>
+              3. Deploy your project to CodeSandbox <Code>codesandbox ./</Code>
+            </SubTitle>
+          </Content>
+        </Container>
+      </Margin>
+    </MaxWidth>
+  );
+};
+
+export default inject(['signals'])(CLIInstructions);
