d2865f9be980be1b6205de1a36613d7d161a928d
Sara Vieira <hey@iamsaravieira.com>
2019-05-28 17:59:40 +0200

    allow webcam and audio when possible

--- packages/app/src/app/pages/common/Modals/ShareModal/getCode.js
+++ packages/app/src/app/pages/common/Modals/ShareModal/getCode.js
@@ -71,7 +71,7 @@ export const getIframeScript = (sandbox, mainModule, state) =>
     state
   )}" title="${getSandboxName(
     sandbox
-  )}" style="width:100%; height:500px; border:0; border-radius: 4px; overflow:hidden;" sandbox="allow-modals allow-forms allow-popups allow-scripts allow-same-origin"></iframe>`;
+  )}" allow="geolocation; microphone; camera; midi; vr; accelerometer; gyroscope; payment; ambient-light-sensor; encrypted-media" style="width:100%; height:500px; border:0; border-radius: 4px; overflow:hidden;" sandbox="allow-modals allow-forms allow-popups allow-scripts allow-same-origin"></iframe>`;
 
 // eslint-disable-next-line
 export const getButtonMarkdown = (sandbox, mainModule, state) => {
