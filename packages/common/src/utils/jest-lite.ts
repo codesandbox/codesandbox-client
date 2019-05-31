ca16af0c47e9424ae69f22e0bd45e64f1761c121
Bogdan Luca <luca.bogdan@gmail.com>
2019-05-31 13:55:42 +0300

    Use our own babel-eslint fork, with the reference to patch-eslint-scope removed, and: - remove unneeded code from webpack config - fix PR build URL to use https - fix the a11y linting bug - extract jest messages to common

---
+++ packages/common/src/utils/jest-lite.ts
@@ -0,0 +1,13 @@
+export enum messages {
+  INITIALIZE = 'initialize_tests',
+  ADD_FILE = 'add_file',
+  REMOVE_FILE = 'remove_file',
+  FILE_ERROR = 'file_error',
+  TOTAL_TEST_START = 'total_test_start',
+  TOTAL_TEST_END = 'total_test_end',
+  TEST_START = 'test_start',
+  TEST_END = 'test_end',
+  DESCRIBE_START = 'describe_start',
+  DESCRIBE_END = 'describe_end',
+  ADD_TEST = 'add_test',
+}
