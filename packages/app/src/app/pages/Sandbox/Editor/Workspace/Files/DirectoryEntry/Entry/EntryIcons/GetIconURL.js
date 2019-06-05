3b3c684eda8c0323ec22d9139b19e8f40b741ad9
Sara Vieira <hey@iamsaravieira.com>
2019-06-05 12:29:48 +0200

    adds more icons (#2023)

--- packages/app/src/app/pages/Sandbox/Editor/Workspace/Files/DirectoryEntry/Entry/EntryIcons/GetIconURL.js
+++ packages/app/src/app/pages/Sandbox/Editor/Workspace/Files/DirectoryEntry/Entry/EntryIcons/GetIconURL.js
@@ -1,11 +1,9 @@
 import folderSvg from '@codesandbox/common/lib/components/icons/folder.svg';
 import folderOpenSvg from '@codesandbox/common/lib/components/icons/folder-open.svg';
-import faviconSvg from '@codesandbox/common/lib/components/icons/favicon.svg';
 import fileSvg from '@codesandbox/common/lib/components/icons/file.svg';
 import imageSvg from '@codesandbox/common/lib/components/icons/image.svg';
 import codesandboxSvg from '@codesandbox/common/lib/components/icons/codesandbox.svg';
 import nowSvg from '@codesandbox/common/lib/components/icons/now.svg';
-import svelteSvg from '@codesandbox/common/lib/components/icons/svelte.svg';
 
 function imageExists(url) {
   return new Promise((resolve, reject) => {
@@ -18,7 +16,7 @@ function imageExists(url) {
 
 async function getIconURL(type) {
   const base =
-    'https://cdn.jsdelivr.net/gh/PKief/vscode-material-icon-theme@e04ab459209a1d0c9fdb7b3682e5b4aa6d586090/icons';
+    'https://cdn.jsdelivr.net/gh/PKief/vscode-material-icon-theme@master/icons';
 
   let url;
 
@@ -27,20 +25,12 @@ async function getIconURL(type) {
       url = codesandboxSvg;
       break;
 
-    case 'favicon':
-      url = faviconSvg;
-      break;
-
     case 'image':
       url = imageSvg;
       break;
 
     case 'now':
       url = nowSvg;
-      break;
-
-    case 'svelte':
-      url = svelteSvg;
       break;
 
     case 'directory':
