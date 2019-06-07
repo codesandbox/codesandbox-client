8f6cbcbef40e7f919167640a2ed169e40dbee194
Sara <hey@iamsaravieira.com>
2019-06-06 21:40:36 -0600

    only add syntax highlight

--- standalone-packages/vscode-extensions/out/extensions/sbrink.elm-0.25.0/out/src/elmConfiguration.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.configuration = {
-    wordPattern: /\w+[\.\w+]*/,
-};
-//# sourceMappingURL=elmConfiguration.js.map
