e754e1b1abef374e87c02717138b134de690a4bd
Drake Costa <drake@saeris.io>
2019-06-11 22:01:28 -0700

    🔨 Refactor Editor Content Tabs
    Applied coding conventions to Tab and ModuleTab and updated import references across the application.
    
    Tested locally, behavior appears to be the same as before.

--- packages/app/src/app/pages/Sandbox/Editor/Content/Tabs/Tab/elements.ts
+++ packages/app/src/app/pages/Sandbox/Editor/Content/Tabs/Tab/elements.ts
@@ -1,57 +1,59 @@
 import styled, { css } from 'styled-components';
 import CloseIcon from 'react-icons/lib/go/x';
 
-export const Container = styled.div<{
-  isOver?: boolean;
-  active?: boolean;
-  dirty?: boolean;
-}>`
-  position: relative;
-  display: inline-flex;
-  justify-content: center;
-  align-items: center;
-  line-height: 1;
-  height: calc(100% - 1px);
-  font-size: 0.875rem;
-  cursor: pointer;
+export const Container = styled.div`
+  ${({
+    active,
+    dirty,
+    isOver,
+    theme,
+  }: {
+    active: boolean;
+    dirty: boolean;
+    isOver: boolean;
+    theme: any;
+  }) => css`
+    position: relative;
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    line-height: 1;
+    height: calc(100% - 1px);
+    font-size: 0.875rem;
+    cursor: pointer;
 
-  border-bottom: 1px solid transparent;
+    border-bottom: 1px solid transparent;
 
-  padding: 0 1rem;
-  padding-left: 0.75rem;
-  padding-right: 0.125rem;
-  color: ${props =>
-    props.theme['tab.inactiveForeground'] ||
-    (props.theme.light ? 'rgba(0, 0, 0, 0.5)' : 'rgba(255, 255, 255, 0.5)')};
+    padding: 0 1rem;
+    padding-left: 0.75rem;
+    padding-right: 0.125rem;
+    color: ${theme[`tab.inactiveForeground`] ||
+      (theme.light ? css`rgba(0, 0, 0, 0.5)` : css`rgba(255, 255, 255, 0.5)`)};
 
-  svg {
-    font-size: 1rem;
-    margin-right: 0.5rem;
-  }
+    svg {
+      font-size: 1rem;
+      margin-right: 0.5rem;
+    }
 
-  background-color: ${props =>
-    props.theme['tab.inactiveBackground'] || 'transparent'};
+    background-color: ${theme[`tab.inactiveBackground`] || css`transparent`};
 
-  ${props =>
-    props.isOver &&
-    css`
-      background-color: ${props.theme.background2.lighten(0.2)};
-    `};
-  ${props =>
-    props.active &&
-    css`
-      background-color: ${props.theme['tab.activeBackground'] ||
-        props.theme.background2};
-      border-color: ${props.theme.secondary};
-      color: ${props.theme['tab.activeForeground'] ||
-        props.theme['editor.foreground'] ||
-        'white'};
-    `};
-  ${props =>
-    props.dirty &&
-    css`
-      font-style: italic;
-    `};
+    ${isOver &&
+      css`
+        background-color: ${theme.background2.lighten(0.2)};
+      `};
+    ${active &&
+      css`
+        background-color: ${theme[`tab.activeBackground`] || theme.background2};
+        border-color: ${theme.secondary};
+        color: ${theme[`tab.activeForeground`] ||
+          theme[`editor.foreground`] ||
+          css`white`};
+      `};
+    ${dirty &&
+      css`
+        font-style: italic;
+      `};
+  `}
 `;
 
 export const TabTitle = styled.div`
@@ -66,19 +68,21 @@ export const TabDir = styled.div`
   white-space: nowrap;
 `;
 
-export const StyledCloseIcon = styled(CloseIcon)<{ show?: boolean | string }>`
-  transition: 0.1s ease opacity;
+export const StyledCloseIcon = styled(CloseIcon)`
+  ${({ show, theme }: { show: boolean; theme: any }) => css`
+    transition: 0.1s ease opacity;
 
-  float: right;
-  opacity: 1;
-  color: ${props =>
-    props.theme.light ? 'rgba(0, 0, 0, 0.9)' : 'rgba(255, 255, 255, 0.9)'};
-  margin-right: 0;
+    float: right;
+    opacity: 1;
+    color: ${theme.light
+      ? css`rgba(0, 0, 0, 0.9)`
+      : css`rgba(255, 255, 255, 0.9)`};
+    margin-right: 0;
 
-  ${props =>
-    !props.show &&
-    css`
-      pointer-events: none;
-      opacity: 0;
-    `};
+    ${!show &&
+      css`
+        pointer-events: none;
+        opacity: 0;
+      `};
+  `}
 `;
