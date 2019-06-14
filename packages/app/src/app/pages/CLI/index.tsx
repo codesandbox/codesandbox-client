d08f7b651ca949a1522b1d15cc1ffee3e2f450ac
Sara <hey@iamsaravieira.com>
2019-06-14 16:38:37 +0200

    fix rebase

--- packages/app/src/app/pages/CLI/index.tsx
+++ packages/app/src/app/pages/CLI/index.tsx
@@ -8,15 +8,9 @@ import { Container } from './elements';
 import Prompt from './Prompt';
 import { User } from '@codesandbox/common/lib/types';
 
-interface Props {
-  authToken: string;
-  error: string;
-  isLoadingCLI: boolean;
-  user: User;
-}
-
-const CLI = ({ authToken, error, isLoadingCLI, user }: Props) => {
+const CLI = () => {
   const { cliMounted, signInCliClicked } = useSignals();
+  const { user, authToken, isLoadingCLI, error } = useStore();
 
   useEffect(() => {
     cliMounted();
