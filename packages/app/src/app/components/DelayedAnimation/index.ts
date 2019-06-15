c59aacdc2e433370471f2dc8b72ccea9da88faef
Drake Costa <drake@saeris.io>
2019-06-14 20:37:41 -0700

    📝 Add Types to app/components/DelayedAnimation

--- packages/app/src/app/components/DelayedAnimation/index.ts
+++ packages/app/src/app/components/DelayedAnimation/index.ts
@@ -2,5 +2,5 @@ import styled from 'styled-components';
 import delayEffect from '@codesandbox/common/lib/utils/animation/delay-effect';
 
 export default styled.div`
-  ${props => delayEffect(props.delay || 0)};
+  ${({ delay }: { delay: number }) => delayEffect(delay || 0)};
 `;
