7bc1cbc17152a863684801ccfaaa729441708583
Drake Costa <drake@saeris.io>
2019-06-15 21:44:08 -0700

    🔨 Refactor app/components/IntegrationModal: Apply Conventions
    TODO: Maybe move this to pages/common/modals? Also maybe move it's dependent sub-components if they aren't being used elsewhere?

--- packages/app/src/app/components/IntegrationModal/elements.ts
+++ packages/app/src/app/components/IntegrationModal/elements.ts
@@ -1,55 +1,54 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  background-color: ${props => props.theme.background};
   margin: 0;
+  background-color: ${props => props.theme.background};
   color: rgba(255, 255, 255, 0.8);
 `;
 
 export const Title = styled.h1`
-  font-weight: 500;
-  font-size: 1.25rem;
-  color: white;
   margin: 0;
-  text-transform: uppercase;
   margin-bottom: 0.5rem;
+  color: white;
+  font-size: 1.25rem;
+  font-weight: 500;
+  text-transform: uppercase;
 `;
 
 export const PoweredBy = styled.h2`
-  font-weight: 400;
-  font-size: 1rem;
-  color: white;
   margin-top: 0 !important;
   margin-bottom: 0;
+  color: white;
+  font-size: 1rem;
+  font-weight: 400;
 `;
 
 export const Header = styled.div`
-  text-align: center;
   padding: 1rem;
   background-color: rgba(0, 0, 0, 0.3);
+  text-align: center;
 `;
 
 export const Division = styled.hr`
-  border: none;
   height: 1px;
-  outline: none;
   margin: 0;
-
+  border: none;
   background-color: rgba(255, 255, 255, 0.1);
+  outline: none;
 `;
 
 export const DisabledOverlay = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
   position: absolute;
+  z-index: 20;
   top: 0;
   bottom: 0;
   right: 0;
   left: 0;
-  background-color: rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
   margin: 0 auto;
+  background-color: rgba(0, 0, 0, 0.5);
   color: white;
   font-size: 1.25rem;
-  z-index: 20;
 `;
