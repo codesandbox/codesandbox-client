c3f868d29d8699051a05c71cab76e841aca8c8bf
Egor Anisimov <Egor_Anisimov@epam.com>
2019-06-18 22:45:12 -0400

    Add debounce for tests in watch mode

--- packages/app/src/sandbox/compile.ts
+++ packages/app/src/sandbox/compile.ts
@@ -724,7 +724,7 @@ async function compile({
   firstLoad = false;
 
   dispatch({ type: 'status', status: 'idle' });
-  dispatch({ type: 'done' });
+  dispatch({ type: 'done', compilatonError: hadError });
 
   if (typeof (window as any).__puppeteer__ === 'function') {
     (window as any).__puppeteer__('done');
