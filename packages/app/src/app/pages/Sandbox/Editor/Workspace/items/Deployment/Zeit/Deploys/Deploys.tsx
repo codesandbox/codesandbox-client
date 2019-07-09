069a553b1904fc8c2e092e2e5f98cec6259ec29e
Michaël De Boey <info@michaeldeboey.be>
2019-07-09 02:10:52 +0200

    Extract Actions

--- packages/app/src/app/pages/Sandbox/Editor/Workspace/items/Deployment/Zeit/Deploys/Deploys.tsx
+++ packages/app/src/app/pages/Sandbox/Editor/Workspace/items/Deployment/Zeit/Deploys/Deploys.tsx
@@ -1,9 +1,7 @@
 import distanceInWordsToNow from 'date-fns/distance_in_words_to_now';
 import React from 'react';
-import LinkIcon from 'react-icons/lib/fa/external-link';
-import TrashIcon from 'react-icons/lib/fa/trash';
 
-import { useSignals, useStore } from 'app/store';
+import { useStore } from 'app/store';
 
 import {
   WorkspaceInputContainer,
@@ -11,23 +9,18 @@ import {
 } from '../../../../elements';
 
 import {
-  Action,
-  ButtonContainer,
   Deploy,
   Deploys as DeploysWrapper,
   DeploysContainer,
-  Link,
   Name,
   State,
 } from '../../elements';
 
+import { Actions } from './Actions';
+
 export const Deploys = () => {
   const {
-    deployment: { aliasDeployment, setDeploymentToDelete },
-    modalOpened,
-  } = useSignals();
-  const {
-    deployment: { hasAlias, sandboxDeploys, ...deployment },
+    deployment: { sandboxDeploys },
   } = useStore();
 
   return (
@@ -61,36 +54,7 @@ export const Deploys = () => {
                 </span>
               ) : null}
 
-              <ButtonContainer>
-                <Link href={`https://${deploy.url}`}>
-                  <LinkIcon /> <span>Visit</span>
-                </Link>
-
-                <Action
-                  disabled={deployment[`${deploy.uid}Deleting`]}
-                  onClick={() => {
-                    setDeploymentToDelete({ id: deploy.uid });
-                    modalOpened({ modal: 'deleteDeployment' });
-                  }}
-                >
-                  {deployment[`${deploy.uid}Deleting`] ? (
-                    'Deleting'
-                  ) : (
-                    <>
-                      <TrashIcon /> <span>Delete</span>
-                    </>
-                  )}
-                </Action>
-
-                {hasAlias && deploy.state === 'READY' ? (
-                  <Action
-                    disabled={deploy.alias.length}
-                    onClick={() => aliasDeployment({ id: deploy.uid })}
-                  >
-                    {deploy.alias.length ? 'Aliased' : 'Alias'}
-                  </Action>
-                ) : null}
-              </ButtonContainer>
+              <Actions deploy={deploy} />
             </Deploy>
           ))}
         </DeploysWrapper>
