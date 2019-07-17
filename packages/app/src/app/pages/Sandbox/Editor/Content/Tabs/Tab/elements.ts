0273d87bccb64b70fa9076349968192e5118a145
Drake Costa <drake@saeris.io>
2019-07-17 06:28:54 -0700

    👌 Implement Requested Changes, Added Types, Rebase onto Master

--- packages/app/src/app/pages/Sandbox/Editor/Content/Tabs/Tab/elements.ts
+++ packages/app/src/app/pages/Sandbox/Editor/Content/Tabs/Tab/elements.ts
@@ -1,46 +1,38 @@
 import styled, { css } from 'styled-components';
 import CloseIcon from 'react-icons/lib/go/x';
 
-export const Container = styled.div`
-  ${({
-    active,
-    dirty,
-    isOver,
-    theme,
-  }: {
-    active: boolean;
-    dirty: boolean;
-    isOver: boolean;
-    theme: any;
-  }) => css`
+export const Container = styled.div<{
+  active: boolean;
+  dirty: boolean;
+  isOver: boolean;
+}>`
+  ${({ active, dirty, isOver, theme }) => css`
     position: relative;
     display: inline-flex;
     justify-content: center;
     align-items: center;
-    line-height: 1;
     height: calc(100% - 1px);
-    font-size: 0.875rem;
-    cursor: pointer;
-
-    border-bottom: 1px solid transparent;
-
     padding: 0 1rem;
     padding-left: 0.75rem;
     padding-right: 0.125rem;
+    border-bottom: 1px solid transparent;
+    background-color: ${theme[`tab.inactiveBackground`] || css`transparent`};
     color: ${theme[`tab.inactiveForeground`] ||
       (theme.light ? css`rgba(0, 0, 0, 0.5)` : css`rgba(255, 255, 255, 0.5)`)};
+    font-size: 0.875rem;
+    line-height: 1;
+    cursor: pointer;
 
     svg {
       font-size: 1rem;
       margin-right: 0.5rem;
     }
 
-    background-color: ${theme[`tab.inactiveBackground`] || css`transparent`};
-
     ${isOver &&
       css`
         background-color: ${theme.background2.lighten(0.2)};
       `};
+
     ${active &&
       css`
         background-color: ${theme[`tab.activeBackground`] || theme.background2};
@@ -49,6 +41,7 @@ export const Container = styled.div`
           theme[`editor.foreground`] ||
           css`white`};
       `};
+
     ${dirty &&
       css`
         font-style: italic;
@@ -57,27 +50,26 @@ export const Container = styled.div`
 `;
 
 export const TabTitle = styled.div`
-  padding-right: 0.5rem;
   padding-left: 6px;
+  padding-right: 0.5rem;
   white-space: nowrap;
 `;
 
 export const TabDir = styled.div`
-  color: rgba(255, 255, 255, 0.3);
   padding-right: 0.5rem;
+  color: rgba(255, 255, 255, 0.3);
   white-space: nowrap;
 `;
 
-export const StyledCloseIcon = styled(CloseIcon)`
-  ${({ show, theme }: { show: boolean; theme: any }) => css`
-    transition: 0.1s ease opacity;
-
+export const StyledCloseIcon = styled(CloseIcon)<{ show?: boolean }>`
+  ${({ show = false, theme }) => css`
     float: right;
-    opacity: 1;
+    margin-right: 0;
     color: ${theme.light
       ? css`rgba(0, 0, 0, 0.9)`
       : css`rgba(255, 255, 255, 0.9)`};
-    margin-right: 0;
+    opacity: 1;
+    transition: 0.1s ease opacity;
 
     ${!show &&
       css`
