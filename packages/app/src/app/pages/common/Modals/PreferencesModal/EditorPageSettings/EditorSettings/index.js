8d77c6bb9390dc6e4ad9bbce0019622ac1500403
Sara Vieira <hey@iamsaravieira.com>
2019-08-09 11:18:57 +0200

    remove vim mode option on ff (#2260)
    * remove vim mode option on ff
    
    * disabled instead of deletinmg

--- packages/app/src/app/pages/common/Modals/PreferencesModal/EditorPageSettings/EditorSettings/index.js
+++ packages/app/src/app/pages/common/Modals/PreferencesModal/EditorPageSettings/EditorSettings/index.js
@@ -11,6 +11,9 @@ import {
 } from '../../elements';
 import VSCodePlaceholder from '../../VSCodePlaceholder';
 
+const isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
+const isFF = navigator.userAgent.toLowerCase().includes('firefox');
+
 function EditorSettings({ store, signals }) {
   const bindValue = name => ({
     value: store.preferences.settings[name],
@@ -70,7 +73,8 @@ function EditorSettings({ store, signals }) {
             </VSCodePlaceholder>
           </PreferenceContainer>
         </VSCodePlaceholder>
-        <PreferenceContainer>
+        {/* {Vim mode does not work on FF or Safari */}
+        <PreferenceContainer disabled={isSafari || isFF}>
           <PaddedPreference
             title="VIM Mode"
             type="boolean"
@@ -81,6 +85,15 @@ function EditorSettings({ store, signals }) {
             page to see the effects
           </SubDescription>
         </PreferenceContainer>
+        {isSafari || isFF ? (
+          <SubDescription
+            css={`
+              margin-top: 0.5rem;
+            `}
+          >
+            The VIM extension currently only works on Chrome and Microsoft Edge.
+          </SubDescription>
+        ) : null}
       </SubContainer>
     </div>
   );
