7d868be88a05f40773e05429bc1eeaf51ba7cc0d
Ryan Lynch <lynch.r.j@gmail.com>
2019-08-09 15:48:58 -0400

    Added knobs and additional stories (#2247)
    * Added knobs to sandboxcard stories
    
    * Refactored knobbedSandbox
    
    * Refactored knobbedSandbox some more
    
    * Changes to knobs for author to ensure they're shown even when original prop is not passed
    
    * removed bad import
    
    * Fixed bad import completely
    
    * moved template types to fixtures
    
    * Fixed empty string edge case in knobbedAuthor
    
    * renamed functions
    
    * completed rename
    
    * Working on flex stories
    
    * Removed position: absolute from content in flex stories. Oops
    
    * Refined flex stories:
    
    * Added knobs for content sizes
    
    * Applied comments from base code review

--- packages/common/src/components/SandboxCard/index.stories.tsx
+++ packages/common/src/components/SandboxCard/index.stories.tsx
@@ -1,16 +1,54 @@
 import * as React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
-import SandboxCard, { Props } from './';
-import * as fake from './fixtures';
+import {
+  withKnobs,
+  text,
+  select,
+  number,
+  array,
+  boolean,
+} from '@storybook/addon-knobs';
+import SandboxCard, { Props, Sandbox } from './';
+import * as fixtures from './fixtures';
 import { ThemeDecorator } from '../../stories/decorators';
 
-const stories = storiesOf('components/SandboxCard', module).addDecorator(
-  ThemeDecorator
-);
+const authorWithKnobs = (
+  group: string,
+  author: Sandbox['author'] = null
+): Sandbox['author'] => {
+  const knobs = {
+    username: text('author.username', author && author.username, group),
+    avatar_url: text('author.avatar_url', author && author.avatar_url, group),
+  };
+
+  if (knobs.username !== null || knobs.avatar_url !== null) {
+    return knobs;
+  } else {
+    return author;
+  }
+};
+
+const sandboxWithKnobs = (group: string, sandbox: Sandbox): Sandbox => ({
+  id: text('id', sandbox.id, group),
+  title: text('title', sandbox.title, group),
+  author: authorWithKnobs(group, sandbox.author),
+  description: text('description', sandbox.description, group),
+  screenshot_url: text('screenshot_url', sandbox.screenshot_url, group),
+  view_count: number('view_count', sandbox.view_count, {}, group),
+  fork_count: number('fork_count', sandbox.fork_count, {}, group),
+  like_count: number('like_count', sandbox.like_count, {}, group),
+  template: select(
+    'template',
+    fixtures.templateOptions,
+    sandbox.template,
+    group
+  ),
+  tags: array('tags', sandbox.tags, ',', group),
+});
 
 const createSandboxStory = ({
-  sandbox = fake.sandbox(),
+  sandbox = fixtures.sandbox(),
   selectSandbox = action('selectSandbox'),
   small,
   noHeight,
@@ -18,68 +56,59 @@ const createSandboxStory = ({
   noMargin,
 }: Partial<Props>) => () => (
   <SandboxCard
-    sandbox={sandbox}
+    sandbox={sandboxWithKnobs('sandbox', sandbox)}
     selectSandbox={selectSandbox}
-    small={small}
-    noHeight={noHeight}
-    defaultHeight={defaultHeight}
-    noMargin={noMargin}
+    small={boolean('small', small)}
+    noHeight={boolean('noHeight', noHeight)}
+    defaultHeight={number('defaultHeight', defaultHeight)}
+    noMargin={boolean('noMargin', noMargin)}
   />
 );
 
-stories.add('basic', createSandboxStory({}));
-
-stories.add('small', createSandboxStory({ small: true }));
-
-stories.add('no height', createSandboxStory({ noHeight: true }));
-
-stories.add('default height', createSandboxStory({ defaultHeight: 500 }));
-
-stories.add('no margin', createSandboxStory({ noMargin: true }));
-
-stories.add('popular', createSandboxStory({ sandbox: fake.popularSandbox() }));
-
-stories.add(
-  'many tags',
-  createSandboxStory({ sandbox: fake.sandboxWithManyTags() })
-);
-
-stories.add(
-  'long title',
-  createSandboxStory({ sandbox: fake.sandboxWithLongTitle() })
-);
-
-stories.add(
-  'long description',
-  createSandboxStory({
-    sandbox: fake.sandboxWithLongDescription(),
-  })
-);
-
-stories.add(
-  'null author',
-  createSandboxStory({
-    sandbox: fake.sandboxWithNullAuthor(),
-  })
-);
-
-stories.add(
-  'undefined author',
-  createSandboxStory({
-    sandbox: fake.sandboxWithUndefinedAuthor(),
-  })
-);
-
-stories.add(
-  'null screenshot url',
-  createSandboxStory({
-    sandbox: fake.sandboxWithNullScreenshotUrl(),
-  })
-);
-
-stories.add(
-  'undefined screenshot url',
-  createSandboxStory({
-    sandbox: fake.sandboxWithUndefinedScreenshotUrl(),
-  })
-);
+storiesOf('components/SandboxCard', module)
+  .addDecorator(ThemeDecorator)
+  .addDecorator(withKnobs)
+  .add('basic', createSandboxStory({}))
+  .add('small', createSandboxStory({ small: true }))
+  .add('no height', createSandboxStory({ noHeight: true }))
+  .add('default height', createSandboxStory({ defaultHeight: 500 }))
+  .add('no margin', createSandboxStory({ noMargin: true }))
+  .add('popular', createSandboxStory({ sandbox: fixtures.popularSandbox() }))
+  .add(
+    'many tags',
+    createSandboxStory({ sandbox: fixtures.sandboxWithManyTags() })
+  )
+  .add(
+    'long title',
+    createSandboxStory({ sandbox: fixtures.sandboxWithLongTitle() })
+  )
+  .add(
+    'long description',
+    createSandboxStory({
+      sandbox: fixtures.sandboxWithLongDescription(),
+    })
+  )
+  .add(
+    'null author',
+    createSandboxStory({
+      sandbox: fixtures.sandboxWithNullAuthor(),
+    })
+  )
+  .add(
+    'undefined author',
+    createSandboxStory({
+      sandbox: fixtures.sandboxWithUndefinedAuthor(),
+    })
+  )
+  .add(
+    'null screenshot url',
+    createSandboxStory({
+      sandbox: fixtures.sandboxWithNullScreenshotUrl(),
+    })
+  )
+  .add(
+    'undefined screenshot url',
+    createSandboxStory({
+      sandbox: fixtures.sandboxWithUndefinedScreenshotUrl(),
+    })
+  );
