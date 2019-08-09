250f931118ce3e752304a3aefa52b9c9fdc1a0c8
christianalfoni <christianalfoni@gmail.com>
2019-08-09 10:49:51 +0200

    fix last types

--- packages/app/src/app/componentConnectors.ts
+++ packages/app/src/app/componentConnectors.ts
@@ -13,7 +13,13 @@ export const observer: any = isOvermind ? connect : mobxObserver;
 export const hooksObserver: any = isOvermind ? connect : mobxHooksObserver;
 
 export const Observer = isOvermind
-  ? connect(({ store, signals, children }) => children({ store, signals }))
+  ? inject('store', 'signals')(
+      connect(({ store, signals, children }: any) =>
+        children({ store, signals })
+      )
+    )
   : inject('store', 'signals')(
-      observer(({ store, signals, children }) => children({ store, signals }))
+      observer(({ store, signals, children }: any) =>
+        children({ store, signals })
+      )
     );
