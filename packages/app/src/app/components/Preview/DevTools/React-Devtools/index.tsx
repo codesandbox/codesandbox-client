db3fd1b4e9efa813ff276a06b8c96eefdb9851f1
Ives van Hoorne <Ives.v.h@gmail.com>
2019-08-19 12:57:22 +0200

    Fix iframe detecting for react devtools if no iframe is loaded or available (#2302)
    Fixes #2300

--- packages/app/src/app/components/Preview/DevTools/React-Devtools/index.tsx
+++ packages/app/src/app/components/Preview/DevTools/React-Devtools/index.tsx
@@ -1,4 +1,5 @@
-import React, { useEffect, useState, useContext } from 'react';
+import React, { useEffect, useState, useContext, useCallback } from 'react';
+import delay from '@codesandbox/common/lib/utils/delay';
 import { initialize } from 'react-devtools-inline/frontend';
 import { dispatch, actions } from 'codesandbox-api';
 import { ThemeContext } from 'styled-components';
@@ -9,23 +10,42 @@ import { DevToolProps } from '..';
 const DevTools = (props: DevToolProps) => {
   const [ReactDevTools, setDevTools] = useState(null);
   const theme = useContext(ThemeContext);
+  const unmounted = React.useRef(false);
 
-  useEffect(() => {
-    const iframe = document.getElementById(
+  const loadIframe = useCallback(async () => {
+    let iframe = document.getElementById(
       'sandbox-preview'
     ) as HTMLIFrameElement;
-    const contentWindow = iframe.contentWindow;
 
-    setDevTools(initialize(contentWindow));
-    iframe.onload = () => {
-      contentWindow.postMessage(
-        {
-          type: 'activate',
-        },
-        '*'
-      );
-    };
+    // iframe hasn't initialized or just isn't there
+    while (iframe === null && !unmounted.current) {
+      // Retry every second
+      // eslint-disable-next-line
+      await delay(1000);
+      iframe = document.getElementById('sandbox-preview') as HTMLIFrameElement;
+    }
+
+    if (iframe) {
+      const contentWindow = iframe.contentWindow;
+
+      setDevTools(initialize(contentWindow));
+      iframe.onload = () => {
+        contentWindow.postMessage(
+          {
+            type: 'activate',
+          },
+          '*'
+        );
+      };
+    }
   }, []);
+
+  useEffect(() => {
+    loadIframe();
+    return () => {
+      unmounted.current = true;
+    };
+  }, [loadIframe]);
 
   if (props.hidden) {
     return null;
