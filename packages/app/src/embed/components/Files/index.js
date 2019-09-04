39c5296ec90a94c13124facc78044b0de37478e2
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2019-09-04 16:45:50 +0200

    embed: make folders collapsable (#2370)
    * embed: make folders collapsable
    
    * move util functions down
    
    * move onClick logic to Files
    
    * put logic for default calculation inside lazy

--- packages/app/src/embed/components/Files/index.js
+++ packages/app/src/embed/components/Files/index.js
@@ -3,7 +3,10 @@
 import * as React from 'react';
 import { sortBy } from 'lodash-es';
 
-import type { Module, Directory } from '@codesandbox/common/lib/types';
+import type {
+  Module,
+  Directory as DirectoryType,
+} from '@codesandbox/common/lib/types';
 
 import { isMainModule } from '@codesandbox/common/lib/sandbox/modules';
 // eslint-disable-next-line import/extensions
@@ -14,8 +17,8 @@ import File from '../File';
 import { Container } from './elements';
 
 type Props = {
-  modules: Array<Module>,
-  directories: Array<Directory>,
+  modules: Module[],
+  directories: DirectoryType[],
   directoryId: ?string,
   depth?: number,
   currentModule: string,
@@ -44,28 +47,21 @@ function Files({
 
   return (
     <Container>
-      {sortBy(childrenDirectories, d => d.title).map(d => (
-        <div key={d.shortid}>
-          <File
-            id={d.id}
-            shortid={d.shortid}
-            title={d.title}
-            type="directory-open"
-            depth={depth}
+      {sortBy(childrenDirectories, directory => directory.title).map(
+        directory => (
+          <Directory
+            key={directory.shortid}
+            directory={directory}
+            currentModuleId={currentModule}
             setCurrentModule={setCurrentModule}
-          />
-          <Files
-            modules={modules}
-            directories={directories}
-            directoryId={d.shortid}
-            depth={depth + 1}
-            setCurrentModule={setCurrentModule}
-            currentModule={currentModule}
             template={template}
             entry={entry}
+            modules={modules}
+            directories={directories}
+            depth={depth}
           />
-        </div>
-      ))}
+        )
+      )}
       {sortBy(childrenModules, m => m.title).map(m => (
         <File
           id={m.id}
@@ -74,12 +70,84 @@ function Files({
           key={m.shortid}
           type={getType(m.title)}
           depth={depth}
-          setCurrentModule={setCurrentModule}
           active={m.id === currentModule}
           alternative={isMainModule(m, modules, directories, entry)}
+          onClick={() => setCurrentModule(m.id)}
         />
       ))}
     </Container>
+  );
+}
+
+/** Utils to help identify module tree */
+
+const getCurrentModule = (modules, currentModuleId) =>
+  modules.find(module => module.id === currentModuleId);
+
+const getParentDirectory = (directories, child) =>
+  directories.find(directory => directory.shortid === child.directoryShortid);
+
+const getCurrentModuleTree = (directories, currentModule) => {
+  const currentModuleTree = [currentModule];
+
+  let parentDirectory = getParentDirectory(directories, currentModule);
+
+  while (parentDirectory) {
+    currentModuleTree.push(parentDirectory);
+    // get parent directory of the parent directory
+    parentDirectory = getParentDirectory(directories, parentDirectory);
+  }
+
+  return currentModuleTree;
+};
+
+function Directory({
+  directory,
+  currentModuleId,
+  setCurrentModule,
+  template,
+  entry,
+  modules,
+  directories,
+  depth,
+}) {
+  /** directory should be open by default if currentModule is inside it */
+  const [open, setOpen] = React.useState(function() {
+    const currentModule = getCurrentModule(modules, currentModuleId);
+    const currentModuleTree = getCurrentModuleTree(directories, currentModule);
+
+    let openByDefault = false;
+    if (currentModuleTree.find(module => module.id === directory.id)) {
+      openByDefault = true;
+    }
+    return openByDefault;
+  });
+
+  return (
+    <div>
+      <File
+        id={directory.id}
+        shortid={directory.shortid}
+        title={directory.title}
+        type={open ? 'directory-open' : 'directory'}
+        depth={depth}
+        onClick={() => {
+          setOpen(!open);
+        }}
+      />
+      {open ? (
+        <Files
+          modules={modules}
+          directories={directories}
+          directoryId={directory.shortid}
+          depth={depth + 1}
+          currentModule={currentModuleId}
+          setCurrentModule={setCurrentModule}
+          template={template}
+          entry={entry}
+        />
+      ) : null}
+    </div>
   );
 }
 
