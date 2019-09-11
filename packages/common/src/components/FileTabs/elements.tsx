ee92b754ab513bae898fa82b4bf4afcee3a63e1d
siddharthkp <siddharth.kshetrapal@gmail.com>
2019-09-11 14:38:43 +0200

    move styles into elements

--- packages/common/src/components/FileTabs/elements.tsx
+++ packages/common/src/components/FileTabs/elements.tsx
@@ -4,8 +4,40 @@ import PlusIcon from 'react-icons/lib/go/plus';
 
 import Row from '../flex/Row';
 
-export const Tabs = props => <Element as={Row} component="Tabs" {...props} />;
-export const Tab = props => <Element as="div" component="Tab" {...props} />;
+const styles = {
+  Tabs: {
+    backgroundColor: 'grays.5',
+    fontSize: 2,
+    boxShadow: 'underline',
+  },
+  Tab: {
+    color: 'white',
+    paddingY: 3,
+    paddingLeft: 2,
+    cursor: 'pointer',
+
+    '&[aria-selected]': {
+      boxShadow: 'active',
+      '& svg': {
+        opacity: 1,
+      },
+    },
+    '&:hover svg': {
+      opacity: 1,
+    },
+  },
+  Close: {
+    fontSize: 1,
+    marginY: 1,
+    opacity: 0,
+    // close icon is a lie, it's just a rotated PlusIcon
+    // TODO: replace it with a skinny X
+    transform: 'rotate(45deg)',
+  },
+};
+
+export const Tabs = props => <Element as={Row} css={styles.Tabs} {...props} />;
+export const Tab = props => <Element as="div" css={styles.Tab} {...props} />;
 export const Close = props => (
-  <Element as={PlusIcon} component="TabCloseIcon" {...props} />
+  <Element as={PlusIcon} css={styles.Close} {...props} />
 );
