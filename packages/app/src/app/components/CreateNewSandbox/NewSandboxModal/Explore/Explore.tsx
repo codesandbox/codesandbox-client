37cbe755ea675a0865488feca8c941724263c566
Sara <hey@iamsaravieira.com>
2019-09-24 09:11:33 +0200

    fix eslint

--- packages/app/src/app/components/CreateNewSandbox/NewSandboxModal/Explore/Explore.tsx
+++ packages/app/src/app/components/CreateNewSandbox/NewSandboxModal/Explore/Explore.tsx
@@ -57,7 +57,7 @@ export const Explore = () => {
       return setCategory(`template: ${e.target.value}`);
     }
 
-    setCategory('');
+    return setCategory('');
   };
 
   return (
--- packages/app/src/app/components/CreateNewSandbox/NewSandboxModal/SandboxCard/SandboxCard.tsx
+++ packages/app/src/app/components/CreateNewSandbox/NewSandboxModal/SandboxCard/SandboxCard.tsx
@@ -1,7 +1,7 @@
 import React, { forwardRef } from 'react';
-import * as Icons from '@codesandbox/template-icons';
+// import * as Icons from '@codesandbox/template-icons';
 import history from 'app/utils/history';
-import getIcon from '@codesandbox/common/lib/templates/icons';
+// import getIcon from '@codesandbox/common/lib/templates/icons';
 import { sandboxUrl } from '@codesandbox/common/lib/utils/url-generator';
 import { useOvermind } from 'app/overmind';
 import {
@@ -22,12 +22,12 @@ interface ISandboxCard {
 export const SandboxCard: React.FC<ISandboxCard> = forwardRef(
   ({ template, official }, ref) => {
     const { source, id: sandboxID, author = {} } = template.sandbox || {};
-    const UserIcon =
-      template.iconUrl && Icons[template.iconUrl]
-        ? Icons[template.iconUrl]
-        : getIcon((source || {}).template);
+    // const UserIcon =
+    //   template.iconUrl && Icons[template.iconUrl]
+    //     ? Icons[template.iconUrl]
+    //     : getIcon((source || {}).template);
 
-    const OfficialIcon = getIcon(template.name);
+    // const OfficialIcon = getIcon(template.name);
     const { actions } = useOvermind();
     const title =
       template.niceName || template.sandbox.title || template.sandbox.id;
@@ -40,7 +40,6 @@ export const SandboxCard: React.FC<ISandboxCard> = forwardRef(
             if (official) {
               history.push(sandboxUrl({ id: template.shortid }));
             } else {
-              console.log(sandboxID);
               history.push(sandboxID);
             }
             actions.modalClosed();
