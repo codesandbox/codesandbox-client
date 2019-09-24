6baa66923762e9888668385eca755dd5ccf11ab5
Drake Costa <drake@saeris.io>
2019-09-23 22:45:59 -0700

    🔨 Switch CodeSadbox to useOvermind, Remove title, add label to Report
    Small refactor to make the switch to Overmind.
    
    Removed the title from the generated crash report, which will now require users to enter their own in order to be able to submit the report. Hopefully this makes crash reports more descriptive. Crash Reports now include the proper label automatically when they are generated.
    
    @christianalfoni there appears to be an issue with Overmind in this component, as isLoggedIn returns false even when the user is in fact logged in (test this by running locally and navigating to http://localhost:3000/codesadbox from another page). Not quite sure how to solve this one, so I'd appreciate it if you can investigate! Probably has something to do with the error boundary.

--- packages/app/src/app/pages/common/ErrorBoundary/CodeSadbox/CodeSadbox.tsx
+++ packages/app/src/app/pages/common/ErrorBoundary/CodeSadbox/CodeSadbox.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import GoHome from 'react-icons/lib/go/home';
 import GoIssueOpened from 'react-icons/lib/go/issue-opened';
-import { inject, hooksObserver } from 'app/componentConnectors';
+import { useOvermind } from 'app/overmind';
 import { Button } from '@codesandbox/common/lib/components/Button';
 import { dashboardUrl } from '@codesandbox/common/lib/utils/url-generator';
 import { Navigation } from 'app/pages/common/Navigation';
@@ -21,51 +21,56 @@ import {
   ButtonIcon,
 } from './elements';
 
-export const CodeSadbox = inject('store')(
-  hooksObserver(
-    ({ error, trace, store }: IFallbackComponentProps & { store: any }) => (
-      <Container>
-        <Header>
-          <Nav>
-            <Navigation title="CodeSadbox" />
-          </Nav>
-        </Header>
-        <Content>
-          <Title>Oh no! Something broke!</Title>
-          <Sadbox scale={3} />
-          <Subtitle>CodeSadbox</Subtitle>
-          <Actions>
-            {store.isLoggedIn ? (
-              <Button small secondary href={dashboardUrl()}>
-                <ButtonIcon>
-                  <Dashboard />
-                </ButtonIcon>
-                Go to Dashboard
-              </Button>
-            ) : (
-              <Button small secondary href="/">
-                <ButtonIcon>
-                  <GoHome />
-                </ButtonIcon>
-                Back to Home
-              </Button>
-            )}
-            {/*
-          // @ts-ignore */}
-            <Button
-              small
-              target="_blank"
-              rel="noopener"
-              href={buildCrashReport({ error, trace })}
-            >
+export const CodeSadbox: React.FC<IFallbackComponentProps> = ({
+  error,
+  trace,
+}) => {
+  const {
+    state: { isLoggedIn },
+  } = useOvermind();
+
+  return (
+    <Container>
+      <Header>
+        <Nav>
+          <Navigation title="CodeSadbox" />
+        </Nav>
+      </Header>
+      <Content>
+        <Title>Oh no! Something broke!</Title>
+        <Sadbox scale={3} />
+        <Subtitle>CodeSadbox</Subtitle>
+        <Actions>
+          {isLoggedIn ? (
+            <Button small secondary href={dashboardUrl()}>
               <ButtonIcon>
-                <GoIssueOpened />
+                <Dashboard />
               </ButtonIcon>
-              Report Crash
+              Go to Dashboard
             </Button>
-          </Actions>
-        </Content>
-      </Container>
-    )
-  )
-);
+          ) : (
+            <Button small secondary href="/">
+              <ButtonIcon>
+                <GoHome />
+              </ButtonIcon>
+              Back to Home
+            </Button>
+          )}
+          {/*
+              // @ts-ignore */}
+          <Button
+            small
+            target="_blank"
+            rel="noopener"
+            href={buildCrashReport({ error, trace })}
+          >
+            <ButtonIcon>
+              <GoIssueOpened />
+            </ButtonIcon>
+            Report Crash
+          </Button>
+        </Actions>
+      </Content>
+    </Container>
+  );
+};
