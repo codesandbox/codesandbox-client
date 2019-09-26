edda89e2d6aea190f29ba95d9a7a244eaade0ad6
Michaël De Boey <info@michaeldeboey.be>
2019-09-26 12:02:59 +0200

    🔨 Switch NotFound to use useOvermind

--- packages/app/src/app/pages/common/NotFound/index.tsx
+++ packages/app/src/app/pages/common/NotFound/index.tsx
@@ -1,31 +1,38 @@
-import React from 'react';
-import { inject, hooksObserver } from 'app/componentConnectors';
-
 import { Button } from '@codesandbox/common/lib/components/Button';
 import { newSandboxWizard } from '@codesandbox/common/lib/utils/url-generator';
+import React, { FunctionComponent } from 'react';
+
+import { useOvermind } from 'app/overmind';
 
 import { Container, Title, SubTitle, Buttons } from './elements';
 
-export const NotFound = inject('store')(
-  hooksObserver(({ store: { hasLogIn } }) => (
+export const NotFound: FunctionComponent = () => {
+  const {
+    state: { hasLogIn },
+  } = useOvermind();
+
+  return (
     <Container>
       <Title>404</Title>
+
       <SubTitle>
         We could not find the page you
         {"'"}
         re looking for.
       </SubTitle>
+
       <Buttons>
         <Button small block style={{ margin: '.5rem' }} to={newSandboxWizard()}>
           Create Sandbox
         </Button>
+
         <Button small block style={{ margin: '.5rem' }} href="/">
           {hasLogIn ? 'Dashboard' : 'Homepage'}
         </Button>
       </Buttons>
     </Container>
-  ))
-);
+  );
+};
 
 // eslint-disable-next-line import/no-default-export
 export default NotFound;
