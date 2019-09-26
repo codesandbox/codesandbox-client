8ea3448ec78688ccd007cc8228a1f9ebffbec40a
Michaël De Boey <info@michaeldeboey.be>
2019-09-26 11:22:35 +0200

    🔨 Switch GithubIntegration to use useOvermind

--- packages/app/src/app/pages/common/GithubIntegration/index.tsx
+++ packages/app/src/app/pages/common/GithubIntegration/index.tsx
@@ -1,36 +1,36 @@
-import { inject, hooksObserver } from 'app/componentConnectors';
-import React from 'react';
-import GithubLogo from 'react-icons/lib/go/mark-github';
+import React, { FunctionComponent } from 'react';
+import GitHubLogo from 'react-icons/lib/go/mark-github';
+
 import { Integration } from 'app/components/Integration';
+import { useOvermind } from 'app/overmind';
 
 type Props = {
   small?: boolean;
-  store: any;
-  signals: any;
 };
-export const GithubIntegration = inject('store', 'signals')(
-  hooksObserver(
-    ({
-      small = false,
-      signals: { signInGithubClicked, signOutGithubIntegration },
-      store: {
-        isLoadingGithub,
-        user: {
-          integrations: { github },
-        },
+export const GithubIntegration: FunctionComponent<Props> = ({
+  small = false,
+}) => {
+  const {
+    actions: { signInGithubClicked, signOutGithubIntegration },
+    state: {
+      isLoadingGithub,
+      user: {
+        integrations: { github },
       },
-    }: Props) => (
-      <Integration
-        bgColor="#4078c0"
-        description={small ? 'Commits & PRs' : 'Committing & Pull Requests'}
-        Icon={GithubLogo}
-        loading={isLoadingGithub}
-        name="GitHub"
-        onSignIn={() => signInGithubClicked({ useExtraScopes: true })}
-        onSignOut={signOutGithubIntegration}
-        small={small}
-        userInfo={github}
-      />
-    )
-  )
-);
+    },
+  } = useOvermind();
+
+  return (
+    <Integration
+      bgColor="#4078c0"
+      description={small ? 'Commits & PRs' : 'Committing & Pull Requests'}
+      Icon={GitHubLogo}
+      loading={isLoadingGithub}
+      name="GitHub"
+      onSignIn={() => signInGithubClicked({ useExtraScopes: true })}
+      onSignOut={signOutGithubIntegration}
+      small={small}
+      userInfo={github}
+    />
+  );
+};
