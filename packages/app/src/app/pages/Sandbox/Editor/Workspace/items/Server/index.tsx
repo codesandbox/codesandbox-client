f76c1611348c52ed46d28449e4b32cbab3ad87f1
Michaël De Boey <info@michaeldeboey.be>
2019-09-26 11:16:16 +0200

    🔨 Switch Server to use useOvermind

--- packages/app/src/app/pages/Sandbox/Editor/Workspace/items/Server/index.tsx
+++ packages/app/src/app/pages/Sandbox/Editor/Workspace/items/Server/index.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
-import { inject, hooksObserver } from 'app/componentConnectors';
-import BrowserIcon from 'react-icons/lib/go/browser';
 import Margin from '@codesandbox/common/lib/components/spacing/Margin';
+import React, { FunctionComponent } from 'react';
+import BrowserIcon from 'react-icons/lib/go/browser';
+
+import { useOvermind } from 'app/overmind';
 
 import {
   Description,
@@ -9,9 +10,6 @@ import {
   EntryContainer,
 } from '../../elements';
 
-import { Status } from './Status';
-import { Tasks } from './Tasks';
-import { EnvironmentVariables } from './EnvVars';
 import {
   SubTitle,
   TasksContainer,
@@ -20,6 +18,9 @@ import {
   ActionButton,
   Power as PowerIcon,
 } from './elements';
+import { EnvironmentVariables } from './EnvVars';
+import { Status } from './Status';
+import { Tasks } from './Tasks';
 
 type Port = {
   main: boolean;
@@ -28,143 +29,157 @@ type Port = {
   name?: string;
 };
 
-export const Server = inject('store', 'signals')(
-  hooksObserver(({ store: { server, editor }, signals }) => {
-    const disconnected = server.status !== 'connected';
-    const sandbox = editor.currentSandbox;
+export const Server: FunctionComponent = () => {
+  const {
+    actions: {
+      server: {
+        onBrowserFromPortOpened,
+        onBrowserTabOpened,
+        restartContainer,
+        restartSandbox,
+      },
+    },
+    state: {
+      server,
+      editor: { currentSandbox: sandbox, parsedConfigurations },
+    },
+  } = useOvermind();
 
-    const openPort = (port: {
-      main: boolean;
-      port: number;
-      hostname: string;
-    }) => {
-      signals.server.onBrowserFromPortOpened({ port });
-    };
+  const disconnected = server.status !== 'connected';
 
-    return (
-      <div>
-        <Description>
-          This sandbox is executed on a server. You can control the server from
-          this panel.
-        </Description>
+  const openPort = (port: Port) => {
+    onBrowserFromPortOpened({ port });
+  };
 
-        <Margin top={1}>
-          <SubTitle>Status</SubTitle>
-          <WorkspaceInputContainer>
-            <Status
-              managerStatus={server.status}
-              containerStatus={server.containerStatus}
+  return (
+    <div>
+      <Description>
+        This sandbox is executed on a server. You can control the server from
+        this panel.
+      </Description>
+
+      <Margin top={1}>
+        <SubTitle>Status</SubTitle>
+
+        <WorkspaceInputContainer>
+          <Status
+            managerStatus={server.status}
+            containerStatus={server.containerStatus}
+          />
+        </WorkspaceInputContainer>
+      </Margin>
+
+      <Margin top={1.5}>
+        <SubTitle>Run Scripts</SubTitle>
+        <Margin top={0.5}>
+          <TasksContainer disconnected={disconnected}>
+            <Tasks
+              package={
+                parsedConfigurations.package &&
+                parsedConfigurations.package.parsed
+              }
             />
-          </WorkspaceInputContainer>
+          </TasksContainer>
         </Margin>
+        s
+      </Margin>
 
-        <Margin top={1.5}>
-          <SubTitle>Run Scripts</SubTitle>
-          <Margin top={0.5}>
-            <TasksContainer disconnected={disconnected}>
-              <Tasks
-                package={
-                  editor.parsedConfigurations.package &&
-                  editor.parsedConfigurations.package.parsed
-                }
-              />
-            </TasksContainer>
-          </Margin>
-        </Margin>
+      <Margin top={1} bottom={0.5}>
+        <SubTitle>Open Ports</SubTitle>
 
-        <Margin top={1} bottom={0.5}>
-          <SubTitle>Open Ports</SubTitle>
-          <Margin top={0.5}>
-            {server.ports.length ? (
-              server.ports.map((port: Port) => (
-                <EntryContainer
-                  style={{ position: 'relative' }}
-                  onClick={() => openPort(port)}
-                >
-                  <Port>
-                    <BrowserIcon />
-                    <div>{port.name || port.port}</div>
-                  </Port>
-                  {port.main && <MainBadge>main</MainBadge>}
-                </EntryContainer>
-              ))
-            ) : (
-              <Description>
-                No ports are opened. Maybe the server is still starting or it
-                doesn{"'"}t open any ports.
-              </Description>
-            )}
-            {(sandbox.template === 'gatsby' ||
-              sandbox.template === 'gridsome') &&
-            server.ports.length ? (
+        <Margin top={0.5}>
+          {server.ports.length ? (
+            server.ports.map((port: Port) => (
               <EntryContainer
                 style={{ position: 'relative' }}
-                onClick={() =>
-                  signals.server.onBrowserTabOpened({
-                    port: {
-                      url:
-                        sandbox.template === 'gridsome'
-                          ? '/___explore'
-                          : '/___graphql',
-                      title: 'GraphiQL',
-                    },
-                  })
-                }
+                onClick={() => openPort(port)}
               >
                 <Port>
                   <BrowserIcon />
-                  <div>GraphiQL</div>
+
+                  <div>{port.name || port.port}</div>
                 </Port>
+
+                {port.main && <MainBadge>main</MainBadge>}
               </EntryContainer>
-            ) : null}
-          </Margin>
-        </Margin>
+            ))
+          ) : (
+            <Description>
+              No ports are opened. Maybe the server is still starting or it
+              doesn{"'"}t open any ports.
+            </Description>
+          )}
 
-        <Margin top={1} bottom={0.5}>
-          <SubTitle style={{ marginBottom: '.5rem' }}>
-            Control Container
-          </SubTitle>
-          <WorkspaceInputContainer>
-            <ActionButton
-              small
-              block
-              disabled={
-                disconnected || server.containerStatus !== 'sandbox-started'
+          {['gatsby', 'gridsome'].includes(sandbox.template) &&
+          server.ports.length ? (
+            <EntryContainer
+              style={{ position: 'relative' }}
+              onClick={() =>
+                onBrowserTabOpened({
+                  port: {
+                    url:
+                      sandbox.template === 'gridsome'
+                        ? '/___explore'
+                        : '/___graphql',
+                    title: 'GraphiQL',
+                  },
+                })
               }
-              onClick={() => signals.server.restartSandbox({})}
             >
-              <>
-                <PowerIcon /> Restart Sandbox
-              </>
-            </ActionButton>
-          </WorkspaceInputContainer>
-          <WorkspaceInputContainer>
-            <ActionButton
-              small
-              block
-              disabled={
-                disconnected || server.containerStatus === 'initializing'
-              }
-              onClick={() => signals.server.restartContainer({})}
-            >
-              <>
-                <PowerIcon /> Restart Server
-              </>
-            </ActionButton>
-          </WorkspaceInputContainer>
-        </Margin>
+              <Port>
+                <BrowserIcon />
 
-        <Margin top={1}>
-          <SubTitle>Secret Keys</SubTitle>
-          <Description>
-            Secrets are available as environment variables. They are kept
-            private and will not be transferred between forks.
-          </Description>
-          <Margin top={0.5}>
-            <EnvironmentVariables />
-          </Margin>
+                <div>GraphiQL</div>
+              </Port>
+            </EntryContainer>
+          ) : null}
         </Margin>
-      </div>
-    );
-  })
-);
+      </Margin>
+
+      <Margin top={1} bottom={0.5}>
+        <SubTitle style={{ marginBottom: '.5rem' }}>Control Container</SubTitle>
+
+        <WorkspaceInputContainer>
+          <ActionButton
+            small
+            block
+            disabled={
+              disconnected || server.containerStatus !== 'sandbox-started'
+            }
+            onClick={() => restartSandbox({})}
+          >
+            <>
+              <PowerIcon /> Restart Sandbox
+            </>
+          </ActionButton>
+        </WorkspaceInputContainer>
+
+        <WorkspaceInputContainer>
+          <ActionButton
+            small
+            block
+            disabled={disconnected || server.containerStatus === 'initializing'}
+            onClick={() => restartContainer({})}
+          >
+            <>
+              <PowerIcon /> Restart Server
+            </>
+          </ActionButton>
+        </WorkspaceInputContainer>
+      </Margin>
+
+      <Margin top={1}>
+        <SubTitle>Secret Keys</SubTitle>
+
+        <Description>
+          Secrets are available as environment variables. They are kept private
+          and will not be transferred between forks.
+        </Description>
+
+        <Margin top={0.5}>
+          <EnvironmentVariables />
+        </Margin>
+      </Margin>
+    </div>
+  );
+};
