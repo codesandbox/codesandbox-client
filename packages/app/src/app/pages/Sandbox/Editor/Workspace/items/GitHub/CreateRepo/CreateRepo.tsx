2f4faaaa49e512218d5b8f11f4a2d457b7447d34
Michaël De Boey <info@michaeldeboey.be>
2019-09-26 11:00:56 +0200

    🔨 Switch CreateRepo to use useOvermind

--- packages/app/src/app/pages/Sandbox/Editor/Workspace/items/GitHub/CreateRepo/CreateRepo.tsx
+++ packages/app/src/app/pages/Sandbox/Editor/Workspace/items/GitHub/CreateRepo/CreateRepo.tsx
@@ -2,33 +2,31 @@ import { Button } from '@codesandbox/common/lib/components/Button';
 import Input from '@codesandbox/common/lib/components/Input';
 import Margin from '@codesandbox/common/lib/components/spacing/Margin';
 import track from '@codesandbox/common/lib/utils/analytics';
+import React, { ChangeEvent, FunctionComponent, HTMLAttributes } from 'react';
+
 import { useOvermind } from 'app/overmind';
-import React from 'react';
 import {
   WorkspaceSubtitle,
   WorkspaceInputContainer,
 } from 'app/pages/Sandbox/Editor/Workspace/elements';
+
 import { Error } from './elements';
 
-interface ICreateRepoProps {
-  style?: React.CSSProperties;
-}
-
-export const CreateRepo: React.FC<ICreateRepoProps> = ({ style }) => {
+type Props = Pick<HTMLAttributes<HTMLDivElement>, 'style'>;
+export const CreateRepo: FunctionComponent<Props> = ({ style }) => {
   const {
+    actions: {
+      git: { createRepoClicked, repoTitleChanged },
+    },
     state: {
       editor: { isAllModulesSynced },
-      git: { repoTitle, error },
-    },
-    actions: {
-      git: { repoTitleChanged, createRepoClicked },
+      git: { error, repoTitle },
     },
   } = useOvermind();
 
   const updateRepoTitle = ({
     target: { value: title },
-  }: React.ChangeEvent<HTMLInputElement>) => repoTitleChanged({ title });
-
+  }: ChangeEvent<HTMLInputElement>) => repoTitleChanged({ title });
   const createRepo = () => {
     track('Export to GitHub Clicked');
     createRepoClicked();
@@ -51,7 +49,7 @@ export const CreateRepo: React.FC<ICreateRepoProps> = ({ style }) => {
       <Margin horizontal={1} bottom={1}>
         <Button
           block
-          disabled={Boolean(error || !repoTitle || !isAllModulesSynced)}
+          disabled={Boolean(error) || !repoTitle || !isAllModulesSynced}
           onClick={createRepo}
           small
         >
