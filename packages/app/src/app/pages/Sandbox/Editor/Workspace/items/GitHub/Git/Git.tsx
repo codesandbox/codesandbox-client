8ad13a272ab3ba1937b8b3a7d558eddb5642017a
Drake Costa <drake@saeris.io>
2019-09-26 01:50:30 -0700

    🔨 Switch Git to useOvermind

--- packages/app/src/app/pages/Sandbox/Editor/Workspace/items/GitHub/Git/Git.tsx
+++ packages/app/src/app/pages/Sandbox/Editor/Workspace/items/GitHub/Git/Git.tsx
@@ -1,163 +1,155 @@
+import React, { useEffect } from 'react';
 import Margin from '@codesandbox/common/lib/components/spacing/Margin';
 import { Button } from '@codesandbox/common/lib/components/Button';
 import GithubBadge from '@codesandbox/common/lib/components/GithubBadge';
 import Input, { TextArea } from '@codesandbox/common/lib/components/Input';
 import Notice from '@codesandbox/common/lib/components/Notice';
 import { githubRepoUrl } from '@codesandbox/common/lib/utils/url-generator';
-import { inject, hooksObserver } from 'app/componentConnectors';
-import React, { useEffect } from 'react';
-
+import { useOvermind } from 'app/overmind';
 import { WorkspaceSubtitle, WorkspaceInputContainer } from '../../../elements';
-
 import { Container, Buttons, ErrorMessage, NoChanges } from './elements';
 import { TotalChanges } from './TotalChanges';
 
 const hasWriteAccess = (rights: 'none' | 'read' | 'write' | 'admin') =>
   ['admin', 'write'].includes(rights);
 
-export const Git = inject('store', 'signals')(
-  hooksObserver(
-    ({
-      signals: {
-        git: {
-          createCommitClicked,
-          createPrClicked,
-          descriptionChanged,
-          gitMounted,
-          subjectChanged,
-        },
+export const Git = () => {
+  const {
+    state: {
+      editor: {
+        currentSandbox: { originalGit },
+        isAllModulesSynced,
       },
-      store: {
-        editor: {
-          currentSandbox: { originalGit },
-          isAllModulesSynced,
-        },
-        git: {
-          description,
-          isFetching,
-          originalGitChanges: gitChanges,
-          subject,
-        },
+      git: { description, isFetching, originalGitChanges: gitChanges, subject },
+    },
+    actions: {
+      git: {
+        createCommitClicked,
+        createPrClicked,
+        descriptionChanged,
+        gitMounted,
+        subjectChanged,
       },
-    }) => {
-      useEffect(() => {
-        gitMounted();
-      }, [gitMounted]);
+    },
+  } = useOvermind();
 
-      const createCommit = () => createCommitClicked();
-      const createPR = () => createPrClicked();
-      const changeSubject = ({
-        target: { value },
-      }: React.ChangeEvent<HTMLInputElement>) =>
-        subjectChanged({ subject: value });
-      const changeDescription = ({
-        target: { value },
-      }: React.ChangeEvent<HTMLTextAreaElement>) =>
-        descriptionChanged({ description: value });
+  useEffect(() => {
+    gitMounted();
+  }, [gitMounted]);
 
-      const modulesNotSaved = !isAllModulesSynced;
-      const changeCount = gitChanges
-        ? gitChanges.added.length +
-          gitChanges.modified.length +
-          gitChanges.deleted.length
-        : 0;
+  const createCommit = () => createCommitClicked();
+  const createPR = () => createPrClicked();
 
-      return (
-        <Container>
-          <Notice>beta</Notice>
+  const changeSubject = ({
+    target: { value },
+  }: React.ChangeEvent<HTMLInputElement>) => subjectChanged({ subject: value });
 
-          <WorkspaceSubtitle>GitHub Repository</WorkspaceSubtitle>
+  const changeDescription = ({
+    target: { value },
+  }: React.ChangeEvent<HTMLTextAreaElement>) =>
+    descriptionChanged({ description: value });
 
-          <Margin margin={1}>
-            <GithubBadge
-              branch={originalGit.branch}
-              repo={originalGit.repo}
-              url={githubRepoUrl(originalGit)}
-              username={originalGit.username}
-            />
-          </Margin>
+  const modulesNotSaved = !isAllModulesSynced;
+  const changeCount = gitChanges
+    ? gitChanges.added.length +
+      gitChanges.modified.length +
+      gitChanges.deleted.length
+    : 0;
 
-          <Margin bottom={0}>
-            <WorkspaceSubtitle>
-              Changes ({isFetching ? '...' : changeCount})
-            </WorkspaceSubtitle>
+  return (
+    <Container>
+      <Notice>beta</Notice>
 
-            {!isFetching ? (
+      <WorkspaceSubtitle>GitHub Repository</WorkspaceSubtitle>
+
+      <Margin margin={1}>
+        <GithubBadge
+          branch={originalGit.branch}
+          repo={originalGit.repo}
+          url={githubRepoUrl(originalGit)}
+          username={originalGit.username}
+        />
+      </Margin>
+
+      <Margin bottom={0}>
+        <WorkspaceSubtitle>
+          Changes ({isFetching ? '...' : changeCount})
+        </WorkspaceSubtitle>
+
+        {!isFetching ? (
+          <Margin top={1}>
+            <TotalChanges gitChanges={gitChanges || {}} />
+
+            {changeCount > 0 ? (
               <Margin top={1}>
-                <TotalChanges gitChanges={gitChanges || {}} />
-
-                {changeCount > 0 ? (
-                  <Margin top={1}>
-                    <WorkspaceSubtitle>Commit Info</WorkspaceSubtitle>
-                    {modulesNotSaved && (
-                      <ErrorMessage>
-                        You need to save all modules before you can commit
-                      </ErrorMessage>
-                    )}
-
-                    <WorkspaceSubtitle
-                      style={{
-                        color: subject.length > 72 ? `#F27777` : `#556362`,
-                        textAlign: 'right',
-                      }}
-                    >
-                      {`${subject.length}/72`}
-                    </WorkspaceSubtitle>
-
-                    <WorkspaceInputContainer>
-                      <Input
-                        block
-                        onChange={changeSubject}
-                        placeholder="Subject"
-                        value={subject}
-                      />
-                    </WorkspaceInputContainer>
-
-                    <WorkspaceInputContainer>
-                      <TextArea
-                        block
-                        onChange={changeDescription}
-                        placeholder="Description"
-                        value={description}
-                      />
-                    </WorkspaceInputContainer>
-
-                    <Buttons>
-                      {hasWriteAccess(gitChanges.rights) && (
-                        <Button
-                          block
-                          disabled={!subject || modulesNotSaved}
-                          onClick={createCommit}
-                          small
-                        >
-                          Commit
-                        </Button>
-                      )}
-
-                      <Button
-                        block
-                        disabled={!subject || modulesNotSaved}
-                        onClick={createPR}
-                        small
-                      >
-                        Open PR
-                      </Button>
-                    </Buttons>
-                  </Margin>
-                ) : (
-                  <Margin bottom={1} horizontal={1}>
-                    <NoChanges>There are no changes</NoChanges>
-                  </Margin>
+                <WorkspaceSubtitle>Commit Info</WorkspaceSubtitle>
+                {modulesNotSaved && (
+                  <ErrorMessage>
+                    You need to save all modules before you can commit
+                  </ErrorMessage>
                 )}
+
+                <WorkspaceSubtitle
+                  style={{
+                    color: subject.length > 72 ? `#F27777` : `#556362`,
+                    textAlign: 'right',
+                  }}
+                >
+                  {`${subject.length}/72`}
+                </WorkspaceSubtitle>
+
+                <WorkspaceInputContainer>
+                  <Input
+                    block
+                    onChange={changeSubject}
+                    placeholder="Subject"
+                    value={subject}
+                  />
+                </WorkspaceInputContainer>
+
+                <WorkspaceInputContainer>
+                  <TextArea
+                    block
+                    onChange={changeDescription}
+                    placeholder="Description"
+                    value={description}
+                  />
+                </WorkspaceInputContainer>
+
+                <Buttons>
+                  {hasWriteAccess(gitChanges.rights) && (
+                    <Button
+                      block
+                      disabled={!subject || modulesNotSaved}
+                      onClick={createCommit}
+                      small
+                    >
+                      Commit
+                    </Button>
+                  )}
+
+                  <Button
+                    block
+                    disabled={!subject || modulesNotSaved}
+                    onClick={createPR}
+                    small
+                  >
+                    Open PR
+                  </Button>
+                </Buttons>
               </Margin>
             ) : (
-              <Margin margin={1}>
-                <div>Fetching changes...</div>
+              <Margin bottom={1} horizontal={1}>
+                <NoChanges>There are no changes</NoChanges>
               </Margin>
             )}
           </Margin>
-        </Container>
-      );
-    }
-  )
-);
+        ) : (
+          <Margin margin={1}>
+            <div>Fetching changes...</div>
+          </Margin>
+        )}
+      </Margin>
+    </Container>
+  );
+};
