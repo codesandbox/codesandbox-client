e94b38434eb27b97193d5680316862bd24cd3cc7
Michaël De Boey <info@michaeldeboey.be>
2019-09-26 11:28:24 +0200

    🔨 Switch TeamInvite to use useOvermind

--- packages/app/src/app/pages/common/Navigation/Notifications/notifications/TeamInvite.tsx
+++ packages/app/src/app/pages/common/Navigation/Notifications/notifications/TeamInvite.tsx
@@ -1,86 +1,90 @@
-import React from 'react';
-
-import { Mutation } from 'react-apollo';
-import history from 'app/utils/history';
-import { teamOverviewUrl } from '@codesandbox/common/lib/utils/url-generator';
 import track from '@codesandbox/common/lib/utils/analytics';
-import { inject, hooksObserver } from 'app/componentConnectors';
+import { teamOverviewUrl } from '@codesandbox/common/lib/utils/url-generator';
+import React, { FunctionComponent } from 'react';
+import { Mutation } from 'react-apollo';
 
-import { NotificationImage as Image } from '../elements';
-import { Container, Buttons, Button, W } from './elements';
+import { useOvermind } from 'app/overmind';
 import {
   REJECT_TEAM_INVITATION,
   ACCEPT_TEAM_INVITATION,
-} from '../../../../Dashboard/queries';
+} from 'app/pages/Dashboard/queries';
+import history from 'app/utils/history';
 
-interface Props {
+import { NotificationImage as Image } from '../elements';
+
+import { Container, Buttons, Button, W } from './elements';
+
+type Props = {
   read: boolean;
   teamId: string;
   teamName: string;
   inviterName: string;
   inviterAvatar: string;
-}
+};
+export const TeamInvite: FunctionComponent<Props> = ({
+  read,
+  teamId,
+  teamName,
+  inviterName,
+  inviterAvatar,
+}) => {
+  const {
+    actions: { notificationAdded },
+  } = useOvermind();
 
-export const TeamInvite = inject('signals')(
-  hooksObserver(
-    ({
-      read,
-      teamId,
-      teamName,
-      inviterName,
-      inviterAvatar,
-      signals: { notificationAdded },
-    }: Props & { signals: any }) => (
-      <div>
-        <Container read={read}>
-          <Image src={inviterAvatar} />
-          <div>
-            <W>{inviterName}</W> invites you to join team <W>{teamName}</W>
-          </div>
-        </Container>
-        {!read && (
-          <Buttons>
-            <Mutation
-              variables={{ teamId }}
-              mutation={REJECT_TEAM_INVITATION}
-              refetchQueries={['RecentNotifications']}
-              onCompleted={() => {
-                track('Team - Invitation Rejected');
-                notificationAdded({
-                  message: `Rejected invitation to ${teamName}`,
-                  type: 'success',
-                });
-              }}
-            >
-              {(mutate, { loading }) => (
-                <Button onClick={() => mutate()} disabled={loading} decline>
-                  Decline
-                </Button>
-              )}
-            </Mutation>
-            <Mutation
-              variables={{ teamId }}
-              mutation={ACCEPT_TEAM_INVITATION}
-              refetchQueries={['RecentNotifications', 'TeamsSidebar']}
-              onCompleted={() => {
-                track('Team - Invitation Accepted');
-                notificationAdded({
-                  message: `Accepted invitation to ${teamName}`,
-                  type: 'success',
-                });
+  return (
+    <div>
+      <Container read={read}>
+        <Image src={inviterAvatar} />
 
-                history.push(teamOverviewUrl(teamId));
-              }}
-            >
-              {(mutate, { loading }) => (
-                <Button onClick={() => mutate()} disabled={loading}>
-                  Accept
-                </Button>
-              )}
-            </Mutation>
-          </Buttons>
-        )}
-      </div>
-    )
-  )
-);
+        <div>
+          <W>{inviterName}</W> invites you to join team <W>{teamName}</W>
+        </div>
+      </Container>
+
+      {!read && (
+        <Buttons>
+          <Mutation
+            variables={{ teamId }}
+            mutation={REJECT_TEAM_INVITATION}
+            refetchQueries={['RecentNotifications']}
+            onCompleted={() => {
+              track('Team - Invitation Rejected');
+              notificationAdded({
+                message: `Rejected invitation to ${teamName}`,
+                type: 'success',
+              });
+            }}
+          >
+            {(mutate, { loading }) => (
+              <Button onClick={() => mutate()} disabled={loading} decline>
+                Decline
+              </Button>
+            )}
+          </Mutation>
+
+          <Mutation
+            variables={{ teamId }}
+            mutation={ACCEPT_TEAM_INVITATION}
+            refetchQueries={['RecentNotifications', 'TeamsSidebar']}
+            onCompleted={() => {
+              track('Team - Invitation Accepted');
+              notificationAdded({
+                message: `Accepted invitation to ${teamName}`,
+                type: 'success',
+              });
+
+              history.push(teamOverviewUrl(teamId));
+            }}
+          >
+            {(mutate, { loading }) => (
+              <Button onClick={() => mutate()} disabled={loading}>
+                Accept
+              </Button>
+            )}
+          </Mutation>
+        </Buttons>
+      )}
+    </div>
+  );
+};
