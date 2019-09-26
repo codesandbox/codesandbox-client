2f1a723843d17ff25215876c6bd8d6a643b0f3a6
Michaël De Boey <info@michaeldeboey.be>
2019-09-26 12:17:00 +0200

    🔨 Switch UserMenu to use useOvermind

--- packages/app/src/app/pages/common/UserMenu/index.tsx
+++ packages/app/src/app/pages/common/UserMenu/index.tsx
@@ -1,44 +1,47 @@
-import * as React from 'react';
 import Relative from '@codesandbox/common/lib/components/Relative';
-import { inject, hooksObserver } from 'app/componentConnectors';
-import { HoverMenu } from 'app/components/HoverMenu';
-import { Menu } from './Menu';
-import { ClickableContainer, ProfileImage } from './elements';
+import React, { FunctionComponent } from 'react';
 
-export const UserMenu = inject('store', 'signals')(
-  hooksObserver(
-    ({
-      store: { user, userMenuOpen },
-      signals: {
-        userMenuClosed,
-        modalOpened,
-        signOutClicked,
-        userMenuOpened,
-        files,
-      },
-    }) => (
-      <Relative>
-        <ClickableContainer onClick={userMenuOpened}>
-          <ProfileImage
-            alt={user.username}
-            width={30}
-            height={30}
-            src={user.avatarUrl}
+import { HoverMenu } from 'app/components/HoverMenu';
+import { useOvermind } from 'app/overmind';
+
+import { ClickableContainer, ProfileImage } from './elements';
+import { Menu } from './Menu';
+
+export const UserMenu: FunctionComponent = () => {
+  const {
+    actions: {
+      userMenuClosed,
+      modalOpened,
+      signOutClicked,
+      userMenuOpened,
+      files,
+    },
+    state: { user, userMenuOpen },
+  } = useOvermind();
+
+  return (
+    <Relative>
+      <ClickableContainer onClick={userMenuOpened}>
+        <ProfileImage
+          alt={user.username}
+          width={30}
+          height={30}
+          src={user.avatarUrl}
+        />
+      </ClickableContainer>
+
+      {userMenuOpen && (
+        <HoverMenu onClose={() => userMenuClosed()}>
+          <Menu
+            openPreferences={() => modalOpened({ modal: 'preferences' })}
+            openStorageManagement={files.gotUploadedFiles}
+            signOut={signOutClicked}
+            username={user.username}
+            curator={user.curatorAt}
+            openFeedback={() => modalOpened({ modal: 'feedback' })}
           />
-        </ClickableContainer>
-        {userMenuOpen && (
-          <HoverMenu onClose={() => userMenuClosed()}>
-            <Menu
-              openPreferences={() => modalOpened({ modal: 'preferences' })}
-              openStorageManagement={files.gotUploadedFiles}
-              signOut={signOutClicked}
-              username={user.username}
-              curator={user.curatorAt}
-              openFeedback={() => modalOpened({ modal: 'feedback' })}
-            />
-          </HoverMenu>
-        )}
-      </Relative>
-    )
-  )
-);
+        </HoverMenu>
+      )}
+    </Relative>
+  );
+};
