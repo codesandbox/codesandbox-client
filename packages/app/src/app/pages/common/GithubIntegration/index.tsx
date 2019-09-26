4da0f77f98d6335ce8acdddd9da8baadbce12267
Michaël De Boey <info@michaeldeboey.be>
2019-09-26 15:58:43 +0200

    Fix types

--- packages/app/src/app/pages/common/GithubIntegration/index.tsx
+++ packages/app/src/app/pages/common/GithubIntegration/index.tsx
@@ -27,8 +27,8 @@ export const GithubIntegration: FunctionComponent<Props> = ({
       Icon={GitHubLogo}
       loading={isLoadingGithub}
       name="GitHub"
-      onSignIn={() => signInGithubClicked({ useExtraScopes: true })}
-      onSignOut={signOutGithubIntegration}
+      onSignIn={() => signInGithubClicked()}
+      onSignOut={() => signOutGithubIntegration()}
       small={small}
       userInfo={github}
     />
