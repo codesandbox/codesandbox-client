0cd07be9d53f51c7ec049cadf5f444d7c776e292
Michaël De Boey <info@michaeldeboey.be>
2019-09-27 14:36:04 +0200

    🔨 Switch Keywords to use useOvermind

--- packages/app/src/app/pages/Sandbox/Editor/Workspace/Project/Keywords.tsx
+++ packages/app/src/app/pages/Sandbox/Editor/Workspace/Project/Keywords.tsx
@@ -1,77 +1,74 @@
-import React from 'react';
-import { inject, hooksObserver, clone } from 'app/componentConnectors';
+import React, { FunctionComponent } from 'react';
 import Tags from '@codesandbox/common/lib/components/Tags';
 import getTemplateDefinition from '@codesandbox/common/lib/templates';
+
+import { clone } from 'app/componentConnectors';
 import { EditableTags } from 'app/components/EditableTags';
+import { useOvermind } from 'app/overmind';
+
 import { Item } from './elements';
 
-interface IKeywordsProps {
+type Props = {
   editable?: boolean;
-  store: any;
-  signals: any;
-}
-
-export const Keywords = inject('store', 'signals')(
-  hooksObserver(
-    ({
-      editable,
-      store: {
-        editor: {
-          currentSandbox: { template, tags },
-        },
-        workspace: {
-          tags: { tagName },
-        },
+};
+export const Keywords: FunctionComponent<Props> = ({ editable }) => {
+  const {
+    actions: {
+      notificationAdded,
+      workspace: { tagAdded, tagChanged, tagRemoved },
+    },
+    state: {
+      editor: {
+        currentSandbox: { template, tags },
       },
-      signals: {
-        notificationAdded,
-        workspace: { tagAdded, tagChanged, tagRemoved },
+      workspace: {
+        tags: { tagName },
       },
-    }: IKeywordsProps) => {
-      const changeTags = (newTags: string[], removedTags: string[]) => {
-        if (tags.length > 5) {
-          notificationAdded('You can have a maximum of 5 tags', 'error');
-          return;
-        }
+    },
+  } = useOvermind();
 
-        const tagWasRemoved =
-          newTags.length < tags.length && removedTags.length === 1;
+  if (tags.length === 0 && !editable) {
+    return null;
+  }
 
-        if (tagWasRemoved) {
-          removedTags.forEach(tag => {
-            tagRemoved({ tag });
-          });
-        } else {
-          tagAdded();
-        }
-      };
-
-      if (tags.length === 0 && !editable) {
-        return null;
-      }
-
-      return (
-        <Item>
-          {editable ? (
-            <EditableTags
-              template={getTemplateDefinition(template)}
-              value={clone(tags)}
-              onChange={changeTags}
-              onChangeInput={(value: string) => {
-                tagChanged({ tagName: value });
-              }}
-              maxTags={5}
-              inputValue={tagName}
-              renderInput={({ addTag, ...props }: any) =>
-                tags.length !== 5 ? <input type="text" {...props} /> : null
-              }
-              onlyUnique
-            />
-          ) : (
-            <Tags style={{ fontSize: 13 }} tags={tags} />
-          )}
-        </Item>
-      );
+  const changeTags = (newTags: string[], removedTags: string[]) => {
+    if (tags.length > 5) {
+      notificationAdded('You can have a maximum of 5 tags', 'error');
+      return;
     }
-  )
-);
+
+    const tagWasRemoved =
+      newTags.length < tags.length && removedTags.length === 1;
+
+    if (tagWasRemoved) {
+      removedTags.forEach(tag => {
+        tagRemoved({ tag });
+      });
+    } else {
+      tagAdded();
+    }
+  };
+
+  return (
+    <Item>
+      {editable ? (
+        <EditableTags
+          template={getTemplateDefinition(template)}
+          value={clone(tags)}
+          onChange={changeTags}
+          onChangeInput={(value: string) => {
+            tagChanged({ tagName: value });
+          }}
+          maxTags={5}
+          inputValue={tagName}
+          renderInput={({ addTag, ...props }: any) =>
+            tags.length !== 5 ? <input type="text" {...props} /> : null
+          }
+          onlyUnique
+        />
+      ) : (
+        <Tags style={{ fontSize: 13 }} tags={tags} />
+      )}
+    </Item>
+  );
+};
