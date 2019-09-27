0f2bf6dfeee35c2d3f4f34ed33b4f3e3d7c9108c
Drake Costa <drake@saeris.io>
2019-09-27 09:46:35 -0700

    🔨 Switch CodeSadbox to useOvermind, Remove title, add label to… (#2498)
    * 🔨 Switch CodeSadbox to useOvermind, Remove title, add label to Report
    
    Small refactor to make the switch to Overmind.
    
    Removed the title from the generated crash report, which will now require users to enter their own in order to be able to submit the report. Hopefully this makes crash reports more descriptive. Crash Reports now include the proper label automatically when they are generated.
    
    @christianalfoni there appears to be an issue with Overmind in this component, as isLoggedIn returns false even when the user is in fact logged in (test this by running locally and navigating to http://localhost:3000/codesadbox from another page). Not quite sure how to solve this one, so I'd appreciate it if you can investigate! Probably has something to do with the error boundary.
    
    * fix mounting logic for codesadbox
    
    * fix types

--- packages/app/src/app/overmind/actions.ts
+++ packages/app/src/app/overmind/actions.ts
@@ -17,6 +17,8 @@ export const sandboxPageMounted: AsyncAction = withLoadApp();
 
 export const searchMounted: AsyncAction = withLoadApp();
 
+export const codesadboxMounted: AsyncAction = withLoadApp();
+
 export const cliMounted: AsyncAction = withLoadApp(
   async ({ state, actions }) => {
     if (state.user) {
