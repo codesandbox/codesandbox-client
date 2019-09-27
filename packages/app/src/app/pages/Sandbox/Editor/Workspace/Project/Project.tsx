521d28c7380eae81e3af27f1e6e36efe4eae4f37
Michaël De Boey <info@michaeldeboey.be>
2019-09-27 14:37:20 +0200

    🔨 Switch Project to use useOvermind

--- packages/app/src/app/pages/Sandbox/Editor/Workspace/Project/Project.tsx
+++ packages/app/src/app/pages/Sandbox/Editor/Workspace/Project/Project.tsx
@@ -7,13 +7,13 @@ import {
   patronUrl,
   sandboxUrl,
 } from '@codesandbox/common/lib/utils/url-generator';
-import { hooksObserver, inject } from 'app/componentConnectors';
-import { PrivacyStatus } from 'app/components/PrivacyStatus';
-import { Stats } from 'app/pages/common/Stats';
-import React from 'react';
+import React, { FunctionComponent } from 'react';
 import TeamIcon from 'react-icons/lib/md/people';
+import { PrivacyStatus } from 'app/components/PrivacyStatus';
+import { useOvermind } from 'app/overmind';
+import { Stats } from 'app/pages/common/Stats';
 
-// import AliasComponent from './Alias';
+// import { Alias } from './Alias';
 import { Author } from './Author';
 import { Description } from './Description';
 import {
@@ -36,159 +36,149 @@ import { Keywords } from './Keywords';
 import { SandboxConfig } from './SandboxConfig';
 import { Title } from './Title';
 
-interface IProjectProps {
+type Props = {
   editable?: boolean;
-  store: any;
-  signals: any;
-}
+};
+export const Project: FunctionComponent<Props> = ({ editable }) => {
+  const {
+    actions: {
+      workspace: { sandboxPrivacyChanged },
+    },
+    state: {
+      editor: { currentSandbox: sandbox },
+      isPatron,
+    },
+  } = useOvermind();
 
-export const Project = inject('store', 'signals')(
-  hooksObserver(
-    ({
-      editable,
-      store: { editor, isPatron },
-      signals: {
-        workspace: { sandboxPrivacyChanged },
-      },
-    }: IProjectProps) => {
-      const sandbox = editor.currentSandbox;
-      const template = getTemplateDefinition(sandbox.template);
-      const { isServer } = template;
+  const template = getTemplateDefinition(sandbox.template);
 
-      return (
-        <Container>
-          <BasicInfo>
-            <Title editable={editable} />
-            <Description editable={editable} />
-            {/* Disable until we also moved SSE over */}
-            {/* <Alias editable={editable} /> */}
-          </BasicInfo>
+  return (
+    <Container>
+      <BasicInfo>
+        <Title editable={editable} />
 
-          {!sandbox.team && sandbox.author && (
-            <Author author={sandbox.author} />
-          )}
+        <Description editable={editable} />
 
-          {sandbox.team && (
-            <Tooltip
-              appendTo="parent"
-              content="This sandbox is owned by this team"
-            >
-              <Item style={{ color: 'white', display: 'flex' }}>
-                <TeamIcon
-                  style={{ fontSize: '1.125em', marginRight: '.5rem' }}
-                />
-                <div>{sandbox.team.name}</div>
-              </Item>
-            </Tooltip>
-          )}
+        {/* Disable until we also moved SSE over */}
+        {/* <Alias editable={editable} /> */}
+      </BasicInfo>
 
-          {sandbox.git && (
-            <Item>
-              <GithubBadge
-                url={githubRepoUrl(sandbox.git)}
-                username={sandbox.git.username}
-                repo={sandbox.git.repo}
-                branch={sandbox.git.branch}
-              />
-            </Item>
-          )}
+      {!sandbox.team && sandbox.author && <Author author={sandbox.author} />}
 
-          <StatsContainer>
-            <Stats sandbox={sandbox} />
-          </StatsContainer>
+      {sandbox.team && (
+        <Tooltip appendTo="parent" content="This sandbox is owned by this team">
+          <Item style={{ color: 'white', display: 'flex' }}>
+            <TeamIcon style={{ fontSize: '1.125em', marginRight: '.5rem' }} />
 
-          <Keywords editable={editable} />
+            <div>{sandbox.team.name}</div>
+          </Item>
+        </Tooltip>
+      )}
 
-          <Group>
-            <Item>
-              <PropertyName>Privacy</PropertyName>
-              <PropertyValue>
-                <PrivacyContainer>
-                  {editable ? (
-                    <>
-                      <PrivacySelect
-                        value={sandbox.privacy}
-                        disabled={!isPatron}
-                        onChange={event =>
-                          sandboxPrivacyChanged({
-                            privacy: Number(event.target.value),
-                          })
-                        }
-                      >
-                        <option value={0}>Public</option>
-                        {isPatron && (
-                          <option value={1}>
-                            Unlisted (only available by url)
-                          </option>
-                        )}
-                        {!isServer && isPatron && (
-                          <option value={2}>Private</option>
-                        )}
-                      </PrivacySelect>
-                    </>
-                  ) : (
-                    <PrivacyStatus privacy={sandbox.privacy} />
-                  )}
-                </PrivacyContainer>
-              </PropertyValue>
-            </Item>
+      {sandbox.git && (
+        <Item>
+          <GithubBadge
+            url={githubRepoUrl(sandbox.git)}
+            username={sandbox.git.username}
+            repo={sandbox.git.repo}
+            branch={sandbox.git.branch}
+          />
+        </Item>
+      )}
 
-            {!isPatron && (
-              <Explanation style={{ marginTop: '-1rem' }}>
-                You can change privacy of a sandbox as a{' '}
-                <a href={patronUrl()} rel="noopener noreferrer" target="_blank">
-                  patron
-                </a>
-                .
-              </Explanation>
-            )}
+      <StatsContainer>
+        <Stats />
+      </StatsContainer>
 
-            {editable && <Frozen />}
+      <Keywords editable={editable} />
 
-            {sandbox.forkedFromSandbox && (
-              <Item>
-                <PropertyName>Forked From</PropertyName>
-                <PropertyValue>
-                  <TemplateStyledLink
-                    to={sandboxUrl(sandbox.forkedFromSandbox)}
+      <Group>
+        <Item>
+          <PropertyName>Privacy</PropertyName>
+
+          <PropertyValue>
+            <PrivacyContainer>
+              {editable ? (
+                <>
+                  <PrivacySelect
+                    value={sandbox.privacy}
+                    disabled={!isPatron}
+                    onChange={event =>
+                      sandboxPrivacyChanged({
+                        privacy: Number(event.target.value),
+                      })
+                    }
                   >
-                    {getSandboxName(sandbox.forkedFromSandbox)}
-                  </TemplateStyledLink>
-                </PropertyValue>
-              </Item>
-            )}
+                    <option value={0}>Public</option>
 
-            <Item>
-              <PropertyName>
-                Environment{' '}
-                <Tooltip
-                  boundary="viewport"
-                  interactive
-                  content={
-                    <>
-                      The environment determines how a sandbox is executed, you
-                      can find more info{' '}
-                      <a target="_blank" href="/docs/environment">
-                        here
-                      </a>
-                      .
-                    </>
-                  }
-                >
-                  <Icon />
-                </Tooltip>
-              </PropertyName>
-              <PropertyValue>
-                <BundlerLink href={template.url}>
-                  {sandbox.template}
-                </BundlerLink>
-              </PropertyValue>
-            </Item>
-          </Group>
+                    {isPatron && (
+                      <option value={1}>
+                        Unlisted (only available by url)
+                      </option>
+                    )}
 
-          {editable && <SandboxConfig />}
-        </Container>
-      );
-    }
-  )
-);
+                    {!template.isServer && isPatron && (
+                      <option value={2}>Private</option>
+                    )}
+                  </PrivacySelect>
+                </>
+              ) : (
+                <PrivacyStatus privacy={sandbox.privacy} />
+              )}
+            </PrivacyContainer>
+          </PropertyValue>
+        </Item>
+
+        {!isPatron && (
+          <Explanation style={{ marginTop: '-1rem' }}>
+            You can change privacy of a sandbox as a{' '}
+            <a href={patronUrl()} rel="noopener noreferrer" target="_blank">
+              patron
+            </a>
+            .
+          </Explanation>
+        )}
+
+        {editable && <Frozen />}
+
+        {sandbox.forkedFromSandbox && (
+          <Item>
+            <PropertyName>Forked From</PropertyName>
+            <PropertyValue>
+              <TemplateStyledLink to={sandboxUrl(sandbox.forkedFromSandbox)}>
+                {getSandboxName(sandbox.forkedFromSandbox)}
+              </TemplateStyledLink>
+            </PropertyValue>
+          </Item>
+        )}
+
+        <Item>
+          <PropertyName>
+            Environment{' '}
+            <Tooltip
+              boundary="viewport"
+              interactive
+              content={
+                <>
+                  The environment determines how a sandbox is executed, you can
+                  find more info{' '}
+                  <a target="_blank" href="/docs/environment">
+                    here
+                  </a>
+                  .
+                </>
+              }
+            >
+              <Icon />
+            </Tooltip>
+          </PropertyName>
+          <PropertyValue>
+            <BundlerLink href={template.url}>{sandbox.template}</BundlerLink>
+          </PropertyValue>
+        </Item>
+      </Group>
+
+      {editable && <SandboxConfig />}
+    </Container>
+  );
+};
