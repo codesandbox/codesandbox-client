61d73524f0fba4ba24ca69804d66ca4f99e2359a
Ives van Hoorne <Ives.v.h@gmail.com>
2019-10-03 14:55:02 +0200

    Add transforms for modules and directories (#2624)

--- packages/app/src/app/overmind/effects/utils/sandbox.ts
+++ packages/app/src/app/overmind/effects/utils/sandbox.ts
@@ -1,20 +1,28 @@
-import { Sandbox } from '@codesandbox/common/lib/types';
+import { Sandbox, Module, Directory } from '@codesandbox/common/lib/types';
+
+export function transformModule(module: Module) {
+  return {
+    ...module,
+    savedCode: null,
+    isNotSynced: false,
+    errors: [],
+    corrections: [],
+    type: 'file' as 'file',
+  };
+}
+
+export function transformDirectory(directory: Directory) {
+  return {
+    ...directory,
+    type: 'directory' as 'directory',
+  };
+}
 
 export function transformSandbox(sandbox: Sandbox) {
   // We need to add client side properties for tracking
   return {
     ...sandbox,
-    modules: sandbox.modules.map(module => ({
-      ...module,
-      savedCode: null,
-      isNotSynced: false,
-      errors: [],
-      corrections: [],
-      type: 'file' as 'file',
-    })),
-    directories: sandbox.directories.map(directory => ({
-      ...directory,
-      type: 'directory' as 'directory',
-    })),
+    modules: sandbox.modules.map(transformModule),
+    directories: sandbox.directories.map(transformDirectory),
   };
 }
