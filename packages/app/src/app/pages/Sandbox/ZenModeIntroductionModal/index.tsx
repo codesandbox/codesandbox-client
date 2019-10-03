8522867dd87dda43964f77b7b0c4f5c70ca5476b
Michaël De Boey <info@michaeldeboey.be>
2019-10-03 16:31:49 +0200

    🔨 Switch ZenModeIntroductionModal to use useOvermind

--- packages/app/src/app/pages/Sandbox/ZenModeIntroductionModal/index.tsx
+++ packages/app/src/app/pages/Sandbox/ZenModeIntroductionModal/index.tsx
@@ -1,11 +1,11 @@
-import React, { FunctionComponent } from 'react';
-import { useOvermind } from 'app/overmind';
-import { Button } from '@codesandbox/common/lib/components/Button';
 import Row from '@codesandbox/common/lib/components/flex/Row';
+import React, { FunctionComponent } from 'react';
 
-import { Container, Heading, Explanation } from './elements';
+import { useOvermind } from 'app/overmind';
 
-export const ZenModeIntroduction: FunctionComponent = () => {
+import { Button, Container, Explanation, Heading } from './elements';
+
+export const ZenModeIntroductionModal: FunctionComponent = () => {
   const {
     actions: { modalClosed },
   } = useOvermind();
@@ -13,17 +13,16 @@ export const ZenModeIntroduction: FunctionComponent = () => {
   return (
     <Container>
       <Heading>Zen Mode Explained</Heading>
+
       <Explanation>
         Zen Mode is perfect for giving instruction videos and presentations. You
-        can toggle the sidebar by double tapping <code>shift</code>. You can
-        leave Zen Mode by hovering over the file name above the editor and
-        clicking the icon on the right.
+        can toggle the sidebar by double tapping <kbd>shift</kbd>. You can leave
+        Zen Mode by hovering over the file name above the editor and clicking
+        the icon on the right.
       </Explanation>
 
       <Row justifyContent="space-around">
-        <Button style={{ marginRight: '.5rem' }} onClick={() => modalClosed()}>
-          Close
-        </Button>
+        <Button onClick={() => modalClosed()}>Close</Button>
       </Row>
     </Container>
   );
