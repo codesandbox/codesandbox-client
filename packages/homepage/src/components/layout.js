7d8e9d2d74de0c7f52425ecfdd1b310f389f1577
Yeison Daza <jahir045@gmail.com>
2019-10-08 07:15:42 -0500

    use nav

--- packages/homepage/src/components/layout.js
+++ packages/homepage/src/components/layout.js
@@ -4,7 +4,7 @@ import { ThemeProvider } from 'styled-components';
 import theme from '@codesandbox/common/lib/theme';
 import Navigation from '@codesandbox/common/lib/components/Navigation';
 import Footer from '@codesandbox/common/lib/components/Footer';
-
+import { VisuallyHidden } from './style';
 import '../css/typography.css';
 import '../css/global.css';
 
@@ -12,10 +12,15 @@ const TemplateWrapper = ({ children }) => (
   <ThemeProvider theme={theme}>
     <div>
       <div style={{ position: 'absolute', left: 0, right: 0, zIndex: 10 }}>
+        <VisuallyHidden as="a" href="#main">
+          Skip to main content
+        </VisuallyHidden>
         <Navigation />
       </div>
 
-      <main style={{ maxWidth: '100vw', overflowX: 'hidden' }}>{children}</main>
+      <main style={{ maxWidth: '100vw', overflowX: 'hidden' }} id="main">
+        {children}
+      </main>
 
       <Footer />
     </div>
