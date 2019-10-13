c32d2c5dd96adce189d8e97946c1d487f187bd03
Ravi Soni <soniravi829@gmail.com>
2019-10-13 18:00:23 +0530

    (Refactor) Destructure namespace from actions

--- packages/app/src/app/pages/common/Modals/DeleteDeploymentModal/index.tsx
+++ packages/app/src/app/pages/common/Modals/DeleteDeploymentModal/index.tsx
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, { FunctionComponent } from 'react';
 import { useOvermind } from 'app/overmind';
 import { Alert } from 'app/components/Alert';
 
-export const DeleteDeploymentModal: React.FC = () => {
-  const { actions } = useOvermind();
+export const DeleteDeploymentModal: FunctionComponent = () => {
+  const {
+    actions: {
+      modalClosed,
+      deployment: { deleteDeployment },
+    },
+  } = useOvermind();
+
   return (
     <Alert
       title="Delete Deployment"
       body="Are you sure you want to delete this Deployment?"
-      onCancel={() => actions.modalClosed()}
-      onConfirm={() => actions.deployment.deleteDeployment()}
+      onCancel={() => modalClosed()}
+      onConfirm={() => deleteDeployment()}
     />
   );
 };
