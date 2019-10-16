dbf3b6a1ec2469fea18e0a499abcef1fb7611398
Christine Oo <ootjinvoon@gmail.com>
2019-10-16 18:56:00 +0900

    rename file extension to .tsx

--- packages/app/src/app/pages/common/Modals/PickSandboxModal/index.tsx
+++ packages/app/src/app/pages/common/Modals/PickSandboxModal/index.tsx
@@ -1,89 +1,93 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
+import { inject, observer } from 'app/componentConnectors';
 
 import { Button } from '@codesandbox/common/lib/components/Button';
 import Row from '@codesandbox/common/lib/components/flex/Row';
 import Input, { TextArea } from '@codesandbox/common/lib/components/Input';
-
-import { useOvermind } from 'app/overmind';
 
 import { Container } from '../LiveSessionEnded/elements';
 import { Heading, Explanation } from '../elements';
 
 import { Field, Label } from './elements';
 
-export const PickSandboxModal: React.FC = () => {
-  const {
-    state: {
-      explore: { pickedSandboxDetails },
-    },
-    actions,
-  } = useOvermind();
+class PickSandboxModal extends Component {
+  state = {
+    title: this.props.store.explore.pickedSandboxDetails.title || '',
+    description:
+      this.props.store.explore.pickedSandboxDetails.description || '',
+  };
 
-  const [title, setTitle] = useState(pickedSandboxDetails.title || '');
-  const [description, setDescription] = useState(
-    pickedSandboxDetails.description || ''
-  );
+  onChange = e =>
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
 
-  const { id } = pickedSandboxDetails;
+  render() {
+    const { signals, store } = this.props;
+    const { id } = store.explore.pickedSandboxDetails;
+    const { title, description } = this.state;
 
-  return (
-    <Container>
-      <Heading>Pick this sandbox</Heading>
-      <Explanation>
-        Please add a title and description to this sandbox if none exists or you
-        think you have a better description for it. This title and description
-        will be the ones used in the explore page.
-      </Explanation>
-      <form
-        onSubmit={e => {
-          e.preventDefault();
-          actions.explore.pickSandbox({
-            id,
-            title,
-            description,
-          });
-        }}
-      >
-        <Field>
-          <Label htmlFor="title">Sandbox name</Label>
-          <Input
-            style={{
-              width: '100%',
-            }}
-            value={title}
-            onChange={event => setTitle(event.target.value)}
-            name="title"
-            id="title"
-            required
-          />
-        </Field>
-        <Field>
-          <Label htmlFor="description">Sandbox Description</Label>
-          <TextArea
-            style={{
-              width: '100%',
-            }}
-            value={description}
-            onChange={event => setDescription(event.target.value)}
-            name="description"
-            id="description"
-            required
-            rows={3}
-          />
-        </Field>
+    return (
+      <Container>
+        <Heading>Pick this sandbox</Heading>
+        <Explanation>
+          Please add a title and description to this sandbox if none exists or
+          you think you have a better description for it. This title and
+          description will be the ones used in the explore page.
+        </Explanation>
+        <form
+          onSubmit={e => {
+            e.preventDefault();
+            signals.explore.pickSandbox({
+              id,
+              title,
+              description,
+            });
+          }}
+        >
+          <Field>
+            <Label htmlFor="title">Sandbox name</Label>
+            <Input
+              style={{
+                width: '100%',
+              }}
+              value={title}
+              onChange={this.onChange}
+              name="title"
+              id="title"
+              required
+            />
+          </Field>
+          <Field>
+            <Label htmlFor="description">Sandbox Description</Label>
+            <TextArea
+              style={{
+                width: '100%',
+              }}
+              value={description}
+              onChange={this.onChange}
+              name="description"
+              id="description"
+              required
+              rows="3"
+            />
+          </Field>
 
-        <Row justifyContent="space-around">
-          <Button type="submit">
-            Ship it{' '}
-            <span role="img" aria-label="rocket">
-              🚀
-            </span>
-          </Button>
-          <Button danger onClick={() => actions.modalClosed()}>
-            Cancel
-          </Button>
-        </Row>
-      </form>
-    </Container>
-  );
-};
+          <Row justifyContent="space-around">
+            <Button type="submit">
+              Ship it{' '}
+              <span role="img" aria-label="rocket">
+                🚀
+              </span>
+            </Button>
+            <Button danger onClick={() => signals.modalClosed()}>
+              Cancel
+            </Button>
+          </Row>
+        </form>
+      </Container>
+    );
+  }
+}
+
+export default inject('signals', 'store')(observer(PickSandboxModal));
