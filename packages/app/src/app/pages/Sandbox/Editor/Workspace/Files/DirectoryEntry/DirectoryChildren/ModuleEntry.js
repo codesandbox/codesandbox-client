203681ae31f0607c7eef6b0e6729acb736495b97
Yeison Daza <jahir045@gmail.com>
2019-10-17 06:59:58 -0500

    add validation

--- packages/app/src/app/pages/Sandbox/Editor/Workspace/Files/DirectoryEntry/DirectoryChildren/ModuleEntry.js
+++ packages/app/src/app/pages/Sandbox/Editor/Workspace/Files/DirectoryEntry/DirectoryChildren/ModuleEntry.js
@@ -4,60 +4,52 @@ import getType from 'app/utils/get-type.ts';
 import React from 'react';
 
 import Entry from '../Entry';
-import validateTitle from '../validateTitle';
 
-class ModuleEntry extends React.PureComponent {
-  validateTitle = (id, title) => {
-    const { directories, modules } = this.props.store.editor.currentSandbox;
-    return !!validateTitle(id, title, [...directories, ...modules]);
-  };
+const ModuleEntry = ({
+  store,
+  module,
+  setCurrentModule,
+  markTabsNotDirty,
+  depth,
+  renameModule,
+  deleteEntry,
+  discardModuleChanges,
+  getModulePath,
+  renameValidator,
+}) => {
+  const { currentModuleShortid } = store.editor;
+  const mainModuleId = store.editor.mainModule.id;
 
-  render() {
-    const {
-      store,
-      module,
-      setCurrentModule,
-      markTabsNotDirty,
-      depth,
-      renameModule,
-      deleteEntry,
-      discardModuleChanges,
-      getModulePath,
-    } = this.props;
-    const { currentModuleShortid } = store.editor;
-    const mainModuleId = store.editor.mainModule.id;
+  const isActive = module.shortid === currentModuleShortid;
+  const isMainModule = module.id === mainModuleId;
+  const type = getType(module.title);
+  const hasError = module.errors.length;
 
-    const isActive = module.shortid === currentModuleShortid;
-    const isMainModule = module.id === mainModuleId;
-    const type = getType(module.title);
-    const hasError = module.errors.length;
+  const liveUsers = store.live.liveUsersByModule[module.shortid] || [];
 
-    const liveUsers = store.live.liveUsersByModule[module.shortid] || [];
+  const isNotSynced = module.savedCode && module.code !== module.savedCode;
 
-    const isNotSynced = module.savedCode && module.code !== module.savedCode;
-
-    return (
-      <Entry
-        id={module.id}
-        shortid={module.shortid}
-        title={module.title}
-        rightColors={liveUsers.map(([a, b, c]) => `rgb(${a}, ${b}, ${c})`)}
-        depth={depth + 1}
-        active={isActive}
-        type={type || 'function'}
-        rename={renameModule}
-        deleteEntry={deleteEntry}
-        isNotSynced={isNotSynced}
-        renameValidator={this.validateTitle}
-        setCurrentModule={setCurrentModule}
-        isMainModule={isMainModule}
-        moduleHasError={hasError}
-        markTabsNotDirty={markTabsNotDirty}
-        discardModuleChanges={discardModuleChanges}
-        getModulePath={getModulePath}
-      />
-    );
-  }
-}
+  return (
+    <Entry
+      id={module.id}
+      shortid={module.shortid}
+      title={module.title}
+      rightColors={liveUsers.map(([a, b, c]) => `rgb(${a}, ${b}, ${c})`)}
+      depth={depth + 1}
+      active={isActive}
+      type={type || 'function'}
+      rename={renameModule}
+      deleteEntry={deleteEntry}
+      isNotSynced={isNotSynced}
+      renameValidator={renameValidator}
+      setCurrentModule={setCurrentModule}
+      isMainModule={isMainModule}
+      moduleHasError={hasError}
+      markTabsNotDirty={markTabsNotDirty}
+      discardModuleChanges={discardModuleChanges}
+      getModulePath={getModulePath}
+    />
+  );
+};
 
 export default inject('store')(observer(ModuleEntry));
