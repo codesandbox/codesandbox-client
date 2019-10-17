a6e318e4a127a0d38f37fe2d15077956a5a15deb
siddharthkp <siddharth.kshetrapal@gmail.com>
2019-10-17 14:03:58 +0200

    bring the app back

--- packages/app/src/embed/components/App/index.js
+++ packages/app/src/embed/components/App/index.js
@@ -1,21 +1,21 @@
 // @flow
 import * as React from 'react';
-// import { ThemeProvider } from 'styled-components';
+import { ThemeProvider } from 'styled-components';
 import { camelizeKeys } from 'humps';
-// import getTemplateDefinition from '@codesandbox/common/lib/templates';
+import getTemplateDefinition from '@codesandbox/common/lib/templates';
 import type { Module, Sandbox } from '@codesandbox/common/lib/types';
-// import Centered from '@codesandbox/common/lib/components/flex/Centered';
+import Centered from '@codesandbox/common/lib/components/flex/Centered';
 import { getSandboxOptions } from '@codesandbox/common/lib/url';
 import {
   findCurrentModule,
   findMainModule,
 } from '@codesandbox/common/lib/sandbox/modules';
-// import { Title } from 'app/components/Title';
-// import { SubTitle } from 'app/components/SubTitle';
-// import Header from '../Header';
-// import Content from '../Content';
+import { Title } from 'app/components/Title';
+import { SubTitle } from 'app/components/SubTitle';
+import Header from '../Header';
+import Content from '../Content';
 import Sidebar from '../Sidebar';
-// import { Container, Fullscreen, Moving } from './elements';
+import { Container, Fullscreen, Moving } from './elements';
 
 // Okay, this looks veeeery strange, we need this because Webpack has a bug currently
 // that makes it think we have core-js/es6/map available in embed, but we don't.
@@ -243,181 +243,172 @@ export default class App extends React.PureComponent<
     }
   };
 
-  // toggleLike = () => {
-  //   const jwt = this.jwt();
-  //
-  //   if (this.state.sandbox.userLiked && jwt) {
-  //     this.setState(s => ({
-  //       sandbox: {
-  //         ...s.sandbox,
-  //         userLiked: false,
-  //         likeCount: s.sandbox.likeCount - 1,
-  //       },
-  //     }));
-  //     fetch(`/api/v1/sandboxes/${this.state.sandbox.id}/likes`, {
-  //       method: 'DELETE',
-  //       headers: {
-  //         'Content-Type': 'application/json',
-  //         Authorization: `Bearer ${jwt}`,
-  //       },
-  //       body: JSON.stringify({
-  //         id: this.state.sandbox.id,
-  //       }),
-  //     })
-  //       .then(x => x.json())
-  //       .then(() => {
-  //         this.setState(s => ({
-  //           sandbox: {
-  //             ...s.sandbox,
-  //             userLiked: false,
-  //             likeCount: s.sandbox.likeCount - 1,
-  //           },
-  //         }));
-  //       })
-  //       .catch(() => {
-  //         this.setState(s => ({
-  //           sandbox: {
-  //             ...s.sandbox,
-  //             userLiked: true,
-  //             likeCount: s.sandbox.likeCount + 1,
-  //           },
-  //         }));
-  //       });
-  //   } else {
-  //     this.setState(s => ({
-  //       sandbox: {
-  //         ...s.sandbox,
-  //         userLiked: true,
-  //         likeCount: s.sandbox.likeCount + 1,
-  //       },
-  //     }));
-  //     fetch(`/api/v1/sandboxes/${this.state.sandbox.id}/likes`, {
-  //       method: 'POST',
-  //       headers: {
-  //         'Content-Type': 'application/json',
-  //         Authorization: `Bearer ${jwt}`,
-  //       },
-  //     })
-  //       .then(x => x.json())
-  //       .then(res => {
-  //         this.setState(s => ({
-  //           sandbox: { ...s.sandbox, userLiked: true, likeCount: res.count },
-  //         }));
-  //       })
-  //       .catch(() => {
-  //         this.setState(s => ({
-  //           sandbox: {
-  //             ...s.sandbox,
-  //             userLiked: false,
-  //             likeCount: s.sandbox.likeCount - 1,
-  //           },
-  //         }));
-  //       });
-  //   }
-  // };
+  toggleLike = () => {
+    const jwt = this.jwt();
 
-  // content = () => {
-  //   if (this.state.notFound) {
-  //     return (
-  //       <Centered vertical horizontal>
-  //         <Title delay={0.1}>Not Found</Title>
-  //         <SubTitle delay={0.05}>
-  //           We could not find the sandbox you
-  //           {"'"}
-  //           re looking for.
-  //         </SubTitle>
-  //       </Centered>
-  //     );
-  //   }
-  //
-  //   const { sandbox } = this.state;
-  //
-  //   if (!sandbox) {
-  //     return (
-  //       <Centered vertical horizontal>
-  //         <Title delay={0.3}>Loading Sandbox...</Title>
-  //       </Centered>
-  //     );
-  //   }
-  //
-  //   const {
-  //     showEditor,
-  //     verticalMode,
-  //     showPreview,
-  //     previewWindow,
-  //     isInProjectView,
-  //     runOnClick,
-  //   } = this.state;
-  //
-  //   return (
-  //     <ThemeProvider
-  //       theme={{
-  //         templateColor: getTemplateDefinition(sandbox.template).color,
-  //       }}
-  //     >
-  //       <Container>
-  //         <Header
-  //           showEditor={showEditor}
-  //           showPreview={showPreview}
-  //           setEditorView={this.setEditorView}
-  //           setPreviewView={this.setPreviewView}
-  //           setMixedView={this.setMixedView}
-  //           sandbox={sandbox}
-  //           toggleSidebar={this.toggleSidebar}
-  //           toggleLike={this.jwt() && this.toggleLike}
-  //           liked={sandbox.userLiked}
-  //         />
-  //         <Content
-  //           showEditor={showEditor}
-  //           showPreview={showPreview}
-  //           previewWindow={previewWindow}
-  //           isInProjectView={isInProjectView}
-  //           setProjectView={this.setProjectView}
-  //           sandbox={sandbox}
-  //           currentModule={this.getCurrentModuleFromPath(sandbox)}
-  //           hideNavigation={this.state.hideNavigation}
-  //           autoResize={this.state.autoResize}
-  //           fontSize={this.state.fontSize}
-  //           initialPath={this.state.initialPath}
-  //           setCurrentModule={this.setCurrentModule}
-  //           useCodeMirror={this.state.useCodeMirror}
-  //           enableEslint={this.state.enableEslint}
-  //           editorSize={this.state.editorSize}
-  //           highlightedLines={this.state.highlightedLines}
-  //           forceRefresh={this.state.forceRefresh}
-  //           expandDevTools={this.state.expandDevTools}
-  //           tabs={this.state.tabs}
-  //           runOnClick={runOnClick}
-  //           verticalMode={verticalMode}
-  //         />
-  //       </Container>
-  //     </ThemeProvider>
-  //   );
-  // };
+    if (this.state.sandbox.userLiked && jwt) {
+      this.setState(s => ({
+        sandbox: {
+          ...s.sandbox,
+          userLiked: false,
+          likeCount: s.sandbox.likeCount - 1,
+        },
+      }));
+      fetch(`/api/v1/sandboxes/${this.state.sandbox.id}/likes`, {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${jwt}`,
+        },
+        body: JSON.stringify({
+          id: this.state.sandbox.id,
+        }),
+      })
+        .then(x => x.json())
+        .then(() => {
+          this.setState(s => ({
+            sandbox: {
+              ...s.sandbox,
+              userLiked: false,
+              likeCount: s.sandbox.likeCount - 1,
+            },
+          }));
+        })
+        .catch(() => {
+          this.setState(s => ({
+            sandbox: {
+              ...s.sandbox,
+              userLiked: true,
+              likeCount: s.sandbox.likeCount + 1,
+            },
+          }));
+        });
+    } else {
+      this.setState(s => ({
+        sandbox: {
+          ...s.sandbox,
+          userLiked: true,
+          likeCount: s.sandbox.likeCount + 1,
+        },
+      }));
+      fetch(`/api/v1/sandboxes/${this.state.sandbox.id}/likes`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${jwt}`,
+        },
+      })
+        .then(x => x.json())
+        .then(res => {
+          this.setState(s => ({
+            sandbox: { ...s.sandbox, userLiked: true, likeCount: res.count },
+          }));
+        })
+        .catch(() => {
+          this.setState(s => ({
+            sandbox: {
+              ...s.sandbox,
+              userLiked: false,
+              likeCount: s.sandbox.likeCount - 1,
+            },
+          }));
+        });
+    }
+  };
+
+  content = () => {
+    if (this.state.notFound) {
+      return (
+        <Centered vertical horizontal>
+          <Title delay={0.1}>Not Found</Title>
+          <SubTitle delay={0.05}>
+            We could not find the sandbox you
+            {"'"}
+            re looking for.
+          </SubTitle>
+        </Centered>
+      );
+    }
+
+    const { sandbox } = this.state;
+
+    if (!sandbox) {
+      return (
+        <Centered vertical horizontal>
+          <Title delay={0.3}>Loading Sandbox...</Title>
+        </Centered>
+      );
+    }
+
+    const {
+      showEditor,
+      verticalMode,
+      showPreview,
+      previewWindow,
+      isInProjectView,
+      runOnClick,
+    } = this.state;
+
+    return (
+      <ThemeProvider
+        theme={{
+          templateColor: getTemplateDefinition(sandbox.template).color,
+        }}
+      >
+        <Container>
+          <Header
+            showEditor={showEditor}
+            showPreview={showPreview}
+            setEditorView={this.setEditorView}
+            setPreviewView={this.setPreviewView}
+            setMixedView={this.setMixedView}
+            sandbox={sandbox}
+            toggleSidebar={this.toggleSidebar}
+            toggleLike={this.jwt() && this.toggleLike}
+            liked={sandbox.userLiked}
+          />
+          <Content
+            showEditor={showEditor}
+            showPreview={showPreview}
+            previewWindow={previewWindow}
+            isInProjectView={isInProjectView}
+            setProjectView={this.setProjectView}
+            sandbox={sandbox}
+            currentModule={this.getCurrentModuleFromPath(sandbox)}
+            hideNavigation={this.state.hideNavigation}
+            autoResize={this.state.autoResize}
+            fontSize={this.state.fontSize}
+            initialPath={this.state.initialPath}
+            setCurrentModule={this.setCurrentModule}
+            useCodeMirror={this.state.useCodeMirror}
+            enableEslint={this.state.enableEslint}
+            editorSize={this.state.editorSize}
+            highlightedLines={this.state.highlightedLines}
+            forceRefresh={this.state.forceRefresh}
+            expandDevTools={this.state.expandDevTools}
+            tabs={this.state.tabs}
+            runOnClick={runOnClick}
+            verticalMode={verticalMode}
+          />
+        </Container>
+      </ThemeProvider>
+    );
+  };
 
   render() {
     const { sandbox } = this.state;
 
     return (
-      sandbox && (
-        <Sidebar
-          setCurrentModule={this.setCurrentModule}
-          currentModule={this.getCurrentModuleFromPath(sandbox).id}
-          sandbox={sandbox}
-        />
-      )
+      <Fullscreen sidebarOpen={this.state.sidebarOpen}>
+        {sandbox && (
+          <Sidebar
+            setCurrentModule={this.setCurrentModule}
+            currentModule={this.getCurrentModuleFromPath(sandbox).id}
+            sandbox={sandbox}
+          />
+        )}
+        <Moving sidebarOpen={this.state.sidebarOpen}>{this.content()}</Moving>
+      </Fullscreen>
     );
   }
 }
-/*
-<Fullscreen sidebarOpen={this.state.sidebarOpen}>
-  {sandbox && (
-    <Sidebar
-      setCurrentModule={this.setCurrentModule}
-      currentModule={this.getCurrentModuleFromPath(sandbox).id}
-      sandbox={sandbox}
-    />
-  )}
-  <Moving sidebarOpen={this.state.sidebarOpen}>{this.content()}</Moving>
-</Fullscreen>
-*/
