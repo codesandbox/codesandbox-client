3321ac5efc1fbd88e15ac97f3cb5b68db06aeada
vanya829 <vanya829@gmail.com>
2019-10-17 14:34:49 +0300

    🔨 Switch LikeHeart to use useOvermind (#2635)
    * Add @vanya829 as a contributor
    
    * refactor LikeHeart to functional component
    
    * convert likeCount to string to match typing
    
    * refactor LikeHeart to functional component
    
    convert likeCount to string to match typing

--- packages/app/src/app/pages/common/LikeHeart/index.tsx
+++ packages/app/src/app/pages/common/LikeHeart/index.tsx
@@ -1,7 +1,7 @@
 import Tooltip from '@codesandbox/common/lib/components/Tooltip';
 import { Sandbox } from '@codesandbox/common/lib/types';
 import noop from 'lodash/noop';
-import { inject, hooksObserver } from 'app/componentConnectors';
+import { useOvermind } from 'app/overmind';
 import React from 'react';
 // @ts-ignore
 import HeartIcon from '-!svg-react-loader!@codesandbox/common/lib/icons/heart-open.svg'; // eslint-disable-line import/no-webpack-loader-syntax
@@ -19,7 +19,7 @@ const MaybeTooltip = ({ loggedIn, disableTooltip, title, children }) =>
     children
   );
 
-interface Props {
+interface ILikeHeartProps {
   sandbox: Sandbox;
   className?: string;
   colorless?: boolean;
@@ -27,50 +27,46 @@ interface Props {
   style?: React.CSSProperties;
   disableTooltip?: boolean;
   highlightHover?: boolean;
-  store: any;
-  signals: any;
 }
 
-export const LikeHeart = inject('store', 'signals')(
-  hooksObserver(
-    ({
-      sandbox,
-      className,
-      colorless,
-      text,
-      style,
-      disableTooltip,
-      highlightHover,
-      store: { isLoggedIn },
-      signals: { editor },
-    }: Props) => (
-      <Container
-        style={style}
-        hasText={text !== undefined}
+export const LikeHeart: React.FC<ILikeHeartProps> = ({
+  sandbox,
+  className,
+  colorless,
+  text,
+  style,
+  disableTooltip,
+  highlightHover,
+}) => {
+  const {
+    state: { isLoggedIn },
+    actions: { editor },
+  } = useOvermind();
+  return (
+    <Container
+      style={style}
+      hasText={text !== undefined}
+      loggedIn={isLoggedIn}
+      liked={sandbox.userLiked}
+      className={className}
+      highlightHover={highlightHover}
+      onClick={
+        isLoggedIn ? () => editor.likeSandboxToggled({ id: sandbox.id }) : noop
+      }
+    >
+      <MaybeTooltip
         loggedIn={isLoggedIn}
-        liked={sandbox.userLiked}
-        className={className}
-        highlightHover={highlightHover}
-        onClick={
-          isLoggedIn
-            ? () => editor.likeSandboxToggled({ id: sandbox.id })
-            : noop
-        }
+        disableTooltip={disableTooltip}
+        title={sandbox.userLiked ? 'Undo like' : 'Like'}
       >
-        <MaybeTooltip
-          loggedIn={isLoggedIn}
-          disableTooltip={disableTooltip}
-          title={sandbox.userLiked ? 'Undo like' : 'Like'}
-        >
-          {sandbox.userLiked ? (
-            <FullHeartIcon style={colorless ? null : { color: '#E01F4E' }} />
-          ) : (
-            <HeartIcon />
-          )}
+        {sandbox.userLiked ? (
+          <FullHeartIcon style={colorless ? null : { color: '#E01F4E' }} />
+        ) : (
+          <HeartIcon />
+        )}
 
-          {text && <span>{text}</span>}
-        </MaybeTooltip>
-      </Container>
-    )
-  )
-);
+        {text && <span>{text}</span>}
+      </MaybeTooltip>
+    </Container>
+  );
+};
