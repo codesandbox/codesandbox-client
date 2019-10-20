8f59b5a27db8672cfbce576c74d2e2b25b14a3ec
Het Patel <patel.het33@gmail.com>
2019-10-20 14:59:14 +0530

    🔨Refactored 🧠 Overmind Hacktober | /app/pages/common/Modals/P… (#2755)
    * refactor app/pages/common/Modals/PRModal/index.tsx : use overmind and convert to tsx
    
    * removed packages/app/src/app/pages/common/Modals/PRModal/index.js

--- packages/app/src/app/pages/common/Modals/PRModal/index.tsx
+++ packages/app/src/app/pages/common/Modals/PRModal/index.tsx
@@ -1,19 +1,26 @@
 import React from 'react';
-import { inject, observer } from 'app/componentConnectors';
+import { useOvermind } from 'app/overmind';
 import { GitProgress } from 'app/components/GitProgress';
 
-function PRModal({ store }) {
+const PRModal: React.FC = () => {
   let result = null;
 
-  if (!store.git.isCreatingPr) {
-    const newUrl = store.git.pr.prURL;
+  const {
+    state: {
+      git: {
+        isCreatingPr,
+        pr: { prURL },
+      },
+    },
+  } = useOvermind();
 
+  if (!isCreatingPr) {
     result = (
       <div>
         Done! We{"'"}ll now open the new sandbox of this PR and GitHub in 3
         seconds...
         <div style={{ fontSize: '.875rem', marginTop: '1rem' }}>
-          <a href={newUrl} target="_blank" rel="noreferrer noopener">
+          <a href={prURL} target="_blank" rel="noreferrer noopener">
             Click here if nothing happens.
           </a>
         </div>
@@ -27,6 +34,6 @@ function PRModal({ store }) {
       message="Forking Repository & Creating PR..."
     />
   );
-}
+};
 
-export default inject('store')(observer(PRModal));
+export default PRModal;
