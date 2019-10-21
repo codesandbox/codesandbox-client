7c2053f014def627c47d2fc4b27ad25b6a98a9a3
Het Patel <patel.het33@gmail.com>
2019-10-21 19:13:25 +0530

    Apply suggestions from code review
    Review Suggestions
    
    Co-Authored-By: Drake Costa <drake@saeris.io>

--- packages/app/src/app/pages/common/Modals/SearchDependenciesModal/index.tsx
+++ packages/app/src/app/pages/common/Modals/SearchDependenciesModal/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useOvermind } from 'app/overmind';
 import SearchDependencies from 'app/pages/Sandbox/SearchDependencies';
 
-const SearchDependenciesModal: React.FC = () => {
+export const SearchDependenciesModal: React.FC = () => {
   const {
     actions: {
       editor: { addNpmDependency },
@@ -15,4 +15,3 @@ const SearchDependenciesModal: React.FC = () => {
   );
 };
 
-export default SearchDependenciesModal;
