f6689e60c8888aea727e47c5a9f84ccb03ec8a2b
yevhen orlov <yevhenorlov@gmail.com>
2019-10-22 15:27:31 +0300

    refactor: Chat to functional component

--- packages/app/src/app/pages/Sandbox/Editor/Workspace/Chat/index.js
+++ packages/app/src/app/pages/Sandbox/Editor/Workspace/Chat/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import styled from 'styled-components';
 import { sortBy, takeRight } from 'lodash-es';
 import { inject, observer } from 'app/componentConnectors';
@@ -22,126 +22,115 @@ const Messages = styled.div`
   flex: 1;
 `;
 
-class ChatComponent extends React.Component {
-  state = {
-    value: '',
-  };
+const ChatComponent = ({ signals, store }) => {
+  const [value, setValue] = useState('');
+  const [height, setHeight] = useState('');
+  const messagesRef = useRef(null);
+  function scrollDown() {
+    if (messagesRef.current) {
+      messagesRef.current.scrollTop = messagesRef.current.scrollHeight;
+    }
+  }
+  useEffect(scrollDown);
 
-  handleKeyDown = (e: KeyboardEvent) => {
+  const handleKeyDown = e => {
     if (e.keyCode === ENTER && !e.shiftKey) {
       e.preventDefault();
       e.stopPropagation();
       // Enter
-      this.props.signals.live.onSendChat({
-        message: this.state.value,
+      signals.live.onSendChat({
+        message: value,
       });
-      this.setState({ value: '' });
-      if (this.messages) {
-        this.messages.scrollTop = this.messages.scrollHeight;
-      }
+      setValue('');
+      scrollDown();
     }
   };
 
-  handleChange = e => {
-    this.setState({ value: e.target.value });
+  const handleChange = e => {
+    setValue(e.target.value);
   };
 
-  componentDidUpdate() {
-    if (this.messages) {
-      this.messages.scrollTop = this.messages.scrollHeight;
-    }
-  }
+  const { messages, users } = store.live.roomInfo.chat;
+  const currentUserId = store.live.liveUserId;
+  const roomInfoUsers = store.live.roomInfo.users;
 
-  componentDidMount() {
-    if (this.messages) {
-      this.messages.scrollTop = this.messages.scrollHeight;
-    }
-  }
-
-  render() {
-    const { store } = this.props;
-    const { messages, users } = store.live.roomInfo.chat;
-    const currentUserId = store.live.liveUserId;
-    const roomInfoUsers = store.live.roomInfo.users;
-
-    return (
-      <Container
-        ref={el => {
-          this.messages = el;
-        }}
-      >
-        <Messages>
-          {messages.length > 0 ? (
-            sortBy(takeRight(messages, 100), 'date').map((message, i) => {
-              const metadata = roomInfoUsers.find(u => u.id === message.userId);
-              const color = metadata
-                ? `rgb(${metadata.color[0]}, ${metadata.color[1]}, ${
-                    metadata.color[2]
-                  })`
-                : '#636363';
-              const name = users.get
-                ? users.get(message.userId)
-                : users[message.userId];
-              return (
-                <div key={message.date}>
-                  {(i === 0 || messages[i - 1].userId !== message.userId) && (
-                    <div
-                      style={{
-                        color,
-                        fontWeight: 600,
-                        marginBottom: '0.25rem',
-                        marginTop: '0.5rem',
-                      }}
-                    >
-                      {name}
-                      {currentUserId === message.userId && ' (you)'}
-                      {!metadata && ' (left)'}
-                    </div>
-                  )}
+  return (
+    <Container
+      ref={el => {
+        messagesRef.current = el;
+      }}
+    >
+      <Messages>
+        {messages.length > 0 ? (
+          sortBy(takeRight(messages, 100), 'date').map((message, i) => {
+            const metadata = roomInfoUsers.find(u => u.id === message.userId);
+            const color = metadata
+              ? `rgb(${metadata.color[0]}, ${metadata.color[1]}, ${
+                  metadata.color[2]
+                })`
+              : '#636363';
+            const name = users.get
+              ? users.get(message.userId)
+              : users[message.userId];
+            return (
+              <div key={message.date}>
+                {(i === 0 || messages[i - 1].userId !== message.userId) && (
                   <div
                     style={{
-                      color: 'rgba(255, 255, 255, 0.7)',
-                      fontWeight: 400,
-                      marginBottom: '.25rem',
+                      color,
+                      fontWeight: 600,
+                      marginBottom: '0.25rem',
+                      marginTop: '0.5rem',
                     }}
                   >
-                    {message.message.split('\n').map(m => (
-                      <span key={m}>
-                        {m}
-                        <br />
-                      </span>
-                    ))}
+                    {name}
+                    {currentUserId === message.userId && ' (you)'}
+                    {!metadata && ' (left)'}
                   </div>
+                )}
+                <div
+                  style={{
+                    color: 'rgba(255, 255, 255, 0.7)',
+                    fontWeight: 400,
+                    marginBottom: '.25rem',
+                  }}
+                >
+                  {message.message.split('\n').map(m => (
+                    <span key={m}>
+                      {m}
+                      <br />
+                    </span>
+                  ))}
                 </div>
-              );
-            })
-          ) : (
-            <div
-              style={{
-                fontStyle: 'italic',
-                color: 'rgba(255, 255, 255, 0.5)',
-              }}
-            >
-              No messages, start sending some!
-            </div>
-          )}
-        </Messages>
-        <AutosizeTextArea
-          useCacheForDOMMeasurements
-          value={this.state.value}
-          onChange={this.handleChange}
-          placeholder="Send a message..."
-          style={{
-            width: '100%',
-            minHeight: this.state.height,
-            marginTop: '0.5rem',
-          }}
-          onKeyDown={this.handleKeyDown}
-          onHeightChange={height => this.setState({ height })}
-        />
-      </Container>
-    );
-  }
-}
+              </div>
+            );
+          })
+        ) : (
+          <div
+            style={{
+              fontStyle: 'italic',
+              color: 'rgba(255, 255, 255, 0.5)',
+            }}
+          >
+            No messages, start sending some!
+          </div>
+        )}
+      </Messages>
+      <AutosizeTextArea
+        useCacheForDOMMeasurements
+        value={value}
+        onChange={handleChange}
+        placeholder="Send a message..."
+        style={{
+          width: '100%',
+          minHeight: height,
+          marginTop: '0.5rem',
+        }}
+        onKeyDown={handleKeyDown}
+        onHeightChange={setHeight}
+      />
+    </Container>
+  );
+};
 
 export const Chat = inject('signals', 'store')(observer(ChatComponent));
