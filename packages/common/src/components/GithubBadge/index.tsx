321441851179dfd70929c74ebc5db870c7e28e9c
Ives van Hoorne <ives.v.h@gmail.com>
2019-10-24 19:46:24 +0200

    Improve showing of commit sha

--- packages/common/src/components/GithubBadge/index.tsx
+++ packages/common/src/components/GithubBadge/index.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useCallback } from 'react';
 import GithubIcon from 'react-icons/lib/go/mark-github';
 import { BorderRadius, Text, Icon, StyledA } from './elements';
 
@@ -7,6 +7,7 @@ type BadgeProps = {
   repo: string;
   url?: string;
   branch: string;
+  commitSha: string;
 };
 
 type DivOrAProps = {
@@ -26,19 +27,36 @@ const GithubBadge: FunctionComponent<BadgeProps> = ({
   repo,
   url,
   branch,
+  commitSha,
   ...props
-}) => (
-  <DivOrA {...props} href={url}>
-    <BorderRadius hasUrl={Boolean(url)}>
-      <Icon>
-        <GithubIcon />
-      </Icon>
-      <Text>
-        {username}/{repo}
-        {branch && branch !== 'master' ? `@${branch}` : ''}
-      </Text>
-    </BorderRadius>
-  </DivOrA>
-);
+}) => {
+  const getBadgeName = useCallback(() => {
+    if (branch === commitSha) {
+      return branch.substr(0, 7);
+    }
+
+    if (branch === 'master') {
+      return undefined;
+    }
+
+    return branch;
+  }, [branch, commitSha]);
+
+  const displayBranch = getBadgeName();
+
+  return (
+    <DivOrA {...props} href={url}>
+      <BorderRadius hasUrl={Boolean(url)}>
+        <Icon>
+          <GithubIcon />
+        </Icon>
+        <Text>
+          {username}/{repo}
+          {displayBranch ? `@${displayBranch}` : ''}
+        </Text>
+      </BorderRadius>
+    </DivOrA>
+  );
+};
 
 export default GithubBadge;
