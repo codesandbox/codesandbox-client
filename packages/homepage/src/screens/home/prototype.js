06a5a4548acf00e3a0c85da0858f03fdf1a7a9e1
Sara <hey@iamsaravieira.com>
2019-10-25 23:50:25 +0200

    add tiny animation for tweet

--- packages/homepage/src/screens/home/prototype.js
+++ packages/homepage/src/screens/home/prototype.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import { motion, useViewportScroll, useTransform } from 'framer-motion';
 import { H2, P, H3, H5 } from '../../components/Typography';
 
 import prototype from '../../assets/images/prototype.png';
@@ -11,43 +12,56 @@ const Grid = styled.div`
   grid-gap: 30px;
 `;
 
-const Prototype = () => (
-  <>
-    <H2>Prototype Quickly</H2>
-    <P muted>Test your ideas early and often</P>
-    <Grid css="">
-      <img src={prototype} alt="prototype" />
+const Prototype = () => {
+  const { scrollY } = useViewportScroll();
 
-      <div
-        css={`
-          position: relative;
-        `}
-      >
-        <H3
+  const y = useTransform(scrollY, [-15, 0], [0, -1], {
+    clamp: false,
+  });
+
+  return (
+    <>
+      <H2>Prototype Quickly</H2>
+      <P muted>Test your ideas early and often</P>
+      <Grid css="">
+        <img src={prototype} alt="prototype" />
+
+        <div
           css={`
-            margin-bottom: 1.5rem;
+            position: relative;
           `}
         >
-          Code from anywhere
-        </H3>
-        <H5>On any device </H5>
-        <P muted>You just need a web browser</P>
-        <H5>With no setup</H5>
-        <P muted>Go straight to coding</P>
-        <H5>VS Code built-in </H5>
-        <P muted>The editor’s full-featured, yet familiar</P>
-        <Tweet
-          tweet={{
-            name: 'Jonnie Hallman',
-            username: 'destroytoday',
-            job: 'Designer Developer, Stripe',
-            quote: 'it feels much more like my local environment',
-            url: 'https://twitter.com/destroytoday/status/1173805935384350720',
-          }}
-        />
-      </div>
-    </Grid>
-  </>
-);
+          <H3
+            css={`
+              margin-bottom: 1.5rem;
+            `}
+          >
+            Code from anywhere
+          </H3>
+          <H5>On any device </H5>
+          <P muted>You just need a web browser</P>
+          <H5>With no setup</H5>
+          <P muted>Go straight to coding</P>
+          <H5>VS Code built-in </H5>
+          <P muted>The editor’s full-featured, yet familiar</P>
+          <div>
+            <motion.div style={{ y }}>
+              <Tweet
+                tweet={{
+                  name: 'Jonnie Hallman',
+                  username: 'destroytoday',
+                  job: 'Designer Developer, Stripe',
+                  quote: 'it feels much more like my local environment',
+                  url:
+                    'https://twitter.com/destroytoday/status/1173805935384350720',
+                }}
+              />
+            </motion.div>
+          </div>
+        </div>
+      </Grid>
+    </>
+  );
+};
 
 export default Prototype;
