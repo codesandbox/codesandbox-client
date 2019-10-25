e97a157a2164656ed1a19fe4fcad19c4748f01d5
Sara <hey@iamsaravieira.com>
2019-10-25 23:31:32 +0200

    clean up

--- packages/homepage/src/components/PageContainer.js
+++ packages/homepage/src/components/PageContainer.js
@@ -3,7 +3,7 @@ import * as React from 'react';
 import MaxWidth from '@codesandbox/common/lib/components/flex/MaxWidth';
 import Padding from '@codesandbox/common/lib/components/spacing/Padding';
 
-export default ({ children, ...props }: { children: React.Node }) => (
+export default ({ children, ...props }) => (
   <MaxWidth width={1440} {...props}>
     <Padding top={8} bottom={1}>
       {children}
