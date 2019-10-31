ddb70e6737e57793ffb74c5424bde888afdc3808
Ives van Hoorne <ives.v.h@gmail.com>
2019-10-31 12:54:28 +0100

    Change deploy script

---
+++ packages/app/scripts/deploy.js
@@ -0,0 +1,31 @@
+const fetch = require('whatwg-fetch');
+
+fetch('https://deployment-api.lbogdan.ro/image', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+    Authorization: `Bearer ${process.env.DEPLOY_TOKEN}`,
+  },
+  body: JSON.stringify({
+    env: 'production',
+    image: 'client',
+    tag: process.env.CIRCLE_SHA1.substr(0, 7),
+    deploy: true,
+  }),
+})
+  .then(x => {
+    if (!x.ok) {
+      throw new Error('Request failed');
+    }
+
+    return x.json();
+  })
+  .then(res => {
+    // eslint-disable-next-line
+    console.log(res);
+    process.exit(0);
+  })
+  .catch(e => {
+    console.error(e);
+    process.exit(1);
+  });
