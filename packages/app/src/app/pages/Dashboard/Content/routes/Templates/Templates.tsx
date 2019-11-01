baa933f588d29147f4331fafba48e4fbb37f1487
jyash97 <jyash97@gmail.com>
2019-11-01 19:37:43 +0530

    feix: spacing issue

--- packages/app/src/app/pages/Dashboard/Content/routes/Templates/Templates.tsx
+++ packages/app/src/app/pages/Dashboard/Content/routes/Templates/Templates.tsx
@@ -57,8 +57,7 @@ export const Templates = ({ match }) => {
             <p style={{ marginBottom: '0.5rem' }}>
               You have not created any templates yet. You can create a template
               by dragging a sandbox from {'"'}My Sandboxes{'"'} to here or by
-              clicking
-              {'"'}Create Template{'"'} from the editor.
+              clicking {'"'}Create Template{'"'} from the editor.
             </p>
             You can learn more about templates{' '}
             <a href="/docs/templates" target="_blank">
