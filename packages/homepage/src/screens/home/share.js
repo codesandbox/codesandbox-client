83c5a406bfa0ec98bb0bc1325dac5dc8cbd29c51
Sara <hey@iamsaravieira.com>
2019-11-07 18:18:37 +0100

    fix animations

--- packages/homepage/src/screens/home/share.js
+++ packages/homepage/src/screens/home/share.js
@@ -36,9 +36,14 @@ const Share = () => {
   const ref = useRef(null);
   const { scrollY } = useViewportScroll();
 
-  const y = useTransform(scrollY, [elementTop, elementTop + 1], [0, -0.1], {
-    clamp: false,
-  });
+  const y = useTransform(
+    scrollY,
+    [elementTop + 3300, elementTop + 3300 + 1],
+    [0, -0.1],
+    {
+      clamp: false,
+    }
+  );
 
   useLayoutEffect(() => {
     const element = ref.current;
