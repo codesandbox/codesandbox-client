ecd45437b1cd59d9a317649f0586f25e913f8bc6
siddharthkp <siddharth.kshetrapal@gmail.com>
2019-11-08 11:47:35 +0100

    use window.outerWidth because that doesnt lie

--- packages/app/src/embed/components/SplitPane/index.js
+++ packages/app/src/embed/components/SplitPane/index.js
@@ -24,7 +24,7 @@ export default function SplitView({
     5. introduce the resizer element with animation
   */
 
-  const windowWidth = window.innerWidth;
+  const windowWidth = window.outerWidth;
   // TODO: pick this from the sidebar or ref instead of hardcoding
   const sidebarWidth = 250;
 
