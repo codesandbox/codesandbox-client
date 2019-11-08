c9119fd0944ebc4a472f979d7bf336205800e692
siddharthkp <siddharth.kshetrapal@gmail.com>
2019-11-08 17:31:38 +0100

    panel + content container

--- packages/app/src/embed/components/Content/elements.ts
+++ packages/app/src/embed/components/Content/elements.ts
@@ -1,40 +1,46 @@
 import styled from 'styled-components';
+import css from '@styled-system/css';
 
-export const Container = styled.div`
-  display: flex;
-  position: relative;
-  background-color: ${props => props.theme['editor.background']};
-  height: 100%;
-`;
+export const Container = styled.div(
+  css({
+    display: 'flex',
+    position: 'relative',
+    backgroundColor: 'editor.background',
+    height: '100%',
+  })
+);
 
-export const Tabs = styled.div`
-  display: flex;
-  height: 33px;
-  min-height: 33px;
-  background-color: ${props => props.theme['tab.inactiveBackground']};
-  /* shadow instead of border to align with the border of the child tab */
-  box-shadow: inset 0px -1px 0 ${props => props.theme['sideBar.border']};
-  overflow-x: auto;
-  font-size: 0.875rem;
+export const Tabs = styled.div(
+  css({
+    display: 'flex',
+    height: 33,
+    minHeight: 33,
+    backgroundColor: 'tab.inactiveBackground',
+    /* shadow instead of border to align with the border of the child tab */
+    boxShadow: theme => 'inset 0px -1px 0 ' + theme['sideBar.border'],
+    overflowX: 'auto',
+    fontSize: 3,
 
-  -ms-overflow-style: none; // IE 10+
-  overflow: -moz-scrollbars-none; // Firefox
+    '-ms-overflow-style': 'none', // IE 10+
+    overflow: '-moz-scrollbars-none', // Firefox
+    '&::-webkit-scrollbar': {
+      height: '2px', // Safari and Chrome
+    },
 
-  &::-webkit-scrollbar {
-    height: 2px; // Safari and Chrome
-  }
+    /* override children, bad but vscode doesnt support
+       tab.hoverForeground so we explicitly set it :shrug:
+    */
+    '> div:hover > div': {
+      color: 'tab.hoverForeground',
+    },
+  })
+);
 
-  /* override children, bad but vscode doesnt support
-     tab.hoverForeground :shrug:
-  */
-  > div:hover > div {
-    color: white !important;
-  }
-`;
-
-export const MenuInTabs = styled.span`
-  display: inline-flex;
-  height: 100%;
-  align-items: center;
-  padding-left: 8px;
-`;
+export const MenuInTabs = styled.span(
+  css({
+    display: 'inline-flex',
+    alignItems: 'center',
+    height: '100%',
+    paddingLeft: 2,
+  })
+);
