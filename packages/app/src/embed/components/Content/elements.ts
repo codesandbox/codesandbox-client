c3fe9d3227a3d9f598863cd62879f1984753cbce
siddharthkp <siddharth.kshetrapal@gmail.com>
2019-11-11 12:27:27 +0100

    fix the height of tabs and navigator

--- packages/app/src/embed/components/Content/elements.ts
+++ packages/app/src/embed/components/Content/elements.ts
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import css from '@styled-system/css';
+import { Container as NavigatorContainer } from '@codesandbox/common/lib/components/Preview/Navigator/elements';
 
 export const Container = styled.div(
   css({
@@ -7,14 +8,20 @@ export const Container = styled.div(
     position: 'relative',
     backgroundColor: 'editor.background',
     height: '100%',
+
+    // overwrite the height of navigation
+    [NavigatorContainer]: {
+      height: 31,
+      minHeight: 31,
+    },
   })
 );
 
 export const Tabs = styled.div(
   css({
     display: 'flex',
-    height: 33,
-    minHeight: 33,
+    height: 32,
+    minHeight: 32,
     backgroundColor: 'tab.inactiveBackground',
     /* shadow instead of border to align with the border of the child tab */
     boxShadow: theme => 'inset 0px -1px 0 ' + theme['sideBar.border'],
