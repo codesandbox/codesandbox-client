20fb6f856aa9e1970acd869a9ee9cae286e31d1f
Christian Alfoni <christianalfoni@gmail.com>
2019-11-13 16:24:21 +0100

    run actual url in state

--- packages/common/src/components/Preview/index.tsx
+++ packages/common/src/components/Preview/index.tsx
@@ -585,7 +585,7 @@ class BasePreview extends React.Component<Props, State> {
               <StyledFrame
                 sandbox="allow-forms allow-scripts allow-same-origin allow-modals allow-popups allow-presentation"
                 allow="geolocation; microphone; camera;midi; vr; accelerometer; gyroscope; payment; ambient-light-sensor; encrypted-media; usb"
-                src={this.currentUrl()}
+                src={this.state.url}
                 ref={this.setIframeElement}
                 title={getSandboxName(sandbox)}
                 id="sandbox-preview"
