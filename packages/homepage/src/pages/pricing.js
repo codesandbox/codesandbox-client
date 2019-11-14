6fde25552c3044ff9c1eae0ee25ba5015cc4f1d5
Sara <hey@iamsaravieira.com>
2019-11-14 15:54:45 +0100

    start pricing

--- packages/homepage/src/pages/pricing.js
+++ packages/homepage/src/pages/pricing.js
@@ -5,6 +5,7 @@ import PageContainer from '../components/PageContainer';
 import TitleAndMetaTags from '../components/TitleAndMetaTags';
 
 import { Title } from '../templates/_feature.elements';
+import { P } from '../components/Typography';
 
 const Card = styled.div`
   width: 100%;
@@ -67,6 +68,50 @@ const Button = styled.a`
   `}
 `;
 
+const FeaturesTableHeader = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 190px 190px;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 23px;
+  line-height: 27px;
+
+  color: ${props => props.theme.homepage.white};
+  padding-bottom: 1rem;
+  margin-bottom: 1rem;
+  border-bottom: 1px solid #242424;
+`;
+
+const FeaturesTable = styled.ul`
+  list-style: none;
+  margin: 0;
+
+  li {
+    display: grid;
+    grid-template-columns: 1fr 190px 190px;
+    margin-bottom: 1rem;
+
+    span {
+      width: 100%;
+      display: block;
+      text-align: center;
+      font-weight: 500;
+      font-size: 23px;
+      line-height: 27px;
+    }
+  }
+`;
+
+const FeatureTitle = styled.span`
+  font-style: normal;
+  font-weight: 500;
+  font-size: 23px;
+
+  color: ${props => props.theme.homepage.white};
+  text-align: left !important;
+  padding-bottom: 0.25rem;
+`;
+
 export default () => (
   <Layout>
     <TitleAndMetaTags title="Pricing - CodeSandbox" />
@@ -123,5 +168,92 @@ export default () => (
         </Button>
       </Card>
     </div>
+    <h3
+      css={`
+        font-weight: 500;
+        font-size: 36px;
+        line-height: 43px;
+
+        color: ${props => props.theme.homepage.white};
+        margin-bottom: 3.75rem;
+        margin-top: 6rem;
+      `}
+    >
+      Features
+    </h3>
+    <FeaturesTableHeader>
+      <span>Development & Prototyping</span>
+      <span
+        css={`
+          text-align: center;
+        `}
+      >
+        Community
+      </span>
+      <span
+        css={`
+          text-align: center;
+        `}
+      >
+        Pro
+      </span>
+    </FeaturesTableHeader>
+    <FeaturesTable>
+      <li>
+        <div>
+          <FeatureTitle>Templates</FeatureTitle>
+          <P muted small>
+            Start from an official template, or create your own
+          </P>
+        </div>
+        <span>✓</span>
+        <span>✓</span>
+      </li>
+
+      <li>
+        <div>
+          <FeatureTitle>Static File Hosting</FeatureTitle>
+          <P muted small>
+            All static files served via CDN
+          </P>
+        </div>
+        <span>✓</span>
+        <span>✓</span>
+      </li>
+
+      <li>
+        <div>
+          <FeatureTitle>Public Sandboxes</FeatureTitle>
+          <P muted small>
+            Sandboxes, both the preview and code, are available publicly by
+            default
+          </P>
+        </div>
+        <span>50</span>
+        <span>Unlimited</span>
+      </li>
+
+      <li>
+        <div>
+          <FeatureTitle>Unlimited Private Sandboxes</FeatureTitle>
+          <P muted small>
+            Set a sandbox as private or unlisted so others can't see the code
+          </P>
+        </div>
+        <span />
+        <span>✓</span>
+      </li>
+
+      <li>
+        <div>
+          <FeatureTitle>Private GitHub Repos</FeatureTitle>
+          <P muted small>
+            Import and sync repos which are private on GitHub to CodeSandbox
+          </P>
+        </div>
+        <span />
+        <span>✓</span>
+      </li>
+    </FeaturesTable>
   </Layout>
 );
