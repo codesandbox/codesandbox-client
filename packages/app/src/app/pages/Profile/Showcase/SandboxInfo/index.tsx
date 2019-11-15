2256508a168bca5be9f614b3b990fdc560a773bd
Michaël De Boey <info@michaeldeboey.be>
2019-11-15 11:53:57 +0100

    Fix SandboxInfo (#2967)

--- packages/app/src/app/pages/Profile/Showcase/SandboxInfo/index.tsx
+++ packages/app/src/app/pages/Profile/Showcase/SandboxInfo/index.tsx
@@ -1,19 +1,30 @@
-import React from 'react';
+import Row from '@codesandbox/common/lib/components/flex/Row';
+import { Sandbox } from '@codesandbox/common/lib/types';
 import { getSandboxName } from '@codesandbox/common/lib/utils/get-sandbox-name';
 import { sandboxUrl } from '@codesandbox/common/lib/utils/url-generator';
-import Row from '@codesandbox/common/lib/components/flex/Row';
+import React, { FunctionComponent } from 'react';
+
 import { Stat } from 'app/components/Stat';
-import SvgButton from './play-button.svg';
+import { useOvermind } from 'app/overmind';
+
 import {
   Container,
-  Title,
-  Like,
   Description,
-  Stats,
+  Like,
   PlayButtonContainer,
+  Stats,
+  Title,
 } from './elements';
+import SvgButton from './play-button.svg';
 
-function SandboxInfo({ sandbox, isLoggedIn }) {
+type Props = {
+  sandbox: Sandbox;
+};
+export const SandboxInfo: FunctionComponent<Props> = ({ sandbox }) => {
+  const {
+    state: { isLoggedIn },
+  } = useOvermind();
+
   return (
     <Container>
       <Row alignItems="center">
@@ -22,21 +33,24 @@ function SandboxInfo({ sandbox, isLoggedIn }) {
           {isLoggedIn ? <Like sandbox={sandbox} /> : null}
         </Title>
       </Row>
+
       <Row alignItems="flex-start">
         <div style={{ flex: 6 }}>
           <Description>{sandbox.description}</Description>
         </div>
+
         <Stats>
           <PlayButtonContainer to={sandboxUrl({ id: sandbox.id })}>
             <img alt="edit" src={SvgButton} />
           </PlayButtonContainer>
+
           <Stat name="likes" count={sandbox.likeCount} />
+
           <Stat name="views" count={sandbox.viewCount} />
+
           <Stat name="forks" count={sandbox.forkCount} />
         </Stats>
       </Row>
     </Container>
   );
-}
-
-export default SandboxInfo;
+};
