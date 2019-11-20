f7616a83fd35a31a9fc5f0e99fe69368f14522de
Sara <hey@iamsaravieira.com>
2019-11-20 18:04:37 +0100

    remove need for google

--- packages/app/src/app/pages/Sandbox/Editor/Workspace/Dependencies/AddFont/FontPicker/index.tsx
+++ packages/app/src/app/pages/Sandbox/Editor/Workspace/Dependencies/AddFont/FontPicker/index.tsx
@@ -1,44 +1,13 @@
-import {
-  FontManager,
-  Font,
-  FONT_FAMILY_DEFAULT,
-  OPTIONS_DEFAULTS,
-} from '@samuelmeuli/font-manager';
 import React, { useState, useEffect, useRef } from 'react';
 import OutsideClickHandler from 'react-outside-click-handler';
 import { Portal } from 'reakit';
 import { SelectedFont } from './elements';
 import { FontList } from './List';
 
-export const FontPicker = ({
-  activeFontFamily = FONT_FAMILY_DEFAULT,
-  onChange,
-  apiKey,
-}) => {
+export const FontPicker = ({ activeFontFamily = 'Open Sans', onChange }) => {
   const [expanded, setExpanded] = useState(false);
-  const [loadingStatus, setLoadingStatus] = useState('loading');
-  const [fontManager, setFontManager] = useState();
   const [style, setStyle] = useState({ x: 0, y: 0 });
   const ref = useRef(null);
-
-  useEffect(() => {
-    const manager = new FontManager(
-      apiKey,
-      activeFontFamily,
-      { ...OPTIONS_DEFAULTS, limit: 200 },
-      onChange
-    );
-    setFontManager(manager);
-
-    manager
-      .init()
-      .then(() => setLoadingStatus('finished'))
-      .catch((err: Error) => {
-        setLoadingStatus('error');
-        console.error(err);
-      });
-    // eslint-disable-next-line
-  }, []);
 
   useEffect(() => {
     if (ref && ref.current) {
@@ -50,48 +19,34 @@ export const FontPicker = ({
     }
   }, [ref]);
 
-  const onSelection = (e: any): void => {
-    const active = e.target.textContent;
-    if (!active) {
-      throw Error(`Missing font family in clicked font button`);
-    }
-    fontManager.setActiveFont(active);
-    toggleExpanded();
-  };
-
   const toggleExpanded = () => setExpanded(exp => !exp);
-
-  const fonts: Font[] =
-    fontManager && Array.from(fontManager.getFonts().values());
-
   return (
     <>
       <SelectedFont
         ref={ref}
         type="button"
-        done={loadingStatus === 'finished'}
         onClick={toggleExpanded}
         onKeyPress={toggleExpanded}
-        disabled={loadingStatus === 'loading'}
       >
-        {loadingStatus === 'loading' ? 'Loading Typefaces' : activeFontFamily}
+        {activeFontFamily}
       </SelectedFont>
-      {expanded && loadingStatus === 'finished' && (
+      {expanded && (
         <Portal>
           <div
             style={{
               position: 'fixed',
               zIndex: 11,
-              top: style.y + 201,
+              top: style.y + 170,
               left: style.x,
             }}
           >
             <OutsideClickHandler onOutsideClick={() => setExpanded(false)}>
               <FontList
-                fonts={fonts}
-                onSelection={onSelection}
+                onSelection={font => {
+                  onChange(font);
+                  setExpanded(false);
+                }}
                 activeFontFamily={activeFontFamily}
-                expanded={expanded}
               />
             </OutsideClickHandler>
           </div>
