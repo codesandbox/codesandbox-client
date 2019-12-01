72932f08ab274fd16502a8761e4cbdf03d71934c
Christian Alfoni <christianalfoni@gmail.com>
2019-12-01 18:50:13 +0100

    fix syncing module state on owned live sandbox

--- packages/app/src/app/overmind/namespaces/editor/actions.ts
+++ packages/app/src/app/overmind/namespaces/editor/actions.ts
@@ -116,6 +116,8 @@ export const sandboxChanged: AsyncAction<{ id: string }> = withLoadApp<{
 
   if (sandbox.owned && !state.live.isLive) {
     actions.files.internal.recoverFiles();
+  } else if (state.live.isLive) {
+    effects.live.sendModuleStateSyncRequest();
   }
 
   effects.vscode.openModule(state.editor.currentModule);
