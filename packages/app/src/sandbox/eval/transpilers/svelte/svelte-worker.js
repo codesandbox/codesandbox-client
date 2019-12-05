af04a077a01b4140dce786324a1064a2e2958e7f
Arthur Denner <arthurdenner7@gmail.com>
2019-12-05 22:19:13 +0100

    fix(svelte): add fallback toUrl method to generate sourcemaps (#3114)
    * fix(svelte): add fallback toUrl method to generate sourcemaps
    
    * chore(svelte): add comment to explain fallback generation of sourcemaps

--- packages/app/src/sandbox/eval/transpilers/svelte/svelte-worker.js
+++ packages/app/src/sandbox/eval/transpilers/svelte/svelte-worker.js
@@ -36,6 +36,14 @@ function getV3Code(code, version, path) {
       });
     });
 
+    // Fallback to generate sourcemaps on Svelte 3.13+
+    // See https://github.com/codesandbox/codesandbox-client/pull/3114
+    if (!js.map.toUrl) {
+      js.map.toUrl = () =>
+        'data:application/json;charset=utf-8;base64,' +
+        btoa(JSON.stringify(js.map));
+    }
+
     return js;
   } catch (e) {
     return self.postMessage({
