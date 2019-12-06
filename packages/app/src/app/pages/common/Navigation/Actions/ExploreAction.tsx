51ec11fad1c42b4386a9c0896b57e209f75909fe
Ives van Hoorne <ives.v.h@gmail.com>
2019-12-06 12:32:33 +0100

    Fix explore link

--- packages/app/src/app/pages/common/Navigation/Actions/ExploreAction.tsx
+++ packages/app/src/app/pages/common/Navigation/Actions/ExploreAction.tsx
@@ -2,16 +2,15 @@ import Tooltip from '@codesandbox/common/lib/components/Tooltip';
 import { exploreUrl } from '@codesandbox/common/lib/utils/url-generator';
 import React, { FunctionComponent } from 'react';
 import FlameIcon from 'react-icons/lib/go/flame';
-import { Link } from 'react-router-dom';
 
 import { Action } from '../elements';
 
 export const ExploreAction: FunctionComponent = () => (
   <Action>
     <Tooltip content="Explore Sandboxes" placement="bottom">
-      <Link style={{ color: 'white' }} to={exploreUrl()}>
+      <a style={{ color: 'white' }} href={exploreUrl()}>
         <FlameIcon height={35} />
-      </Link>
+      </a>
     </Tooltip>
   </Action>
 );
