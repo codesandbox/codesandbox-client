00b0d0b03d54b7302a398c2f0d76a9695eb1aabc
Michaël De Boey <info@michaeldeboey.be>
2019-12-09 16:08:01 +0100

    🔨 Switch NewSandbox to use useOvermind (#3057)

--- packages/app/src/app/pages/NewSandbox/index.tsx
+++ packages/app/src/app/pages/NewSandbox/index.tsx
@@ -1,17 +1,18 @@
-import React, { useEffect } from 'react';
-import Media from 'react-media';
 import Centered from '@codesandbox/common/lib/components/flex/Centered';
-import MaxWidth from '@codesandbox/common/lib/components/flex/MaxWidth';
 import Margin from '@codesandbox/common/lib/components/spacing/Margin';
-import { Navigation } from 'app/pages/common/Navigation';
-import { useOvermind } from 'app/overmind';
+import React, { FunctionComponent, useEffect } from 'react';
+import Media from 'react-media';
 
 import {
   CreateSandbox,
   COLUMN_MEDIA_THRESHOLD,
 } from 'app/components/CreateNewSandbox/CreateSandbox';
+import { useOvermind } from 'app/overmind';
+import { Navigation } from 'app/pages/common/Navigation';
 
-export const NewSandbox: React.FC = () => {
+import {MaxWidth} from './elements'
+
+export const NewSandbox: FunctionComponent = () => {
   const {
     actions: { sandboxPageMounted },
   } = useOvermind();
@@ -21,13 +22,10 @@ export const NewSandbox: React.FC = () => {
   }, [sandboxPageMounted]);
 
   return (
-    <MaxWidth
-      css={`
-        height: 100vh;
-      `}
-    >
+    <MaxWidth>
       <Margin horizontal={1.5} style={{ height: '100%' }} vertical={1.5}>
         <Navigation title="New Sandbox" />
+
         <Margin top={5}>
           <Centered horizontal vertical>
             <Media query={`(min-width: ${COLUMN_MEDIA_THRESHOLD}px)`}>
