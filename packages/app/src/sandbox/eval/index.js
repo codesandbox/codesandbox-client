75928978faac7eb1e172e3d2122a670163c63cfe
Sara Vieira <hey@iamsaravieira.com>
2019-12-11 18:06:58 +0100

    remove import

--- packages/app/src/sandbox/eval/index.js
+++ packages/app/src/sandbox/eval/index.js
@@ -18,7 +18,6 @@ import reactPreset from './presets/create-react-app';
 import reactTsPreset from './presets/create-react-app-typescript';
 import vuePreset from './presets/vue-cli';
 import preactPreset from './presets/preact-cli';
-import stencilPreset from './presets/stencil';
 import sveltePreset from './presets/svelte';
 import angularPreset from './presets/angular-cli';
 import parcelPreset from './presets/parcel';
