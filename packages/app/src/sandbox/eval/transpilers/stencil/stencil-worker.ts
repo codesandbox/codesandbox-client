76f5dc425a4043739be557f22cc8564165571943
Ives van Hoorne <Ives.v.h@gmail.com>
2019-12-11 19:27:37 +0100

    Stencil Compiler POC (#2203)
    * Stencil POC
    
    * Base version of compiler from dependencies
    
    * Use imports info from stencil compiler
    
    * remove setting everything to stencil
    
    * remove import
    
    * fix lint
    
    
    Co-authored-by: Sara Vieira <hey@iamsaravieira.com>

---
+++ packages/app/src/sandbox/eval/transpilers/stencil/stencil-worker.ts
@@ -0,0 +1,42 @@
+const ctx = self as any;
+
+let loadedStencilVersion: string;
+const loadStencilVersion = (version: string) => {
+  if (version !== loadedStencilVersion) {
+    loadedStencilVersion = version;
+
+    ctx.importScripts(
+      `https://unpkg.com/@stencil/core@${version}/compiler/stencil.js`
+    );
+  }
+};
+
+ctx.importScripts('https://unpkg.com/typescript@3.5.3/lib/typescript.js');
+ctx.postMessage('ready');
+
+ctx.addEventListener('message', event => {
+  const { code, path, stencilVersion } = event.data;
+
+  loadStencilVersion(stencilVersion);
+
+  const opts = {
+    file: path,
+    module: 'cjs',
+  };
+
+  ctx.stencil.compile(code, opts).then(results => {
+    results.imports.forEach(dependency => {
+      ctx.postMessage({
+        type: 'add-dependency',
+        path: dependency.path,
+        isGlob: false,
+      });
+    });
+
+    ctx.postMessage({
+      type: 'result',
+      // Code won't execute with an import.meta in the code, so removing it now as a hack
+      transpiledCode: results.code.replace(/^.*import\.meta.*$/gm, ''),
+    });
+  });
+});
