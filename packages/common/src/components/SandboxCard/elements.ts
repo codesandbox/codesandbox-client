65aac8c153b136dfe20c98d2ea6822aea049461f
Sara Vieira <hey@iamsaravieira.com>
2019-12-13 17:35:07 +0100

    Fix Dashboard truncate description (#3201)
    * fix text overflow in dashboard
    
    * show ellipses
    
    * remove unsued prop

--- packages/common/src/components/SandboxCard/elements.ts
+++ packages/common/src/components/SandboxCard/elements.ts
@@ -94,6 +94,14 @@ export const SandboxDescription = styled.p`
   margin: 0;
   margin-bottom: 16px;
   font-size: 12px;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  display: -webkit-box;
+  -webkit-box-orient: vertical;
+  -webkit-line-clamp: 5;
+  max-height: 100%;
+
+  word-break: break-all;
 `;
 
 export const SandboxImage = styled.img`
