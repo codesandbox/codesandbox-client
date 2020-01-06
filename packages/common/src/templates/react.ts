0ddabec6aad82b1968ff1089bdad243e8a041f22
Ives van Hoorne <ives.v.h@gmail.com>
2020-01-06 12:20:23 +0100

    Change default file opened for React templates

--- packages/common/src/templates/react.ts
+++ packages/common/src/templates/react.ts
@@ -13,7 +13,13 @@ export default new ReactTemplate(
     showOnHomePage: true,
     popular: true,
     main: true,
-    mainFile: ['/src/index.js', '/src/index.tsx', '/src/index.ts'],
+    mainFile: [
+      '/src/App.js',
+      '/src/App.tsx',
+      '/src/index.js',
+      '/src/index.tsx',
+      '/src/index.ts',
+    ],
     extraConfigurations: {
       '/jsconfig.json': configurations.jsconfig,
       '/tsconfig.json': configurations.tsconfig,
