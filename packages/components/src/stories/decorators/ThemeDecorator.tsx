da2a56694160eb08c624d25d788ae5f514061d4f
siddharthkp <siddharth.kshetrapal@gmail.com>
2020-01-07 10:34:02 +0100

    merge design language and vscode theme at top

--- packages/components/src/stories/decorators/ThemeDecorator.tsx
+++ packages/components/src/stories/decorators/ThemeDecorator.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
 
-import theme from '@codesandbox/common/lib/theme';
+import deepmerge from 'deepmerge';
+import designLanguage from '@codesandbox/common/lib/design-language';
+
+// black is the default, it would be helpful to use storybook-addon-themes
+// to test our components across multiple themes
+import vsCodeTheme from '@codesandbox/common/lib/themes/codesandbox-black';
+
+const theme = deepmerge(designLanguage, vsCodeTheme);
 
 export const ThemeDecorator = (fn: () => JSX.Element) => (
   <ThemeProvider theme={theme}>{fn()}</ThemeProvider>
