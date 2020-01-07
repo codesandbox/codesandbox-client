6bb3a3b63502a7471e86a0cb4e42c2d9b4fca306
siddharthkp <siddharth.kshetrapal@gmail.com>
2020-01-07 14:32:56 +0100

    add comments on theme file

--- packages/components/.storybook/theme.ts
+++ packages/components/.storybook/theme.ts
@@ -1,11 +1,24 @@
+/**
+ * There are 3 layers to our component styles.
+ *
+ * design language - spacing, fontsizes, radii, etc.
+ * vscode theme - color tokens
+ * polyfill - color tokens missing from vscode
+ */
+
 import dot from 'dot-object';
 import deepmerge from 'deepmerge';
-import designLanguage from '@codesandbox/common/lib/design-language';
-import polyfillTheme from '../src/utils/polyfill-theme';
 
+// Layer 1
+import designLanguage from '@codesandbox/common/lib/design-language';
+
+// Layer 2
 // black is the default, it would be helpful to use storybook-addon-themes
 // to test our components across multiple themes
 import vsCodeTheme from '@codesandbox/common/lib/themes/codesandbox-black';
+
+// Layer 3
+import polyfillTheme from '../src/utils/polyfill-theme';
 
 // convert vscode colors to dot notation so that we can use them in tokens
 const vsCodeColors = dot.object({ ...vsCodeTheme.colors });
