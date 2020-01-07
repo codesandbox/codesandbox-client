bbd6c696121ac9afbfa83a1b8a1b19a0796fe9c4
Ives van Hoorne <ives.v.h@gmail.com>
2020-01-07 14:09:22 +0100

    Fix upload error

--- packages/app/src/app/overmind/namespaces/files/actions.ts
+++ packages/app/src/app/overmind/namespaces/files/actions.ts
@@ -427,12 +427,16 @@ export const filesUploaded: AsyncAction<{
       });
     } catch (error) {
       if (error.message.indexOf('413') !== -1) {
-        return;
+        actions.internal.handleError({
+          message: `The uploaded file is bigger than 7MB, contact hello@codesandbox.io if you want to raise this limit`,
+          error,
+        });
+      } else {
+        actions.internal.handleError({
+          message: 'Unable to upload files',
+          error,
+        });
       }
-      actions.internal.handleError({
-        message: 'Unable to upload files',
-        error,
-      });
     }
 
     actions.internal.closeModals(false);
