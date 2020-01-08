d52b27b0976c8969c63784e6701480190f48db9b
Sara <hey@iamsaravieira.com>
2020-01-08 19:02:39 +0100

    move svg to varuable

--- packages/components/src/components/Select/index.tsx
+++ packages/components/src/components/Select/index.tsx
@@ -5,6 +5,9 @@ import VisuallyHidden from '@reach/visually-hidden';
 import { uniqueId } from 'lodash-es';
 import { Text } from '../Text';
 
+// Svg used for the icon
+const svg = `"data:image/svg+xml,%3Csvg width='8' height='24' viewBox='0 0 8 24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M4.00006 17L1 13L7 13L4.00006 17Z' fill='%23757575'/%3E%3Cpath d='M3.99994 7L7 11H1L3.99994 7Z' fill='%23757575'/%3E%3C/svg%3E%0A"`;
+
 export const SelectComponent = styled.select(
   css({
     width: '100%',
@@ -17,7 +20,7 @@ export const SelectComponent = styled.select(
     borderColor: 'input.border',
     color: 'input.placeholderForeground',
     appearance: 'none',
-    backgroundImage: `url("data:image/svg+xml,%3Csvg width='8' height='24' viewBox='0 0 8 24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M4.00006 17L1 13L7 13L4.00006 17Z' fill='%23757575'/%3E%3Cpath d='M3.99994 7L7 11H1L3.99994 7Z' fill='%23757575'/%3E%3C/svg%3E%0A")`,
+    backgroundImage: `url(${svg})`,
     backgroundPosition: 'calc(100% - 8px) center',
     backgroundRepeat: 'no-repeat',
   })
