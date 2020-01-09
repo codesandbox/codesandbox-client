0a94d48517bcc167dba84308d142cd31b77c80f9
Sara Vieira <hey@iamsaravieira.com>
2020-01-09 15:19:52 +0100

    Add Search Input (#3297)
    * add more tests cases
    
    * use token instead of color

--- packages/components/.storybook/config.tsx
+++ packages/components/.storybook/config.tsx
@@ -33,7 +33,6 @@ const GlobalStyle = createGlobalStyle`
   ${global};
   html body {
     font-family: 'Inter', sans-serif;
-
     background-color: ${props =>
       // @ts-ignore
       props.theme.colors.sideBar.background} !important;
