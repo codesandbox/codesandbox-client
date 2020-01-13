4a0f84d8dacbb5c3ed6cdef777f7a8e07cf9c76f
Sara <hey@iamsaravieira.com>
2020-01-13 15:49:37 +0100

    add config example

---
+++ packages/components/src/index.ts
@@ -0,0 +1,20 @@
+// primitives
+export * from './components/Element';
+export * from './components/ThemeProvider';
+
+// atoms
+export * from './components/Avatar';
+export * from './components/Input';
+export * from './components/SearchInput';
+export * from './components/Select';
+export * from './components/Text';
+export * from './components/Textarea';
+export * from './components/Button';
+
+// molecules
+export * from './components/Collapsible';
+export * from './components/List';
+
+// layout
+export * from './components/Grid';
+export * from './components/Stack';
