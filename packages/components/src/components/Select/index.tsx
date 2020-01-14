29b6e6b43e151f7f555a7b433a1931b7821cba89
Sara <hey@iamsaravieira.com>
2020-01-14 11:16:12 +0100

    Revert "update storybook"
    This reverts commit f77730922d25554d88455d7cdd68dfe70faefb0f.

--- packages/components/src/components/Select/index.tsx
+++ packages/components/src/components/Select/index.tsx
@@ -7,36 +7,37 @@ import { Text } from '../Text';
 import { Element } from '../Element';
 
 // Svg used for the icon
-const svg = (color: string) =>
+const svg = color =>
   `"data:image/svg+xml,%3Csvg width='8' height='24' viewBox='0 0 8 24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M4.00006 17L1 13L7 13L4.00006 17Z' fill='%23${
     color.split('#')[1]
   }'/%3E%3Cpath d='M3.99994 7L7 11H1L3.99994 7Z' fill='%23${
     color.split('#')[1]
   }'/%3E%3C/svg%3E%0A"`;
 
-export const SelectComponent = styled.select<{ icon?: boolean }>(
-  ({ icon, theme }) =>
-    css({
-      width: '100%',
-      height: 6,
-      paddingX: 2,
-      paddingLeft: icon ? 6 : 2,
-      fontSize: 3,
-      borderRadius: 'small',
-      backgroundColor: 'input.background',
-      border: '1px solid',
-      borderColor: 'input.border',
-      color: 'input.placeholderForeground',
-      appearance: 'none',
-      backgroundImage: `url(${svg(theme.colors.input.placeholderForeground)})`,
-      backgroundPosition: 'calc(100% - 8px) center',
-      backgroundRepeat: 'no-repeat',
+export const SelectComponent = styled.select<{ icon?: boolean }>(props =>
+  css({
+    width: '100%',
+    height: 6,
+    paddingX: 2,
+    paddingLeft: props.icon ? 6 : 2,
+    fontSize: 3,
+    borderRadius: 'small',
+    backgroundColor: 'input.background',
+    border: '1px solid',
+    borderColor: 'input.border',
+    color: 'input.placeholderForeground',
+    appearance: 'none',
+    backgroundImage: `url(${svg(
+      props.theme.colors.input.placeholderForeground
+    )})`,
+    backgroundPosition: 'calc(100% - 8px) center',
+    backgroundRepeat: 'no-repeat',
 
-      ':hover': {
-        color: 'input.foreground',
-        backgroundImage: `url(${svg(theme.colors.input.foreground)})`,
-      },
-    })
+    ':hover': {
+      color: 'input.foreground',
+      backgroundImage: `url(${svg(props.theme.colors.input.foreground)})`,
+    },
+  })
 );
 
 const IconWrapper = styled(Element)(
