3939f1c24ca89aadd6cc53a05e9ca068f28660c7
yangfan <18767120422@163.com>
2020-01-16 12:48:57 +0800

    feat: remove not exist icon

--- packages/common/src/templates/icons.ts
+++ packages/common/src/templates/icons.ts
@@ -6,7 +6,7 @@ import {
   ReasonIcon,
   EmberIcon,
   PreactIcon,
-  RaxIcon,
+  // RaxIcon,
   VueIcon,
   SvelteIcon,
   DojoIcon,
@@ -70,8 +70,8 @@ export default function getIcon(theme: TemplateType) {
       return VueIcon;
     case preact.name:
       return PreactIcon;
-    case rax.name:
-      return RaxIcon;  
+    // case rax.name:
+    //   return RaxIcon;  
     case reactTs.name:
       return ReactIcon;
     case svelte.name:
