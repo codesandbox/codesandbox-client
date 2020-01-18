72dbe969bfa4ccecced9577843cc46bc5be7253d
Ives van Hoorne <ives.v.h@gmail.com>
2020-01-18 19:04:17 +0100

    Make react devtools removing property fail safely

--- packages/app/src/sandbox/eval/presets/create-react-app/v3.ts
+++ packages/app/src/sandbox/eval/presets/create-react-app/v3.ts
@@ -27,8 +27,16 @@ async function initializeReactDevTools() {
     );
     // The dispatch needs to happen before initializing, so that the backend can already listen
     dispatch({ type: 'activate-react-devtools' });
-    // @ts-ignore We need to make sure that the existing chrome extension doesn't interfere
-    delete window.__REACT_DEVTOOLS_GLOBAL_HOOK__;
+
+    // @ts-ignore
+    if (typeof window.__REACT_DEVTOOLS_GLOBAL_HOOK__ !== 'undefined') {
+      try {
+        // @ts-ignore We need to make sure that the existing chrome extension doesn't interfere
+        delete window.__REACT_DEVTOOLS_GLOBAL_HOOK__;
+      } catch (e) {
+        /* ignore */
+      }
+    }
     // Call this before importing React (or any other packages that might import React).
     initializeDevTools(window);
     activate(window);
