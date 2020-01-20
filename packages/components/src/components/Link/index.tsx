25c11ca7deb7746ce6b1e6514af27d2e54b317e5
siddharthkp <siddharth.kshetrapal@gmail.com>
2020-01-20 16:14:11 +0100

    Add link component

---
+++ packages/components/src/components/Link/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import styled from 'styled-components';
+import css from '@styled-system/css';
+import { Text } from '../Text';
+
+interface ILinkProps {
+  href: string;
+  target?: string;
+  rel?: string;
+}
+
+const LinkElement = styled(Text).attrs({ as: 'a' })<ILinkProps>(
+  css({
+    color: 'mutedForeground',
+    cursor: 'pointer',
+    ':hover, :focus': {
+      color: 'foreground',
+    },
+  })
+);
+
+export const Link: React.FC<ILinkProps> = props => (
+  <LinkElement
+    rel={props.target === '_blank' ? 'noopener noreferrer' : null}
+    {...props}
+  />
+);
