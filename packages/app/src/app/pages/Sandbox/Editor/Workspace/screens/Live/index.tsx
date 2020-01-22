8da4c0b9b8d91b1447990d9ce883fa23e79bd37c
siddharthkp <siddharth.kshetrapal@gmail.com>
2020-01-22 14:35:22 +0100

    remove clutter

--- packages/app/src/app/pages/Sandbox/Editor/Workspace/screens/Live/index.tsx
+++ packages/app/src/app/pages/Sandbox/Editor/Workspace/screens/Live/index.tsx
@@ -1,5 +1,7 @@
 /**
- * TODO: Handle not logged in
+ * TODO:
+ * 1. Not logged in
+ * 2. Not owner
  */
 
 import React, { FunctionComponent } from 'react';
@@ -10,29 +12,9 @@ import { LiveNow } from './LiveNow';
 export const Live: FunctionComponent = () => {
   const {
     state: {
-      editor: {
-        currentSandbox: { owned },
-      },
       live: { isLive },
-      isLoggedIn,
     },
   } = useOvermind();
 
-  const showPlaceholder = (!isLive && !owned) || !isLoggedIn;
-
-  if (showPlaceholder) {
-    const message = isLoggedIn ? (
-      <>
-        You need to own this sandbox to open a live session to collaborate with
-        others in real time.{' '}
-        <p>Fork this sandbox to live share it with others!</p>
-      </>
-    ) : (
-      `You need to be signed in to open a live session to collaborate with others in real time. Sign in to live share this sandbox!`
-    );
-    return <div>{message}</div>;
-    // return <More message={message} id="live" />;
-  }
-
-  return <div>{isLive ? <LiveNow /> : <NotLive />}</div>;
+  return isLive ? <LiveNow /> : <NotLive />;
 };
