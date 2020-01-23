30185cbb5f73c850283c3dcc147253976641a68f
siddharthkp <siddharth.kshetrapal@gmail.com>
2020-01-23 11:17:07 +0100

    handle not logged in state

--- packages/app/src/app/pages/Sandbox/Editor/Workspace/screens/Live/index.tsx
+++ packages/app/src/app/pages/Sandbox/Editor/Workspace/screens/Live/index.tsx
@@ -1,20 +1,17 @@
-/**
- * TODO:
- * 1. Not logged in
- * 2. Not owner
- */
-
 import React, { FunctionComponent } from 'react';
 import { useOvermind } from 'app/overmind';
+import { NotLoggedIn } from './NotLoggedIn';
 import { LiveNow } from './LiveNow';
 import { NotLive } from './NotLive';
 
 export const Live: FunctionComponent = () => {
   const {
     state: {
+      isLoggedIn,
       live: { isLive },
     },
   } = useOvermind();
 
+  if (!isLoggedIn) return <NotLoggedIn />;
   return isLive ? <LiveNow /> : <NotLive />;
 };
