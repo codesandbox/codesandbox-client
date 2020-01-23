e04a9320a3c0de0f0870f344001daadedcb99a50
Ives van Hoorne <ives.v.h@gmail.com>
2020-01-23 15:54:35 +0100

    Force esmodules

--- packages/app/src/sandbox/eval/utils/resolve-utils.ts
+++ packages/app/src/sandbox/eval/utils/resolve-utils.ts
@@ -1,7 +1,7 @@
 import { PackageJSON } from '@codesandbox/common/lib/types';
 
 export function packageFilter(p: PackageJSON) {
-  if (!p.main && p.module) {
+  if (p.module) {
     // eslint-disable-next-line
     p.main = p.module;
   }
