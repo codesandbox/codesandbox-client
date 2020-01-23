827b4d1e6624dc5266bbad37d4edbcedd1e71086
siddharthkp <siddharth.kshetrapal@gmail.com>
2020-01-23 15:18:08 +0100

    add new explorer skeleton

---
+++ packages/app/src/app/pages/Sandbox/Editor/Workspace/screens/Explorer/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Collapsible } from '@codesandbox/components';
+
+export const Explorer = () => (
+  <>
+    <Collapsible title="Files">1</Collapsible>
+    <Collapsible title="Dependencies">2</Collapsible>
+    <Collapsible title="External Resources">3</Collapsible>
+  </>
+);
