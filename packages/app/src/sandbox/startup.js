905cc7a8997afd11b6332c281706e22a879a25b8
Bogdan Luca <luca.bogdan@gmail.com>
2020-01-24 20:33:01 +0200

    Fix checking if preview is standalone (#3393)
    * Fix checking if preview is standalone.
    
    * Drone: bump deploy-plugin version.
    
    * Drone: update signature.
    
    * Disable metrics in PR builds.
    
    * Disable analytics in development and PR builds.

--- packages/app/src/sandbox/startup.js
+++ packages/app/src/sandbox/startup.js
@@ -4,13 +4,14 @@ import BabelWorker from 'worker-loader?publicPath=/&name=babel-transpiler.[hash:
 import hookConsole from 'sandbox-hooks/console';
 import setupHistoryListeners from 'sandbox-hooks/url-listeners';
 import { listenForPreviewSecret } from 'sandbox-hooks/preview-secret';
+import { isStandalone } from 'codesandbox-api';
 
 window.babelworkers = [];
 for (let i = 0; i < 3; i++) {
   window.babelworkers.push(new BabelWorker());
 }
 
-if (!window.opener) {
+if (!isStandalone) {
   // Means we're in the editor
   setupHistoryListeners();
   hookConsole();
