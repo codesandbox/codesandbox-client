303b60f31b8d76c6224c3cf42d84c0953b897cf0
Sara <hey@iamsaravieira.com>
2020-01-28 14:50:46 +0100

    add sign in

--- packages/app/src/app/pages/Sandbox/Editor/Workspace/screens/GitHub/index.tsx
+++ packages/app/src/app/pages/Sandbox/Editor/Workspace/screens/GitHub/index.tsx
@@ -36,24 +36,7 @@ export const GitHub = () => {
 
   if (!isLoggedIn) return <NotLoggedIn />;
   if (!owned) return <NotOwner />;
-
-  if (!user.integrations.github) {
-    return (
-      <Collapsible title="Github" defaultOpen>
-        <Element
-          css={css({
-            paddingX: 2,
-          })}
-        >
-          <Text variant="muted" marginBottom={4} block>
-            You can create commits and open pull requests if you add GitHub to
-            your integrations.
-          </Text>
-          <GithubLogin />
-        </Element>
-      </Collapsible>
-    );
-  }
+  if (!user.integrations.github) return <GithubLogin />;
 
   return (
     <>
@@ -97,48 +80,3 @@ export const GitHub = () => {
     </>
   );
 };
-
-// <Collapsible title="Github" defaultOpen>
-//       <Element css={{ paddingX: 2 }}>
-//         <Stack gap={2} marginBottom={6}>
-//           {GitHubIcon}
-//           <Text size={2}>codesandbox/components</Text>
-//         </Stack>
-//       </Element>
-
-//       <Element>
-//         <Text size={3} block marginBottom={2} marginX={2}>
-//           Changes (4)
-//         </Text>
-//         <List marginBottom={6}>
-//           <ListAction gap={2}>
-//             {addedIcon} <Text variant="muted">src/index.js</Text>
-//           </ListAction>
-//           <ListAction gap={2}>
-//             {deletedIcon} <Text variant="muted">src/style.css</Text>
-//           </ListAction>
-//           <ListAction gap={2}>
-//             {changedIcon} <Text variant="muted">package.json</Text>
-//           </ListAction>
-//           <ListAction gap={2}>
-//             {changedIcon} <Text variant="muted">dist.js</Text>
-//           </ListAction>
-//         </List>
-
-//         <Text size={3} block marginBottom={2} marginX={2}>
-//           Commit Message
-//         </Text>
-//         <Stack as="form" direction="vertical" gap={1} marginX={2}>
-//           <Input placeholder="Subject" />
-//           <Textarea maxLength={280} placeholder="Description" />
-//           <Button variant="secondary">Open Pull Request</Button>
-//         </Stack>
-//       </Element>
-//     </Collapsible>
-//     <Collapsible title="Export to GitHub" defaultOpen>
-//       <Element css={{ paddingX: 2 }}>
-//         <Stack as="form" direction="vertical" gap={2}>
-//           <Input type="text" placeholder="Enter repository url" />
-//           <Button variant="secondary">Create Repository</Button>
-//         </Stack>
-//       </Element>
