307c20a150b2eca630156311fa8db10f8ea53d01
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2020-01-29 12:51:52 +0100

    Sidebar -  remove old files from new design (#3406)

--- packages/app/src/app/pages/Sandbox/Editor/Workspace/screens/Explorer/index.tsx
+++ packages/app/src/app/pages/Sandbox/Editor/Workspace/screens/Explorer/index.tsx
@@ -4,14 +4,9 @@ import { Files } from './Files';
 import { Dependencies } from './Dependencies';
 import { ExternalResources } from './ExternalResources';
 
-import { Files as Old } from '../../Files';
-
 export const Explorer = () => (
   <>
     <Files />
-    <Collapsible title="Files" defaultOpen>
-      <Old setEditActions={() => {}} />
-    </Collapsible>
     <Dependencies />
     <ExternalResources />
   </>
