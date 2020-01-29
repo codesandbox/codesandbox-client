37d5c444761b428c77d26b3eec0cebe503fda20d
Sara <hey@iamsaravieira.com>
2020-01-29 12:09:04 +0100

    remove listAction

--- packages/app/src/app/pages/Sandbox/Editor/Workspace/screens/GitHub/Changes.tsx
+++ packages/app/src/app/pages/Sandbox/Editor/Workspace/screens/GitHub/Changes.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, List, ListAction } from '@codesandbox/components';
+import { Text, List, ListItem } from '@codesandbox/components';
 import { AddedIcon, DeletedIcon, ChangedIcon } from './Icons';
 
 const getChanges = changes => changes.slice().sort();
@@ -7,19 +7,19 @@ const getChanges = changes => changes.slice().sort();
 export const Changes = ({ added, modified, deleted }) => (
   <List paddingBottom={6}>
     {getChanges(added).map(change => (
-      <ListAction gap={2}>
+      <ListItem gap={2}>
         <AddedIcon /> <Text variant="muted">{change}</Text>
-      </ListAction>
+      </ListItem>
     ))}
     {getChanges(modified).map(change => (
-      <ListAction gap={2}>
+      <ListItem gap={2}>
         <DeletedIcon /> <Text variant="muted">{change}</Text>
-      </ListAction>
+      </ListItem>
     ))}
     {getChanges(deleted).map(change => (
-      <ListAction gap={2}>
+      <ListItem gap={2}>
         <ChangedIcon /> <Text variant="muted">{change}</Text>
-      </ListAction>
+      </ListItem>
     ))}
   </List>
 );
