4e97e96eafe82337401a8668375d2bb84513b2c4
Ives van Hoorne <ives.v.h@gmail.com>
2020-02-03 16:19:36 +0100

    Convert client

--- packages/app/src/app/overmind/effects/live/clients.ts
+++ packages/app/src/app/overmind/effects/live/clients.ts
@@ -90,7 +90,7 @@ export default (
         client = this.create(moduleShortid, revision);
       }
 
-      return client;
+      return client!;
     },
     create(moduleShortid, initialRevision) {
       const client = new CodeSandboxOTClient(
