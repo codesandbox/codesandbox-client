0ded7dbdd71ef5aff99f4efd9ce09e5c4b8e8c43
Bogdan Luca <luca.bogdan@gmail.com>
2020-02-04 16:52:00 +0000

    Allow configuring the editor default opened file in sandbox.config.json.

--- packages/app/src/app/overmind/utils/main-module.ts
+++ packages/app/src/app/overmind/utils/main-module.ts
@@ -2,6 +2,18 @@ import { Sandbox } from '@codesandbox/common/lib/types';
 import getDefinition from '@codesandbox/common/lib/templates';
 import { resolveModuleWrapped } from './resolve-module-wrapped';
 
+function getOpenFile(sandboxConfigJson: string): string | null {
+  try {
+    const { openFile } = JSON.parse(sandboxConfigJson);
+    if (openFile) {
+      return openFile[0] === '/' ? openFile : '/' + openFile;
+    }
+  } catch (e) {
+    // ignore
+  }
+  return null;
+}
+
 export function mainModule(sandbox: Sandbox, parsedConfigurations: any) {
   const templateDefinition = getDefinition(sandbox.template);
 
@@ -23,10 +35,15 @@ export function defaultOpenedModule(
 
   const resolve = resolveModuleWrapped(sandbox);
 
-  const module = templateDefinition
-    .getDefaultOpenedFiles(parsedConfigurations)
-    .map(p => resolve(p))
-    .find(m => Boolean(m));
+  const files = templateDefinition.getDefaultOpenedFiles(parsedConfigurations);
+  if (parsedConfigurations.sandbox) {
+    const openFile = getOpenFile(parsedConfigurations.sandbox.code);
+    console.warn('openFile:', openFile);
+    if (openFile) {
+      files.unshift(openFile);
+    }
+  }
+  const module = files.map(p => resolve(p)).find(m => Boolean(m));
 
   return module || sandbox.modules[0];
 }
