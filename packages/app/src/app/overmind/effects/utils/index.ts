8f4024d7da7cbcf88229cca955e9ac72796a9b48
Ives van Hoorne <Ives.v.h@gmail.com>
2020-02-04 14:29:42 +0100

    Only send updates to non-optimistic modules in live (#3463)

--- packages/app/src/app/overmind/effects/utils/index.ts
+++ packages/app/src/app/overmind/effects/utils/index.ts
@@ -2,10 +2,11 @@ import { resolveModule } from '@codesandbox/common/lib/sandbox/modules';
 import { isEqual } from 'lodash-es';
 
 let nextOptimisticId = 0;
+export const OPTIMISTIC_ID_PREFIX = 'OPTIMISTIC_';
 
 export default {
   createOptimisticId() {
-    return 'OPTIMISTIC_' + nextOptimisticId++;
+    return OPTIMISTIC_ID_PREFIX + nextOptimisticId++;
   },
   resolveModule,
   isEqual,
