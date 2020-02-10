163ac07bff6761a504fc54e975f3e0ca8f8a73f8
Sara <hey@iamsaravieira.com>
2020-02-10 15:50:08 +0100

    add version

--- packages/app/src/app/pages/Sandbox/Editor/Workspace/screens/ProjectInfo/Delete.tsx
+++ packages/app/src/app/pages/Sandbox/Editor/Workspace/screens/ProjectInfo/Delete.tsx
@@ -1,7 +1,18 @@
 import React, { MouseEvent } from 'react';
+import VERSION from '@codesandbox/common/lib/version';
 import { useOvermind } from 'app/overmind';
 import { css } from '@styled-system/css';
-import { Button, Stack } from '@codesandbox/components';
+import { Button, Stack, Text, Element, Link } from '@codesandbox/components';
+import { SpectrumLogo, GithubIcon, TwitterIcon } from './icons';
+
+const links = [
+  { href: 'https://twitter.com/codesandbox', icon: <TwitterIcon /> },
+  {
+    href: 'https://github.com/codesandbox/codesandbox-client',
+    icon: <GithubIcon />,
+  },
+  { href: 'https://spectrum.chat/codesandbox', icon: <SpectrumLogo /> },
+];
 
 export const Delete = () => {
   const {
@@ -27,17 +38,45 @@ export const Delete = () => {
   };
 
   return (
-    <Stack justify="center" marginBottom={3}>
-      <Button
-        // @ts-ignore
-        onClick={onDelete}
-        variant="link"
+    <Element>
+      <Stack justify="center" marginBottom={6}>
+        <Button
+          // @ts-ignore
+          onClick={onDelete}
+          variant="link"
+          css={css({
+            ':hover,:focus': { color: 'errorForeground' },
+          })}
+        >
+          {`Delete ${customTemplate ? `Template` : `Sandbox`}`}
+        </Button>
+      </Stack>
+      <Element
+        marginBottom={3}
         css={css({
-          ':hover,:focus': { color: 'errorForeground' },
+          paddingX: 2,
         })}
       >
-        {`Delete ${customTemplate ? `Template` : `Sandbox`}`}
-      </Button>
-    </Stack>
+        <Stack gap={2}>
+          {links.map(link => (
+            <Link
+              target="_blank"
+              rel="noopener noreferrer"
+              css={css({ color: 'sideBar.border' })}
+              href={link.href}
+            >
+              {link.icon}
+            </Link>
+          ))}
+        </Stack>
+        <Text
+          size={1}
+          block
+          css={css({ color: 'sideBar.border', textAlign: 'right' })}
+        >
+          {VERSION}
+        </Text>
+      </Element>
+    </Element>
   );
 };
