283f1a789ec363e5251367b28697536347f3ffbf
siddharthkp <siddharth.kshetrapal@gmail.com>
2020-02-11 11:18:27 +0100

    auto grow input in tags

--- packages/components/src/components/TagInput/index.tsx
+++ packages/components/src/components/TagInput/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import TagsInput from 'react-tagsinput';
+import VisuallyHidden from '@reach/visually-hidden';
 import styled from 'styled-components';
 import css from '@styled-system/css';
 
 import { Input } from '../Input';
+import { Text } from '../Text';
 import { Tag } from '../Tags/Tag';
 
 /**
@@ -25,10 +27,13 @@ const Layout = styled(Input).attrs({
       height: 4,
       paddingX: 0,
       fontSize: 2,
-      width: 100,
+      width: 64, // initial width, enough to fit placeholder
+      minWidth: 64,
+      maxWidth: '100%', // max width = size of container
       border: 'none',
       marginY: 1,
     },
+
     ':focus-within': {
       borderColor: 'inputOption.activeBorder',
     },
@@ -41,23 +46,43 @@ const Layout = styled(Input).attrs({
 );
 
 export function TagInput({ value, onChange, inputValue, onChangeInput }) {
+  const spanRef = React.useRef<HTMLDivElement>(null);
+
+  // Input takes the size of the content inside it by using
+  // a decoy span to calculate width
+  const onInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const inputElement = event.target;
+    if (spanRef.current) {
+      inputElement.style.width = spanRef.current.offsetWidth + 20 + 'px';
+    }
+  };
+
   return (
-    <TagsInput
-      value={value}
-      onChange={onChange}
-      inputValue={inputValue}
-      onChangeInput={onChangeInput}
-      onlyUnique
-      renderTag={({ key, tag, onRemove }) => (
-        <Tag tag={tag} onRemove={() => onRemove(key)} />
-      )}
-      renderInput={props => <Input id="tags-input" {...props} />}
-      renderLayout={(tagsComponent, inputComponent) => (
-        <Layout>
-          {tagsComponent}
-          {inputComponent}
-        </Layout>
-      )}
-    />
+    <>
+      <TagsInput
+        value={value}
+        onChange={onChange}
+        inputValue={inputValue}
+        onChangeInput={onChangeInput}
+        onlyUnique
+        renderTag={({ key, tag, onRemove }) => (
+          <Tag tag={tag} onRemove={() => onRemove(key)} />
+        )}
+        renderInput={props => (
+          <Input id="tags-input" onInput={onInput} {...props} />
+        )}
+        renderLayout={(tagsComponent, inputComponent) => (
+          <Layout>
+            {tagsComponent}
+            {inputComponent}
+          </Layout>
+        )}
+      />
+      <VisuallyHidden>
+        <Text size={2} id="tags-input-span" ref={spanRef}>
+          {inputValue}
+        </Text>
+      </VisuallyHidden>
+    </>
   );
 }
