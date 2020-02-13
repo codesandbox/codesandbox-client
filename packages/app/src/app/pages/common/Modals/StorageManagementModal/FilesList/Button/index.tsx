31cb827afa0dbfa956f2df06fc2658b6326714eb
Sara <hey@iamsaravieira.com>
2020-02-13 15:56:54 +0100

    fix buttons

--- packages/app/src/app/pages/common/Modals/StorageManagementModal/FilesList/Button/index.tsx
+++ packages/app/src/app/pages/common/Modals/StorageManagementModal/FilesList/Button/index.tsx
@@ -1,6 +1,22 @@
 import React, { ComponentType, FunctionComponent, HTMLAttributes } from 'react';
-
+import styled from 'styled-components';
 import { Tooltip } from './elements';
+
+const ButtonComponent = styled.button`
+  font-size: 1.2em;
+  background-color: inherit;
+  border: none;
+  padding: 5px 6px 9px 6px;
+  color: rgba(255, 255, 255, 0.5);
+  cursor: pointer;
+  &:hover {
+    color: rgba(255, 255, 255, 1);
+  }
+  &[disabled] {
+    opacity: 0.5;
+    cursor: default;
+  }
+`;
 
 type Props = {
   disabled?: boolean;
@@ -14,8 +30,8 @@ export const Button: FunctionComponent<Props> = ({
   tooltip,
 }) => (
   <Tooltip content={tooltip} isEnabled={!disabled}>
-    <button type="button" onClick={onClick}>
+    <ButtonComponent type="button" onClick={onClick}>
       <Icon />
-    </button>
+    </ButtonComponent>
   </Tooltip>
 );
