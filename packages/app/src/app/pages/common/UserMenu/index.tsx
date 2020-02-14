0887dd3e97bd9a3bf884b74c754c6c2b8bb8af6b
siddharthkp <siddharth.kshetrapal@gmail.com>
2020-02-14 11:59:02 +0100

    move usermenu container styles to usermenu

--- packages/app/src/app/pages/common/UserMenu/index.tsx
+++ packages/app/src/app/pages/common/UserMenu/index.tsx
@@ -4,7 +4,11 @@ import React, { FunctionComponent } from 'react';
 import { useMenuState, MenuDisclosure } from 'reakit/Menu';
 import { useOvermind } from 'app/overmind';
 
-import { ClickableContainer, ProfileImage } from './elements';
+import {
+  UserMenuContainer,
+  ClickableContainer,
+  ProfileImage,
+} from './elements';
 import { Menu } from './Menu';
 
 export const UserMenu: FunctionComponent = props => {
@@ -21,36 +25,38 @@ export const UserMenu: FunctionComponent = props => {
   });
 
   return (
-    <Relative>
-      <MenuDisclosure
-        as={ClickableContainer}
-        {...menu}
-        aria-label="profile menu"
-      >
-        {props.children || (
-          <ProfileImage
-            alt={user.username}
-            width={30}
-            height={30}
-            src={user.avatarUrl}
-          />
-        )}
-      </MenuDisclosure>
+    <UserMenuContainer>
+      <Relative>
+        <MenuDisclosure
+          as={ClickableContainer}
+          {...menu}
+          aria-label="profile menu"
+        >
+          {props.children || (
+            <ProfileImage
+              alt={user.username}
+              width={30}
+              height={30}
+              src={user.avatarUrl}
+            />
+          )}
+        </MenuDisclosure>
 
-      <Menu
-        openPreferences={() => modalOpened({ modal: 'preferences' })}
-        openStorageManagement={() => gotUploadedFiles(null)}
-        signOut={() => signOutClicked()}
-        username={user.username}
-        curator={user.curatorAt}
-        openFeedback={() => modalOpened({ modal: 'feedback' })}
-        menuProps={menu}
-        showPatron={user.subscription && user.subscription.plan === 'patron'}
-        showManageSubscription={
-          user.subscription && user.subscription.plan === 'pro'
-        }
-        showBecomePro={!user.subscription}
-      />
-    </Relative>
+        <Menu
+          openPreferences={() => modalOpened({ modal: 'preferences' })}
+          openStorageManagement={() => gotUploadedFiles(null)}
+          signOut={() => signOutClicked()}
+          username={user.username}
+          curator={user.curatorAt}
+          openFeedback={() => modalOpened({ modal: 'feedback' })}
+          menuProps={menu}
+          showPatron={user.subscription && user.subscription.plan === 'patron'}
+          showManageSubscription={
+            user.subscription && user.subscription.plan === 'pro'
+          }
+          showBecomePro={!user.subscription}
+        />
+      </Relative>
+    </UserMenuContainer>
   );
 };
