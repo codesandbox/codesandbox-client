7d0188f80f318a1970c2e0cbe7903144e71d698f
jyash97 <jyash97@gmail.com>
2020-02-16 22:42:27 +0530

    fix(trash): accessing from undefined after delete

--- packages/app/src/app/pages/Dashboard/Content/Sandboxes/utils.ts
+++ packages/app/src/app/pages/Dashboard/Content/Sandboxes/utils.ts
@@ -1,13 +1,14 @@
-import { uniqBy } from 'lodash-es';
+import { uniqBy, get } from 'lodash-es';
 import getDefinition from '@codesandbox/common/lib/templates';
 
 export function getPossibleTemplates(sandboxes: any[]) {
   return uniqBy(
     sandboxes.map(x => {
-      const template = getDefinition(x.source.template);
+      const templateId = get(x, 'source.template');
+      const template = getDefinition(templateId);
 
       return {
-        id: x.source.template,
+        id: templateId,
         color: template.color,
         name: template.name,
         niceName: template.niceName,
