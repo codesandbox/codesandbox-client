7e4a905be180f3cd5f131b58573ee11d576f3e8b
Sara Vieira <hey@iamsaravieira.com>
2020-02-21 10:43:59 +0100

    update the configuration of zeit copy (#3560)

--- packages/common/src/templates/configuration/now/index.ts
+++ packages/common/src/templates/configuration/now/index.ts
@@ -3,8 +3,9 @@ import { ConfigurationFile } from '../types';
 const config: ConfigurationFile = {
   title: 'now.json',
   type: 'now',
-  description: 'Configuration for your deployments on now.',
-  moreInfoUrl: 'https://zeit.co/docs/features/configuration',
+  description: 'Configuration for your deployments on ZEIT Now.',
+  moreInfoUrl:
+    'https://zeit.co/docs/configuration#introduction/configuration-reference',
 
   getDefaultCode: () => JSON.stringify({}, null, 2),
 };
