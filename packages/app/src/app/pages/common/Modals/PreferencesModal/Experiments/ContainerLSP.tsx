cd63ff2606f62a61e7e4e69cf67cb9affb702c3c
Michaël De Boey <info@michaeldeboey.be>
2020-02-25 13:32:52 +0100

    Revert 6e8d7e6

--- packages/app/src/app/pages/common/Modals/PreferencesModal/Experiments/ContainerLSP.tsx
+++ packages/app/src/app/pages/common/Modals/PreferencesModal/Experiments/ContainerLSP.tsx
@@ -1,39 +1,33 @@
-import { CONTAINER_LSP } from '@codesandbox/common/lib/utils/feature-flags';
-import React, { useState, useEffect } from 'react';
+import React, { FunctionComponent } from 'react';
 import { useOvermind } from 'app/overmind';
 import { SubDescription, PaddedPreference } from '../elements';
 
-export const ContainerLSP: React.FunctionComponent = () => {
-  const { state } = useOvermind();
-  const [containerLSP, setContainerLSP] = useState(false);
-  useEffect(() => {
-    if (CONTAINER_LSP === 'true') {
-      return setContainerLSP(true);
-    }
-    return setContainerLSP(false);
-  }, []);
+export const ContainerLSP: FunctionComponent = () => {
+  const {
+    actions: {
+      preferences: { toggleContainerLspExperiment },
+    },
+    state: {
+      user: {
+        experiments: { containerLsp },
+      },
+    },
+  } = useOvermind();
 
-  const setValue = (val: boolean) => {
-    setContainerLSP(val);
-    window.localStorage.setItem('CONTAINER_LSP', val.toString());
-    location.reload();
-
-    // TODO(@someone): call the API call on POST /api/v1/users/current with body `{ experiments: { container_lsp: true } }`
-  };
-
-  return state.user ? (
+  return (
     <>
       <PaddedPreference
+        setValue={() => toggleContainerLspExperiment()}
         title="Use container language server"
-        type="boolean"
-        value={containerLSP}
-        setValue={val => setValue(val)}
         tooltip="Language server"
+        type="boolean"
+        value={containerLsp}
       />
+
       <SubDescription>
         As part of making containers more powerful we now allow the language
         server to run there. Please help us test it :-)
       </SubDescription>
     </>
-  ) : null;
+  );
 };
