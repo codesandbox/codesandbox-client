e014ea6afbf95badadb4b6da654cdfd7d3acb5a3
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2020-02-27 13:53:49 +0100

    make reach menu look good with global styles (#3592)

--- packages/components/src/index.ts
+++ packages/components/src/index.ts
@@ -24,6 +24,7 @@ export * from './components/FormField';
 export * from './components/Tags';
 export * from './components/Tags/Tag';
 export * from './components/TagInput';
+export * from './components/Menu';
 
 // layout
 export * from './components/Grid';
