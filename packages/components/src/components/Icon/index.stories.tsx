ec76125dfb6efb40f083f7db1c85d5157d7d0bc0
siddharthkp <siddharth.kshetrapal@gmail.com>
2020-02-28 13:48:19 +0100

    I AM A TYPESCRIPT GOD

--- packages/components/src/components/Icon/index.stories.tsx
+++ packages/components/src/components/Icon/index.stories.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Icon } from '.';
-import { Stack } from '../Stack';
-import { Text } from '../Text';
+import { SidebarRow, Text, Stack } from '../..';
 import * as icons from './icons';
 
 export default {
@@ -17,10 +16,25 @@ export const Simple = () => (
   </Stack>
 );
 
+export const Comments = () => (
+  <SidebarRow justify="space-between" paddingX={4}>
+    <Text>Comments</Text>
+    <Icon name="filter" />
+  </SidebarRow>
+);
+
 export const List = () => (
   <Stack css={{ flexWrap: 'wrap' }}>
     {Object.keys(icons).map(name => (
-      <Stack direction="vertical" align="center" css={{ width: 64, margin: 4 }}>
+      <Stack
+        key={name}
+        direction="vertical"
+        align="center"
+        gap={2}
+        css={{ width: 64, margin: 4 }}
+      >
+        {/* 
+        // @ts-ignore you would only do this in a story, never in the app */}
         <Icon name={name} />
         <Text size={2} variant="muted">
           {name}
--- packages/components/src/components/Icon/index.tsx
+++ packages/components/src/components/Icon/index.tsx
@@ -1,8 +1,21 @@
 import React from 'react';
+import { IElementProps } from '../Element';
 import * as icons from './icons';
 
-export const Icon = ({ size = 16, color = 'inherit', ...props }) => {
-  const SVG = icons[props.name] || icons.notFound;
+type IconProps = React.SVGAttributes<SVGElement> &
+  IElementProps & {
+    name: keyof typeof icons;
+    size?: number;
+    color?: string;
+  };
+
+export const Icon: React.FC<IconProps> = ({
+  name = 'notFound',
+  size = 16,
+  color = 'inherit',
+  ...props
+}) => {
+  const SVG = icons[name];
 
   return <SVG width={size} height={size} color={color} {...props} />;
 };
