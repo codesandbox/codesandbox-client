a8b641bf6e054ae17d805b637e56e6bb380bc3c2
Yash Joshi <jyash97@gmail.com>
2020-03-03 20:59:50 +0530

    Use Optional Chaining
    Co-Authored-By: Michaël De Boey <info@michaeldeboey.be>

--- packages/app/src/app/pages/Dashboard/Content/Sandboxes/utils.ts
+++ packages/app/src/app/pages/Dashboard/Content/Sandboxes/utils.ts
@@ -4,7 +4,7 @@ import getDefinition from '@codesandbox/common/lib/templates';
 export function getPossibleTemplates(sandboxes: any[]) {
   return uniqBy(
     sandboxes.map(x => {
-      const templateId = get(x, 'source.template');
+      const templateId = x.source?.template;
       const template = getDefinition(templateId);
 
       return {
