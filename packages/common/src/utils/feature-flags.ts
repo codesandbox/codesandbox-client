0dde7e9e0ff299b6dd020f0ba9180b613b6aab5d
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2020-03-04 15:42:04 +0100

    Turn on sidebar experiment for everyone (#3605)
    * fix github repo creation
    
    * prevent form submit + alignment
    
    * add bookmark for teams
    
    * added advertisement
    
    * turn on new design by default
    
    * sigh always true
    
    * supposed to be a string damn
    
    * dont show bookmark to logged in user
    
    * changed the wrong file :facepalm:
    
    Co-authored-by: Christian Alfoni <christianalfoni@gmail.com>

--- packages/common/src/utils/feature-flags.ts
+++ packages/common/src/utils/feature-flags.ts
@@ -6,9 +6,8 @@
   it's a TS file, so you can add whatever logic you want as long as it's static
 */
 
-export const REDESIGNED_SIDEBAR =
-  localStorage.getItem('REDESIGNED_SIDEBAR') || false;
 
+export const REDESIGNED_SIDEBAR = 'true';
 export const COMMENTS =
   localStorage.getItem('COMMENTS_CODESANDBOX') === 'true' || false;
 export const ACCESS_SHEET = localStorage.getItem('ACCESS_SHEET') || false;
