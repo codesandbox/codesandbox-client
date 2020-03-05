b44c92d79b33e0c4f5afeed187a4719d5d2fd7dc
Michaël De Boey <info@michaeldeboey.be>
2020-03-05 02:37:52 +0100

    Enable optional chaining

--- packages/components/.storybook/webpack.config.js
+++ packages/components/.storybook/webpack.config.js
@@ -6,6 +6,9 @@ module.exports = ({ config }) => {
         loader: require.resolve('babel-loader'),
         options: {
           presets: [require.resolve('babel-preset-react-app')],
+          plugins: [
+            require.resolve('@babel/plugin-proposal-optional-chaining'),
+          ],
         },
       },
     ],
