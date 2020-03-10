dd7920fecffe6303ce79b34a068b3e7664feb691
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2020-03-10 11:33:39 +0100

    add open transition to menu (#3670)

--- packages/components/src/components/Menu/menu.stories.tsx
+++ packages/components/src/components/Menu/menu.stories.tsx
@@ -16,10 +16,10 @@ export const Access = () => {
 
   return (
     <Stack justify="flex-end" align="center" css={{ '> *': { lineHeight: 1 } }}>
+      <Text size={2} variant="muted">
+        Everyone with link
+      </Text>{' '}
       <Menu>
-        <Text size={2} variant="muted">
-          Everyone with link
-        </Text>{' '}
         <Menu.Button css={{ color: 'white' }}>
           {selected}{' '}
           <Element
@@ -54,28 +54,14 @@ export const IconButton = () => {
   const [selected, select] = React.useState(options[0]);
 
   return (
-    <Stack justify="flex-end" align="center" css={{ '> *': { lineHeight: 1 } }}>
+    <Stack align="center" css={{ '> *': { lineHeight: 1 } }}>
       <Menu>
-        <Menu.Button
+        <Menu.IconButton
+          name="filter"
           css={{
             [selected !== options[0] && 'color']: 'blues.500',
           }}
-        >
-          <svg
-            width="14"
-            height="10"
-            viewBox="0 0 14 10"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              fillRule="evenodd"
-              clipRule="evenodd"
-              d="M0 1C0 0.447715 0.447715 0 1 0H13C13.5523 0 14 0.447715 14 1C14 1.55228 13.5523 2 13 2H1C0.447716 2 0 1.55228 0 1ZM2 5C2 4.44772 2.44772 4 3 4H11C11.5523 4 12 4.44772 12 5C12 5.55228 11.5523 6 11 6H3C2.44772 6 2 5.55228 2 5ZM5 8C4.44772 8 4 8.44771 4 9C4 9.55229 4.44772 10 5 10H9C9.55228 10 10 9.55229 10 9C10 8.44771 9.55228 8 9 8H5Z"
-              fill="currentColor"
-            />
-          </svg>
-        </Menu.Button>
+        />
         <Menu.List>
           {options.map(option => (
             <Menu.Item onSelect={() => select(option)}>{option}</Menu.Item>
