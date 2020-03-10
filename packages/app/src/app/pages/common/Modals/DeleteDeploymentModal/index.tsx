b093a5a900d7d1ea36db86cce72068ee30d104da
Sara Vieira <hey@iamsaravieira.com>
2020-03-10 18:38:07 +0100

    add deleteDeployment

--- packages/app/src/app/pages/common/Modals/DeleteDeploymentModal/index.tsx
+++ packages/app/src/app/pages/common/Modals/DeleteDeploymentModal/index.tsx
@@ -1,7 +1,8 @@
 import React, { FunctionComponent } from 'react';
 
-import { Alert } from 'app/components/Alert';
+import { Element, Button, Text, Stack } from '@codesandbox/components';
 import { useOvermind } from 'app/overmind';
+import css from '@styled-system/css';
 
 export const DeleteDeploymentModal: FunctionComponent = () => {
   const {
@@ -12,11 +13,34 @@ export const DeleteDeploymentModal: FunctionComponent = () => {
   } = useOvermind();
 
   return (
-    <Alert
-      body="Are you sure you want to delete this Deployment?"
-      onCancel={() => modalClosed()}
-      onConfirm={() => deleteDeployment()}
-      title="Delete Deployment"
-    />
+    <Element padding={4} paddingTop={6}>
+      <Text weight="bold" block size={4} paddingBottom={2}>
+        Delete Deployment
+      </Text>
+      <Text marginBottom={6} size={3} block>
+        Are you sure you want to delete this Deployment?
+      </Text>
+
+      <Stack gap={2} align="center" justify="flex-end">
+        <Button
+          css={css({
+            width: 'auto',
+          })}
+          variant="link"
+          onClick={() => modalClosed()}
+        >
+          Cancel
+        </Button>
+        <Button
+          css={css({
+            width: 'auto',
+          })}
+          variant="danger"
+          onClick={() => deleteDeployment()}
+        >
+          Delete
+        </Button>
+      </Stack>
+    </Element>
   );
 };
