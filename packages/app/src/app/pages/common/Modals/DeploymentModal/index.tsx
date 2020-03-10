8ebaa3684bd9dc77ed771e077ea4b62952dd89c5
Sara Vieira <hey@iamsaravieira.com>
2020-03-10 15:54:30 +0100

    add zeit

---
+++ packages/app/src/app/pages/common/Modals/DeploymentModal/index.tsx
@@ -0,0 +1,96 @@
+import React, { FunctionComponent } from 'react';
+import { Element, Button, Text, Stack, Link } from '@codesandbox/components';
+import { useOvermind } from 'app/overmind';
+import css from '@styled-system/css';
+import track from '@codesandbox/common/lib/utils/analytics';
+import { ZeitIcon } from './ZeitLogo';
+
+export const DeploymentModal: FunctionComponent = () => {
+  const {
+    state: {
+      user,
+      deployment: { deploying, url },
+    },
+    actions: {
+      deployment: { deployClicked },
+      signInZeitClicked,
+    },
+  } = useOvermind();
+
+  if (!user) return null;
+
+  const {
+    integrations: { zeit },
+  } = user;
+  const zeitSignedIn = !!zeit;
+
+  return (
+    <Element padding={4} paddingTop={6}>
+      <Text weight="bold" block size={4} paddingBottom={2}>
+        Deployment
+      </Text>
+      <Text marginBottom={6} size={3} block>
+        Deploy a production version of your Sandbox to ZEIT Now
+      </Text>
+      {url ? (
+        <Element marginBottom={4}>
+          <Text weight="bold" block size={4} align="center" paddingBottom={4}>
+            Deployed!
+          </Text>
+          <Link variant="muted" block size={3} align="center" href={url}>
+            {url}
+          </Link>
+          <Text paddingTop={4} block size={3} align="center">
+            You can manage your deployments{' '}
+            <Link
+              variant="muted"
+              href="https://zeit.co/dashboard"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              here
+            </Link>
+            .
+          </Text>
+        </Element>
+      ) : (
+        <Stack
+          align="center"
+          marginBottom={4}
+          css={css({ border: '1px solid', borderColor: 'sideBar.border' })}
+        >
+          <ZeitIcon />
+          <Element paddingLeft={4}>
+            {zeitSignedIn ? (
+              <>
+                <Text size={3} block paddingBottom={1} variant="muted">
+                  Signed in with
+                </Text>
+                <Text size={3}>{zeit.email || 'Loading...'}</Text>
+              </>
+            ) : (
+              <>
+                <Text size={3} block paddingBottom={1}>
+                  Please sign in
+                </Text>
+                <Button onClick={signInZeitClicked}>Sign In</Button>
+              </>
+            )}
+          </Element>
+        </Stack>
+      )}
+      <Stack justify="flex-end">
+        <Button
+          css={css({ width: 'auto' })}
+          onClick={() => {
+            track('Deploy Clicked', { provider: 'zeit' });
+            deployClicked();
+          }}
+          disabled={!zeitSignedIn || deploying}
+        >
+          {deploying ? 'Deploying' : 'Deploy Sandbox'}
+        </Button>
+      </Stack>
+    </Element>
+  );
+};
