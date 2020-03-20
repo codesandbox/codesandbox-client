3a7fe93661e018151938791845d441fb00648b28
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2020-03-20 14:22:09 +0100

    Comments polish 1 (#3728)
    * rewrite message for no comments
    
    * make the inner list of comments+replies scrollable
    
    * fix margin between header and content
    
    * shadow on scroll
    
    * better starting y position

--- packages/app/src/app/pages/Sandbox/Editor/Workspace/screens/CommentThreads/Dialog/index.tsx
+++ packages/app/src/app/pages/Sandbox/Editor/Workspace/screens/CommentThreads/Dialog/index.tsx
@@ -19,6 +19,7 @@ import Draggable from 'react-draggable';
 
 import { Markdown } from './Markdown';
 import { Reply } from './Reply';
+import { useScrollTop } from './use-scroll-top';
 
 export const CommentDialog = props =>
   ReactDOM.createPortal(<Dialog {...props} />, document.body);
@@ -32,7 +33,7 @@ export const Dialog = props => {
   const [editValue, setEditValue] = useState(thread.initialComment.content);
   const [position, setPosition] = useState({
     x: props.x || 200,
-    y: props.y || 100,
+    y: props.y || 40,
   });
 
   const closeDialog = () => actions.editor.selectCommentThread(null);
@@ -55,9 +56,12 @@ export const Dialog = props => {
     });
   };
 
+  const { ref: listRef, scrollTop } = useScrollTop();
+
   return (
     <Draggable handle=".handle" position={position} onStop={onDragStop}>
-      <Element
+      <Stack
+        direction="vertical"
         css={css({
           position: 'absolute',
           zIndex: 2,
@@ -69,19 +73,26 @@ export const Dialog = props => {
           width: 420,
           height: 'auto',
           maxHeight: '80vh',
-          overflow: 'auto',
           fontFamily: 'Inter, sans-serif',
+          overflow: 'hidden',
           boxShadow: 2,
         })}
       >
         <Stack
           className="handle"
-          css={{ cursor: 'move' }}
           align="center"
           justify="space-between"
           padding={4}
           paddingRight={2}
           marginBottom={2}
+          css={css({
+            cursor: 'move',
+            zIndex: 2,
+            boxShadow: theme =>
+              scrollTop > 0
+                ? `0px 32px 32px ${theme.colors.dialog.background}`
+                : 'none',
+          })}
         >
           <Text size={3} weight="bold">
             Comment
@@ -113,141 +124,136 @@ export const Dialog = props => {
         </Stack>
 
         {thread && (
-          <>
-            <Stack
-              align="flex-start"
-              justify="space-between"
-              marginBottom={4}
-              marginLeft={4}
-              marginRight={2}
-            >
-              <Stack gap={2} align="center">
-                <Avatar user={thread.initialComment.user} />
-                <Stack direction="vertical" justify="center" gap={1}>
-                  <Link
-                    size={3}
-                    weight="bold"
-                    href={`/u/${thread.initialComment.user.username}`}
-                    variant="body"
-                  >
-                    {thread.initialComment.user.username}
-                  </Link>
-                  <Text size={2} variant="muted">
-                    {formatDistance(new Date(thread.insertedAt), new Date(), {
-                      addSuffix: true,
-                    })}
-                  </Text>
+          <Stack direction="vertical" css={{ overflow: 'auto' }} ref={listRef}>
+            <Stack direction="vertical" gap={4}>
+              <Stack
+                align="flex-start"
+                justify="space-between"
+                marginLeft={4}
+                marginRight={2}
+              >
+                <Stack gap={2} align="center">
+                  <Avatar user={thread.initialComment.user} />
+                  <Stack direction="vertical" justify="center" gap={1}>
+                    <Link
+                      size={3}
+                      weight="bold"
+                      href={`/u/${thread.initialComment.user.username}`}
+                      variant="body"
+                    >
+                      {thread.initialComment.user.username}
+                    </Link>
+                    <Text size={2} variant="muted">
+                      {formatDistance(new Date(thread.insertedAt), new Date(), {
+                        addSuffix: true,
+                      })}
+                    </Text>
+                  </Stack>
                 </Stack>
+                {state.user.id === thread.initialComment.user.id && (
+                  <Stack align="center">
+                    <Menu>
+                      <Menu.IconButton
+                        name="more"
+                        title="Comment actions"
+                        size={12}
+                      />
+                      <Menu.List>
+                        <Menu.Item
+                          onSelect={() =>
+                            actions.editor.deleteComment({
+                              threadId: thread.id,
+                              commentId: thread.initialComment.id,
+                            })
+                          }
+                        >
+                          Delete
+                        </Menu.Item>
+                        <Menu.Item onSelect={() => setEdit(true)}>
+                          Edit Comment
+                        </Menu.Item>
+                      </Menu.List>
+                    </Menu>
+                  </Stack>
+                )}
               </Stack>
-              {state.user.id === thread.initialComment.user.id && (
-                <Stack align="center">
-                  <Menu>
-                    <Menu.IconButton
-                      name="more"
-                      title="Comment actions"
-                      size={12}
-                    />
-                    <Menu.List>
-                      <Menu.Item
-                        onSelect={() =>
-                          actions.editor.deleteComment({
+              <Element
+                as={edit ? 'div' : 'p'}
+                marginY={0}
+                marginX={4}
+                paddingBottom={6}
+                css={css({
+                  borderBottom: '1px solid',
+                  borderColor: 'sideBar.border',
+                })}
+              >
+                {!edit ? (
+                  <Markdown source={thread.initialComment.content} />
+                ) : (
+                  <>
+                    <Element marginBottom={2}>
+                      <Textarea
+                        autosize
+                        value={editValue}
+                        onChange={e => setEditValue(e.target.value)}
+                      />
+                    </Element>
+                    <Element
+                      css={css({
+                        display: 'grid',
+                        gridTemplateColumns: '1fr 1fr',
+                        gridGap: 2,
+                      })}
+                    >
+                      <Button variant="link" onClick={() => setEdit(false)}>
+                        Cancel
+                      </Button>
+
+                      <Button
+                        disabled={!editValue}
+                        variant="secondary"
+                        onClick={async () => {
+                          await actions.editor.updateComment({
                             threadId: thread.id,
                             commentId: thread.initialComment.id,
-                          })
-                        }
+                            content: editValue,
+                          });
+                          setEdit(false);
+                        }}
                       >
-                        Delete
-                      </Menu.Item>
-                      <Menu.Item onSelect={() => setEdit(true)}>
-                        Edit Comment
-                      </Menu.Item>
-                    </Menu.List>
-                  </Menu>
-                </Stack>
-              )}
+                        Save
+                      </Button>
+                    </Element>
+                  </>
+                )}
+              </Element>
             </Stack>
-            <Element
-              as={edit ? 'div' : 'p'}
-              marginY={0}
-              marginX={4}
-              paddingBottom={6}
-              css={css({
-                borderBottom: '1px solid',
-                borderColor: 'sideBar.border',
+            <>
+              {thread.comments.map((reply, i) => {
+                if (i === 0) return null;
+                return <Reply reply={reply} threadId={thread.id} />;
               })}
-            >
-              {!edit ? (
-                <Markdown source={thread.initialComment.content} />
-              ) : (
-                <>
-                  <Element marginBottom={2}>
-                    <Textarea
-                      autosize
-                      value={editValue}
-                      onChange={e => setEditValue(e.target.value)}
-                    />
-                  </Element>
-                  <Element
-                    css={css({
-                      display: 'grid',
-                      gridTemplateColumns: '1fr 1fr',
-                      gridGap: 2,
-                    })}
-                  >
-                    <Button variant="link" onClick={() => setEdit(false)}>
-                      Cancel
-                    </Button>
-
-                    <Button
-                      disabled={!editValue}
-                      variant="secondary"
-                      onClick={async () => {
-                        await actions.editor.updateComment({
-                          threadId: thread.id,
-                          commentId: thread.initialComment.id,
-                          content: editValue,
-                        });
-                        setEdit(false);
-                      }}
-                    >
-                      Save
-                    </Button>
-                  </Element>
-                </>
-              )}
-            </Element>
-          </>
+            </>
+          </Stack>
         )}
 
-        {thread &&
-          thread.comments.map((reply, i) => {
-            if (i === 0) return null;
-            return <Reply reply={reply} threadId={thread.id} />;
-          })}
-
-        <Element
+        <Textarea
+          autosize
           css={css({
+            overflow: 'hidden',
+            border: 'none',
+            display: 'block',
             borderTop: '1px solid',
             borderColor: 'sideBar.border',
           })}
-        >
-          <Textarea
-            autosize
-            css={css({
-              overflow: 'hidden',
-
-              border: 'none',
-              display: 'block',
-            })}
-            value={value}
-            onChange={e => setValue(e.target.value)}
-            placeholder={thread ? 'Reply' : 'Write a comment...'}
-            onKeyDown={event => {
-              if (event.keyCode === ENTER && !event.shiftKey) onSubmit();
-            }}
-          />
-        </Element>
-      </Element>
+          value={value}
+          onChange={e => setValue(e.target.value)}
+          placeholder={thread ? 'Reply' : 'Write a comment...'}
+          onKeyDown={event => {
+            if (event.keyCode === ENTER && !event.shiftKey) onSubmit();
+          }}
+        />
+      </Stack>
     </Draggable>
   );
 };
