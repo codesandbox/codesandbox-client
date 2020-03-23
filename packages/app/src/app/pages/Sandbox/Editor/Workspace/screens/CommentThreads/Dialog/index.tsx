9ba12f3eb0901e1199440bcae48381556896a0a6
siddharthkp <siddharth.kshetrapal@gmail.com>
2020-03-23 16:18:41 +0100

    remove react-draggable coz we have framer motino

--- packages/app/src/app/pages/Sandbox/Editor/Workspace/screens/CommentThreads/Dialog/index.tsx
+++ packages/app/src/app/pages/Sandbox/Editor/Workspace/screens/CommentThreads/Dialog/index.tsx
@@ -16,7 +16,6 @@ import { OPTIMISTIC_COMMENT_THREAD_ID } from 'app/overmind/namespaces/comments/s
 import { formatDistance } from 'date-fns';
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
-import Draggable from 'react-draggable';
 import { motion } from 'framer-motion';
 
 import { Markdown } from './Markdown';
@@ -73,239 +72,225 @@ export const Dialog: React.FC<DialogProps> = ({ triggerRef, ...props }) => {
     }
   }, [triggerRef]);
 
-  const onDragStop = (event, data) => {
-    setPosition({ x: data.x, y: data.y });
-  };
-
   const { ref: listRef, scrollTop } = useScrollTop();
 
   return (
-    <Draggable handle=".handle" position={position} onStop={onDragStop}>
-      <motion.div
-        initial={{ ...animateFrom, scale: 0.5 }}
-        animate={{ ...position, scale: 1 }}
-        drag
-        dragMomentum={false}
-        transition={{ duration: 0.25 }}
-        style={{ position: 'absolute', zIndex: 2 }}
+    <motion.div
+      initial={{ ...animateFrom, scale: 0.5 }}
+      animate={{ ...position, scale: 1 }}
+      drag
+      dragMomentum={false}
+      transition={{ duration: 0.25 }}
+      style={{ position: 'absolute', zIndex: 2 }}
+    >
+      <Stack
+        direction="vertical"
+        css={css({
+          position: 'absolute',
+          zIndex: 2,
+          backgroundColor: 'dialog.background',
+          color: 'dialog.foreground',
+          border: '1px solid',
+          borderColor: 'dialog.border',
+          borderRadius: 4,
+          width: 420,
+          height: 'auto',
+          maxHeight: '80vh',
+          fontFamily: 'Inter, sans-serif',
+          overflow: 'hidden',
+          boxShadow: 2,
+        })}
       >
         <Stack
-          direction="vertical"
+          className="handle"
+          align="center"
+          justify="space-between"
+          padding={4}
+          paddingRight={2}
+          marginBottom={2}
           css={css({
-            position: 'absolute',
+            cursor: 'move',
             zIndex: 2,
-            backgroundColor: 'dialog.background',
-            color: 'dialog.foreground',
-            border: '1px solid',
-            borderColor: 'dialog.border',
-            borderRadius: 4,
-            width: 420,
-            height: 'auto',
-            maxHeight: '80vh',
-            fontFamily: 'Inter, sans-serif',
-            overflow: 'hidden',
-            boxShadow: 2,
+            boxShadow: theme =>
+              scrollTop > 0
+                ? `0px 32px 32px ${theme.colors.dialog.background}`
+                : 'none',
           })}
         >
-          <Stack
-            className="handle"
-            align="center"
-            justify="space-between"
-            padding={4}
-            paddingRight={2}
-            marginBottom={2}
-            css={css({
-              cursor: 'move',
-              zIndex: 2,
-              boxShadow: theme =>
-                scrollTop > 0
-                  ? `0px 32px 32px ${theme.colors.dialog.background}`
-                  : 'none',
-            })}
-          >
-            <Text size={3} weight="bold">
-              Comment
-            </Text>
-            <Stack align="center">
-              <IconButton
-                onClick={() =>
-                  actions.comments.resolveCommentThread({
-                    commentThreadId: thread.id,
-                    isResolved: !thread.isResolved,
-                  })
-                }
-                disabled={isOptimisticThread}
-                name="check"
-                size={14}
-                title="Resolve Comment"
-                css={css({
-                  transition: 'color',
-                  transitionDuration: theme => theme.speeds[1],
-                  color: thread.isResolved ? 'green' : 'mutedForeground',
-                })}
-              />
-              <IconButton
-                name="cross"
-                size={10}
-                title="Close comment dialog"
-                onClick={closeDialog}
-              />
-            </Stack>
-          </Stack>
-
-          {thread && (
-            <Stack
-              direction="vertical"
-              css={{ overflow: 'auto' }}
-              ref={listRef}
-            >
-              <Stack direction="vertical" gap={4}>
-                <Stack
-                  align="flex-start"
-                  justify="space-between"
-                  marginLeft={4}
-                  marginRight={2}
-                >
-                  <Stack gap={2} align="center">
-                    <Avatar user={thread.initialComment.user} />
-                    <Stack direction="vertical" justify="center" gap={1}>
-                      <Link
-                        size={3}
-                        weight="bold"
-                        href={`/u/${thread.initialComment.user.username}`}
-                        variant="body"
-                      >
-                        {thread.initialComment.user.username}
-                      </Link>
-                      <Text size={2} variant="muted">
-                        {formatDistance(
-                          new Date(thread.insertedAt),
-                          new Date(),
-                          {
-                            addSuffix: true,
-                          }
-                        )}
-                      </Text>
-                    </Stack>
-                  </Stack>
-                  {state.user.id === thread.initialComment.user.id &&
-                    !isOptimisticThread && (
-                      <Stack align="center">
-                        <Menu>
-                          <Menu.IconButton
-                            name="more"
-                            title="Comment actions"
-                            size={12}
-                          />
-                          <Menu.List>
-                            <Menu.Item
-                              onSelect={() =>
-                                actions.comments.deleteComment({
-                                  threadId: thread.id,
-                                  commentId: thread.initialComment.id,
-                                })
-                              }
-                            >
-                              Delete
-                            </Menu.Item>
-                            <Menu.Item onSelect={() => setEdit(true)}>
-                              Edit Comment
-                            </Menu.Item>
-                          </Menu.List>
-                        </Menu>
-                      </Stack>
-                    )}
-                </Stack>
-                <Element
-                  as={edit ? 'div' : 'p'}
-                  marginY={0}
-                  marginX={4}
-                  paddingBottom={6}
-                  css={css({
-                    borderBottom: '1px solid',
-                    borderColor: 'sideBar.border',
-                  })}
-                >
-                  {!edit ? (
-                    <Markdown source={thread.initialComment.content} />
-                  ) : (
-                    <>
-                      <Element marginBottom={2}>
-                        <Textarea
-                          autosize
-                          autoFocus
-                          value={editValue}
-                          onChange={e => setEditValue(e.target.value)}
-                        />
-                      </Element>
-                      <Element
-                        css={css({
-                          display: 'grid',
-                          gridTemplateColumns: '1fr 1fr',
-                          gridGap: 2,
-                        })}
-                      >
-                        <Button
-                          variant="link"
-                          onClick={() => {
-                            if (isOptimisticThread) {
-                              closeDialog();
-                            } else {
-                              setEdit(false);
-                            }
-                          }}
-                        >
-                          Cancel
-                        </Button>
-
-                        <Button
-                          disabled={!editValue}
-                          variant="secondary"
-                          onClick={async () => {
-                            await actions.comments.updateComment({
-                              threadId: thread.id,
-                              commentId: thread.initialComment.id,
-                              content: editValue,
-                            });
-                            setEdit(false);
-                          }}
-                        >
-                          Save
-                        </Button>
-                      </Element>
-                    </>
-                  )}
-                </Element>
-              </Stack>
-              <>
-                {thread.comments.map((reply, i) => {
-                  if (i === 0) return null;
-                  return <Reply reply={reply} threadId={thread.id} />;
-                })}
-              </>
-            </Stack>
-          )}
-
-          {isOptimisticThread ? null : (
-            <Textarea
-              autosize
+          <Text size={3} weight="bold">
+            Comment
+          </Text>
+          <Stack align="center">
+            <IconButton
+              onClick={() =>
+                actions.comments.resolveCommentThread({
+                  commentThreadId: thread.id,
+                  isResolved: !thread.isResolved,
+                })
+              }
+              disabled={isOptimisticThread}
+              name="check"
+              size={14}
+              title="Resolve Comment"
               css={css({
-                overflow: 'hidden',
-                border: 'none',
-                display: 'block',
-                borderTop: '1px solid',
-                borderColor: 'sideBar.border',
+                transition: 'color',
+                transitionDuration: theme => theme.speeds[1],
+                color: thread.isResolved ? 'green' : 'mutedForeground',
               })}
-              value={value}
-              onChange={e => setValue(e.target.value)}
-              placeholder={thread ? 'Reply' : 'Write a comment...'}
-              onKeyDown={event => {
-                if (event.keyCode === ENTER && !event.shiftKey) onSubmit();
-              }}
             />
-          )}
+            <IconButton
+              name="cross"
+              size={10}
+              title="Close comment dialog"
+              onClick={closeDialog}
+            />
+          </Stack>
         </Stack>
-      </motion.div>
-    </Draggable>
+
+        {thread && (
+          <Stack direction="vertical" css={{ overflow: 'auto' }} ref={listRef}>
+            <Stack direction="vertical" gap={4}>
+              <Stack
+                align="flex-start"
+                justify="space-between"
+                marginLeft={4}
+                marginRight={2}
+              >
+                <Stack gap={2} align="center">
+                  <Avatar user={thread.initialComment.user} />
+                  <Stack direction="vertical" justify="center" gap={1}>
+                    <Link
+                      size={3}
+                      weight="bold"
+                      href={`/u/${thread.initialComment.user.username}`}
+                      variant="body"
+                    >
+                      {thread.initialComment.user.username}
+                    </Link>
+                    <Text size={2} variant="muted">
+                      {formatDistance(new Date(thread.insertedAt), new Date(), {
+                        addSuffix: true,
+                      })}
+                    </Text>
+                  </Stack>
+                </Stack>
+                {state.user.id === thread.initialComment.user.id &&
+                  !isOptimisticThread && (
+                    <Stack align="center">
+                      <Menu>
+                        <Menu.IconButton
+                          name="more"
+                          title="Comment actions"
+                          size={12}
+                        />
+                        <Menu.List>
+                          <Menu.Item
+                            onSelect={() =>
+                              actions.comments.deleteComment({
+                                threadId: thread.id,
+                                commentId: thread.initialComment.id,
+                              })
+                            }
+                          >
+                            Delete
+                          </Menu.Item>
+                          <Menu.Item onSelect={() => setEdit(true)}>
+                            Edit Comment
+                          </Menu.Item>
+                        </Menu.List>
+                      </Menu>
+                    </Stack>
+                  )}
+              </Stack>
+              <Element
+                as={edit ? 'div' : 'p'}
+                marginY={0}
+                marginX={4}
+                paddingBottom={6}
+                css={css({
+                  borderBottom: '1px solid',
+                  borderColor: 'sideBar.border',
+                })}
+              >
+                {!edit ? (
+                  <Markdown source={thread.initialComment.content} />
+                ) : (
+                  <>
+                    <Element marginBottom={2}>
+                      <Textarea
+                        autosize
+                        autoFocus
+                        value={editValue}
+                        onChange={e => setEditValue(e.target.value)}
+                      />
+                    </Element>
+                    <Element
+                      css={css({
+                        display: 'grid',
+                        gridTemplateColumns: '1fr 1fr',
+                        gridGap: 2,
+                      })}
+                    >
+                      <Button
+                        variant="link"
+                        onClick={() => {
+                          if (isOptimisticThread) {
+                            closeDialog();
+                          } else {
+                            setEdit(false);
+                          }
+                        }}
+                      >
+                        Cancel
+                      </Button>
+
+                      <Button
+                        disabled={!editValue}
+                        variant="secondary"
+                        onClick={async () => {
+                          await actions.comments.updateComment({
+                            threadId: thread.id,
+                            commentId: thread.initialComment.id,
+                            content: editValue,
+                          });
+                          setEdit(false);
+                        }}
+                      >
+                        Save
+                      </Button>
+                    </Element>
+                  </>
+                )}
+              </Element>
+            </Stack>
+            <>
+              {thread.comments.map((reply, i) => {
+                if (i === 0) return null;
+                return <Reply reply={reply} threadId={thread.id} />;
+              })}
+            </>
+          </Stack>
+        )}
+
+        {isOptimisticThread ? null : (
+          <Textarea
+            autosize
+            css={css({
+              overflow: 'hidden',
+              border: 'none',
+              display: 'block',
+              borderTop: '1px solid',
+              borderColor: 'sideBar.border',
+            })}
+            value={value}
+            onChange={e => setValue(e.target.value)}
+            placeholder={thread ? 'Reply' : 'Write a comment...'}
+            onKeyDown={event => {
+              if (event.keyCode === ENTER && !event.shiftKey) onSubmit();
+            }}
+          />
+        )}
+      </Stack>
+    </motion.div>
   );
 };
