8b3fedc34a4b7a3c6c4f9539634eff4f8d25f175
Sara Vieira <hey@iamsaravieira.com>
2020-04-15 19:42:06 +0200

    fix patron

--- packages/app/src/app/pages/Patron/index.tsx
+++ packages/app/src/app/pages/Patron/index.tsx
@@ -7,6 +7,8 @@ import { useOvermind } from 'app/overmind';
 import { Title } from 'app/components/Title';
 import { SubTitle } from 'app/components/SubTitle';
 import { Navigation } from 'app/pages/common/Navigation';
+import codeSandboxBlackTheme from '@codesandbox/common/lib/themes/codesandbox-black';
+import { ThemeProvider } from '@codesandbox/components';
 import { PricingModal } from './PricingModal';
 import { Content } from './elements';
 
@@ -16,51 +18,54 @@ const Patron: React.FC = () => {
     actions,
   } = useOvermind();
 
-  if (!hasLogIn) {
-    location.href = '/pro';
-  }
-  // don't send them away before authentication
-  if (hasLoadedApp && user) {
-    if (!user.subscription) {
-      location.href = '/pricing'; // if no subscription, to pricing with you!
-    } else if (
-      user.subscription.plan !== 'patron' // if subscription but not patron, go to pro
-    ) {
-      location.href = '/pro';
-    }
-  }
+  // if (!hasLogIn) {
+  //   location.href = '/pro';
+  // }
+  // // don't send them away before authentication
+  // if (hasLoadedApp && user) {
+  //   if (!user.subscription) {
+  //     location.href = '/pricing'; // if no subscription, to pricing with you!
+  //   } else if (
+  //     user.subscription.plan !== 'patron' // if subscription but not patron, go to pro
+  //   ) {
+  //     location.href = '/pro';
+  //   }
+  // }
 
   useEffect(() => {
     actions.patron.patronMounted();
   }, [actions]);
 
   return (
-    <MaxWidth>
-      <>
-        <Helmet>
-          <title>Patron - CodeSandbox</title>
-        </Helmet>
-        <Margin vertical={1.5} horizontal={1.5}>
-          <Navigation title="Become a Patron" />
-          <Content>
-            <MaxWidth width={1024}>
-              <>
-                <Title>Become a CodeSandbox Patron!</Title>
-                <SubTitle>
-                  You can support us by paying a monthly amount of your choice.
-                  <br />
-                  The money goes to all expenses of CodeSandbox.
-                </SubTitle>
+    <ThemeProvider theme={codeSandboxBlackTheme}>
+      <MaxWidth>
+        <>
+          <Helmet>
+            <title>Patron - CodeSandbox</title>
+          </Helmet>
+          <Margin vertical={1.5} horizontal={1.5}>
+            <Navigation title="Become a Patron" />
+            <Content>
+              <MaxWidth width={1024}>
+                <>
+                  <Title>Become a CodeSandbox Patron!</Title>
+                  <SubTitle>
+                    You can support us by paying a monthly amount of your
+                    choice.
+                    <br />
+                    The money goes to all expenses of CodeSandbox.
+                  </SubTitle>
 
-                <Centered horizontal>
-                  <PricingModal />
-                </Centered>
-              </>
-            </MaxWidth>
-          </Content>
-        </Margin>
-      </>
-    </MaxWidth>
+                  <Centered horizontal>
+                    <PricingModal />
+                  </Centered>
+                </>
+              </MaxWidth>
+            </Content>
+          </Margin>
+        </>
+      </MaxWidth>
+    </ThemeProvider>
   );
 };
 
