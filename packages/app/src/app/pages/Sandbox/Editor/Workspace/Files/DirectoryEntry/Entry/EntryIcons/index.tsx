9c2caacf4145ee0641c123c382f43ba27db12b91
siddharthkp <siddharth.kshetrapal@gmail.com>
2020-04-16 19:08:12 +0200

    bring back things used in embed?!

---
+++ packages/app/src/app/pages/Sandbox/Editor/Workspace/Files/DirectoryEntry/Entry/EntryIcons/index.tsx
@@ -0,0 +1,48 @@
+import React, { FunctionComponent, useEffect, useState } from 'react';
+import ErrorIcon from 'react-icons/lib/md/error';
+
+import { Container, RedIcon, SVGIcon } from './elements';
+import { getIconURL } from './getIconURL';
+
+type Props = {
+  error?: boolean;
+  height?: number;
+  type: string;
+  width?: number;
+};
+
+const EntryIcon: FunctionComponent<Props> = ({
+  error,
+  height = 16,
+  type,
+  width = 16,
+}) => {
+  const [icon, setIcon] = useState<string>(null);
+
+  useEffect(() => {
+    const fetchIconURL = async () => {
+      const iconURL = await getIconURL(type);
+
+      setIcon(iconURL);
+    };
+
+    fetchIconURL();
+  }, [type]);
+
+  const Icon = () =>
+    error ? (
+      <RedIcon height={height} width={width}>
+        <ErrorIcon height={height} width={width} />
+      </RedIcon>
+    ) : (
+      <SVGIcon height={height} url={icon} width={width} />
+    );
+
+  return (
+    <Container>
+      <Icon />
+    </Container>
+  );
+};
+
+export default EntryIcon;
