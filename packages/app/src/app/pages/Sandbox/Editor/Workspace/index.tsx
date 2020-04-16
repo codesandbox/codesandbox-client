048dc2cbedecf26284b1e2539533d87808c1b86a
siddharthkp <siddharth.kshetrapal@gmail.com>
2020-04-16 13:03:38 +0200

    nobody uses downtime notice

--- packages/app/src/app/pages/Sandbox/Editor/Workspace/index.tsx
+++ packages/app/src/app/pages/Sandbox/Editor/Workspace/index.tsx
@@ -5,9 +5,6 @@ import { useOvermind } from 'app/overmind';
 import { COMMENTS } from '@codesandbox/common/lib/utils/feature-flags';
 import { ThemeProvider } from '@codesandbox/components';
 
-import { Chat } from './Chat';
-import { Container } from './elements';
-
 import { ProjectInfo } from './screens/ProjectInfo';
 import { NotOwnedSandboxInfo } from './screens/NotOwnedSandboxInfo';
 import { GitHub } from './screens/GitHub';
@@ -18,6 +15,9 @@ import { Live } from './screens/Live';
 import { Server } from './screens/Server';
 import { Comments } from './screens/Comments';
 import { More } from './items/More';
+
+import { Chat } from './Chat';
+import { Container } from './elements';
 
 const workspaceTabs = {
   project: ProjectInfo,
