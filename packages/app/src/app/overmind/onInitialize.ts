80a34f182b5697948da92a53eada6e7304bc0b45
Sara Vieira <hey@iamsaravieira.com>
2020-04-16 19:27:14 +0200

    removbe ininitialize code

--- packages/app/src/app/overmind/onInitialize.ts
+++ packages/app/src/app/overmind/onInitialize.ts
@@ -5,7 +5,6 @@ export const onInitialize: OnInitialize = async (
   overmindInstance
 ) => {
   const provideJwtToken = () => state.jwt || effects.jwt.get();
-  state.currentModal = 'preferences';
 
   state.isFirstVisit = Boolean(
     !effects.jwt.get() && !effects.browser.storage.get('hasVisited')
