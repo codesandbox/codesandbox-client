0c063c3df7d471f53e61bfabf19ecec328cb2e38
Ives van Hoorne <Ives.v.h@gmail.com>
2020-04-20 13:36:28 +0200

    Don't allow AB test to reset once it's set (#3945)

--- packages/homepage/src/pages/index.js
+++ packages/homepage/src/pages/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { identify } from '@codesandbox/common/lib/utils/analytics';
 
 import TitleAndMetaTags from '../components/TitleAndMetaTags';
 import Layout, { WRAPPER_STYLING } from '../components/layout';
@@ -13,6 +12,8 @@ import Teams from '../screens/home/teams';
 import Share from '../screens/home/share';
 import Join from '../screens/home/join';
 import Explore from '../screens/home/explore';
+
+import { identifyFromCookie } from '../utils/identifyFromCookie';
 
 // eslint-disable-next-line
 console.log(
@@ -59,6 +60,6 @@ const Homepage = () => (
   </Layout>
 );
 
-identify('hero0420', 'A');
+identifyFromCookie('heroAB', 'hero0420');
 
 export default Homepage;
