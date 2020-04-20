01fd376dda556c3e5ec786b15e4b095a32e460c3
Sara Vieira <hey@iamsaravieira.com>
2020-04-20 16:40:37 +0200

    new pages (#3946)
    * new pages
    
    * Update packages/app/src/app/pages/NewDashboard/utils/get-child-collections.js
    
    Co-Authored-By: Michaël De Boey <info@michaeldeboey.be>
    
    Co-authored-by: Michaël De Boey <info@michaeldeboey.be>

---
+++ packages/app/src/app/pages/NewDashboard/Content/index.tsx
@@ -0,0 +1,34 @@
+// import React from 'react';
+// import { Route, Switch, Redirect, withRouter } from 'react-router-dom';
+
+// import { RecentSandboxes } from './routes/RecentSandboxes';
+// import PathedSandboxes from './routes/PathedSandboxes';
+// import { Templates } from './routes/Templates';
+// import DeletedSandboxes from './routes/DeletedSandboxes';
+// import SearchSandboxes from './routes/SearchSandboxes';
+// import CreateTeam from './routes/CreateTeam';
+// import TeamView from './routes/TeamView';
+
+// const Content = () => (
+//   <Switch>
+//     <Route path="/dashboard/recent" component={RecentSandboxes} />
+//     <Route path="/dashboard/trash" component={DeletedSandboxes} />
+//     <Route path="/dashboard/templates" exact component={Templates} />
+//     <Route path="/dashboard/sandboxes/:path*" component={PathedSandboxes} />
+//     <Route path="/dashboard/search" component={SearchSandboxes} />
+//     <Route path="/dashboard/teams/new" component={CreateTeam} />
+//     <Route exact path="/dashboard/teams/:teamId" component={TeamView} />
+//     <Route
+//       path="/dashboard/teams/:teamId/sandboxes/:path*"
+//       component={PathedSandboxes}
+//     />
+//     <Route
+//       path="/dashboard/teams/:teamId/templates"
+//       component={Templates}
+//       exact
+//     />
+//     <Redirect to="/dashboard/recent" />
+//   </Switch>
+// );
+
+// export default withRouter(Content);
---
+++ packages/app/src/app/pages/NewDashboard/Sidebar/index.tsx
@@ -0,0 +1,4 @@
+import React from 'react';
+import { Element } from '@codesandbox/components';
+
+export const Sidebar = () => <Element>I AM A SIDEBAR</Element>;
