0c1f58da85046b34b00a732418e8dda2d91592fe
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2020-04-21 15:25:15 +0200

    Dashboard: responsive grid - first draft (#3958)
    * responsive grid - first draft
    
    * 220 not 240!

--- packages/app/src/app/pages/NewDashboard/Components/SandboxCard/index.tsx
+++ packages/app/src/app/pages/NewDashboard/Components/SandboxCard/index.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import { sandboxUrl } from '@codesandbox/common/lib/utils/url-generator';
 import { Element, Link } from '@codesandbox/components';
+import css from '@styled-system/css';
 
 export const SandboxCard = ({ sandbox }) => (
-  <Element>
+  <Element
+    css={css({
+      width: '100%',
+      height: 240,
+      border: '1px solid',
+      borderColor: 'grays.600',
+      borderRadius: 'medium',
+    })}
+  >
     <Link href={sandboxUrl({ id: sandbox.id, alias: sandbox.alias })}>
       {sandbox.title || sandbox.alias || sandbox.id}
     </Link>
--- packages/app/src/app/pages/NewDashboard/Content/index.tsx
+++ packages/app/src/app/pages/NewDashboard/Content/index.tsx
@@ -9,12 +9,21 @@ import { Route, Switch, Redirect, withRouter } from 'react-router-dom';
 // import CreateTeam from './routes/CreateTeam';
 // import TeamView from './routes/TeamView';
 import { Element } from '@codesandbox/components';
+import css from '@styled-system/css';
 import { StartSandboxes } from './routes/StartSandboxes';
 import { Templates } from './routes/Templates';
 import { Deleted } from './routes/Deleted';
 
 const ContentComponent = () => (
-  <Element style={{ width: 960, margin: '40px auto' }}>
+  <Element
+    as="main"
+    css={css({
+      maxWidth: 992,
+      paddingX: 4,
+      width: '100%',
+      margin: '40px auto',
+    })}
+  >
     <Switch>
       <Route path="/new-dashboard/start" component={StartSandboxes} />
       <Route path="/new-dashboard/templates" component={Templates} />
