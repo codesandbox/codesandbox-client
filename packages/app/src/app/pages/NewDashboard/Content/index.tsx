30f1a2cca9ca33dd767a10b86204514e7b8edf05
siddharthkp <siddharth.kshetrapal@gmail.com>
2020-04-28 12:06:06 +0200

    Revert "remove team switcher from content"
    This reverts commit b8394fa48920d3dce0ffd694e632f955d70ebb61.

--- packages/app/src/app/pages/NewDashboard/Content/index.tsx
+++ packages/app/src/app/pages/NewDashboard/Content/index.tsx
@@ -1,30 +1,59 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Route, Switch, Redirect, withRouter } from 'react-router-dom';
-import { Element } from '@codesandbox/components';
+import { Element, Button, Stack } from '@codesandbox/components';
 import css from '@styled-system/css';
+import { useOvermind } from 'app/overmind';
 import { StartSandboxes } from './routes/StartSandboxes';
 import { Templates } from './routes/Templates';
 import { Deleted } from './routes/Deleted';
 import { Drafts } from './routes/Drafts';
 import { Recent } from './routes/Recent';
 
-const ContentComponent = () => (
-  <Element
-    as="main"
-    css={css({
-      maxWidth: 992,
-      paddingX: 4,
-      width: '100%',
-      margin: '40px auto',
-    })}
-  >
-    <Switch>
-      <Route path="/new-dashboard/start" component={StartSandboxes} />
-      <Route path="/new-dashboard/templates" component={Templates} />
-      <Route path="/new-dashboard/deleted" component={Deleted} />
-      <Route path="/new-dashboard/drafts" component={Drafts} />
-      <Route path="/new-dashboard/recent" component={Recent} />
-      {/* <Route path="/dashboard/trash" component={DeletedSandboxes} />
+const ContentComponent = () => {
+  const { actions, state } = useOvermind();
+  useEffect(() => {
+    actions.dashboard.getTeams();
+  }, [actions.dashboard, state.user]);
+
+  return (
+    <Element
+      as="main"
+      css={css({
+        maxWidth: 992,
+        paddingX: 4,
+        width: '100%',
+        margin: '40px auto',
+      })}
+    >
+      <Stack gap={2}>
+        <Button
+          variant={
+            state.dashboard.activeTeam === null ? 'primary' : 'secondary'
+          }
+          style={{ width: 'auto' }}
+          onClick={() => actions.dashboard.setActiveTeam({ id: null })}
+        >
+          Me
+        </Button>
+        {state.dashboard.teams.map(team => (
+          <Button
+            variant={
+              state.dashboard.activeTeam === team.id ? 'primary' : 'secondary'
+            }
+            onClick={() => actions.dashboard.setActiveTeam({ id: team.id })}
+            style={{ width: 'auto' }}
+          >
+            {team.name}
+          </Button>
+        ))}
+      </Stack>
+      <Switch>
+        <Route path="/new-dashboard/start" component={StartSandboxes} />
+        <Route path="/new-dashboard/templates" component={Templates} />
+        <Route path="/new-dashboard/deleted" component={Deleted} />
+        <Route path="/new-dashboard/drafts" component={Drafts} />
+        <Route path="/new-dashboard/recent" component={Recent} />
+        {/* <Route path="/dashboard/trash" component={DeletedSandboxes} />
     <Route path="/dashboard/templates" exact component={Templates} />
     <Route path="/dashboard/sandboxes/:path*" component={PathedSandboxes} />
     <Route path="/dashboard/search" component={SearchSandboxes} />
@@ -39,9 +68,10 @@ const ContentComponent = () => (
       component={Templates}
       exact
     /> */}
-      <Redirect to="/new-dashboard/start" />
-    </Switch>
-  </Element>
-);
+        <Redirect to="/new-dashboard/start" />
+      </Switch>
+    </Element>
+  );
+};
 
 export const Content = withRouter(ContentComponent);
