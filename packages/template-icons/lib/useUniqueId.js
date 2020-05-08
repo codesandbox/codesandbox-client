7e2dcb1158904e2e6e06aed588484fa42d51483f
Sara Vieira <hey@iamsaravieira.com>
2020-05-08 13:00:06 +0200

    update circle ci

--- packages/template-icons/lib/useUniqueId.js
+++ packages/template-icons/lib/useUniqueId.js
@@ -1,5 +1,1 @@
-Object.defineProperty(exports,"__esModule",{value:!0}),exports.useUniqueId=void 0;const _react=require("react");
-
- let counter=0; const useUniqueId=function(){return(0,_react.useMemo)(function(){return++counter},[])};
-
-exports.useUniqueId=useUniqueId;
+"use strict";Object.defineProperty(exports,"__esModule",{value:!0}),exports.useUniqueId=void 0;var _react=require("react"),counter=0,useUniqueId=function(){return(0,_react.useMemo)(function(){return++counter},[])};exports.useUniqueId=useUniqueId;
