c203b8b8f6400f1f174539c06c2f89bdd60aa4d9
Sara Vieira <hey@iamsaravieira.com>
2020-05-11 19:41:34 +0200

    add max width

--- packages/app/src/app/pages/common/Navigation/index.tsx
+++ packages/app/src/app/pages/common/Navigation/index.tsx
@@ -1,5 +1,5 @@
 import { useOvermind } from 'app/overmind';
-import React, { FunctionComponent } from 'react';
+import React from 'react';
 import {
   Stack,
   ThemeProvider,
@@ -8,7 +8,7 @@ import {
   Text,
   Link,
 } from '@codesandbox/components';
-import { Link as LinkBase } from 'react-router-dom';
+import { Link as LinkBase, withRouter } from 'react-router-dom';
 import codeSandboxBlack from '@codesandbox/components/lib/themes/codesandbox-black';
 import css from '@styled-system/css';
 
@@ -16,21 +16,20 @@ import { UserMenu } from '../UserMenu';
 import { Logo } from './Icons';
 
 type Props = {
-  title: string;
+  match?: any;
+  title?: string;
 };
-export const Navigation: FunctionComponent<Props> = ({ title }) => {
+
+export const Navigation = withRouter(({ title, match }: Props) => {
   const {
     actions: { modalOpened, signInClicked },
     state: { isLoggedIn, isAuthenticating, user },
   } = useOvermind();
   const link = isLoggedIn ? '/dashboard' : '/';
-
   return (
     <ThemeProvider theme={codeSandboxBlack}>
       <Stack
         as="header"
-        justify="space-between"
-        align="center"
         paddingX={4}
         css={css({
           boxSizing: 'border-box',
@@ -41,60 +40,69 @@ export const Navigation: FunctionComponent<Props> = ({ title }) => {
           borderColor: 'titleBar.border',
         })}
       >
-        <Stack gap={4} align="center">
-          <Link to={link} as={LinkBase}>
-            <Logo />
-          </Link>
-          <Text
-            size={3}
-            css={css({
-              fontWeight: 500,
-            })}
-          >
-            CodeSandbox - {title}
-          </Text>
-        </Stack>
-        {!isAuthenticating ? (
-          <Stack align="center" gap={6}>
-            {!isLoggedIn ? (
-              <Button
-                css={{ width: 'auto' }}
-                variant="link"
-                onClick={() => signInClicked()}
-              >
-                Sign In
-              </Button>
-            ) : null}
-            <Button
-              variant="primary"
-              css={css({ width: 'auto', paddingX: 3 })}
-              onClick={() => modalOpened({ modal: 'newSandbox' })}
+        <Stack
+          css={css({ maxWith: '80%', width: 1080, marginX: 'auto' })}
+          justify="space-between"
+          align="center"
+        >
+          <Stack gap={4} align="center">
+            <Link to={link} as={LinkBase}>
+              <Logo />
+            </Link>
+            <Text
+              size={3}
+              css={css({
+                fontWeight: 500,
+                '@media screen and (max-width: 768px)': {
+                  display: 'none',
+                },
+              })}
             >
-              Create Sandbox
-            </Button>
-            {isLoggedIn ? (
-              <UserMenu>
-                <Button
-                  variant="secondary"
-                  css={css({
-                    padding: 0,
-                    height: 'auto',
-                    border: 'none',
-                  })}
-                >
-                  <Avatar
-                    css={css({
-                      width: 26,
-                      height: 26,
-                    })}
-                    user={user}
-                  />
-                </Button>
-              </UserMenu>
-            ) : null}
+              CodeSandbox - {title}
+            </Text>
           </Stack>
-        ) : null}
+          {!isAuthenticating ? (
+            <Stack align="center" gap={6}>
+              {!isLoggedIn && match.path !== '/signin' ? (
+                <Button
+                  css={{ width: 'auto' }}
+                  variant="link"
+                  onClick={() => signInClicked()}
+                >
+                  Sign In
+                </Button>
+              ) : null}
+              <Button
+                variant="primary"
+                css={css({ width: 'auto', paddingX: 3 })}
+                onClick={() => modalOpened({ modal: 'newSandbox' })}
+              >
+                Create Sandbox
+              </Button>
+              {isLoggedIn ? (
+                <UserMenu>
+                  <Button
+                    variant="secondary"
+                    css={css({
+                      padding: 0,
+                      height: 'auto',
+                      border: 'none',
+                    })}
+                  >
+                    <Avatar
+                      css={css({
+                        width: 26,
+                        height: 26,
+                      })}
+                      user={user}
+                    />
+                  </Button>
+                </UserMenu>
+              ) : null}
+            </Stack>
+          ) : null}
+        </Stack>
       </Stack>
     </ThemeProvider>
   );
-};
+});
