1e55d34c79a6cf0c8919ff7f86d5b138905690e4
Ives van Hoorne <ives.v.h@gmail.com>
2020-05-16 18:16:30 +0200

    Make esmodule the default when loading a library

--- packages/app/src/sandbox/eval/utils/resolve-utils.ts
+++ packages/app/src/sandbox/eval/utils/resolve-utils.ts
@@ -1,8 +1,7 @@
 import { PackageJSON } from '@codesandbox/common/lib/types';
 
 export function packageFilter(p: PackageJSON) {
-  if (p.module && !p.main) {
-    // eslint-disable-next-line
+  if (p.module) {
     p.main = p.module;
   }
 
