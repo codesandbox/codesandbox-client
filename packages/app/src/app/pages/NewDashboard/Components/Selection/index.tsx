e4597b6286020c89f0ff550ad7c322211976693d
siddharthkp <siddharth.kshetrapal@gmail.com>
2020-05-19 14:54:34 +0200

    multiselect with shift click

--- packages/app/src/app/pages/NewDashboard/Components/Selection/index.tsx
+++ packages/app/src/app/pages/NewDashboard/Components/Selection/index.tsx
@@ -17,7 +17,39 @@ export const SelectionProvider = ({ sandboxes = [], ...props }) => {
     if (event.ctrlKey || event.metaKey) {
       // select multiple with modifier
 
-      setSelectedIds([...selectedIds, sandboxId]);
+      if (selectedIds.includes(sandboxId)) {
+        setSelectedIds(selectedIds.filter(id => id !== sandboxId));
+      } else {
+        setSelectedIds([...selectedIds, sandboxId]);
+      }
+
+      event.stopPropagation();
+    } else if (event.shiftKey) {
+      // start = find index for last inserted
+      // end = find index for sandboxId
+      // find everything in between and add them
+      const start = sandboxes.findIndex(
+        sandbox => sandbox.id === selectedIds[selectedIds.length - 1]
+      );
+      const end = sandboxes.findIndex(sandbox => sandbox.id === sandboxId);
+
+      const sandboxesToAdd = [];
+
+      if (start >= 0 && end >= 0) {
+        const increment = end > start ? +1 : -1;
+
+        for (
+          let index = start;
+          increment > 0 ? index <= end : index >= end;
+          index += increment
+        ) {
+          sandboxesToAdd.push(sandboxes[index].id);
+        }
+      } else {
+        sandboxesToAdd.push(sandboxId);
+      }
+
+      setSelectedIds([...selectedIds, ...sandboxesToAdd]);
       event.stopPropagation();
     } else {
       setSelectedIds([sandboxId]);
