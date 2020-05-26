b70169e2ebabefc0147a5437eb4833cf16990ddd
Sara Vieira <hey@iamsaravieira.com>
2020-05-26 14:02:15 +0200

    fix two bugs

--- packages/notifications/src/component/elements.ts
+++ packages/notifications/src/component/elements.ts
@@ -24,6 +24,7 @@ export const InnerWrapper = styled(Stack)`
   ${({ theme }) => css`
     width: 100%;
     border: 1px solid ${theme.colors.grays[600]};
+    border-radius: 0 ${theme.radii.medium}px ${theme.radii.medium}px 0;
   `}
 `;
 
@@ -32,7 +33,7 @@ export const Container = styled(Stack)`
     background: ${theme.colors.grays[700]};
     box-sizing: border-box;
     box-shadow: ${theme.shadows[2]};
-    border-radius: ${theme.radii.medium}px;
+    border-radius: ${theme.radii.medium}px 0 0 ${theme.radii.medium}px;
     position: relative;
     font-size: ${theme.fontSizes[3]}px;
     color: ${theme.colors.white};
