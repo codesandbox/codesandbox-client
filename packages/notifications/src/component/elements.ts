389e9921b6c710fa206a393d188a1a60dd61348e
Sara Vieira <hey@iamsaravieira.com>
2020-05-26 14:42:59 +0200

    fix shadow

--- packages/notifications/src/component/elements.ts
+++ packages/notifications/src/component/elements.ts
@@ -24,16 +24,15 @@ export const InnerWrapper = styled(Stack)`
   ${({ theme }) => css`
     width: 100%;
     border: 1px solid ${theme.colors.grays[600]};
-    border-radius: 0 ${theme.radii.medium}px ${theme.radii.medium}px 0;
+    background: ${theme.colors.grays[700]};
   `}
 `;
 
 export const Container = styled(Stack)`
   ${({ theme }) => css`
-    background: ${theme.colors.grays[700]};
     box-sizing: border-box;
     box-shadow: ${theme.shadows[2]};
-    border-radius: ${theme.radii.medium}px 0 0 ${theme.radii.medium}px;
+    border-radius: ${theme.radii.medium}px;
     position: relative;
     font-size: ${theme.fontSizes[3]}px;
     color: ${theme.colors.white};
