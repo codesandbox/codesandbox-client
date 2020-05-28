3d08c3a4efbb46c7405e9531e4ba7a86a458f7ea
Ives van Hoorne <ives.v.h@gmail.com>
2020-05-28 22:14:02 +0200

    Add data about user

--- packages/app/src/app/overmind/factories.ts
+++ packages/app/src/app/overmind/factories.ts
@@ -43,7 +43,8 @@ export const withLoadApp = <T>(
         state.dashboard.activeTeam = localStorageTeam;
       }
       try {
-        actions.internal.trackCurrentTeams();
+        actions.internal.trackCurrentTeams().catch(e => {});
+        actions.internal.identifyCurrentUser().catch(e => {});
       } catch (e) {
         // Not majorly important
       }
