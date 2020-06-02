f3c1beaad03fcb6471184d8cd3fd0049889d74d1
Ives van Hoorne <ives@codesandbox.io>
2020-06-02 13:19:24 +0200

    Add a fallback to commonjs if esmodule is not found (#4283)
    * Add a fallback to commonjs if esmodule is not found
    
    * Fix convert esmodule

--- packages/app/src/sandbox/eval/utils/resolve-utils.ts
+++ packages/app/src/sandbox/eval/utils/resolve-utils.ts
@@ -1,10 +1,49 @@
+import { join } from 'path';
+import Cache from 'lru-cache';
 import { PackageJSON } from '@codesandbox/common/lib/types';
 
-export function packageFilter(p: PackageJSON) {
+const pkgCache = new Cache<string, PackageJSON>(10000);
+
+function replaceModuleField(
+  isFile: (p: string) => boolean,
+  p: PackageJSON,
+  pkgLocation: string
+): PackageJSON {
+  const checks = [
+    [pkgLocation, p.module],
+    [pkgLocation, p.module, 'index.js'],
+    [pkgLocation, p.module, 'index.mjs'],
+  ];
   if (p.module) {
-    // eslint-disable-next-line
-    p.main = p.module;
+    const foundFile = checks.find(pathSegments => {
+      const path = join(...pathSegments);
+      return isFile(path);
+    });
+
+    if (foundFile) {
+      // eslint-disable-next-line
+      p.main = p.module;
+    }
   }
 
   return p;
 }
+
+export const packageFilter = (isFile: (p: string) => boolean) => (
+  p: PackageJSON,
+  pkgLocation: string
+) => {
+  const id = p.name + p.version;
+  const cache = pkgCache.get(id);
+
+  if (cache) {
+    return cache;
+  }
+
+  // measure('replace-module-field');
+  const result = replaceModuleField(isFile, p, pkgLocation);
+  // endMeasure('replace-module-field');
+  pkgCache.set(id, result);
+
+  return result;
+};
