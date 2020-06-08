6b096fd20c5a746c9d63b7b5e6bbe7205cd2ee67
Sara Vieira <hey@iamsaravieira.com>
2020-06-08 14:53:09 +0200

    skeloiton bg as background for temnplate universe

--- packages/app/src/app/pages/NewSandbox/index.tsx
+++ packages/app/src/app/pages/NewSandbox/index.tsx
@@ -8,8 +8,11 @@ import {
   COLUMN_MEDIA_THRESHOLD,
 } from 'app/components/CreateNewSandbox/CreateSandbox';
 import { useOvermind } from 'app/overmind';
+
 import { Navigation } from 'app/pages/common/Navigation';
 import MaxWidth from '@codesandbox/common/lib/components/flex/MaxWidth';
+import { ContentSkeleton } from '../Sandbox/Editor/Skeleton';
+import { Navigation as SideNav } from '../Sandbox/Editor/Navigation';
 
 export const NewSandbox: FunctionComponent = () => {
   const {
@@ -26,10 +29,34 @@ export const NewSandbox: FunctionComponent = () => {
         css={css({
           width: '100vw',
           height: '100vh',
-          backgroundColor: 'sideBar.background',
         })}
       >
-        <Navigation title="New Sandbox" />
+        <Element
+          css={css({
+            'header > div': {
+              maxWidth: '100%',
+              width: '100%',
+            },
+          })}
+        >
+          <Navigation title="New Sandbox" />
+        </Element>
+        <Element css={css({ pointerEvents: 'none', opacity: 0.8 })}>
+          <Stack>
+            <SideNav topOffset={48} bottomOffset={0} />
+            <ContentSkeleton
+              style={{
+                opacity: 1,
+                zIndex: 0,
+                top: 48,
+                left: 57,
+                width: 'calc(100vw - 57px)',
+                height: 'calc(100vh - 48px)',
+                overflow: 'hidden',
+              }}
+            />
+          </Stack>
+        </Element>
         <MaxWidth>
           <Element margin={6} style={{ height: '100%' }}>
             <Element marginTop={80}>
@@ -37,10 +64,12 @@ export const NewSandbox: FunctionComponent = () => {
                 <Media query={`(min-width: ${COLUMN_MEDIA_THRESHOLD}px)`}>
                   {matches => (
                     <Element
-                      style={{
+                      css={css({
+                        backgroundColor: 'sideBar.background',
                         maxWidth: '100%',
                         width: matches ? 1200 : 900,
-                      }}
+                        position: 'relative',
+                      })}
                       marginTop={8}
                     >
                       <CreateSandbox />
