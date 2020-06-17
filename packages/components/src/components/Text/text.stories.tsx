09e35018c648ddf52cf84844d26b479f7b0bce66
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2020-06-17 12:02:58 +0200

    i just need a new commit tbh

--- packages/components/src/components/Text/text.stories.tsx
+++ packages/components/src/components/Text/text.stories.tsx
@@ -69,5 +69,5 @@ export const Align = () => (
 );
 
 export const MaxWidth = () => (
-  <Text maxWidth={200}>this text will get cropped after 200px</Text>
+  <Text maxWidth={200}>this text will get cropped beyond 200px</Text>
 );
