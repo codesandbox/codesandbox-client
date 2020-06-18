e00690fddd1f0d5f846afcc6237130960359927e
Sara Vieira <hey@iamsaravieira.com>
2020-06-18 15:47:16 +0200

    almost and wifi died

--- packages/app/src/app/pages/Sandbox/Editor/Workspace/screens/Comments/AddComment.tsx
+++ packages/app/src/app/pages/Sandbox/Editor/Workspace/screens/Comments/AddComment.tsx
@@ -1,7 +1,14 @@
 import { ENTER } from '@codesandbox/common/lib/utils/keycodes';
-import { Element, FormField, Textarea } from '@codesandbox/components';
+import {
+  Element,
+  FormField,
+  Textarea,
+  List,
+  ListAction,
+} from '@codesandbox/components';
 import { css } from '@styled-system/css';
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
+import { useMention } from './hooks/useMention';
 
 type Props = {
   onSubmit: (value: string) => void;
@@ -9,6 +16,14 @@ type Props = {
 
 export const AddComment: React.FC<Props> = ({ onSubmit }) => {
   const [value, setValue] = useState('');
+  const textArea = useRef(null);
+  const [filteredUsers, onKeyDown, onKeyUp, loadingUsers, mention] = useMention(
+    {
+      ref: textArea,
+      value,
+      setValue,
+    }
+  );
 
   const submit = event => {
     event.preventDefault();
@@ -18,8 +33,13 @@ export const AddComment: React.FC<Props> = ({ onSubmit }) => {
     }
   };
 
+  console.log({
+    filteredUsers,
+  });
+
   // Form elements submit on Enter, except Textarea :)
   const submitOnEnter = event => {
+    onKeyDown(event);
     if (event.keyCode === ENTER && !event.shiftKey) {
       event.preventDefault();
       submit(event);
@@ -40,14 +60,50 @@ export const AddComment: React.FC<Props> = ({ onSubmit }) => {
       <form onSubmit={submit}>
         <FormField label="Add a comment" hideLabel>
           <Textarea
+            ref={textArea}
             autosize
             value={value}
             onChange={e => setValue(e.target.value)}
             onKeyDown={submitOnEnter}
+            onKeyUp={onKeyUp}
             placeholder="Write a comment"
             style={{ lineHeight: 1.2, minHeight: 32 }}
           />
         </FormField>
+        {mention ? (
+          <List
+            css={css({
+              position: 'absolute',
+              width: '100%',
+              borderBottomLeftRadius: 'small',
+              borderBottomRightRadius: 'small',
+              boxShadow: 1,
+              fontSize: 3,
+              backgroundColor: 'dialog.background',
+              border: '1px solid',
+              borderColor: 'dialog.border',
+            })}
+          >
+            {filteredUsers.map((item, index) => (
+              <ListAction
+                key={item.id}
+                // isActive={highlightedIndex === index}
+              >
+                <img
+                  alt={item.username}
+                  css={css({
+                    borderRadius: 2,
+                    marginRight: 2,
+                  })}
+                  width={24}
+                  height={24}
+                  src={item.avatar_url}
+                />{' '}
+                {item.username}
+              </ListAction>
+            ))}
+          </List>
+        ) : null}
       </form>
     </Element>
   );
---
+++ packages/app/src/app/pages/Sandbox/Editor/Workspace/screens/Comments/hooks/useMention.tsx
@@ -0,0 +1,197 @@
+import React from 'react';
+import getCaretCoordinates from 'textarea-caret';
+
+export const useMention = ({ ref, value, setValue }) => {
+  const [mention, setMention] = React.useState(null);
+  const [loadingUsers, setLoadingUsers] = React.useState(false);
+  const [users, setUsers] = React.useState([]);
+  const [menuIndex, setMenuIndex] = React.useState(0);
+  const [userResourses, updateUserResources] = React.useState({});
+  const query =
+    mention &&
+    ref.current.value.substr(
+      mention.startIndex + 1,
+      mention.endIndex - mention.startIndex
+    );
+
+  React.useEffect(() => {
+    let timeoutId;
+    if (mention && query.length > 2) {
+      setLoadingUsers(true);
+      // Small debounce
+      timeoutId = window.setTimeout(() => {
+        fetch(`/api/v1/users/search?username=${query}`)
+          .then(x => x.json())
+          .then(x => {
+            console.log(x);
+            setLoadingUsers(false);
+            setUsers(x);
+          })
+          .catch(e => {
+            console.log(e);
+            setLoadingUsers(false);
+          });
+      }, 300);
+    }
+
+    return () => {
+      if (timeoutId) {
+        clearTimeout(timeoutId);
+      }
+    };
+  }, [query, mention]);
+
+  const onKeyDown = event => {
+    // If we have a mention and using the arrow keys or hitting enter
+    if (
+      mention &&
+      (event.keyCode === 38 || event.keyCode === 40 || event.keyCode === 13)
+    ) {
+      event.preventDefault();
+
+      // Hitting enter adds the mention
+      if (event.keyCode === 13) {
+        updateUserResources({
+          ...userResourses,
+          ['@' + filteredUsers[menuIndex]]: '/whatever',
+        });
+        setValue(
+          value.substr(0, mention.startIndex) +
+            '@' +
+            filteredUsers[menuIndex] +
+            value.substr(mention.endIndex + 1)
+        );
+        setMenuIndex(0);
+      } else {
+        // Change selection in the mentions list
+        const add = event.keyCode === 38 ? -1 : 1;
+        const change = menuIndex + add;
+
+        if (change < 0) {
+          setMenuIndex(0);
+        } else if (change > filteredUsers.length - 1) {
+          setMenuIndex(filteredUsers.length - 1);
+        } else {
+          setMenuIndex(change);
+        }
+      }
+    }
+    // When hitting "@" at the very start or after a space, open
+    // the users list
+    if (
+      event.key === '@' &&
+      (!event.target.value[event.target.selectionStart - 1] ||
+        event.target.value[event.target.selectionStart - 1] === ' ')
+    ) {
+      openUsersList();
+    } else if (mention) {
+      // If we have a mention and hit space we want to hide it
+      if (event.keyCode === 32) {
+        setMention(null);
+      } else {
+        // Or we want to update the mention range
+        const newLength = event.target.value.length;
+        const endIndex = mention.endIndex + (newLength - mention.length);
+        setMention({
+          ...mention,
+          endIndex,
+          length: newLength,
+        });
+      }
+    }
+  };
+
+  const onKeyUp = React.useCallback(() => {
+    // When we clear out all text we want to close mention
+    if (!value.length) {
+      setMention(null);
+    }
+    // If we have moved the selection out of range of existing mention,
+    // close it
+    if (
+      mention &&
+      (ref.current.selectionStart < mention.startIndex ||
+        ref.current.selectionStart > mention.endIndex + 1)
+    ) {
+      setMention(null);
+    } else {
+      const words = value.split(' ');
+      // Based on current caret position, figure out if we have entered
+      // a mention
+      const { mention: mentionNew } = words.reduce(
+        (aggr, word) => {
+          if (aggr.mention) {
+            return aggr;
+          }
+
+          const currentIndex = aggr.index;
+          if (
+            word.startsWith('@') &&
+            !userResourses[word] &&
+            ref.current.selectionStart >= currentIndex &&
+            ref.current.selectionStart <= currentIndex + word.length
+          ) {
+            const caret = getCaretCoordinates(ref.current, currentIndex + 1);
+            return {
+              mention: {
+                startIndex: currentIndex,
+                endIndex: currentIndex + word.length - 1,
+                length: ref.current.value.length,
+                top: caret.top,
+                left: caret.left,
+              },
+            };
+          }
+
+          return {
+            index: currentIndex + word.length + 1, // 1 is for the spacing
+          };
+        },
+        {
+          index: 0,
+          mention: null,
+        }
+      );
+      if (mentionNew) {
+        setMention(mentionNew);
+      }
+    }
+  }, [mention, value, userResourses]);
+
+  const filteredUsers = query
+    ? users.filter(user => user.toLowerCase().startsWith(query.toLowerCase()))
+    : [];
+
+  // Replaces the comment content with links to mentions
+  const output = Object.keys(userResourses).reduce((aggr, userMention) => {
+    return aggr.replace(
+      userMention,
+      `[${userMention}](user://${userResourses[userMention]})`
+    );
+  }, value);
+
+  value
+    .split(' ')
+    .map(word => {
+      if (userResourses[word]) {
+        return `[${word}](user://${userResourses[word]})`;
+      }
+
+      return word;
+    })
+    .join(' ');
+
+  // Opens the mentions list by looking at the current selection
+  function openUsersList() {
+    const caret = getCaretCoordinates(ref.current, ref.current.selectionEnd);
+    setMention({
+      startIndex: ref.current.selectionStart,
+      endIndex: ref.current.selectionStart,
+      length: ref.current.value.length,
+      top: caret.top,
+      left: caret.left,
+    });
+  }
+
+  return [filteredUsers, onKeyDown, onKeyUp, loadingUsers, mention];
+};
