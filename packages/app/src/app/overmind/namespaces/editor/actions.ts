ec86ab2d738c4209b22a27e35df1b7a3dde47c25
Christian Alfoni <christianalfoni@gmail.com>
2020-06-22 13:03:26 +0200

    use permission system instead

--- packages/app/src/app/overmind/namespaces/editor/actions.ts
+++ packages/app/src/app/overmind/namespaces/editor/actions.ts
@@ -290,7 +290,7 @@ export const sandboxChanged: AsyncAction<{ id: string }> = withLoadApp<{
     actions.comments.getSandboxComments(sandbox.id);
   }
 
-  if (sandbox.originalGit && sandbox.owned) {
+  if (sandbox.originalGit && hasPermission(sandbox.authorization, 'owner')) {
     actions.git.loadGitSource();
   }
 
