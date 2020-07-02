40e957efd09af015dbd96d4a0e6efbffee54ef8b
Sara Vieira <hey@iamsaravieira.com>
2020-07-02 16:00:57 +0200

    delete comment

--- packages/app/src/app/pages/NewDashboard/Components/VariableGrid/index.tsx
+++ packages/app/src/app/pages/NewDashboard/Components/VariableGrid/index.tsx
@@ -90,7 +90,6 @@ const ComponentForTypes: ComponentForTypes = {
   )),
   folder: props => <Folder {...props.item} />,
   repo: props => <Repo {...props.item} isScrolling={props.isScrolling} />,
-  // @ts-ignore TODO: find a better way to type this
   'new-folder': props => <CreateFolder {...props.item} />,
   'new-sandbox': () => <NewSandbox />,
   'new-repo': () => <ImportRepo />,
