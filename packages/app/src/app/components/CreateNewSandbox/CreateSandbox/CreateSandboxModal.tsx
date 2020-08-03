848b4818c6181dad49ee502a4f201b095b4c747f
Sara Vieira <hey@iamsaravieira.com>
2020-08-03 15:32:01 +0200

    add prop

--- packages/app/src/app/components/CreateNewSandbox/CreateSandbox/CreateSandboxModal.tsx
+++ packages/app/src/app/components/CreateNewSandbox/CreateSandbox/CreateSandboxModal.tsx
@@ -47,10 +47,10 @@ export const CreateSandboxModal = () => {
   return (
     <ThemeProvider>
       <Modal
-        style={{ margin: 0, zIndex: 999 }}
         isOpen={modals.newSandboxModal.isCurrent}
         onClose={() => modalsActions.newSandboxModal.close()}
         width={window.outerWidth > COLUMN_MEDIA_THRESHOLD ? 1200 : 950}
+        fullWidth={window.screen.availWidth < 800}
       >
         <CreateSandbox
           isModal
