3f8092dbf0edf483958c8879dfd36665609760ef
siddharthkp <siddharth.kshetrapal@gmail.com>
2020-08-13 14:35:51 +0200

    pull all sandboxes out

--- packages/app/src/app/pages/Profile2/index.tsx
+++ packages/app/src/app/pages/Profile2/index.tsx
@@ -1,19 +1,10 @@
 import React from 'react';
-import {
-  ThemeProvider,
-  Element,
-  Stack,
-  Grid,
-  Column,
-  Text,
-  Stats,
-  Link,
-} from '@codesandbox/components';
-import css from '@styled-system/css';
-import { sandboxUrl } from '@codesandbox/common/lib/utils/url-generator';
 import { useOvermind } from 'app/overmind';
+import { ThemeProvider, Element, Stack } from '@codesandbox/components';
+import css from '@styled-system/css';
 import { Header } from './Header';
 import { ProfileCard } from './ProfileCard';
+import { AllSandboxes } from './AllSandboxes';
 
 export const Profile = props => {
   const { username } = props.match.params;
@@ -76,86 +67,5 @@ export const Profile = props => {
         </Stack>
       </Stack>
     </ThemeProvider>
-  );
-};
-
-const AllSandboxes = () => {
-  const {
-    actions: {
-      profile: { sandboxesPageChanged },
-    },
-    state: {
-      profile: {
-        current: { username },
-        isLoadingSandboxes,
-        sandboxes: fetchedSandboxes,
-      },
-    },
-  } = useOvermind();
-
-  const [page] = React.useState(0);
-
-  React.useEffect(() => {
-    sandboxesPageChanged(page);
-  }, [sandboxesPageChanged, page]);
-
-  if (isLoadingSandboxes) return <span>loading</span>;
-
-  if (!fetchedSandboxes[username]) return <span>none</span>;
-
-  const sandboxes = fetchedSandboxes[username][page];
-
-  return (
-    <Grid
-      rowGap={6}
-      columnGap={6}
-      css={{
-        gridTemplateColumns: 'repeat(auto-fit, minmax(300px, 1fr))',
-      }}
-    >
-      {sandboxes.map(sandbox => (
-        <Column key={sandbox.id}>
-          <Stack
-            as={Link}
-            href={sandboxUrl({ id: sandbox.id, alias: sandbox.alias })}
-            direction="vertical"
-            gap={4}
-            css={css({
-              backgroundColor: 'grays.700',
-              border: '1px solid',
-              borderColor: 'grays.600',
-              borderRadius: 'medium',
-              overflow: 'hidden',
-              ':hover, :focus, :focus-within': {
-                boxShadow: theme => '0 4px 16px 0 ' + theme.colors.grays[900],
-              },
-            })}
-          >
-            <div
-              css={css({
-                display: 'flex',
-                alignItems: 'center',
-                justifyContent: 'center',
-                height: '160px',
-                backgroundColor: 'grays.600',
-                backgroundSize: 'cover',
-                backgroundPosition: 'top center',
-                backgroundRepeat: 'no-repeat',
-                borderBottom: '1px solid',
-                borderColor: 'grays.600',
-              })}
-              style={{
-                backgroundImage: `url(${sandbox.screenshotUrl ||
-                  `/api/v1/sandboxes/${sandbox.id}/screenshot.png`})`,
-              }}
-            />
-            <Stack direction="vertical" gap={2} marginX={4} marginBottom={4}>
-              <Text>{sandbox.title || sandbox.alias || sandbox.id}</Text>
-              <Stats sandbox={sandbox} />
-            </Stack>
-          </Stack>
-        </Column>
-      ))}
-    </Grid>
   );
 };
