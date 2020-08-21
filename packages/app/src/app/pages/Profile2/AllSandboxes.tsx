675965f6f7ad0ac8bde8af13289751fbc0bd14cd
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2020-08-21 11:26:15 +0200

    WIP: Profile pagination (#4744)

--- packages/app/src/app/pages/Profile2/AllSandboxes.tsx
+++ packages/app/src/app/pages/Profile2/AllSandboxes.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Grid, Column, Stack, Text } from '@codesandbox/components';
+import { Grid, Column, Stack, Text, IconButton } from '@codesandbox/components';
+import css from '@styled-system/css';
 import { useOvermind } from 'app/overmind';
-import { SandboxCard } from './SandboxCard';
+import { SandboxCard, SkeletonCard } from './SandboxCard';
 
 export const AllSandboxes = ({ menuControls }) => {
   const {
@@ -11,25 +12,25 @@ export const AllSandboxes = ({ menuControls }) => {
     state: {
       profile: {
         current: { username, featuredSandboxes },
+        currentSandboxesPage,
         isLoadingSandboxes,
         sandboxes: fetchedSandboxes,
       },
     },
   } = useOvermind();
 
-  const [page] = React.useState(0);
-
-  React.useEffect(() => {
-    sandboxesPageChanged(page);
-  }, [sandboxesPageChanged, page]);
-
-  if (isLoadingSandboxes) return <span>loading</span>;
-
-  if (!fetchedSandboxes[username]) return <span>none</span>;
-
   const featuredSandboxIds = featuredSandboxes.map(sandbox => sandbox.id);
 
-  const sandboxes = fetchedSandboxes[username][page]
+  // explicitly call it on first page render
+  React.useEffect(() => {
+    if (currentSandboxesPage === 1) sandboxesPageChanged(currentSandboxesPage);
+  }, [currentSandboxesPage, sandboxesPageChanged]);
+
+  const sandboxes = (
+    (fetchedSandboxes[username] &&
+      fetchedSandboxes[username][currentSandboxesPage]) ||
+    []
+  )
     // filter out featured sandboxes so that we don't show them twice
     .filter(sandbox => !featuredSandboxIds.includes(sandbox.id))
     // only show public sandboxes on profile
@@ -49,12 +50,71 @@ export const AllSandboxes = ({ menuControls }) => {
           gridTemplateColumns: 'repeat(auto-fit, minmax(300px, 1fr))',
         }}
       >
-        {sandboxes.map(sandbox => (
-          <Column key={sandbox.id}>
-            <SandboxCard sandbox={sandbox} menuControls={menuControls} />
-          </Column>
-        ))}
+        {isLoadingSandboxes
+          ? Array(15)
+              .fill(true)
+              .map((_, index) => (
+                // eslint-disable-next-line
+                <Column key={index}>
+                  <SkeletonCard />
+                </Column>
+              ))
+          : sandboxes.map((sandbox, index) => (
+              <Column key={sandbox.id}>
+                <SandboxCard sandbox={sandbox} menuControls={menuControls} />
+              </Column>
+            ))}
       </Grid>
+      <Pagination />
     </Stack>
+  );
+};
+
+const SANDBOXES_PER_PAGE = 15;
+const Pagination = () => {
+  const {
+    actions: {
+      profile: { sandboxesPageChanged },
+    },
+    state: {
+      profile: {
+        currentSandboxesPage,
+        current: { sandboxCount, templateCount },
+      },
+    },
+  } = useOvermind();
+
+  const numberOfPages = Math.ceil(
+    (sandboxCount + templateCount) / SANDBOXES_PER_PAGE
+  );
+
+  return (
+    <nav role="navigation" aria-label="Pagination Navigation">
+      <Stack
+        as="ul"
+        gap={4}
+        justify="center"
+        align="center"
+        css={css({ marginX: 0, marginY: 10, listStyle: 'none' })}
+      >
+        <li>
+          <IconButton
+            name="backArrow"
+            title="Previous page"
+            onClick={() => sandboxesPageChanged(currentSandboxesPage - 1)}
+            disabled={currentSandboxesPage === 1}
+          />
+        </li>
+        <li>
+          <IconButton
+            name="backArrow"
+            title="Next page"
+            style={{ transform: 'scaleX(-1)' }}
+            onClick={() => sandboxesPageChanged(currentSandboxesPage + 1)}
+            disabled={currentSandboxesPage === numberOfPages}
+          />
+        </li>
+      </Stack>
+    </nav>
   );
 };
