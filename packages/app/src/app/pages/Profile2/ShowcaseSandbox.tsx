faa69c77dde187ad5d6a629dff83bb9616177a7a
siddharthkp <siddharth.kshetrapal@gmail.com>
2020-09-09 10:07:28 +0200

    add open sandbox button to showcased

--- packages/app/src/app/pages/Profile2/ShowcaseSandbox.tsx
+++ packages/app/src/app/pages/Profile2/ShowcaseSandbox.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useOvermind } from 'app/overmind';
-import { Element } from '@codesandbox/components';
+import { Element, Button } from '@codesandbox/components';
+import { sandboxUrl } from '@codesandbox/common/lib/utils/url-generator';
 import css from '@styled-system/css';
 
 export const ShowcaseSandbox = () => {
@@ -13,21 +14,32 @@ export const ShowcaseSandbox = () => {
   if (!showcasedSandbox) return null;
 
   return (
-    <Element
-      as="iframe"
-      src={`https://${showcasedSandbox.id}.csb.app?standalone=1`}
-      css={css({
-        backgroundColor: 'white',
-        width: '100%',
-        height: 360,
-        borderRadius: '4px',
-        overflow: 'hidden',
-        border: '1px solid',
-        borderColor: 'grays.600',
-      })}
-      title="React"
-      allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
-      sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
-    />
+    <Element css={{ position: 'relative' }}>
+      <Element
+        as="iframe"
+        src={`https://${showcasedSandbox.id}.csb.app?standalone=1`}
+        css={css({
+          backgroundColor: 'white',
+          width: '100%',
+          height: 360,
+          borderRadius: '4px',
+          overflow: 'hidden',
+          border: '1px solid',
+          borderColor: 'grays.600',
+        })}
+        title="React"
+        allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
+        sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
+      />
+      <Button
+        as="a"
+        href={sandboxUrl({ id: showcasedSandbox.id })}
+        variant="secondary"
+        autoWidth
+        style={{ position: 'absolute', bottom: 16, right: 16 }}
+      >
+        Open sandbox
+      </Button>
+    </Element>
   );
 };
