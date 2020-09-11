da8559943a7a2e27cf5f673ff76f72f01f0c89b1
Christian Alfoni <christianalfoni@gmail.com>
2020-09-11 10:41:11 +0000

    try with utf-8

--- packages/app/src/app/overmind/namespaces/git/actions.ts
+++ packages/app/src/app/overmind/namespaces/git/actions.ts
@@ -860,7 +860,7 @@ export const _getGitChanges: AsyncAction<void, GitChanges> = async ({
           return {
             path,
             content: await effects.http.blobToBase64(module!.code),
-            encoding: 'base64' as 'base64',
+            encoding: 'utf-8' as 'utf-8',
           };
         }
 
