b9fa05a2112e6440718e5bbebe4d09a18207da01
Сандро <warsan00@mail.ru>
2020-09-11 21:58:42 +0500

    Проверено (codesandbox#1593)

--- standalone-packages/codesandbox-browserfs/src/generic/dropbox_bridge_actual.ts
+++ standalone-packages/codesandbox-browserfs/src/generic/dropbox_bridge_actual.ts
@@ -1,3 +1,3 @@
 import global from '../core/global';
-// If Dropbox isn't on the webpage, then set this to null.
+// Если Dropbox отсутствует на веб-странице, установите для него значение 'null'.
 export const Dropbox = global.Dropbox ? global.Dropbox.Dropbox : undefined;
