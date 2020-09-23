de2fbe0937645e77e4d4d48f8ab4ca09bc6a2674
Sara Vieira <hey@iamsaravieira.com>
2020-09-23 14:28:40 +0200

    add optional

--- packages/app/src/app/pages/Sandbox/Editor/Workspace/screens/Explorer/Dependencies/AddDependency.tsx
+++ packages/app/src/app/pages/Sandbox/Editor/Workspace/screens/Explorer/Dependencies/AddDependency.tsx
@@ -72,7 +72,7 @@ export const AddDependency: FunctionComponent<{ readonly?: boolean }> = () => {
           if (dependency === 'OPEN_MODAL') {
             modalOpened({ modal: 'searchDependencies' });
             clearExplorerDependencies();
-          } else if (dependency.name) {
+          } else if (dependency?.name) {
             addNpmDependency({
               name: dependency.name,
               version: dependency.tags.latest,
