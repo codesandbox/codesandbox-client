f0c2166925a44e063bf8353b4e3d5c31455a8090
Sara Vieira <hey@iamsaravieira.com>
2020-10-06 19:02:27 +0200

    first version of jobs

--- packages/homepage/src/pages/jobs/_elements.js
+++ packages/homepage/src/pages/jobs/_elements.js
@@ -1,16 +1,22 @@
 import styled from 'styled-components';
 
 export const PageTitle = styled.h1`
-  font-weight: 600;
-  font-size: 36px;
+  font-weight: 900;
+  font-size: 48px;
   color: ${props => props.theme.white};
+  margin-bottom: 40px;
+
+  @media screen and (max-width: 900px) {
+    text-align: center;
+  }
 `;
 
 export const PageSubtitle = styled.h2`
-  font-weight: 600;
-  font-size: 24px;
+  font-size: 23px;
   line-height: 1.5;
   color: ${props => props.theme.white};
+  font-weight: bold;
+  margin-bottom: 24px;
 `;
 
 export const TitleDescription = styled.p`
@@ -23,21 +29,15 @@ export const TitleDescription = styled.p`
 
 export const Job = styled.li`
   list-style: none;
-  font-weight: 400;
-  font-size: 18px;
+  font-size: 19px;
   line-height: 33px;
-  letter-spacing: -0.04em;
-  margin-bottom: 16px;
+  height: 64px;
   color: ${props => props.theme.lightText};
   display: flex;
   justify-content: space-between;
   align-items: center;
-
-  a {
-    font-size: 14px;
-    min-width: 100px;
-    line-height: 1;
-  }
+  border-top: 1px solid #343434;
+  border-bottom: 1px solid #343434;
 `;
 
 export const Jobs = styled.ul`
@@ -46,26 +46,49 @@ export const Jobs = styled.ul`
   margin-bottom: 50px;
 `;
 
-export const OtherJobs = styled.div`
-  display: flex;
-  color: ${props => props.theme.lightText};
-  font-size: 14px;
-  margin-top: 12px;
-  justify-content: space-between;
-  align-items: center;
-  padding-bottom: 3rem;
+export const HeroSection = styled.div`
+  display: grid;
+  grid-template-columns: 522px 1fr;
+  grid-gap: 100px;
+  margin-bottom: 140px;
 
-  align-items: flex-start;
+  @media screen and (max-width: 900px) {
+    grid-template-columns: 522px;
+    justify-content: center;
+    grid-gap: 40px;
+  }
+`;
 
-  @media screen and (max-width: 500px) {
-    flex-direction: column;
+export const ImageGallery = styled.div`
+  margin-bottom: 140px;
+  display: grid;
+  grid-template-columns: 322px 304px 443px;
+  grid-gap: 8;
+  justify-content: center;
+
+  section {
+    display: grid;
+    grid-template-rows: repeat(2, 1fr);
+    align-items: stretch;
   }
 
-  p {
-    max-width: 70%;
+  img {
+    max-height: 100%;
+  }
 
-    @media screen and (max-width: 500px) {
-      max-width: 100%;
+  @media screen and (max-width: 1200px) {
+    grid-template-columns: 322px 304px;
+
+    > img:last-child {
+      display: none;
+    }
+  }
+
+  @media screen and (max-width: 660px) {
+    grid-template-columns: 322px;
+
+    > img:nth-child(2) {
+      display: none;
     }
   }
 `;
--- packages/homepage/src/pages/jobs/index.js
+++ packages/homepage/src/pages/jobs/index.js
@@ -1,12 +1,16 @@
 import { graphql, Link } from 'gatsby';
 import React from 'react';
 
-import { Button, ThemeProvider } from '@codesandbox/components';
+import { ThemeProvider } from '@codesandbox/components';
 import codesandboxBlack from '@codesandbox/components/lib/themes/codesandbox-black';
 
 import Layout from '../../components/layout';
 import PageContainer from '../../components/PageContainer';
 import TitleAndMetaTags from '../../components/TitleAndMetaTags';
+import imageOne from '../../assets/images/jobs/one.png';
+import imageTwo from '../../assets/images/jobs/two.png';
+import imageThree from '../../assets/images/jobs/three.png';
+import imageFour from '../../assets/images/jobs/four.png';
 
 import {
   PageTitle,
@@ -14,7 +18,8 @@ import {
   TitleDescription,
   Job,
   Jobs,
-  OtherJobs,
+  ImageGallery,
+  HeroSection,
 } from './_elements';
 
 const Careers = ({
@@ -30,71 +35,160 @@ const Careers = ({
           title="Careers - CodeSandbox"
         />
 
-        <PageTitle>Join the team</PageTitle>
-
-        <TitleDescription>
-          CodeSandbox is a company that aims to make it easy for everyone to
-          create applications. We want to cut away the hassle of setting up the
-          environment, installing the tooling and sharing your project with
-          others. We are specifically focused on lowering the barrier to entry
-          of web development and want to make creating applications both easier
-          and faster.
-          <br />
-          <br />
-          Do you resonate with this goal? Then CodeSandbox is the right place
-          for you! At CodeSandbox you have the chance to work on development
-          tooling that’s used by more than a million people on a monthly basis.
-          You’ll get the chance to be part of the founding team and will have a
-          big impact on how the online development tooling will be shaped in the
-          future. We’re also an open source company, the majority of our work is
-          in the open and the community is a big part of us.
-          <br />
-          <br />
-          Our team is remote-first, you can work from wherever you’d like. Our
-          current team is distributed across The Netherlands, Romania and
-          Germany.
-          <br />
-          <br />
-          We’re looking forward to meeting you!
-        </TitleDescription>
-
-        <PageSubtitle>Job Openings</PageSubtitle>
+        <PageTitle>Join CodeSandbox</PageTitle>
+        <HeroSection>
+          <TitleDescription>
+            CodeSandbox provides free, instant, collaborative sandboxes for
+            rapid web development. Founded in 2017, we’ve raised more than $15M
+            from top-tier VCs and prominent industry figures, including EQT
+            Ventures, Kleiner Perkins, Christian Bach & Mathias Biilmann
+            (Netlify), and Dylan Field (Figma).
+            <br />
+            <br />
+            Used by over two million developers each month, including within
+            organizations like Shopify, Atlassian, and Stripe, creators have
+            crafted over 10M apps on the platform since launch. It’s used by
+            thousands of open source projects, including React, Vue, and Babel,
+            among others.
+            <br />
+            <br />
+            We’re making it easier for everyone to create things with code.
+            We’re removing the hassle of setting up a development environment,
+            installing tooling, and sharing your project with others. Join us at
+            CodeSandbox and help build the future of coding on the web.
+          </TitleDescription>
+          <div>
+            The Perks:
+            <ul
+              css={`
+                margin: 0;
+                margin-top: 8px;
+                padding: 0;
+                list-style: none;
+                li {
+                  span {
+                    color: #2286f7;
+                  }
+                }
+              `}
+            >
+              <li>
+                <span>✓</span> Remote-first
+              </li>
+              <li>
+                <span>✓</span> Flexible hours
+              </li>
+              <li>
+                <span>✓</span> Unlimited vacation
+              </li>
+              <li>
+                <span>✓</span> Choose your own equipment
+              </li>
+              <li>
+                <span>✓</span> Generous parental leave
+              </li>
+            </ul>
+          </div>
+        </HeroSection>
+        <ImageGallery>
+          <section>
+            <img src={imageOne} alt="Office" />
+            <img src={imageTwo} alt="Office" />
+          </section>
+          <img src={imageThree} alt="Amsterdam" />
+          <img src={imageFour} alt="Office" />
+        </ImageGallery>
+        <div
+          css={`
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+          `}
+        >
+          <PageTitle>Our Values</PageTitle>
+          <TitleDescription
+            css={`
+              width: 640px;
+              max-width: 80%;
+            `}
+          >
+            Everything we do at CodeSandbox derives from our values. The product
+            we create is a reflection of how we work together as a team. That’s
+            why our values are the same, both inside and out.
+            <br />
+            <br />
+            We’re building a team, tool, and community that’s:
+            <br />
+            <br />
+            <strong>Accessible</strong> - we’re making development accessible to
+            all, building a reality where everyone can be a creator.
+            <br />
+            <br />
+            <strong>Collaborative</strong> - providing people with the means to
+            encourage and help each other.
+            <br />
+            <br />
+            <strong>Empowering</strong> - enabling creators to feel like they
+            can build without limits.
+          </TitleDescription>
+        </div>
+        <div
+          css={`
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+          `}
+        >
+          <PageTitle>How we work</PageTitle>
+          <TitleDescription
+            css={`
+              width: 640px;
+              max-width: 80%;
+            `}
+          >
+            We follow a peer-led development approach built around autonomous
+            teams. These teams form to create a feature or complete a project
+            from start to finish. We ideate, scope, and implement—we’re not code
+            monkeys or pixel pushers, but creative professionals who use our
+            full range of skills to get things done.
+            <br />
+            <br />
+            With advice from peers, a clear strategy, and a supportive culture,
+            it’s still challenging work that can push us out of our comfort
+            zones. Ultimately though, we find it more fulfilling and delivers
+            better results than other approaches.
+          </TitleDescription>
+        </div>
+        <PageSubtitle>Open Positions</PageSubtitle>
 
         {jobs.map(({ node: { fields: { slug, title }, id } }) => (
           <Jobs key={id}>
-            <Job>
-              {title}
+            <Link
+              to={`/job/${slug}`}
+              css={`
+                text-decoration: none;
+              `}
+            >
+              <Job>
+                <span
+                  css={`
+                    font-weight: bold;
+                  `}
+                >
+                  {title}
+                </span>
 
-              <Button autoWidth as={Link} to={`/job/${slug}`}>
-                Learn more
-              </Button>
-            </Job>
+                <span
+                  css={`
+                    color: #757575;
+                  `}
+                >
+                  Remote (EU timezones)
+                </span>
+              </Job>
+            </Link>
           </Jobs>
         ))}
-
-        <TitleDescription
-          css={`
-            margin-bottom: 0;
-          `}
-        >
-          Don't see a position that fits your skill set?
-        </TitleDescription>
-
-        <OtherJobs>
-          <p>
-            We are always looking for talented, hard working people. Drop us a
-            line and show us your work.
-          </p>
-
-          <Button
-            as="a"
-            href="mailto:careers@codesandbox.io"
-            autoWidth
-            target="_blank"
-          >
-            Contact Us
-          </Button>
-        </OtherJobs>
       </PageContainer>
     </ThemeProvider>
   </Layout>
