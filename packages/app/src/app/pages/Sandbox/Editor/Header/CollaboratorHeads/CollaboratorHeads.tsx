f50def4916b4bfaccd9faaf23dc6565b5a1f982f
Ives van Hoorne <ives.v.h@gmail.com>
2020-10-06 17:28:58 +0200

    Fix collaborator heads not updating

--- packages/app/src/app/pages/Sandbox/Editor/Header/CollaboratorHeads/CollaboratorHeads.tsx
+++ packages/app/src/app/pages/Sandbox/Editor/Header/CollaboratorHeads/CollaboratorHeads.tsx
@@ -105,10 +105,7 @@ const CollaboratorHead = (props: ICollaboratorHeadProps) => (
 
 export const CollaboratorHeads: FunctionComponent = () => {
   const { state, actions } = useOvermind();
-
-  const liveUsers = React.useMemo(() => state.live.roomInfo?.users || [], [
-    state.live.roomInfo,
-  ]);
+  const liveUsers = state.live.roomInfo?.users || [];
 
   const liveUserId = state.live.liveUserId;
   const followingUserId = state.live.followingUserId;
