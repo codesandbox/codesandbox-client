365c65549d7ef50b56288bcc4a35d8d8ab9f1b99
Yash Joshi <jyash97@gmail.com>
2020-10-08 02:48:29 +0530

    feat(dependencies): show installed version on dependency modal (#4943)
    Co-authored-by: Sara Vieira <hey@iamsaravieira.com>

--- packages/app/src/app/pages/Sandbox/SearchDependencies/Dependency.tsx
+++ packages/app/src/app/pages/Sandbox/SearchDependencies/Dependency.tsx
@@ -15,77 +15,96 @@ import {
   Link,
   Button,
 } from '@codesandbox/components';
+import { notificationState } from '@codesandbox/common/lib/utils/notifications';
+import { NotificationStatus } from '@codesandbox/notifications';
 import { HomeIcon, GitHubIcon, CSBIcon } from './icons';
 
-const checkboxStyles = css({
-  position: 'relative',
-  display: 'flex',
-  alignItems: 'center',
-
-  "input[type='checkbox']": {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    height: 32,
-    width: 32,
-    appearance: 'none',
-    cursor: 'pointer',
-    border: 'none',
-    background: 'transparent',
-  },
-  'input[type="checkbox"]:checked + label:after': {
-    transform: 'scale(1)',
-  },
-  'input[type="checkbox"]:checked + label:before': {
-    borderColor: 'transparent',
-  },
-  label: {
-    display: 'flex',
-    cursor: 'pointer',
+const checkboxStyles = ({ areSameVersion }) =>
+  css({
     position: 'relative',
+    display: 'flex',
+    alignItems: 'center',
 
-    ':after, :before': {
-      pointerEvents: 'none',
-      cursor: 'pointer',
-    },
-    ':before': {
-      display: 'flex',
-      content: "' '",
-      height: 21,
-      width: 21,
-      borderWidth: '1px',
-      borderStyle: 'solid',
-      borderColor: 'grays.400',
-      borderRadius: '50%',
-      marginRight: 2,
-      boxSizing: 'border-box',
-    },
-    ':after': {
+    "input[type='checkbox']": {
       position: 'absolute',
       top: 0,
       left: 0,
-      display: 'flex',
-      content: "' '",
-      height: 21,
-      width: 21,
-      backgroundImage: `url('data:image/svg+xml,%3Csvg width="22" height="23" viewBox="0 0 22 23" fill="none" xmlns="http://www.w3.org/2000/svg"%3E%3Cpath fill-rule="evenodd" clip-rule="evenodd" d="M11 22.2808C17.0751 22.2808 22 17.3559 22 11.2808C22 5.20563 17.0751 0.280762 11 0.280762C4.92487 0.280762 0 5.20563 0 11.2808C0 17.3559 4.92487 22.2808 11 22.2808ZM17 7.28076L10.4971 13.603L6.39416 9.6141L5 11.0481L10.4971 16.3925L18.3942 8.71475L17 7.28076Z" fill="%235BC266"/%3E%3C/svg%3E%0A')`,
-      transform: 'scale(0)',
-      borderRadius: '50%',
-      transition: 'transform .3s ease',
+      height: 32,
+      width: 32,
+      appearance: 'none',
+      cursor: 'pointer',
+      border: 'none',
+      background: 'transparent',
     },
-  },
-});
+    'input[type="checkbox"]:checked + label:after': {
+      transform: 'scale(1)',
+    },
+    'input[type="checkbox"]:checked + label:before': {
+      borderColor: 'transparent',
+    },
+    label: {
+      display: 'flex',
+      cursor: 'pointer',
+      position: 'relative',
+
+      ':after, :before': {
+        pointerEvents: 'none',
+        cursor: 'pointer',
+      },
+      ':before': {
+        display: 'flex',
+        content: "' '",
+        height: 21,
+        width: 21,
+        borderWidth: '1px',
+        borderStyle: 'solid',
+        borderColor: 'grays.400',
+        borderRadius: '50%',
+        marginRight: 2,
+        boxSizing: 'border-box',
+      },
+      ':after': {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        display: 'flex',
+        content: "' '",
+        height: 21,
+        width: 21,
+        /* Show a gray background if the version is already installed, else show a green color background for a new version */
+        backgroundImage: `url('data:image/svg+xml,%3Csvg width="22" height="23" viewBox="0 0 22 23" fill="none" xmlns="http://www.w3.org/2000/svg"%3E%3Cpath fill-rule="evenodd" clip-rule="evenodd" d="M11 22.2808C17.0751 22.2808 22 17.3559 22 11.2808C22 5.20563 17.0751 0.280762 11 0.280762C4.92487 0.280762 0 5.20563 0 11.2808C0 17.3559 4.92487 22.2808 11 22.2808ZM17 7.28076L10.4971 13.603L6.39416 9.6141L5 11.0481L10.4971 16.3925L18.3942 8.71475L17 7.28076Z" fill="%23${
+          areSameVersion ? 'fff3' : '5BC266'
+        }"/%3E%3C/svg%3E%0A')`,
+        transform: 'scale(0)',
+        borderRadius: '50%',
+        transition: 'transform .3s ease',
+      },
+    },
+  });
 
 export const Dependency = ({ dependency }: { dependency: DependencyType }) => {
   const {
     state: {
       workspace: { selectedDependencies, hitToVersionMap },
+      editor: {
+        parsedConfigurations: {
+          package: {
+            parsed: { dependencies: installedDependencies },
+          },
+        },
+      },
     },
     actions,
   } = useOvermind();
 
   const selectedVersion =
     hitToVersionMap[dependency.objectID] || dependency.tags.latest;
+
+  const versionInstalled = installedDependencies[dependency.name];
+
+  const isDependencySelected = Boolean(
+    json(selectedDependencies)[dependency.objectID]
+  );
 
   const versions = Object.keys(dependency.versions).sort((a, b) => {
     try {
@@ -98,10 +117,42 @@ export const Dependency = ({ dependency }: { dependency: DependencyType }) => {
   const getTagName = (tags: DependencyType['tags'], version: string) =>
     Object.keys(tags).find(key => tags[key] === version);
 
+  const onVersionSelect = e => {
+    const {
+      target: { value },
+    } = e;
+
+    /*
+      Since the installed version is not added in selected dependency,
+      we add it and than change the version.
+    */
+    if (versionInstalled && !isDependencySelected) {
+      actions.workspace.setSelectedDependencies(dependency);
+    }
+
+    actions.workspace.handleVersionChange({
+      dependency,
+      version: value,
+    });
+  };
+
+  const onDependencySelect = () => {
+    /* Ignore the dependency if the same version is already installed */
+    if (versionInstalled === selectedVersion) {
+      notificationState.addNotification({
+        message: `${dependency.name} v${versionInstalled} is already installed`,
+        status: NotificationStatus.WARNING,
+      });
+      return;
+    }
+
+    actions.workspace.setSelectedDependencies(dependency);
+  };
+
   return (
     <Button
       variant="link"
-      onClick={() => actions.workspace.setSelectedDependencies(dependency)}
+      onClick={onDependencySelect}
       css={css({
         height: 'auto',
         ':hover, :focus': {
@@ -128,14 +179,16 @@ export const Dependency = ({ dependency }: { dependency: DependencyType }) => {
           borderBottomColor: 'sideBar.border',
         })}
       >
-        <Element css={checkboxStyles}>
+        <Element
+          css={checkboxStyles({
+            areSameVersion: selectedVersion === versionInstalled,
+          })}
+        >
           <input
             type="checkbox"
             id={dependency.name}
-            checked={Boolean(json(selectedDependencies)[dependency.objectID])}
-            onChange={() =>
-              actions.workspace.setSelectedDependencies(dependency)
-            }
+            checked={Boolean(isDependencySelected || versionInstalled)}
+            onChange={onDependencySelect}
           />
           <label htmlFor={dependency.name} />
         </Element>
@@ -191,13 +244,8 @@ export const Dependency = ({ dependency }: { dependency: DependencyType }) => {
           <Element css={{ flexShrink: 0, width: 208 }}>
             <Select
               onClick={e => e.stopPropagation()}
-              defaultValue={selectedVersion}
-              onChange={e => {
-                actions.workspace.handleVersionChange({
-                  dependency,
-                  version: e.target.value,
-                });
-              }}
+              value={selectedVersion || versionInstalled}
+              onChange={onVersionSelect}
             >
               {versions.map(v => {
                 const tagName = getTagName(dependency.tags, v);
