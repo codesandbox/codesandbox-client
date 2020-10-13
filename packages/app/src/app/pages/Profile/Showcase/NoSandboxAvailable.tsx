65d4d9354e25e2599db92af1e57d2f09640f470a
Arthur Denner <arthurdenner7@gmail.com>
2020-10-13 13:48:09 +0200

    feat(profile): allow showcased sandbox selection when none is selected

--- packages/app/src/app/pages/Profile/Showcase/NoSandboxAvailable.tsx
+++ packages/app/src/app/pages/Profile/Showcase/NoSandboxAvailable.tsx
@@ -1,6 +1,7 @@
 import React, { FunctionComponent } from 'react';
 import Centered from '@codesandbox/common/lib/components/flex/Centered';
 import Margin from '@codesandbox/common/lib/components/spacing/Margin';
+import { Button } from '@codesandbox/common/lib/components/Button';
 
 import { useOvermind } from 'app/overmind';
 
@@ -8,6 +9,9 @@ import { ErrorTitle } from './elements';
 
 export const NoSandboxAvailable: FunctionComponent = () => {
   const {
+    actions: {
+      profile: { selectSandboxClicked },
+    },
     state: {
       profile: { isProfileCurrentUser },
     },
@@ -21,6 +25,15 @@ export const NoSandboxAvailable: FunctionComponent = () => {
             isProfileCurrentUser ? `You don't` : `This user doesn't`
           } have a showcased sandbox yet`}
         </ErrorTitle>
+        {isProfileCurrentUser && (
+          <Centered horizontal>
+            <Margin top={1}>
+              <Button onClick={() => selectSandboxClicked()} small>
+                Select Sandbox
+              </Button>
+            </Margin>
+          </Centered>
+        )}
       </Margin>
     </Centered>
   );
