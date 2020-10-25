e0a3678e4866a0f00c21fc33e80316e198203c2c
Yash Joshi <jyash97@gmail.com>
2020-10-25 22:26:10 +0530

    fix(dependency): add theme support on searchbox

--- packages/app/src/app/pages/Sandbox/SearchDependencies/SearchBox.tsx
+++ packages/app/src/app/pages/Sandbox/SearchDependencies/SearchBox.tsx
@@ -5,9 +5,11 @@ import css from '@styled-system/css';
 import useKeys from 'react-use/lib/useKeyboardJs';
 import { AlgoliaIcon } from './icons';
 
-const getBackgroundColor = focus =>
+const getBackgroundColor = ({ focus, theme }) =>
   `url('data:image/svg+xml,%3Csvg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg"%3E%3Cpath fill-rule="evenodd" clip-rule="evenodd" d="M7.75208 8.73995C6.9345 9.40212 5.89308 9.79879 4.75902 9.79879C2.13068 9.79879 0 7.66811 0 5.03978C0 2.41145 2.13068 0.280762 4.75902 0.280762C7.38735 0.280762 9.51803 2.41145 9.51803 5.03978C9.51803 6.17384 9.12136 7.21526 8.45919 8.03285L13.3536 12.9272L12.6464 13.6343L7.75208 8.73995ZM8.51803 5.03978C8.51803 7.11583 6.83506 8.79879 4.75902 8.79879C2.68297 8.79879 1 7.11583 1 5.03978C1 2.96373 2.68297 1.28076 4.75902 1.28076C6.83506 1.28076 8.51803 2.96373 8.51803 5.03978Z" fill="%23${
-    focus ? 'FFFFFF' : '757575'
+    focus
+      ? theme?.colors?.input?.foreground?.replace('#', '')
+      : theme?.colors?.mutedForeground?.replace('#', '')
   }"/%3E%3C/svg%3E%0A')`;
 
 export const SearchBox = ({ handleManualSelect, onChange, listRef }) => {
@@ -89,7 +91,7 @@ export const SearchBox = ({ handleManualSelect, onChange, listRef }) => {
             content: '""',
             position: 'absolute',
             top: 0,
-            backgroundImage: getBackgroundColor(focus),
+            backgroundImage: (theme) => getBackgroundColor({ focus, theme }),
             backgroundRepeat: 'no-repeat',
             backgroundPosition: 'center center',
             opacity: focus ? 1 : 0.8,
@@ -106,7 +108,7 @@ export const SearchBox = ({ handleManualSelect, onChange, listRef }) => {
             paddingLeft: 10,
             height: 65,
             fontSize: 4,
-            color: 'white',
+            color: 'input.foreground',
             backgroundColor: 'sideBar.background',
             border: 'none',
             ':focus, :hover': {
