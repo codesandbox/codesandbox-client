82f08ec21a81d334df897f4044b20d963004da73
Christian Alfoni <christianalfoni@gmail.com>
2020-11-05 14:36:56 +0000

    move to variable

--- packages/sandbox-hooks/screenshot.js
+++ packages/sandbox-hooks/screenshot.js
@@ -4,14 +4,18 @@ listen(data => {
   if (data.type === 'take-screenshot') {
     import('html2canvas').then(lib => {
       const html2canvas = lib.default;
+
       html2canvas(document.body, {
         useCORS: true,
         logging: false,
         allowTaint: false,
+        // proxy: 'http://localhost:3002'
       }).then(canvas => {
+        const screenshot = canvas.toDataURL();
+
         dispatch({
           type: 'screenshot-generated',
-          screenshot: canvas.toDataURL(),
+          screenshot,
         });
       });
     });
