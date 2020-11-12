cd7e3280e9e88cd9007dbd43f453ac5bab2c60b5
Ives van Hoorne <ives.v.h@gmail.com>
2020-11-12 10:17:32 +0100

    Create binary before sending it to webpack loader

--- packages/sandpack-core/src/transpiler/transpilers/webpack/index.ts
+++ packages/sandpack-core/src/transpiler/transpilers/webpack/index.ts
@@ -8,7 +8,11 @@ import { Transpiler, TranspilerResult } from '../..';
  * transpilers. It's a best effort on making webpack loaders work dynamically in Sandpack.
  */
 export class WebpackTranspiler extends Transpiler {
-  private webpackLoader: Promise<(code: string) => string>;
+  private webpackLoader: Promise<{
+    raw?: boolean;
+    (code: string | Buffer): string;
+  }>;
+
   constructor(webpackLoader: string, evaluator: IEvaluator) {
     super(webpackLoader);
 
@@ -29,9 +33,18 @@ export class WebpackTranspiler extends Transpiler {
       return result;
     };
 
+    const codeIsHttp = code.startsWith('http');
+    const webpackCode = codeIsHttp
+      ? await fetch(loaderContext._module.module.code)
+          .then(x => x.arrayBuffer())
+          .then(buffer => Buffer.from(buffer))
+      : Buffer.from(code);
+
     const webpackLoaderContext = { ...loaderContext, async: asyncFunc };
 
-    const webpackResult = await loader.apply(webpackLoaderContext, [code]);
+    const webpackResult = await loader.apply(webpackLoaderContext, [
+      loader.raw ? webpackCode : webpackCode.toString('utf-8'),
+    ]);
 
     return {
       transpiledCode: webpackResult,
