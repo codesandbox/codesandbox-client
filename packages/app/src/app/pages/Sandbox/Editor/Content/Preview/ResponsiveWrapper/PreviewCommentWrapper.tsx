2acd9c4711662aaff63721a9cd4ebabc19334576
Christian Alfoni <christianalfoni@gmail.com>
2020-12-06 12:45:23 +0000

    fix issue with cursor

--- packages/app/src/app/pages/Sandbox/Editor/Content/Preview/ResponsiveWrapper/PreviewCommentWrapper.tsx
+++ packages/app/src/app/pages/Sandbox/Editor/Content/Preview/ResponsiveWrapper/PreviewCommentWrapper.tsx
@@ -18,8 +18,8 @@ const Screenshot = styled.div<{ showCommentCursor: boolean }>(props => ({
   position: 'absolute',
   left: 0,
   top: 0,
-  width: '100%',
-  height: '100%',
+  width: 0,
+  height: 0,
   zIndex: 1,
   color: '#FF3B30',
   cursor: props.showCommentCursor
@@ -58,56 +58,54 @@ type Props = {
 export const PreviewCommentWrapper = ({ children, scale }: Props) => {
   const { state, actions } = useOvermind();
   const previewReference = getPreviewReference(state.comments.currentComment);
-  const screenshotRef = React.useRef(null);
 
-  // Fixes issue with cursor not changing
-  React.useEffect(() => {
-    if (state.preview.screenshot.source) {
-      requestAnimationFrame(() => {
-        screenshotRef.current.style.display = 'block';
-      });
+  const getScreenshotStyle = () => {
+    if (
+      state.preview.mode !== 'add-comment' &&
+      state.preview.mode !== 'responsive-add-comment'
+    ) {
+      return {};
     }
-  }, [state.preview.screenshot.source]);
+
+    return state.preview.screenshot.source
+      ? {
+          backgroundImage: `url(${state.preview.screenshot.source})`,
+          width: '100%',
+          height: '100%',
+        }
+      : {
+          backgroundColor: state.preview.screenshot.isLoading
+            ? 'rgba(0,0,0,0.5)'
+            : 'rgba(0,0,0,0)',
+          width: '100%',
+          height: '100%',
+        };
+  };
 
   return (
     <Wrapper>
       {children}
-      {state.preview.mode === 'add-comment' ||
-      state.preview.mode === 'responsive-add-comment' ? (
-        <Screenshot
-          ref={screenshotRef}
-          style={
-            state.preview.screenshot.source
-              ? {
-                  backgroundImage: `url(${state.preview.screenshot.source})`,
-                  display: 'none',
-                }
-              : {
-                  backgroundColor: state.preview.screenshot.isLoading
-                    ? 'rgba(0,0,0,0.5)'
-                    : 'rgba(0,0,0,0)',
-                }
+      <Screenshot
+        style={getScreenshotStyle()}
+        showCommentCursor={
+          !state.comments.currentComment &&
+          Boolean(state.preview.screenshot.source)
+        }
+        onClick={event => {
+          if (state.preview.screenshot.isLoading) {
+            return;
           }
-          showCommentCursor={
-            !state.comments.currentComment &&
-            Boolean(state.preview.screenshot.source)
-          }
-          onClick={event => {
-            if (state.preview.screenshot.isLoading) {
-              return;
-            }
 
-            const parentBounds = (event.target as any).parentNode.getBoundingClientRect();
+          const parentBounds = (event.target as any).parentNode.getBoundingClientRect();
 
-            actions.comments.addOptimisticPreviewComment({
-              x: event.clientX - parentBounds.left,
-              y: event.clientY - parentBounds.top,
-              screenshot: state.preview.screenshot.source,
-              scale,
-            });
-          }}
-        />
-      ) : null}
+          actions.comments.addOptimisticPreviewComment({
+            x: event.clientX - parentBounds.left,
+            y: event.clientY - parentBounds.top,
+            screenshot: state.preview.screenshot.source,
+            scale,
+          });
+        }}
+      />
       {previewReference &&
       (state.preview.mode === 'add-comment' ||
         state.preview.mode === 'responsive-add-comment') ? (
