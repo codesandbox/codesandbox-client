80499539c5f56ddb5b47c785d003bc771b57b401
Christian Alfoni <christianalfoni@gmail.com>
2020-12-11 11:48:27 +0000

    fix instant bubble only working on body

--- packages/sandbox-hooks/screenshot.js
+++ packages/sandbox-hooks/screenshot.js
@@ -48,10 +48,10 @@ export default function setupScreenshotListener() {
         });
       });
     } else if (data.type === 'show-screenshot-cursor') {
-      existingCursor = document.body.style.cursor;
-      document.body.style.cursor = `url('data:image/svg+xml;utf8,<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M0 8C0 3.58172 3.58172 0 8 0C12.4183 0 16 3.58172 16 8C16 12.4183 12.4183 16 8 16H0V8Z" fill="%23FF3B30"/></svg>'), auto`;
+      existingCursor = document.documentElement.style.cursor;
+      document.documentElement.style.cursor = `url('data:image/svg+xml;utf8,<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M0 8C0 3.58172 3.58172 0 8 0C12.4183 0 16 3.58172 16 8C16 12.4183 12.4183 16 8 16H0V8Z" fill="%23FF3B30"/></svg>'), auto`;
     } else if (data.type === 'hide-screenshot-cursor') {
-      document.body.style.cursor = existingCursor;
+      document.documentElement.style.cursor = existingCursor;
     }
   });
 
