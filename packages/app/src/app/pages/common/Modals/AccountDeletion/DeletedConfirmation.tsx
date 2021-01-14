81c1eb9c06518aed87ad71fc4e0cb3319c995a40
Sara Vieira <hey@iamsaravieira.com>
2021-01-14 18:23:24 +0100

    escape chars

--- packages/app/src/app/pages/common/Modals/AccountDeletion/DeletedConfirmation.tsx
+++ packages/app/src/app/pages/common/Modals/AccountDeletion/DeletedConfirmation.tsx
@@ -15,7 +15,7 @@ export const AccountDeletionConfirmationModal: FunctionComponent = () => {
           Thanks for your request. <br />
           Your account will be deleted within 3 working days. <br />
           <br />
-          If you've changed your mind, please email us at{" "}
+          If you{"'"}ve changed your mind, please email us at{" "}
           <a href="mailto:hello@codesandbox.io">hello@codesandbox.io</a> with
           your username.
         </>
