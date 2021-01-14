a4628648eaa471b8a77b4227b77d5a65d24e258d
Sara Vieira <hey@iamsaravieira.com>
2021-01-14 18:07:43 +0100

    Update DeletedConfirmation.tsx

--- packages/app/src/app/pages/common/Modals/AccountDeletion/DeletedConfirmation.tsx
+++ packages/app/src/app/pages/common/Modals/AccountDeletion/DeletedConfirmation.tsx
@@ -10,7 +10,10 @@ export const AccountDeletionConfirmationModal: FunctionComponent = () => {
   return (
     <Alert
       title="Request for account deletion sent"
-      description="Your account will be deleted by our support team in one working day."
+      description={`
+        Thanks for your request. Your account will be deleted within 3 working days. <br/>
+        If you've changed your mind, please email us at <a href="mailto:hello@codesandbox.io">hello@codesandbox.io</a> with your username.
+      `}
       onPrimaryAction={async () => {
         await signOutClicked();
 
