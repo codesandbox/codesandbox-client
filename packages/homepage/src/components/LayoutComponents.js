8bbd5ccc3be6ea09e4c2d0cf8cdbb529cb6f67bc
Sara Vieira <hey@iamsaravieira.com>
2021-01-15 15:06:31 +0100

    more pricing

--- packages/homepage/src/components/LayoutComponents.js
+++ packages/homepage/src/components/LayoutComponents.js
@@ -38,16 +38,16 @@ export const ContentBlock = styled.div`
     grid-template-columns: repeat(${cols || '3'}, 1fr);
     grid-gap: 3rem 5rem;
     font-size: 19px;
-    line-height:23px;
+    line-height: 23px;
     color: ${theme.homepage.muted};
 
     ${props => props.theme.breakpoints.lg} {
-      font-size:16px;
+      font-size: 16px;
       grid-template-columns: repeat(1, 1fr);
     }
 
     ${props => props.theme.breakpoints.sm} {
-      font-size:16px;
+      font-size: 16px;
     }
 
     ${center && `text-align: center;`}
