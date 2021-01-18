11578ee03d783fceb11d34cff0dcead98f9b412a
Danny Ruchtie <druchtie@gmail.com>
2021-01-18 10:20:54 +0100

    Updated icons for submenu

--- packages/homepage/src/assets/icons/Prototype.js
+++ packages/homepage/src/assets/icons/Prototype.js
@@ -5,17 +5,16 @@ const Prototype = props => {
   const white = useTheme().homepage.white;
   return (
     <svg
-      width={40}
-      height={35}
+      width={32}
+      height={32}
+      viewBox="0 0 24 24"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
       {...props}
     >
-      <circle cx={33.75} cy={6.25} r={6.25} fill={white} />
-      <path
-        d="M9.836 3.886l12.472 12.472-17.037 4.565L9.836 3.886zM25.133 19.5l12.074 3.235-3.235 12.074-12.074-3.235z"
-        fill={white}
-      />
+<circle cx="15.5584" cy="5.63961" r="3.63961" fill="white"/>
+<path d="M5.68746 5.68241L11.6309 11.6259L3.512 13.8013L5.68746 5.68241Z" fill="white"/>
+<rect x="17.066" y="11.7056" width="7.27922" height="7.27922" transform="rotate(45 17.066 11.7056)" fill="white"/>
     </svg>
   );
 };
