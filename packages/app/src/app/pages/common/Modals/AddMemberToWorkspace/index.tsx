6e79c30a274687c381851b8ffee03d6be77e06ac
siddharthkp <siddharth.kshetrapal@gmail.com>
2021-01-20 11:49:39 +0100

    prorate message

--- packages/app/src/app/pages/common/Modals/AddMemberToWorkspace/index.tsx
+++ packages/app/src/app/pages/common/Modals/AddMemberToWorkspace/index.tsx
@@ -1,12 +1,6 @@
 import React, { FunctionComponent, useState } from 'react';
-import {
-  Input,
-  Checkbox,
-  Text,
-  Label,
-  Button,
-  Stack,
-} from '@codesandbox/components';
+import { Checkbox, Text, Button, Stack } from '@codesandbox/components';
+import css from '@styled-system/css';
 import { useOvermind } from 'app/overmind';
 import { Alert } from '../Common/Alert';
 
@@ -39,13 +33,19 @@ export const AddMemberToWorkspace: FunctionComponent = () => {
 
   return (
     <Alert title="Add New Member">
-      <Text size={3} block marginTop={4} marginBottom={10}>
-        <Checkbox
-          checked={confirmed}
-          onChange={e => setConfirmed(e.target.checked)}
-          label={`By adding an extra editor, I confirm ${value} will be added to my invoice.`}
-        />
-      </Text>
+      <Stack direction="vertical" gap={4} marginBottom={10}>
+        <Text size={3} block marginTop={4}>
+          <Checkbox
+            checked={confirmed}
+            onChange={e => setConfirmed(e.target.checked)}
+            label={`By adding an extra editor, I confirm ${value} will be added to my invoice.`}
+          />
+        </Text>
+        <Text size={3} variant="muted" css={css({ marginLeft: 6 })}>
+          For the current month, the amount will be prorated based on the days
+          remaining in the month.
+        </Text>
+      </Stack>
       <Stack gap={2} justify="flex-end">
         <Button autoWidth variant="secondary" onClick={modalClosed}>
           Cancel
