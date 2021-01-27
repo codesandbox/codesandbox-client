77b7c030576f618ba737031328f87be5a6285e6f
Sara Vieira <hey@iamsaravieira.com>
2021-01-27 19:33:34 +0100

    Fix React transform not found in newer version of react (#5408)

--- packages/common/src/utils/supports-new-react-transform.ts
+++ packages/common/src/utils/supports-new-react-transform.ts
@@ -10,9 +10,15 @@ export function supportsNewReactTransform(
   const reactVersion = dependencies.react;
   const reactDomVersion = dependencies['react-dom'];
   if (reactScriptsVersion && reactVersion && reactDomVersion) {
+    // tests if react scripts has a version above 4
+    // and react/react-dom are 17 or they are something like 0.0.0-testname since the react team uses that to test
     return (
       /^[a-z]/.test(reactScriptsVersion) ||
-      semver.intersects(reactScriptsVersion, '^4.0.0')
+      (semver.intersects(reactScriptsVersion, '^4.0.0') &&
+        (semver.lt(reactVersion, '0.0.1') ||
+          semver.intersects(reactVersion, '^17.0.0')) &&
+        (semver.lt(reactDomVersion, '0.0.1') ||
+          semver.intersects(reactDomVersion, '^17.0.0')))
     );
   }
 
