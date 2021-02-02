2f16f896de823f57ef7403e0107c0c2abcf28452
Sara Vieira <hey@iamsaravieira.com>
2021-02-02 22:04:26 +0100

    redirect user if branch is gone (#5441)

--- packages/app/src/app/pages/common/Modals/index.tsx
+++ packages/app/src/app/pages/common/Modals/index.tsx
@@ -41,6 +41,7 @@ import { MinimumPrivacyModal } from './MinimumPrivacyModal';
 import { GenericAlertModal } from './GenericAlertModal';
 import { AccountDeletionModal } from './AccountDeletion';
 import { AccountDeletionConfirmationModal } from './AccountDeletion/DeletedConfirmation';
+import { NotFoundBranchModal } from './NotFoundBranchModal';
 
 const modals = {
   preferences: {
@@ -166,6 +167,10 @@ const modals = {
   },
   deleteConfirmation: {
     Component: AccountDeletionConfirmationModal,
+    width: 450,
+  },
+  notFoundBranchModal: {
+    Component: NotFoundBranchModal,
     width: 450,
   },
 };
