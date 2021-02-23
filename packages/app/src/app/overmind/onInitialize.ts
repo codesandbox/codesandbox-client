7e8b854777a2d2106473755cdefc92af9bd7b31f
Sara Vieira <hey@iamsaravieira.com>
2021-02-23 14:39:13 +0100

    Uses our own builder instead of using aws for building netlifly websites (#5434)

--- packages/app/src/app/overmind/onInitialize.ts
+++ packages/app/src/app/overmind/onInitialize.ts
@@ -72,6 +72,13 @@ export const onInitialize: OnInitialize = async (
     getUserId() {
       return state.user?.id ?? null;
     },
+    provideJwtToken() {
+      if (process.env.LOCAL_SERVER || process.env.STAGING) {
+        return localStorage.getItem('devJwt');
+      }
+
+      return null;
+    },
   });
 
   effects.prettyfier.initialize({
