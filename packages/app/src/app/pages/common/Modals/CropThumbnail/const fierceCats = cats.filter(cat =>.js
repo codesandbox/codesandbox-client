0f07efab641e93b03d93369bb084e7aab8987a48
Sara Vieira <hey@iamsaravieira.com>
2021-03-01 17:23:51 +0100

    fix lint

--- packages/app/src/app/pages/common/Modals/CropThumbnail/const fierceCats = cats.filter(cat =>.js
+++
@@ -1,6 +0,0 @@
-const fierceCats = cats.filter(cat => cat.personality === 'fierce');
-
-const fierceCats = cats.filter(cat => {
-  // will return true or false
-  return cat.personality === 'fierce';
-});
