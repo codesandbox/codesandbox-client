934c18b7b8d2f77bc59c40bde3b4459460ab7ea9
Sara Vieira <hey@iamsaravieira.com>
2021-03-01 18:13:08 +0100

    update date

--- packages/app/src/app/overmind/internalActions.ts
+++ packages/app/src/app/overmind/internalActions.ts
@@ -539,18 +539,17 @@ export const showPrivacyPolicyNotification: Action = ({ effects, state }) => {
   }
 
   if (!state.isFirstVisit) {
-    effects.analytics.track('Saw Privacy Policy Notification');
+    effects.analytics.track('Saw Terms of Use Notification');
     effects.notificationToast.add({
       message:
-        'Hello, we are changing our Terms of Use effective Mar 14, 2021 12:00 UTC. Please read them, or see commit message for a brief sum up.',
-      title: 'Updated Privacy',
+        'Hello, we are changing our Terms of Use effective Mar 31, 2021 12:00 UTC. Please read them, or see commit message for a brief sum up.',
+      title: 'Updated Terms of Use',
       status: NotificationStatus.NOTICE,
       sticky: true,
       actions: {
         secondary: {
           label: 'Open Commit Message',
           run: () => {
-            // GH LINK HERE
             window.open(
               'https://github.com/codesandbox/codesandbox-client/commit/36b0f928cb863868bbfd93bb455a74ff46951edc',
               '_blank'
