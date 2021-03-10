23372b68bc61d0cfd384ae2d4777af475f3ccdbc
Alex Moldovan <alex.n.moldovan@gmail.com>
2021-03-10 19:00:09 +0200

    fix for codesandbox

--- packages/codesandbox-api/src/dispatcher/index.ts
+++ packages/codesandbox-api/src/dispatcher/index.ts
@@ -137,10 +137,10 @@ function notifyFrames(message: object) {
 }
 
 function eventListener(e: MessageEvent) {
-  if (e.data.type === 'initialized') {
-    // iframe handshake done for parent when initialized is received
-    // only the child needs to wait for the handshake (receiving an id)
-    // before receiving messages
+  if (e.data.type === 'initialized' || isStandalone) {
+    // iframe handshake is auto-resolved in the parent, only the child needs to wait for it
+    // we detect the parent either by the initialized message which only the parent receives
+    // or by the isStandalone flag which works for codesandbox.io and when sandpack is not inside an iframe
     iframeHandShakeDone = true;
   }
 
