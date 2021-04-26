73c2f321ae3a08fc7a5e46d0331082f6ceb35593
Jasper De Moor <jasperdemoor@gmail.com>
2021-04-26 19:55:37 +0200

    clone html elements in a way that allows code execution (#5691)
    * clone html elements in a way that allows code execution
    
    * remove commetns
    
    * use htmlparser2 instead
    
    * use domhandler directly
    
    * remove types and refresh on any html change
    
    * testing the tests
    
    * newline attempt
    
    * add another test
    
    * example was invalid...
    
    * reduce wait times for integration tests
    
    Co-authored-by: Alex Moldovan <alex.n.moldovan@gmail.com>

---
+++ packages/app/src/sandbox/html-writer.ts
@@ -0,0 +1,58 @@
+import { Parser } from 'htmlparser2';
+import { DomHandler } from 'domhandler';
+
+const EXCLUDED_TAGS = ['noscript'];
+
+function parseDOM(content: string) {
+  return new Promise((resolve, reject) => {
+    const domHandler = new DomHandler((err, dom) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(dom);
+      }
+    });
+    const parser = new Parser(domHandler);
+    parser.write(content);
+    parser.end();
+  });
+}
+
+function addTagNode(node: any, firstElement: ChildNode, parent: HTMLElement) {
+  // Skip noscript nodes as this is a script...
+  if (EXCLUDED_TAGS.includes(node.name)) {
+    return;
+  }
+
+  const nodeToInsert = document.createElement(node.name);
+  Object.entries(node.attribs).forEach(([key, value]) => {
+    nodeToInsert.setAttribute(key, value);
+  });
+  if (node.name === 'script' && !node.attribs.async) {
+    nodeToInsert.setAttribute('async', 'false');
+  }
+  if (node.children) {
+    writeDOMNodes(node.children, nodeToInsert);
+  }
+  parent.insertBefore(nodeToInsert, firstElement);
+}
+
+function writeDOMNodes(nodes: Array<any>, parent: HTMLElement) {
+  const firstElement = parent.firstChild;
+
+  nodes.forEach(node => {
+    if (node.type === 'text') {
+      parent.insertBefore(document.createTextNode(node.data), firstElement);
+    } else if (node.type === 'comment') {
+      parent.insertBefore(document.createComment(node.data), firstElement);
+    } else if (typeof node.attribs !== 'undefined') {
+      // We can't use node.type === 'tag' as that's different for script and style
+      addTagNode(node, firstElement, parent);
+    }
+  });
+}
+
+export async function appendHTML(origin: string, parent: HTMLElement) {
+  const domTree: any = await parseDOM(origin);
+  writeDOMNodes(domTree, parent);
+}
