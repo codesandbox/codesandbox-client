6c0110ef454029caca9d1df4bf9a7e22e10ac620
siddharthkp <siddharth.kshetrapal@gmail.com>
2021-04-28 14:30:47 +0200

    keep the picked sandboxes long

--- packages/app/src/app/pages/Dashboard/Content/routes/Discover/index.tsx
+++ packages/app/src/app/pages/Dashboard/Content/routes/Discover/index.tsx
@@ -76,8 +76,8 @@ export const Discover = () => {
     [curatedAlbums]
   );
 
-  const fiveRandomAlbums = fiveRandomAlbumIds.map((albumId) =>
-    curatedAlbums.find((album) => album.id === albumId)
+  const fiveRandomAlbums = fiveRandomAlbumIds.map(albumId =>
+    curatedAlbums.find(album => album.id === albumId)
   );
 
   return (
@@ -166,7 +166,7 @@ const PickedSandboxes = () => {
       columnGap={6}
       css={{
         gridTemplateColumns: 'repeat(auto-fit, minmax(300px, 1fr))',
-        height: '528px',
+        height: 'max(60vh, 528px)',
         overflow: 'hidden',
       }}
     >
@@ -218,7 +218,7 @@ export const PickedSandbox = ({ sandbox }) => {
         block
         css={css({
           width: '100%',
-          height: '528px',
+          height: 'max(60vh, 528px)',
           border: '1px solid',
           borderColor: 'grays.600',
           borderRadius: 'medium',
