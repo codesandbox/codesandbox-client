342a6669f7d3a996e1ef0ee7d00d28c3b4c12ebe
Danilo Woznica <danilowoz@gmail.com>
2021-05-17 16:01:40 +0100

    fix(client login): ui tweaks

--- packages/app/src/app/pages/Client/Prompt/elements.ts
+++ packages/app/src/app/pages/Client/Prompt/elements.ts
@@ -1,10 +1,11 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  height: 100%;
+  min-height: calc(100vh - 48px);
   width: 100%;
-  margin-top: 10%;
+  padding-top: 10%;
   text-align: center;
+  box-sizing: border-box;
 `;
 
 export const Buttons = styled.div`
