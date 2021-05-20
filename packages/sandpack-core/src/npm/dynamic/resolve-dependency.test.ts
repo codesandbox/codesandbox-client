28fbf7e58cb8295d9295c1e0f1d8c5f674eff122
Jasper De Moor <jasperdemoor@gmail.com>
2021-05-20 15:47:25 +0200

    finally figured out why test was failing...

--- packages/sandpack-core/src/npm/dynamic/resolve-dependency.test.ts
+++ packages/sandpack-core/src/npm/dynamic/resolve-dependency.test.ts
@@ -3,22 +3,25 @@ import fetch from 'fetch-vcr';
 // @ts-ignore
 window.fetch = fetch;
 
-// eslint-disable-next-line import/first
-// import { resolveDependencyInfo } from './resolve-dependency';
-
 describe.skip('resolve-dependency', () => {
-  //   jest.mock('./fetch-protocols/tar', () => ({
-  //     TarFetcher: class A {},
-  //   }));
+  // eslint-disable-next-line global-require
+  const resolveDependencyInfo: typeof import('./resolve-dependency').resolveDependencyInfo = require('./resolve-dependency')
+    .resolveDependencyInfo;
+
+  jest.mock('./fetch-protocols/tar', () => ({
+    TarFetcher: class A {},
+    isTarDependency: () => true,
+  }));
 
   it('marks the original dependency requester as parent on subdependencies', async () => {
-    // const result = await resolveDependencyInfo(
-    //   'antd',
-    //   'https://pkg.csb.dev/ant-design/ant-design/commit/ad3a0ecb/antd',
-    //   []
-    // );
-    // expect(result.dependencyDependencies['@babel/runtime'].parents).toContain(
-    //   'antd'
-    // );
+    const result = await resolveDependencyInfo(
+      'antd',
+      'https://pkg.csb.dev/ant-design/ant-design/commit/ad3a0ecb/antd',
+      []
+    );
+
+    expect(result.dependencyDependencies['@babel/runtime'].parents).toContain(
+      'antd'
+    );
   });
 });
