8b305ec40c55dda84b1339a3db9f4a3fa1099064
Ives van Hoorne <ives.v.h@gmail.com>
2021-05-21 12:04:05 +0200

    Update cache version of vscode extensions

--- packages/app/src/app/overmind/effects/vscode/constants.ts
+++ packages/app/src/app/overmind/effects/vscode/constants.ts
@@ -1,6 +1,6 @@
 export const EXTENSIONS_LOCATION = process.env.VSCODE
   ? '/vscode/extensions-bundle'
-  : '/public/vscode-extensions/v14';
+  : '/public/vscode-extensions/v15';
 
 export const VIM_EXTENSION_ID = 'vscodevim.vim';
 
