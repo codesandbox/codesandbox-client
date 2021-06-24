6d350ffe67e2020c7eb9a90da338926893f4add9
Ives van Hoorne <ives.v.h@gmail.com>
2021-06-24 11:09:09 +0200

    Remove service worker rule

--- packages/sandpack-core/src/npm/dynamic/fetch-protocols/npm-registry.ts
+++ packages/sandpack-core/src/npm/dynamic/fetch-protocols/npm-registry.ts
@@ -134,6 +134,8 @@ export class NpmRegistryFetcher implements FetchProtocol {
     return fetchWithRetries(url, 3, this.getRequestInit())
       .then(x => x.json())
       .catch(async e => {
+        console.error(e);
+
         let errorMessage = 'Make sure the right auth token and URL are set';
         if (e.responseObject) {
           const res = await e.responseObject.json();
