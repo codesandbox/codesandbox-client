cabd0adf8a75bfe40025ee1fbdd1a4e6cc4c145d
Danilo Woznica <danilowoz@gmail.com>
2021-06-28 10:13:28 +0100

    fix(ab env): check the current host to determine the env

--- packages/common/src/config/env.ts
+++ packages/common/src/config/env.ts
@@ -10,9 +10,9 @@ const STAGING_API = Boolean(JSON.stringify(process.env.STAGING_API));
 
 const AB_TESTING_URL_STAGING = 'https://ab-testing.codesandbox.stream';
 const AB_TESTING_URL_PRODUCTION = 'https://ab-testing.codesandbox.io';
-export const AB_TESTING_URL = STAGING_API
-  ? AB_TESTING_URL_STAGING
-  : AB_TESTING_URL_PRODUCTION;
+export const AB_TESTING_URL = /codesandbox\.io/.test(window.location.host)
+  ? AB_TESTING_URL_PRODUCTION
+  : AB_TESTING_URL_STAGING;
 
 export const getExperimentUserId = () => {
   const KEY_NAME = 'csb-ab-user-id';
