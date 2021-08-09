06c340a4a13be71d511f03915b15b618cd5ceda8
Jasper De Moor <jasperdemoor@gmail.com>
2021-08-09 16:40:10 +0200

    Fix issues with jspm and loader import syntax conflicts (#6030)
    * don't handle jspm format as webpack loader syntax
    
    * handle more edge cases for splitQuery

--- packages/sandpack-core/src/transpiled-module/utils/query-path.ts
+++ packages/sandpack-core/src/transpiled-module/utils/query-path.ts
@@ -1,12 +1,26 @@
+import { isUrl } from '@codesandbox/common/lib/utils/is-url';
+
+const isFilePath = (path: string): boolean =>
+  /^\.?\/.*/.test(path) || isUrl(path);
+
 export const splitQueryFromPath = (
   path: string
 ): { queryPath: string; modulePath: string } => {
-  if (path.includes('!')) {
-    const queryPath = path.split('!');
-    const modulePath = queryPath.pop()!;
+  if (path.includes('!') && !isFilePath(path)) {
+    const parts = path.split('!');
 
+    let modulePathIndex: number = [...parts]
+      .reverse()
+      .findIndex(v => isFilePath(v));
+    if (modulePathIndex < 0) {
+      modulePathIndex = parts.length - 1;
+    } else {
+      modulePathIndex = parts.length - modulePathIndex - 1;
+    }
+
+    const modulePath = parts.splice(modulePathIndex).join('!')!;
     return {
-      queryPath: queryPath.join('!'),
+      queryPath: parts.join('!'),
       modulePath,
     };
   }
