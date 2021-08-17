2071339c5d5bedf9185c664ee2481227f72f5754
Jasper De Moor <jasperdemoor@gmail.com>
2021-08-17 11:06:24 +0200

    add url to react refresh entry module

--- packages/app/src/sandbox/eval/presets/create-react-app/utils/createRefreshEntry.ts
+++ packages/app/src/sandbox/eval/presets/create-react-app/utils/createRefreshEntry.ts
@@ -1,12 +1,14 @@
-import { Manager } from 'sandpack-core';
+import { Manager, getModuleUrl } from 'sandpack-core';
 
 /**
  * When using React Refresh we need to evaluate some code before 'react-dom' is initialized
  * (https://github.com/facebook/react/issues/16604#issuecomment-528663101) this is the code.
  */
 export async function createRefreshEntry(manager: Manager) {
+  const path = '/node_modules/__csb/react-dom-entrypoint.js';
   const entryModule = {
-    path: '/node_modules/__csb/react-dom-entrypoint.js',
+    path,
+    url: getModuleUrl(path),
     code: `if (typeof window !== 'undefined') {
 const runtime = require('react-refresh/runtime');
 runtime.injectIntoGlobalHook(window);
