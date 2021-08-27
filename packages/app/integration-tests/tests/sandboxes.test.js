e79aa5e5d89231051dcbc12d4227014ef2914914
Jasper De Moor <jasperdemoor@gmail.com>
2021-08-27 14:13:59 +0200

    add integration test for vue 2 css modules, couldnt really get the snapshot to work anymore though :(

--- packages/app/integration-tests/tests/sandboxes.test.js
+++ packages/app/integration-tests/tests/sandboxes.test.js
@@ -42,6 +42,7 @@ const SANDBOXES = [
   'circle-svg', // svgs don't render properly if you use document.createElement
   'vue-3-basics-program-easily', // vue w/ custom html
   'scss-mixins',
+  'vue-2-css-module-vuzkt',
 ];
 
 // Logic for parallelizing the tests
