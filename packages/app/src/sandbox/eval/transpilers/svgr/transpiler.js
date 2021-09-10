6c36dcd099239d112bfd740c70d0843b97ca7c62
Jasper De Moor <jasperdemoor@gmail.com>
2021-09-10 14:48:40 +0200

    add default config for svgr back (#6125)

--- packages/app/src/sandbox/eval/transpilers/svgr/transpiler.js
+++ packages/app/src/sandbox/eval/transpilers/svgr/transpiler.js
@@ -2,8 +2,25 @@ import jsxPlugin from '@svgr/plugin-jsx';
 import { expandState } from '@svgr/core/lib/state';
 import { getPlugins, resolvePlugin } from '@svgr/core/lib/plugins';
 
+const DEFAULT_CONFIG = {
+  dimensions: true,
+  expandProps: 'end',
+  icon: false,
+  native: false,
+  typescript: false,
+  memo: false,
+  ref: false,
+  replaceAttrValues: null,
+  svgProps: null,
+  template: null,
+  titleProp: false,
+  runtimeConfig: true,
+  plugins: null,
+  namedExport: 'ReactComponent',
+};
+
 export async function svgrTransform(filePath: string, inputCode: string) {
-  const config = {};
+  const config = { ...DEFAULT_CONFIG };
   const state = {
     caller: {
       name: '@codesandbox/svgr-loader',
