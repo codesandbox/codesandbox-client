f51bfda542c7bf9893b58ab246f533e40d801596
Tamas Szuromi <tromika@gmail.com>
2021-10-12 15:22:43 +0200

    feat(editor): new value proposition in the signup banner (#6195)
    * clean up the fixed signup banner experiment; new value prop
    
    * fix(experiments): linting
    
    Co-authored-by: Tamas Szuromi <tamas@codesandbox.io>
    Co-authored-by: Danilo Woznica <danilowoz@gmail.com>

--- packages/app/src/app/pages/Sandbox/Editor/Header/index.tsx
+++ packages/app/src/app/pages/Sandbox/Editor/Header/index.tsx
@@ -1,39 +1,15 @@
 import { Stack } from '@codesandbox/components';
 import css from '@styled-system/css';
 import { useAppState } from 'app/overmind';
-import React, { useState, useEffect } from 'react';
-import { ExperimentValues, useExperimentResult } from '@codesandbox/ab';
+import React from 'react';
 
 import { Actions } from './Actions';
 import { AppMenu } from './AppMenu';
 import { SandboxName } from './SandboxName';
 import { WorkspaceName } from './WorkspaceName';
-import { SignInBanner } from './SignInAd';
 
 export const Header = () => {
-  const { editor, isAuthenticating, activeTeamInfo, user } = useAppState();
-
-  /**
-   * A/B
-   */
-  const experimentPromise = useExperimentResult('fixed-signin-banner');
-  const [newSignInBanner, setNewSignInBanner] = useState(false);
-  useEffect(() => {
-    /* Wait for the API */
-    experimentPromise.then(experiment => {
-      if (experiment === ExperimentValues.A) {
-        /**
-         * A
-         */
-        setNewSignInBanner(false);
-      } else if (experiment === ExperimentValues.B) {
-        /**
-         * B
-         */
-        setNewSignInBanner(true);
-      }
-    });
-  }, [experimentPromise]);
+  const { editor, isAuthenticating, activeTeamInfo } = useAppState();
 
   const renderWorkspace = () => {
     if (activeTeamInfo) {
@@ -49,31 +25,28 @@ export const Header = () => {
   };
 
   return (
-    <>
-      {!user && !newSignInBanner && <SignInBanner />}
-      <Stack
-        as="header"
-        justify="space-between"
-        align="center"
-        paddingX={2}
-        css={css({
-          boxSizing: 'border-box',
-          fontFamily: 'Inter, sans-serif',
-          height: 12,
-          backgroundColor: 'titleBar.activeBackground',
-          color: 'titleBar.activeForeground',
-          borderBottom: '1px solid',
-          borderColor: 'titleBar.border',
-        })}
-      >
-        <Stack align="center">
-          <AppMenu />
-          {renderWorkspace()}
-        </Stack>
-
-        {editor.currentSandbox && !isAuthenticating ? <SandboxName /> : null}
-        {editor.currentSandbox && !isAuthenticating ? <Actions /> : null}
+    <Stack
+      as="header"
+      justify="space-between"
+      align="center"
+      paddingX={2}
+      css={css({
+        boxSizing: 'border-box',
+        fontFamily: 'Inter, sans-serif',
+        height: 12,
+        backgroundColor: 'titleBar.activeBackground',
+        color: 'titleBar.activeForeground',
+        borderBottom: '1px solid',
+        borderColor: 'titleBar.border',
+      })}
+    >
+      <Stack align="center">
+        <AppMenu />
+        {renderWorkspace()}
       </Stack>
-    </>
+
+      {editor.currentSandbox && !isAuthenticating ? <SandboxName /> : null}
+      {editor.currentSandbox && !isAuthenticating ? <Actions /> : null}
+    </Stack>
   );
 };
