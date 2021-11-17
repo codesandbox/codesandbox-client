709941a987eda99b3c0b1d2fa3cd22fd51923f92
Ives van Hoorne <ives.v.h@gmail.com>
2021-11-17 10:56:12 +0100

    Improve showing of results

--- packages/app/src/app/overmind/effects/algoliaSearch.ts
+++ packages/app/src/app/overmind/effects/algoliaSearch.ts
@@ -1,6 +1,6 @@
 import search from 'algoliasearch';
 import { Dependency } from '@codesandbox/common/lib/types/algolia';
-import { getGreensockAlias } from './utils/greensock';
+import { getGreensockAlias, GREENSOCK_ALIASES } from './utils/greensock';
 
 const client = search('OFCNCOG2CU', '00383ecd8441ead30b1b0ff981c426f5');
 const NPMSearchIndex = client.initIndex('npm-search');
@@ -27,10 +27,17 @@ export default {
     let searchResults: Dependency[];
     if (greensockAlias) {
       const [depResults, gsap] = await Promise.all([
-        searchAlgolia(value, 3),
+        searchAlgolia(value, 3 + GREENSOCK_ALIASES.length),
         searchAlgolia(greensockAlias, 1),
       ]);
-      searchResults = [gsap[0], ...depResults];
+      // Alias any greensock result out, so that we don't have duplicate results and we don't show
+      // gsap for a plugin where only gsap-trial is applicable.
+      const filteredDepResults = depResults.filter(
+        d => !GREENSOCK_ALIASES.includes(d.name)
+      );
+      // Then finally truncate the results back to 3
+      filteredDepResults.length = 3;
+      searchResults = [gsap[0], ...filteredDepResults];
     } else {
       searchResults = await searchAlgolia(value, 4);
     }
