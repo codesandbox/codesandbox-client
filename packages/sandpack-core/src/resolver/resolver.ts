fc9a9951051285ab5286ef64a3cecbd1c03c45f1
Jasper De Moor <jasperdemoor@gmail.com>
2021-11-18 20:51:47 +0100

    feat: Load tsconfig paths and baseUrl (#6304)
    * tsconfig fixtures
    
    * support tsconfig paths and baseUrl
    
    * rename packageCache to resolverCache
    
    * add missing dep

--- packages/sandpack-core/src/resolver/resolver.ts
+++ packages/sandpack-core/src/resolver/resolver.ts
@@ -7,8 +7,13 @@ import * as pathUtils from '@codesandbox/common/lib/utils/path';
 import { ModuleNotFoundError } from './errors/ModuleNotFound';
 import { ProcessedPackageJSON, processPackageJSON } from './utils/pkg-json';
 import { isFile, FnIsFile, FnReadFile, getParentDirectories } from './utils/fs';
+import {
+  ProcessedTSConfig,
+  processTSConfig,
+  getPotentialPathsFromTSConfig,
+} from './utils/tsconfig';
 
-export type PackageCache = Map<string, any>;
+export type ResolverCache = Map<string, any>;
 
 export interface IResolveOptionsInput {
   filename: string;
@@ -16,12 +21,12 @@ export interface IResolveOptionsInput {
   isFile: FnIsFile;
   readFile: FnReadFile;
   moduleDirectories?: string[];
-  packageCache?: PackageCache;
+  resolverCache?: ResolverCache;
 }
 
 interface IResolveOptions extends IResolveOptionsInput {
   moduleDirectories: string[];
-  packageCache: PackageCache;
+  resolverCache: ResolverCache;
 }
 
 function normalizeResolverOptions(opts: IResolveOptionsInput): IResolveOptions {
@@ -38,7 +43,7 @@ function normalizeResolverOptions(opts: IResolveOptionsInput): IResolveOptions {
     isFile: opts.isFile,
     readFile: opts.readFile,
     moduleDirectories: [...normalizedModuleDirectories],
-    packageCache: opts.packageCache || new Map(),
+    resolverCache: opts.resolverCache || new Map(),
   };
 }
 
@@ -55,16 +60,16 @@ function* loadPackageJSON(
   const directories = getParentDirectories(filepath, rootDir);
   for (const directory of directories) {
     const packageFilePath = pathUtils.join(directory, 'package.json');
-    let packageContent = opts.packageCache.get(packageFilePath);
+    let packageContent = opts.resolverCache.get(packageFilePath);
     if (packageContent === undefined) {
       try {
         packageContent = processPackageJSON(
           JSON.parse(yield* opts.readFile(packageFilePath)),
           pathUtils.dirname(packageFilePath)
         );
-        opts.packageCache.set(packageFilePath, packageContent);
+        opts.resolverCache.set(packageFilePath, packageContent);
       } catch (err) {
-        opts.packageCache.set(packageFilePath, false);
+        opts.resolverCache.set(packageFilePath, false);
       }
     }
     if (packageContent) {
@@ -259,6 +264,37 @@ export function normalizeModuleSpecifier(specifier: string): string {
   return normalized;
 }
 
+const TS_CONFIG_CACHE_KEY = '__root_tsconfig';
+function* getTSConfig(
+  opts: IResolveOptions
+): Generator<any, ProcessedTSConfig | false, any> {
+  const cachedConfig = opts.resolverCache.get(TS_CONFIG_CACHE_KEY);
+  if (cachedConfig != null) {
+    return cachedConfig;
+  }
+
+  let config: ProcessedTSConfig | false = false;
+  try {
+    const contents = yield* opts.readFile('/tsconfig.json');
+    const processed = processTSConfig(contents);
+    if (processed) {
+      config = processed;
+    }
+  } catch (err) {
+    try {
+      const contents = yield* opts.readFile('/jsconfig.json');
+      const processed = processTSConfig(contents);
+      if (processed) {
+        config = processed;
+      }
+    } catch {
+      // do nothing
+    }
+  }
+  opts.resolverCache.set(TS_CONFIG_CACHE_KEY, config);
+  return config;
+}
+
 export const resolver = gensync<
   (moduleSpecifier: string, inputOpts: IResolveOptionsInput) => string
 >(function* resolve(moduleSpecifier, inputOpts): Generator<any, string, any> {
@@ -267,6 +303,24 @@ export const resolver = gensync<
   const modulePath = yield* resolveModule(normalizedSpecifier, opts);
 
   if (modulePath[0] !== '/') {
+    // This isn't a node module, we can attempt to resolve using a tsconfig/jsconfig
+    if (!opts.filename.includes('/node_modules')) {
+      const parsedTSConfig = yield* getTSConfig(opts);
+      if (parsedTSConfig) {
+        const potentialPaths = getPotentialPathsFromTSConfig(
+          modulePath,
+          parsedTSConfig
+        );
+        for (const potentialPath of potentialPaths) {
+          try {
+            return yield* resolve(potentialPath, opts);
+          } catch {
+            // do nothing, it's probably a node_module in this case
+          }
+        }
+      }
+    }
+
     try {
       return yield* resolveNodeModule(modulePath, opts);
     } catch (e) {
