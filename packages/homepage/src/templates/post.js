2e17d81eb4382227255db7aab99b5dfe0e2a6ca5
Danilo Woznica <danilowoz@gmail.com>
2021-12-02 10:35:07 +0000

    add sandpack in blogpost

--- packages/homepage/src/templates/post.js
+++ packages/homepage/src/templates/post.js
@@ -1,11 +1,15 @@
 import { format } from 'date-fns';
 import { graphql, Link } from 'gatsby';
 import React from 'react';
+import camelCase from "lodash.camelCase"
 
+import {Sandpack} from "@codesandbox/sandpack-react"
 import Layout from '../components/layout';
 import PageContainer from '../components/PageContainer';
 import { AuthorImage } from '../components/PostElements';
 import TitleAndMetaTags from '../components/TitleAndMetaTags';
+import rehypeReact from "./rehype"
+import "@codesandbox/sandpack-react/dist/index.css"
 
 import {
   Article,
@@ -17,6 +21,20 @@ import {
   PostContainer,
 } from './_post.elements';
 
+
+const renderAst = new rehypeReact({
+  createElement: React.createElement,
+  components: { "sandpack": ({children, ...props}) =>  {
+    const sandpackProps = Object.entries(props).reduce((acc, [key, value]) => {
+      const kebabCase = camelCase(key)
+
+      return {...acc, [kebabCase]: /{/.test(value) ? JSON.parse(value): value}
+    }, {})
+
+    return <Sandpack theme="sandpack-dark" {...sandpackProps} />;
+  } }
+}).Compiler
+
 export default ({
   data: {
     blogPost: {
@@ -25,42 +43,47 @@ export default ({
         banner: { publicURL: banner },
       },
       html,
+      htmlAst
     },
   },
-}) => (
-  <Layout>
-    <Article>
-      <TitleAndMetaTags
-        description={description}
-        image={banner}
-        title={`${title} - CodeSandbox Blog`}
-      />
+}) => {
 
-      <Header>
-        <Link to="blog">CodeSandbox Blog</Link>
+    return (
+      <Layout>
+        <Article>
+          <TitleAndMetaTags
+            description={description}
+            image={banner}
+            title={`${title} - CodeSandbox Blog`} />
 
-        <PostTitle>{title}</PostTitle>
+          <Header>
+            <Link to="blog">CodeSandbox Blog</Link>
 
-        <MetaData>
-          {authors.map(author => (
-            <AuthorContainer key={author}>
-              {authors.length === 1 && <AuthorImage alt={author} src={photo} />}
+            <PostTitle>{title}</PostTitle>
 
-              <h4>{author}</h4>
-              <date>{format(date, 'MMM / DD / YYYY')}</date>
-            </AuthorContainer>
-          ))}
-        </MetaData>
-      </Header>
+            <MetaData>
+              {authors.map(author => (
+                <AuthorContainer key={author}>
+                  {authors.length === 1 && <AuthorImage alt={author} src={photo} />}
 
-      <Image alt={title} src={banner} />
+                  <h4>{author}</h4>
+                  <date>{format(date, 'MMM / DD / YYYY')}</date>
+                </AuthorContainer>
+              ))}
+            </MetaData>
+          </Header>
 
-      <PageContainer width={768}>
-        <PostContainer dangerouslySetInnerHTML={{ __html: html }} />
-      </PageContainer>
-    </Article>
-  </Layout>
-);
+          <Image alt={title} src={banner} />
+
+          <PageContainer width={768}>
+            <PostContainer>
+              {renderAst(htmlAst)}
+            </PostContainer>
+          </PageContainer>
+        </Article>
+      </Layout>
+    );
+  };
 
 export const pageQuery = graphql`
   query Post($id: String) {
@@ -79,6 +102,7 @@ export const pageQuery = graphql`
         }
       }
       html
+      htmlAst
     }
   }
 `;
