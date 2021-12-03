a79692f472a579557aef84af18491fb367f43f20
Danilo Woznica <danilowoz@gmail.com>
2021-12-03 19:43:36 +0000

    draft

--- packages/sandpack-core/src/manager.ts
+++ packages/sandpack-core/src/manager.ts
@@ -164,6 +164,8 @@ export default class Manager implements IEvaluator {
     };
   };
 
+  reactDevTools: boolean;
+
   envVariables: { [envName: string]: string } = {};
   preset: Preset;
   modules: ModuleObject;
