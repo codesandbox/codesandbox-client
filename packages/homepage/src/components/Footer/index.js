f74995ebc41c4ad6d8732d313f206c57240cc024
Adewale Abati <acekyd01@gmail.com>
2022-03-08 14:16:11 +0100

    Chore/add discord help (#6519)
    * Update Discord invite
    
    * Add Discord help menu
    
    * Update discord link for tracking
    
    * Add dashboard wide social icons for support
    
    * delete duplicate icons

--- packages/homepage/src/components/Footer/index.js
+++ packages/homepage/src/components/Footer/index.js
@@ -158,7 +158,7 @@ const Footer = () => (
         </a>
       </li>
       <li>
-        <a title="Join our Discord Server" href="https://discord.gg/5BpufEP7MH">
+        <a title="Join our Discord Server" href="https://discord.gg/C6vfhW3H6e">
           <Discord />
         </a>
       </li>
