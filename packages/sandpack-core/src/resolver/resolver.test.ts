68c664d6aa4e31eeeb54843bb0a189a91f344768
Jasper De Moor <jasperdemoor@gmail.com>
2022-03-19 21:38:48 +0100

    fix: Make sure resolver stops after expanding index once (#6560)
    * fix the resolver for instantsearch
    
    * other tweaks

--- packages/sandpack-core/src/resolver/resolver.test.ts
+++ packages/sandpack-core/src/resolver/resolver.test.ts
@@ -258,6 +258,19 @@ describe('resolve', () => {
         new ModuleNotFoundError('unknown-module/test.js', '/nested/test.js')
       );
     });
+
+    it('should handle instantsearch.js index palooza', () => {
+      expect(() => {
+        resolveSync('instantsearch.js/es/widgets', {
+          filename: '/foo.js',
+          extensions: ['.ts', '.tsx', '.js', '.jsx'],
+          isFile,
+          readFile,
+        });
+      }).toThrow(
+        `Cannot find module 'instantsearch.js/es/widgets' from '/foo.js'`
+      );
+    });
   });
 
   describe('package#browser', () => {
@@ -469,14 +482,14 @@ describe('resolve', () => {
     });
 
     it('should not load exports from the root package.json', () => {
-      expect(() =>
+      expect(() => {
         resolveSync('a-custom-export', {
           filename: '/foo.js',
           extensions: ['.ts', '.tsx', '.js', '.jsx'],
           isFile,
           readFile,
-        })
-      ).toThrow();
+        });
+      }).toThrow();
     });
 
     it('should not fail on wildcard *.js and folder references', () => {
