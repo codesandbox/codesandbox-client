e72f056099bce9d97bcc898acceee499a9962dea
Jasper De Moor <jasperdemoor@gmail.com>
2022-04-21 11:52:44 +0200

    use unique id for icon

--- packages/template-icons/src/SolidIcon.tsx
+++ packages/template-icons/src/SolidIcon.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import { SVGIcon, ISVGIconProps } from './SVGIcon';
+import { useUniqueId } from './useUniqueId';
 
 export const SolidIcon: React.FC<ISVGIconProps> = ({ ...props }) => {
+  const id = useUniqueId();
+
   return (
     <SVGIcon {...props} viewBox="0 0 166 155.3">
       <path
@@ -9,7 +12,7 @@ export const SolidIcon: React.FC<ISVGIconProps> = ({ ...props }) => {
         d="M163 35S110-4 69 5l-3 1c-6 2-11 5-14 9l-2 3-15 26 26 5c11 7 25 10 38 7l46 9 18-30z"
       />
       <linearGradient
-        id="a"
+        id={`${id}-a`}
         x1="27.5"
         x2="152"
         y1="3"
@@ -21,7 +24,7 @@ export const SolidIcon: React.FC<ISVGIconProps> = ({ ...props }) => {
         <stop offset="1" stopColor="#76b3e1" />
       </linearGradient>
       <path
-        fill="url(#a)"
+        fill={`url(#${id}-a)`}
         d="M163 35S110-4 69 5l-3 1c-6 2-11 5-14 9l-2 3-15 26 26 5c11 7 25 10 38 7l46 9 18-30z"
         opacity="0.3"
       />
@@ -30,7 +33,7 @@ export const SolidIcon: React.FC<ISVGIconProps> = ({ ...props }) => {
         d="M52 35l-4 1c-17 5-22 21-13 35 10 13 31 20 48 15l62-21S92 26 52 35z"
       />
       <linearGradient
-        id="b"
+        id={`${id}-b`}
         x1="95.8"
         x2="74"
         y1="32.6"
@@ -42,12 +45,12 @@ export const SolidIcon: React.FC<ISVGIconProps> = ({ ...props }) => {
         <stop offset="1" stopColor="#1f3b77" />
       </linearGradient>
       <path
-        fill="url(#b)"
+        fill={`url(#${id}-b)`}
         d="M52 35l-4 1c-17 5-22 21-13 35 10 13 31 20 48 15l62-21S92 26 52 35z"
         opacity="0.3"
       />
       <linearGradient
-        id="c"
+        id={`${id}-c`}
         x1="18.4"
         x2="144.3"
         y1="64.2"
@@ -59,11 +62,11 @@ export const SolidIcon: React.FC<ISVGIconProps> = ({ ...props }) => {
         <stop offset="1" stopColor="#315aa9" />
       </linearGradient>
       <path
-        fill="url(#c)"
+        fill={`url(#${id}-c)`}
         d="M134 80a45 45 0 00-48-15L24 85 4 120l112 19 20-36c4-7 3-15-2-23z"
       />
       <linearGradient
-        id="d"
+        id={`${id}-d`}
         x1="75.2"
         x2="24.4"
         y1="74.5"
@@ -75,7 +78,7 @@ export const SolidIcon: React.FC<ISVGIconProps> = ({ ...props }) => {
         <stop offset="1" stopColor="#1a336b" />
       </linearGradient>
       <path
-        fill="url(#d)"
+        fill={`url(#${id}-d)`}
         d="M114 115a45 45 0 00-48-15L4 120s53 40 94 30l3-1c17-5 23-21 13-34z"
       />
     </SVGIcon>
