b078f6e21cc87734b036eec5f9a9309a21526abf
Danilo Woznica <danilowoz@gmail.com>
2022-04-27 11:08:12 +0100

    reactive personal plan

--- packages/app/src/app/pages/Dashboard/Content/routes/Settings/UserSettings/ManageSubscription/Stripe.tsx
+++ packages/app/src/app/pages/Dashboard/Content/routes/Settings/UserSettings/ManageSubscription/Stripe.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import { Stack, Link } from '@codesandbox/components';
+import { Stack, Text, Link, Button } from '@codesandbox/components';
 import { useAppState } from 'app/overmind';
+import { format } from 'date-fns';
 
 import css from '@styled-system/css';
 import { useCreateCustomerPortal } from '../../../../../../Pro/upgrade/utils';
 
 export const Stripe = () => {
-  const { activeTeam } = useAppState();
+  const { activeTeam, activeTeamInfo: team } = useAppState();
   const [loading, createCustomerPortal] = useCreateCustomerPortal(activeTeam);
 
   return (
@@ -19,6 +20,26 @@ export const Stripe = () => {
       >
         {loading ? 'Loading...' : 'Manage subscription'}
       </Link>
+
+      {!loading && team.subscription.cancelAt && (
+        <Text size={3} css={css({ color: '#F7CC66' })}>
+          Your subscription expires on{' '}
+          {format(new Date(team.subscription.cancelAt), 'PP')}.{' '}
+          <Button
+            autoWidth
+            variant="link"
+            css={css({
+              color: 'inherit',
+              padding: 0,
+              textDecoration: 'underline',
+              fontSize: 3,
+            })}
+            onClick={createCustomerPortal}
+          >
+            Reactivate subscription
+          </Button>
+        </Text>
+      )}
     </Stack>
   );
 };
