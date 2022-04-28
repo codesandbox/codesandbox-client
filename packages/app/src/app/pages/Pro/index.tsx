6fab98131b3f3220b93c9d0e484345c1bca5a6f1
Danilo Woznica <danilowoz@gmail.com>
2022-04-28 14:42:13 +0100

    patron page

--- packages/app/src/app/pages/Pro/index.tsx
+++ packages/app/src/app/pages/Pro/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { useAppState, useActions } from 'app/overmind';
-import { SubscriptionPaymentProvider } from 'app/graphql/types';
+import {
+  SubscriptionOrigin,
+  SubscriptionPaymentProvider,
+} from 'app/graphql/types';
 import { useHistory } from 'react-router-dom';
 import { signInPageUrl } from '@codesandbox/common/lib/utils/url-generator';
 
@@ -25,10 +28,13 @@ export const ProPage: React.FC = () => {
 
   if (!activeTeamInfo === undefined) return <p>Loading...</p>;
 
-  if (
+  const isPaddle =
     activeTeamInfo?.subscription?.paymentProvider ===
-    SubscriptionPaymentProvider.Paddle
-  ) {
+    SubscriptionPaymentProvider.Paddle;
+  const isPatron =
+    activeTeamInfo?.subscription?.origin === SubscriptionOrigin.Patron;
+
+  if (isPaddle || isPatron) {
     return <ProLegacy />;
   }
 
