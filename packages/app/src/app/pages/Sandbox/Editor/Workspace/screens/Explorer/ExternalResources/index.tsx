62ecc049ea055d4d571ba22a68dbaf7fa16f3173
Akash-Ramjyothi <akash.ramjyothi@kivihealth.com>
2022-05-16 02:58:46 +0530

    Added External Resources back to Vanilla

--- packages/app/src/app/pages/Sandbox/Editor/Workspace/screens/Explorer/ExternalResources/index.tsx
+++ packages/app/src/app/pages/Sandbox/Editor/Workspace/screens/Explorer/ExternalResources/index.tsx
@@ -180,6 +180,9 @@ const getFontFamily = (search: string) => {
 
   return {
     name: family.split('+').join(' '),
-    id: family.split('+').join('-').toLowerCase(),
+    id: family
+      .split('+')
+      .join('-')
+      .toLowerCase(),
   };
 };
