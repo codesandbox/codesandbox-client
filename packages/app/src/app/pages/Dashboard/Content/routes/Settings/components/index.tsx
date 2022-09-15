2d248d77e0d4245779fb549384cc3724013e0012
Lena Sotto Mayor <lena@codesandbox.io>
2022-09-15 17:40:21 +0000

    Adjust color of team card on team settings

--- packages/app/src/app/pages/Dashboard/Content/routes/Settings/components/index.tsx
+++ packages/app/src/app/pages/Dashboard/Content/routes/Settings/components/index.tsx
@@ -8,7 +8,8 @@ export const Card = props => (
     css={css({
       minHeight: 200,
       padding: 6,
-      background: 'card.background',
+      background:
+        '#1D1D1D' /* file not supporting tokens yet, TODO: change to card.background */,
       border: '1px solid',
       borderColor: 'transparent',
       borderRadius: 'medium',
