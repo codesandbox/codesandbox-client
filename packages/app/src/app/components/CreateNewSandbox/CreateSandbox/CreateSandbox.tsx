76f2826b05d0a03c12c65af264234ce6923825cc
Tristan Dubbeld <tristan.dubbeld@gmail.com>
2022-09-16 16:42:53 +0200

    Remove dupe tabs

--- packages/app/src/app/components/CreateNewSandbox/CreateSandbox/CreateSandbox.tsx
+++ packages/app/src/app/components/CreateNewSandbox/CreateSandbox/CreateSandbox.tsx
@@ -128,33 +128,6 @@ export const CreateSandbox: React.FC<CreateSandboxProps> = ({
             <Tab {...tab} stopId="databases">
               Databases
             </Tab>
-            <Tab {...tab} stopId="my-templates">
-              My templates
-            </Tab>
-            <Tab {...tab} stopId="csb-templates">
-              CodeSandbox templates
-            </Tab>
-            <Tab {...tab} stopId="react-essentials">
-              React essentials
-            </Tab>
-            <Tab {...tab} stopId="vue-essentials">
-              Vue essentials
-            </Tab>
-            <Tab {...tab} stopId="angular-essentials">
-              Angular essentials
-            </Tab>
-            <Tab {...tab} stopId="ui-frameworks">
-              UI frameworks
-            </Tab>
-            <Tab {...tab} stopId="component-libraries">
-              Component libraries
-            </Tab>
-            <Tab {...tab} stopId="starters">
-              Web App and API Starters
-            </Tab>
-            <Tab {...tab} stopId="databases">
-              Databases
-            </Tab>
           </Tabs>
         </Stack>
         <Panel tab={tab} id="quickstart">
