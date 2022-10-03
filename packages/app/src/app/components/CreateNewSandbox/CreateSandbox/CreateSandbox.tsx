1e2c38466cd4e913befb39e432bf72eeef735501
Alex Moldovan <alex.n.moldovan@gmail.com>
2022-10-03 09:25:22 +0000

    chore(create-modal): replace vue with future nuxt template

--- packages/app/src/app/components/CreateNewSandbox/CreateSandbox/CreateSandbox.tsx
+++ packages/app/src/app/components/CreateNewSandbox/CreateSandbox/CreateSandbox.tsx
@@ -37,10 +37,10 @@ export const COLUMN_MEDIA_THRESHOLD = 1600;
 const QUICK_START_IDS = [
   'new',
   'rjk9n4zj7m', // static v1
-  'k8dsq1', // static v2 or blank?
+  'k8dsq1', // blank v2
   'fxis37', // next v2
   'prp60l', // remix v2
-  '371lz2', // vue v2 (should be nuxt?)
+  '6xxu1m', // nuxt (TODO: move it to v2 and make it official)
   'vue',
   'svelte',
   'angular',
