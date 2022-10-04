0ddfd4dedfa6c86bf6c884c24d160286bc1c33fd
Tristan Dubbeld <tristan.dubbeld@gmail.com>
2022-10-04 20:44:40 +0200

    Repositories empty state

--- packages/app/src/app/pages/Dashboard/Components/EmptyScreen/index.tsx
+++ packages/app/src/app/pages/Dashboard/Components/EmptyScreen/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
+import Media from 'react-media';
 import { Element, Stack, Text, Button, Icon } from '@codesandbox/components';
 import css from '@styled-system/css';
 import { useActions } from 'app/overmind';
 import { NewSandbox } from '../Sandbox/NewSandbox';
 import { PageTypes } from '../../types';
-import { ImportRepo } from '../Repo/ImportRepo';
 import { GRID_MAX_WIDTH, GUTTER } from '../VariableGrid';
 
 interface EmptyScreenProps {
@@ -41,24 +41,95 @@ export const EmptyScreen: React.FC<EmptyScreenProps> = ({
 
   if (page === 'repositories') {
     return (
-      <Stack justify="center" align="center" marginTop={120}>
-        <Stack
-          direction="vertical"
-          align="center"
-          gap={8}
-          css={{ width: 500, height: '100vh', userSelect: 'none' }}
-        >
-          <Stack align="center" css={{ width: 220 }}>
-            <ImportRepo />
-          </Stack>
+      <Media query="(max-width: 960px)">
+        {match => {
+          return (
+            <Stack
+              direction={'vertical'}
+              gap={8}
+              css={css({
+                width: `calc(100% - ${2 * GUTTER}px)`,
+                maxWidth: GRID_MAX_WIDTH - 2 * GUTTER,
+                marginX: 'auto',
+                marginTop: 8,
+              })}
+            >
+              <Stack direction="vertical" gap={4} css={{ maxWidth: '831px' }}>
+                <Text
+                  as="h2"
+                  size={32}
+                  weight="500"
+                  css={{ marginTop: 0, fontFamily: 'Everett, sans-serif' }}
+                >
+                  <Text css={css({ color: '#EDFFA5' })}>Repositories:</Text> an
+                  improved git workflow powered by the cloud.
+                </Text>
 
-          <Stack direction="vertical" align="center" gap={1}>
-            <Text variant="muted" align="center">
-              Uh oh, you haven’t imported any repositories.
-            </Text>
-          </Stack>
-        </Stack>
-      </Stack>
+                <Text
+                  as="p"
+                  weight="400"
+                  variant="muted"
+                  size={16}
+                  css={{ margin: 0, lineHeight: '24px' }}
+                >
+                  Open any open-source repository on CodeSandbox and just click
+                  &apos;Branch&apos; to start contributing. Don&apos;t worry
+                  about forking and setting up a new repository. We&apos;ll take
+                  care of that.
+                </Text>
+              </Stack>
+              <Stack direction={match ? 'vertical' : 'horizontal'} gap={3}>
+                <Button
+                  onClick={() => {
+                    openCreateSandboxModal({ initialTab: 'import' });
+                  }}
+                  variant="secondary"
+                  css={css({
+                    height: 'auto',
+                    padding: '16px 32px',
+                  })}
+                  autoWidth={!match}
+                >
+                  <Stack gap={5} align="center">
+                    <Icon name="github" size={20} />
+                    <Text size={16} css={css({ lineHeight: '24px' })}>
+                      Import from GitHub
+                    </Text>
+                  </Stack>
+                </Button>
+                <Button
+                  onClick={() => {
+                    // TODO: start from template
+                    openCreateSandboxModal({ initialTab: 'import' });
+                  }}
+                  variant="secondary"
+                  css={css({
+                    height: 'auto',
+                    padding: '16px 32px',
+                  })}
+                  autoWidth={!match}
+                >
+                  <Text size={16} css={css({ lineHeight: '24px' })}>
+                    Start from a template
+                  </Text>
+                </Button>
+                {/* ❗️ TODO: Insert right link */}
+                <Button
+                  variant="link"
+                  css={css({
+                    height: 'auto',
+                    padding: '16px 32px',
+                    fontSize: '16px',
+                  })}
+                  autoWidth={!match}
+                >
+                  Learn more
+                </Button>
+              </Stack>
+            </Stack>
+          );
+        }}
+      </Media>
     );
   }
 
@@ -136,6 +207,7 @@ export const EmptyScreen: React.FC<EmptyScreenProps> = ({
               css={css({
                 height: 'auto',
                 padding: '16px 32px',
+                fontSize: '16px',
               })}
               autoWidth
             >
