eafe9ff40b089c6b95ca9878930b65fed29aa2bd
Tristan Dubbeld <tristan.dubbeld@gmail.com>
2022-10-04 18:10:25 +0200

    Create contributions empty state

--- packages/app/src/app/pages/Dashboard/Components/EmptyScreen/index.tsx
+++ packages/app/src/app/pages/Dashboard/Components/EmptyScreen/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
-import { Stack, Text, Button, Icon } from '@codesandbox/components';
+import { Element, Stack, Text, Button, Icon } from '@codesandbox/components';
+import css from '@styled-system/css';
 import { useActions } from 'app/overmind';
 import { NewSandbox } from '../Sandbox/NewSandbox';
 import { PageTypes } from '../../types';
 import { ImportRepo } from '../Repo/ImportRepo';
+import { GRID_MAX_WIDTH, GUTTER } from '../VariableGrid';
 
 interface EmptyScreenProps {
   collectionId?: string;
@@ -62,21 +64,73 @@ export const EmptyScreen: React.FC<EmptyScreenProps> = ({
 
   if (page === 'my-contributions') {
     return (
-      <Stack justify="center" align="center" marginTop={120}>
-        <Stack
-          direction="vertical"
-          align="center"
-          gap={8}
-          css={{ width: 500, height: '100vh', userSelect: 'none' }}
-        >
-          <Stack align="center" css={{ width: 220 }}>
-            <ImportRepo />
-          </Stack>
-
-          <Stack direction="vertical" align="center" gap={1}>
-            <Text variant="muted" align="center">
-              Uh oh, you don’t have any open source contributions.
+      <Stack
+        gap={12}
+        direction="horizontal"
+        align="stretch"
+        css={css({
+          width: `calc(100% - ${2 * GUTTER}px)`,
+          maxWidth: GRID_MAX_WIDTH - 2 * GUTTER,
+          marginX: 'auto',
+          marginTop: 8,
+        })}
+      >
+        <Element
+          as="img"
+          src="/static/img/contributions-branch.png"
+          alt=""
+          css={{ display: 'block', width: 259, height: 221 }}
+        />
+        <Stack gap={8} direction="vertical" justify="space-between">
+          <Stack gap={4} direction="vertical" css={{ maxWidth: '674px' }}>
+            <Text
+              as="h2"
+              size={32}
+              weight="500"
+              css={{ marginTop: 0, fontFamily: 'Everett, sans-serif' }}
+            >
+              Open-source development made easy
             </Text>
+            <Text
+              as="p"
+              weight="400"
+              variant="muted"
+              size={16}
+              css={{ margin: 0, lineHeight: '24px' }}
+            >
+              Open any open-source repository on CodeSandbox and just click
+              &apos;Branch&apos; to start contributing. Don&apos;t worry about
+              forking and setting up a new repository. We&apos;ll take care of
+              that.
+            </Text>
+          </Stack>
+          <Stack direction="horizontal" gap={1}>
+            <Button
+              variant="secondary"
+              css={css({
+                height: 'auto',
+                padding: '16px 32px',
+              })}
+              autoWidth
+            >
+              <Stack gap={5} align="center">
+                <Icon name="github" size={20} />
+                <Text size={16} css={css({ lineHeight: '24px' })}>
+                  Import from GitHub
+                </Text>
+              </Stack>
+            </Button>
+            {/* ❗️ TODO: Insert right link */}
+            <Button
+              variant="link"
+              css={css({
+                height: 'auto',
+                padding: '16px 32px',
+              })}
+              autoWidth
+            >
+              Learn more
+            </Button>
           </Stack>
         </Stack>
       </Stack>
