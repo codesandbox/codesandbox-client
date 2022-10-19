eaaa117ac9830b8b2222c1cfcfa4a02f25c63592
Maria Clara <claarasantana@gmail.com>
2022-10-19 11:12:23 +0000

    fixup

--- packages/app/src/app/pages/common/Modals/NewTeamModal/TeamSubscription.tsx
+++ packages/app/src/app/pages/common/Modals/NewTeamModal/TeamSubscription.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import { useAppState } from 'app/overmind';
+import { useActions, useAppState } from 'app/overmind';
 import { Button, Stack, Text } from '@codesandbox/components';
 
 export const TeamSubscription: React.FC = () => {
   const { activeTeamInfo } = useAppState();
+  const actions = useActions();
 
   // Only teams that never had a subscription are elligible for
   // the 14-day free trial.
@@ -13,12 +14,14 @@ export const TeamSubscription: React.FC = () => {
     <Stack direction="vertical">
       <Text as="p">{activeTeamInfo.name}</Text>
       <Text as="h2">Try Team Pro for free</Text>
-      <Button>
+      <Button disabled>
         {isElligibleForTrial
           ? 'Start 14 day free trial'
           : 'Proceed to checkout'}
       </Button>
-      <Button variant="secondary">Continue with free plan</Button>
+      <Button onClick={() => actions.modalClosed()} variant="secondary">
+        Continue with free plan
+      </Button>
     </Stack>
   );
 };
