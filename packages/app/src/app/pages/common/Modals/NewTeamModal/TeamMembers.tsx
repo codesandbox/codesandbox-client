2ebecdb3e1aaa55fed4ec196cc84a4b36bcf4b2e
Tristan Dubbeld <tristan.dubbeld@gmail.com>
2022-10-23 16:51:56 +0200

    Email submission WIP

--- packages/app/src/app/pages/common/Modals/NewTeamModal/TeamMembers.tsx
+++ packages/app/src/app/pages/common/Modals/NewTeamModal/TeamMembers.tsx
@@ -1,13 +1,52 @@
-import React from 'react';
+import React, { FormEvent, useState } from 'react';
 import { Button, Stack, Text } from '@codesandbox/components';
 import { useAppState } from 'app/overmind';
 import { StyledButton } from 'app/components/dashboard/Button';
 import { Textarea } from 'app/components/dashboard/Textarea';
 
+// function validateEmail(email: string) {
+//   // Test for "anything@anything.anything" and check for
+//   // multiple @ signs.
+//   return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+// }
+
 export const TeamMembers: React.FC<{ onComplete: () => void }> = ({
   onComplete,
 }) => {
   const { activeTeamInfo } = useAppState();
+  const [addressesString, setAddressesString] = useState<string>();
+  const [invalidEmails /* , setInvalidEmails */] = useState<string[]>();
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+
+    /**
+     * Submitting emails is a work in progress. We're not sure if we
+     * can invite as many users at the same time.
+     */
+
+    // Remove spaces and split email addresses
+    // const emails = addressesString.replace(' ', '').split(',');
+
+    // // Validate emails
+    // const invalid = emails.filter(email => {
+    //   const isValidEmail = validateEmail(email);
+
+    //   // Return when the email is not valid
+    //   return !isValidEmail;
+    // });
+
+    // if (invalid.length === 0) {
+    //   // Send invites
+    //   emails.forEach(email => {
+    //     // send invite
+    //     // TODO: WIP
+    //   });
+    // } else {
+    //   // Set error with invalid
+    //   setInvalidEmails(invalid);
+    // }
+  };
 
   return (
     <Stack
@@ -38,7 +77,7 @@ export const TeamMembers: React.FC<{ onComplete: () => void }> = ({
       </Text>
       <Stack
         as="form"
-        onSubmit={e => e.preventDefault()}
+        onSubmit={handleSubmit}
         direction="vertical"
         gap={6}
         css={{ width: '100%' }}
@@ -50,7 +89,21 @@ export const TeamMembers: React.FC<{ onComplete: () => void }> = ({
           autoFocus
           required
           rows={3}
+          value={addressesString}
+          onChange={e => {
+            setAddressesString(e.target.value);
+          }}
         />
+        {invalidEmails?.length > 0 ? (
+          <Text size={2} variant="danger">
+            There seems to be an error in the following addresses, please
+            review:{' '}
+            {invalidEmails.map((invalidEmail, index, arr) => {
+              const isLastEmail = arr.length - 1 === index;
+              return `${invalidEmail}${isLastEmail ? '.' : ', '}`;
+            })}
+          </Text>
+        ) : null}
         <StyledButton type="submit" disabled>
           Invite members
         </StyledButton>
