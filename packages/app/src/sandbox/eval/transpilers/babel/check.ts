508dcb0ae6b8d66ad5f910810ac1a2e2a1459bbd
Jasper De Moor <jasperdemoor@gmail.com>
2022-10-27 09:22:48 +0200

    chore(sandpack): remove unused ast utils (#7069)
    * chore: remove old ast utils
    
    * further cleanup

--- packages/app/src/sandbox/eval/transpilers/babel/check.ts
+++
@@ -1,6 +0,0 @@
-import { getSyntaxInfoFromCode } from './ast/syntax-info';
-
-export function shouldTranspile(code: string, path: string) {
-  const syntaxInformation = getSyntaxInfoFromCode(code, path);
-  return syntaxInformation.esm || syntaxInformation.jsx;
-}
