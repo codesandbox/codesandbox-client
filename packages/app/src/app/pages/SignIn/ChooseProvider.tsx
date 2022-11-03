eb654f72ef36fd2e4622c575db24563a47761077
Maria Clara <claarasantana@gmail.com>
2022-11-03 11:23:54 -0300

    feat(signin): update page ui (#7090)
    * update signin page
    
    * fixup
    
    * remove unused elements

--- packages/app/src/app/pages/SignIn/ChooseProvider.tsx
+++ packages/app/src/app/pages/SignIn/ChooseProvider.tsx
@@ -1,31 +1,24 @@
 import React from 'react';
-import { useAppState, useActions } from 'app/overmind';
+import { Button as MainButton, Stack, Text } from '@codesandbox/components';
 import {
   AppleIcon,
   github as GitHubIcon,
   CodeSandboxIcon,
   GoogleIcon,
 } from '@codesandbox/components/lib/components/Icon/icons';
-import {
-  Element,
-  Stack,
-  Text,
-  Button as MainButton,
-} from '@codesandbox/components';
-import { css } from '@styled-system/css';
+import { useActions, useAppState } from 'app/overmind';
 import history from 'app/utils/history';
 
 import { Button } from './components/Button';
 
-interface ChooseProviderProps {
+type ChooseProviderProps = {
   redirectTo?: string;
   onSignIn?: () => void;
-}
-
-export const ChooseProvider = ({
+};
+export const ChooseProvider: React.FC<ChooseProviderProps> = ({
   redirectTo,
   onSignIn,
-}: ChooseProviderProps) => {
+}) => {
   const { loadingAuth, cancelOnLogin } = useAppState();
   const {
     signInButtonClicked,
@@ -57,105 +50,128 @@ export const ChooseProvider = ({
 
     setLoadingAuth(provider);
   };
-
   return (
     <Stack
-      padding={8}
-      direction="vertical"
-      justify="center"
       align="center"
-      gap={3}
-      css={{ textAlign: 'center' }}
+      css={{
+        width: '100%',
+        height: '100%',
+        padding: '48px 0',
+        color: '#fff',
+      }}
+      direction="vertical"
+      justify="space-between"
     >
-      <CodeSandboxIcon width={56} height={56} />
-      <Text
-        as="h2"
-        weight="medium"
-        align="center"
-        css={{
-          fontSize: 32,
-          fontFamily: 'Inter, sans-serif',
-          margin: 0,
-          letterSpacing: '-.025em',
-        }}
-        block
-      >
-        Sign in to CodeSandbox
-      </Text>
-      <Text
-        as="p"
-        variant="muted"
-        size={3}
-        css={{
-          fontSize: 13,
-          fontFamily: 'Inter, sans-serif',
-          marginBottom: '40px!important',
-          margin: 0,
-        }}
-        block
-      >
-        Create, share, and get feedback with collaborative code.
-      </Text>
-      <Button
-        loading={loadingAuth.github}
-        onClick={() => handleSignIn('github')}
-      >
-        <GitHubIcon />
-        <Element>Sign in with GitHub</Element>
-      </Button>
-      <Text
-        variant="muted"
-        size={3}
-        css={{
-          fontSize: 13,
-          margin: 0,
-        }}
-        block
-      >
-        -
-      </Text>
-      <Button
-        loading={loadingAuth.google}
-        onClick={() => handleSignIn('google')}
-        secondary
-      >
-        <GoogleIcon />
-        <Element>Sign in with Google</Element>
-      </Button>
-      <Button
-        loading={loadingAuth.apple}
-        onClick={() => handleSignIn('apple')}
-        secondary
-      >
-        <AppleIcon />
-        <Element>Sign in with Apple</Element>
-      </Button>
-      {cancelOnLogin && (
-        <MainButton
-          variant="link"
-          type="button"
-          css={{
-            marginBottom: 53,
-            color: '#000',
-            '&:hover:not(:disabled)': {
-              color: '#000',
-              opacity: 0.7,
-            },
-          }}
-          onClick={() => {
-            cancelOnLogin();
-            toggleSignInModal();
-          }}
-        >
-          <Text>Continue without an account</Text>
-        </MainButton>
-      )}
+      <CodeSandboxIcon width={48} height={48} />
+      <Stack direction="vertical" gap={8}>
+        <Stack direction="vertical" gap={3}>
+          <Text
+            as="h1"
+            css={{
+              maxWidth: '396px',
+              margin: 0,
+              textAlign: 'center',
+              fontFamily: 'Everett, sans-serif',
+              lineHeight: 1.17,
+              letterSpacing: '-0.018em',
+            }}
+            size={48}
+            weight="medium"
+          >
+            Sign in to CodeSandbox
+          </Text>
+          <Text
+            css={{
+              textAlign: 'center',
+              lineHeight: 1.5,
+              opacity: 0.6,
+            }}
+            size={4}
+          >
+            Login or register to start building your projects today.
+          </Text>
+        </Stack>
+        <Stack direction="vertical" gap={3}>
+          <Button
+            css={{
+              width: '100%',
+              height: '48px',
+            }}
+            loading={loadingAuth.github}
+            onClick={() => handleSignIn('github')}
+          >
+            <GitHubIcon />
+            <Text
+              as="span"
+              css={{
+                lineHeight: 1,
+              }}
+              size={4}
+              weight="medium"
+            >
+              Sign in with GitHub
+            </Text>
+          </Button>
+          <Stack
+            css={{
+              '@media screen and (max-width: 440px)': {
+                flexDirection: 'column',
+              },
+            }}
+            gap={3}
+          >
+            <Button
+              css={{
+                width: '100%',
+                height: '40px',
+                flex: 1,
+              }}
+              loading={loadingAuth.google}
+              onClick={() => handleSignIn('google')}
+              secondary
+            >
+              <GoogleIcon />
+              <Text as="span" css={{ lineHeight: 1 }} size={3} weight="medium">
+                Sign in with Google
+              </Text>
+            </Button>
+            <Button
+              css={{
+                width: '100%',
+                height: '40px',
+                flex: 1,
+              }}
+              loading={loadingAuth.apple}
+              onClick={() => handleSignIn('apple')}
+              secondary
+            >
+              <AppleIcon />
+              <Text as="span" css={{ lineHeight: 1 }} size={3} weight="medium">
+                Sign in with Apple
+              </Text>
+            </Button>
+          </Stack>
+        </Stack>
+        {cancelOnLogin && (
+          <MainButton
+            variant="link"
+            type="button"
+            onClick={() => {
+              cancelOnLogin();
+              toggleSignInModal();
+            }}
+          >
+            <Text>Continue without an account</Text>
+          </MainButton>
+        )}
+      </Stack>
+
       <Text
         variant="muted"
         align="center"
         size={10}
         block
-        css={css({
+        css={{
           lineHeight: '13px',
           maxWidth: '200px',
           margin: '24px 0 0 0',
@@ -163,7 +179,7 @@ export const ChooseProvider = ({
           a: {
             color: 'inherit',
           },
-        })}
+        }}
       >
         By continuing, you agree to CodeSandbox{' '}
         <a
