17bb51c3a49028926cf6581c0f6cbeabb3af7968
Danilo Woznica <danilowoz@gmail.com>
2022-11-09 15:44:05 +0000

    Update gatsby-ssr.jsx

--- packages/homepage/gatsby-ssr.jsx
+++ packages/homepage/gatsby-ssr.jsx
@@ -1,23 +1,6 @@
 import * as React from 'react';
 import { getSandpackCssText } from '@codesandbox/sandpack-react';
 
-// https://mathiasbynens.be/notes/globalthis
-// The polyfill starts here.
-/* eslint-disable no-undef,no-extend-native */
-(function () {
-  if (typeof globalThis === 'object') return;
-  Object.defineProperty(Object.prototype, '__magic__', {
-    get() {
-      return this;
-    },
-    configurable: true,
-  });
-  __magic__.globalThis = __magic__;
-  delete Object.prototype.__magic__;
-})();
-// The polyfill ends here.
-// #endregion The polyfill ends here.
-
 exports.onRenderBody = ({ setHeadComponents }) => {
   setHeadComponents([
     <style
