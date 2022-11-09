56b790b579c885eff7c86a60c6a5bacb4fd98d83
Maria Clara <claarasantana@gmail.com>
2022-11-09 18:42:34 +0000

    update useDismissible to reflect action instantly

--- packages/app/src/app/hooks/useDismissible.ts
+++ packages/app/src/app/hooks/useDismissible.ts
@@ -1,20 +1,50 @@
+import { useState } from 'react';
 import { useEffects } from 'app/overmind';
 
 /**
  * Localstorage keys used for dismissible modals and banners.
  */
-type DismissibleKeys =
-  // TODO - Any key in this type should refer to a dismissible item. Right
-  // now there are none yet.
-  'TODO';
+type DismissibleKeys = 'DASHBOARD_RECENT_UPGRADE';
 
 type Dismissibles = Partial<Record<DismissibleKeys, true>>;
 
-export const useDismissible = (key: DismissibleKeys) => {
-  const { browser } = useEffects();
+// export const useLocalPersistedState = <T>(
+//   key: string,
+//   initialValue: T
+// ): [T, (arg: T) => void] => {
 
-  const dismissibles = browser.storage.get<Dismissibles>('DISMISSIBLES');
-  const isDismissed = dismissibles?.[key] === true;
+//   const [storedValue, setStoredValue] = useState<T>(() => {
+//     try {
+//       const item: T | null = storage.get(key);
+//       return item ?? initialValue;
+//     } catch (error) {
+//       logger.error(
+//         `[persisted-state]: Failed to retrieve data from local storage`,
+//         error
+//       );
+//       return initialValue;
+//     }
+//   });
+
+//   const setValue = (value: T) => {
+//     try {
+//       const valueToStore =
+//         value instanceof Function ? value(storedValue) : value;
+//       setStoredValue(value);
+//       storage.set(key, valueToStore);
+//     } catch (error) {
+//       logger.error(`[persisted-state]:`, error);
+//     }
+//   };
+//   return [storedValue, setValue];
+// };
+
+export const useDismissible = (key: DismissibleKeys): [boolean, () => void] => {
+  const { browser } = useEffects();
+  const [isDismissed, setIsDismissed] = useState(() => {
+    const dismissibles = browser.storage.get<Dismissibles>('DISMISSIBLES');
+    return dismissibles?.[key] === true;
+  });
 
   const dismiss = () => {
     // Getting the dismissibles again to make sure other instances aren't
@@ -26,6 +56,7 @@ export const useDismissible = (key: DismissibleKeys) => {
       ...prevDismissibles,
       [key]: true,
     });
+    setIsDismissed(true);
   };
 
   return [isDismissed, dismiss];
