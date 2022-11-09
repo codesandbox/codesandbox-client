2b8cf8bd556c4c902d952a366332098a6e296030
Maria Clara <claarasantana@gmail.com>
2022-11-09 18:51:40 +0000

    fixup

--- packages/app/src/app/hooks/useDismissible.ts
+++ packages/app/src/app/hooks/useDismissible.ts
@@ -8,37 +8,6 @@ type DismissibleKeys = 'DASHBOARD_RECENT_UPGRADE';
 
 type Dismissibles = Partial<Record<DismissibleKeys, true>>;
 
-// export const useLocalPersistedState = <T>(
-//   key: string,
-//   initialValue: T
-// ): [T, (arg: T) => void] => {
-
-//   const [storedValue, setStoredValue] = useState<T>(() => {
-//     try {
-//       const item: T | null = storage.get(key);
-//       return item ?? initialValue;
-//     } catch (error) {
-//       logger.error(
-//         `[persisted-state]: Failed to retrieve data from local storage`,
-//         error
-//       );
-//       return initialValue;
-//     }
-//   });
-
-//   const setValue = (value: T) => {
-//     try {
-//       const valueToStore =
-//         value instanceof Function ? value(storedValue) : value;
-//       setStoredValue(value);
-//       storage.set(key, valueToStore);
-//     } catch (error) {
-//       logger.error(`[persisted-state]:`, error);
-//     }
-//   };
-//   return [storedValue, setValue];
-// };
-
 export const useDismissible = (key: DismissibleKeys): [boolean, () => void] => {
   const { browser } = useEffects();
   const [isDismissed, setIsDismissed] = useState(() => {
