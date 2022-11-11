67a224aefd393c29b18adabf89aad72860008285
Maria Clara <claarasantana@gmail.com>
2022-11-11 03:09:29 +0000

    fixup

--- packages/app/src/app/components/StripeMessages/PaymentPending.tsx
+++ packages/app/src/app/components/StripeMessages/PaymentPending.tsx
@@ -4,25 +4,35 @@ import { MessageStripe } from '@codesandbox/components';
 import { useCreateCustomerPortal } from 'app/hooks/useCreateCustomerPortal';
 import { useAppState } from 'app/overmind';
 import { useLocation } from 'react-router-dom';
+import { useWorkspaceAuthorization } from 'app/hooks/useWorkspaceAuthorization';
 
 export const PaymentPending: React.FC = () => {
   const { activeTeam } = useAppState();
   const { pathname } = useLocation();
+  const { isTeamAdmin } = useWorkspaceAuthorization();
   const [
     loadingCustomerPortal,
     createCustomerPortal,
   ] = useCreateCustomerPortal({ team_id: activeTeam, return_path: pathname });
 
+  if (isTeamAdmin) {
+    return (
+      <MessageStripe variant="warning">
+        There are some issues with your payment. Please update your payment
+        details.
+        <MessageStripe.Action
+          loading={loadingCustomerPortal}
+          onClick={createCustomerPortal}
+        >
+          Update payment
+        </MessageStripe.Action>
+      </MessageStripe>
+    );
+  }
   return (
     <MessageStripe variant="warning">
-      There are some issues with your payment. Please update your payment
-      details.
-      <MessageStripe.Action
-        loading={loadingCustomerPortal}
-        onClick={createCustomerPortal}
-      >
-        Update payment
-      </MessageStripe.Action>
+      There are some issues with your payment. Please contact your team admin to
+      update your payment details.
     </MessageStripe>
   );
 };
