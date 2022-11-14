8dd714265ffe28247c34d5ccb91a0a6866495966
Maria Clara <claarasantana@gmail.com>
2022-11-14 20:58:26 +0000

    create useGetCheckoutURL

--- packages/app/src/app/hooks/useCreateCheckout.ts
+++ packages/app/src/app/hooks/useCreateCheckout.ts
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useEffects } from 'app/overmind';
 import { dashboard } from '@codesandbox/common/lib/utils/url-generator';
 
@@ -8,8 +8,8 @@ type CheckoutStatus =
   | { status: 'error'; error: string };
 
 type CheckoutOptions = {
-  team_id: string;
-  recurring_interval: string;
+  team_id: string | undefined;
+  recurring_interval?: string;
   success_path?: string;
   cancel_path?: string;
 };
@@ -23,7 +23,7 @@ export const useCreateCheckout = (): [
 
   const createCheckout = async ({
     team_id,
-    recurring_interval,
+    recurring_interval = 'month',
     success_path = dashboard.settings(team_id) + '&payment_pending=true',
     cancel_path = '/pro',
   }: CheckoutOptions) => {
@@ -49,4 +49,70 @@ export const useCreateCheckout = (): [
   };
 
   return [status, createCheckout];
+};
+
+type CheckoutState =
+  | { state: 'IDLE' }
+  | {
+      state: 'READY';
+      url: string;
+    }
+  | { state: 'LOADING' }
+  | { state: 'ERROR'; error: string };
+
+// TODO: replace useCreateCheckout usages
+// with useGetCheckoutURL to remove the
+// loading state when redirecting users
+// to the checkout page.
+export const useGetCheckoutURL = ({
+  team_id,
+  success_path = dashboard.settings(team_id) + '&payment_pending=true',
+  cancel_path = '/pro',
+  recurring_interval = 'month',
+}: CheckoutOptions): CheckoutState => {
+  const [checkout, setCheckout] = useState<CheckoutState>({ state: 'IDLE' });
+  const { api } = useEffects();
+
+  const getCheckoutUrl = async () => {
+    if (!team_id) {
+      setCheckout({
+        state: 'IDLE',
+      });
+
+      return;
+    }
+
+    try {
+      setCheckout({
+        state: 'LOADING',
+      });
+
+      const payload = await api.stripeCreateCheckout({
+        success_path,
+        cancel_path,
+        team_id,
+        recurring_interval,
+      });
+
+      if (payload.stripeCheckoutUrl) {
+        setCheckout({
+          state: 'READY',
+          url: payload.stripeCheckoutUrl,
+        });
+      }
+    } catch (error) {
+      setCheckout({
+        state: 'ERROR',
+        error:
+          JSON.stringify(error?.messagge || error) ??
+          'Failed to get checkout URL.',
+      });
+    }
+  };
+
+  useEffect(() => {
+    getCheckoutUrl();
+  }, [team_id]);
+
+  return checkout;
 };
