3e71a8351e19c0b647c68b9920425356c2562919
Maria Clara <claarasantana@gmail.com>
2022-11-15 22:57:57 +0000

    wip

--- packages/app/src/app/components/CreateSandbox/Import/importLimits.tsx
+++ packages/app/src/app/components/CreateSandbox/Import/importLimits.tsx
@@ -7,20 +7,29 @@ import {
 import { useGetCheckoutURL } from 'app/hooks/useCreateCheckout';
 import { useSubscription } from 'app/hooks/useSubscription';
 import { useWorkspaceAuthorization } from 'app/hooks/useWorkspaceAuthorization';
-import { useAppState } from 'app/overmind';
+import { useActions, useAppState } from 'app/overmind';
 import React from 'react';
 import { Link as RouterLink, useLocation } from 'react-router-dom';
 
-const getEventName = (isEligibleForTrial: boolean) =>
-  isEligibleForTrial
-    ? 'Limit banner: import - Start Trial'
-    : 'Limit banner: import - Upgrade';
+const getEventName = (
+  isEligibleForTrial: boolean,
+  isTeamAdmin?: boolean
+): string => {
+  if (isTeamAdmin) {
+    return isEligibleForTrial
+      ? 'Limit banner: import - Start Trial'
+      : 'Limit banner: import - Upgrade';
+  }
+
+  return 'Limit banner: import - Learn more';
+};
 
 export const MaxPublicRepos: React.FC = () => {
   const { activeTeam } = useAppState();
   const { isEligibleForTrial } = useSubscription();
   const { isTeamAdmin, isPersonalSpace } = useWorkspaceAuthorization();
   const { pathname } = useLocation();
+  const { modals } = useActions();
 
   const checkout = useGetCheckoutURL({
     team_id: isTeamAdmin || isPersonalSpace ? activeTeam : undefined,
@@ -44,6 +53,14 @@ export const MaxPublicRepos: React.FC = () => {
                 to: '/pro',
               })}
           onClick={() => {
+            // If we don't have a checkout URL, we
+            // default to `/pro` which uses a link
+            // from react router so we have to
+            // imperatively close the modal.
+            if (checkout.state !== 'READY') {
+              modals.newSandboxModal.close();
+            }
+
             track(getEventName(isEligibleForTrial), {
               codesandbox: 'V1',
               event_source: 'UI',
@@ -62,6 +79,7 @@ export const PrivateRepoFreeTeam: React.FC = () => {
   const { isEligibleForTrial } = useSubscription();
   const { isTeamAdmin, isPersonalSpace } = useWorkspaceAuthorization();
   const { pathname } = useLocation();
+  const { modals } = useActions();
 
   const checkout = useGetCheckoutURL({
     team_id: isTeamAdmin || isPersonalSpace ? activeTeam : undefined,
@@ -86,11 +104,29 @@ export const PrivateRepoFreeTeam: React.FC = () => {
     <>
       The free plan only allows public repos. For private repositories,{' '}
       <StyledLink
+        {...(checkoutURL.startsWith('/pro')
+          ? {
+              as: 'a',
+              href: checkoutURL,
+            }
+          : {
+              as: RouterLink,
+              to: '/pro',
+            })}
         css={{
           padding: 0,
         }}
         color="#FFFFFF"
-        href={checkoutURL}
+        onClick={() => {
+          if (checkoutURL.startsWith('/pro')) {
+            modals.newSandboxModal.close();
+          }
+
+          track(getEventName(isEligibleForTrial, isTeamAdmin), {
+            codesandbox: 'V1',
+            event_source: 'UI',
+          });
+        }}
       >
         upgrade to{' '}
         <Element as="span" css={{ textTransform: 'uppercase' }}>
