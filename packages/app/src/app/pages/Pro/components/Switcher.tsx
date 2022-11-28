76fad4204c9e4f8d25fd496b4e2a22f7a41f4ee7
Tristan Dubbeld <tristan.dubbeld@gmail.com>
2022-11-28 11:10:14 +0100

    Clean up Switcher

--- packages/app/src/app/pages/Pro/components/Switcher.tsx
+++ packages/app/src/app/pages/Pro/components/Switcher.tsx
@@ -5,12 +5,10 @@ import styled from 'styled-components';
 import {
   Badge,
   Stack,
-  Avatar,
   Menu,
   Icon,
   Element,
   Text,
-  Tooltip,
 } from '@codesandbox/components';
 import {
   CurrentTeamInfoFragmentFragment,
@@ -18,7 +16,152 @@ import {
   TeamFragmentDashboardFragment,
   TeamMemberAuthorization,
 } from 'app/graphql/types';
-import { Caption } from './elements';
+import { useWorkspaceSubscription } from 'app/hooks/useWorkspaceSubscription';
+
+export const Switcher: React.FC<{
+  workspaces: TeamFragmentDashboardFragment[];
+  setActiveTeam: (payload?: { id: string }) => Promise<void>;
+  workspaceType: 'pro' | 'teamPro';
+  activeTeamInfo: CurrentTeamInfoFragmentFragment;
+  personalWorkspaceId: string;
+  userId: string;
+  openCreateTeamModal: () => void;
+}> = ({
+  workspaces,
+  setActiveTeam,
+  personalWorkspaceId,
+  activeTeamInfo,
+  userId,
+  openCreateTeamModal,
+}) => {
+  const { isFree } = useWorkspaceSubscription();
+  // TODO: move this check up a component, shouldn't happen
+  if (!workspaces || workspaces.length === 0) return null;
+
+  return (
+    <Menu>
+      <MenuTrigger>
+        <Text>{activeTeamInfo.name}</Text>
+        {isFree ? <Badge variant="trial">Free</Badge> : null}
+      </MenuTrigger>
+
+      <MenuList>
+        <Element css={{ minWidth: 290, maxHeight: 600, overflow: 'auto' }}>
+          <Text
+            size={12}
+            lineHeight="16px"
+            css={{ display: 'block', padding: '8px 24px' }}
+          >
+            Select team to upgrade
+          </Text>
+
+          {workspaces.map(workspace => {
+            if (!workspace) return null;
+
+            const isAdmin =
+              workspace.userAuthorizations.find(team => team.userId === userId)
+                .authorization === TeamMemberAuthorization.Admin;
+
+            const isPro = [
+              SubscriptionType.TeamPro,
+              SubscriptionType.PersonalPro,
+            ].includes(workspace.subscription?.type);
+
+            const disabled = !isAdmin;
+
+            return (
+              <MenuItem
+                onSelect={() => setActiveTeam(workspace)}
+                key={workspace.id}
+                disabled={disabled}
+                style={{ opacity: disabled ? 0.5 : 1, padding: 0 }}
+              >
+                <Stack css={{ padding: '12px 24px' }} align="center" gap={4}>
+                  <TeamAvatar
+                    size="small"
+                    avatar={workspace.avatarUrl}
+                    name={workspace.name}
+                    css={{ border: '1px solid #5f5f5d', borderRadius: '2px' }}
+                  />
+
+                  <Stack
+                    align="center"
+                    justify="space-between"
+                    gap={3}
+                    css={{ flex: 1 }}
+                  >
+                    <Text
+                      size={4}
+                      css={{
+                        maxWidth: '180px',
+                        overflow: 'hidden',
+                        textOverflow: 'ellipsis',
+                      }}
+                    >
+                      {workspace.id === personalWorkspaceId
+                        ? 'Personal'
+                        : workspace.name}
+                    </Text>
+                    {!isPro && <Badge variant="trial">Free</Badge>}
+                  </Stack>
+                </Stack>
+              </MenuItem>
+            );
+          })}
+
+          <Element marginY={2} css={{ borderTop: '1px solid #5f5f5d' }} />
+
+          <MenuItem onSelect={openCreateTeamModal}>
+            <Stack
+              gap={4}
+              align="center"
+              as={Element}
+              marginY={2}
+              paddingY={3}
+              paddingX={6}
+            >
+              <Stack
+                justify="center"
+                align="center"
+                css={{
+                  width: '24px', // To match Avatar
+                  height: '24px', // To match Avatar
+                  border: '1px solid #5f5f5d',
+                  borderRadius: '2px',
+                }}
+              >
+                <Icon name="plus" size={16} css={{ display: 'block' }} />
+              </Stack>
+
+              <Text size={4}>Create a new team</Text>
+            </Stack>
+          </MenuItem>
+        </Element>
+      </MenuList>
+    </Menu>
+  );
+};
+
+const MenuList = styled(Menu.List)`
+  &[data-reach-menu-list][data-component='MenuList'] {
+    background-color: #373737;
+    margin-top: 10px;
+    margin-left: -2px;
+    border: 0;
+    border-radius: 0;
+    overflow: visible;
+  }
+`;
+
+const MenuItem = styled(Menu.Item)`
+  &[data-reach-menu-item][data-component='MenuItem'] {
+    padding: 0;
+
+    &[data-selected] {
+      background-color: #484848;
+    }
+  }
+`;
 
 const StyledMenuButton = styled(Menu.Button)`
   height: auto;
@@ -48,235 +191,3 @@ const MenuTrigger = ({ children }: { children: React.ReactNode }) => {
     </Stack>
   );
 };
-
-export const Switcher: React.FC<{
-  workspaces: TeamFragmentDashboardFragment[];
-  setActiveTeam: (payload?: { id: string }) => Promise<void>;
-  workspaceType: 'pro' | 'teamPro';
-  activeTeamInfo: CurrentTeamInfoFragmentFragment;
-  personalWorkspaceId: string;
-  userId: string;
-  openCreateTeamModal: () => void;
-}> = ({
-  workspaces,
-  workspaceType,
-  setActiveTeam,
-  personalWorkspaceId,
-  activeTeamInfo,
-  userId,
-  openCreateTeamModal,
-}) => {
-  // move this check up, shouldn't happen
-  if (!workspaces || workspaces.length === 0) return null;
-
-  // 🚮
-  const members = activeTeamInfo.users.length;
-  const memberLabel = `${members} member${members > 1 ? 's' : ''}`;
-  const isPersonalWorkspace = workspaceType === 'pro';
-  const isFreeWorkspace = ![
-    SubscriptionType.TeamPro,
-    SubscriptionType.PersonalPro,
-  ].includes(activeTeamInfo.subscription?.type);
-
-  return (
-    <Menu>
-      <MenuTrigger>
-        <Text>{activeTeamInfo.name}</Text>
-        {isFreeWorkspace ? <Badge variant="trial">Free</Badge> : null}
-      </MenuTrigger>
-
-      <MenuList>
-        <Element css={{ minWidth: 290, maxHeight: 600, overflow: 'auto' }}>
-          <Caption
-            css={{
-              margin: 0,
-              height: 40,
-              lineHeight: '40px',
-              paddingTop: 8,
-              paddingLeft: 24,
-              color: '#999999',
-            }}
-          >
-            Select team to upgrade
-          </Caption>
-
-          {workspaces.map(workspace => {
-            if (!workspace) return null;
-
-            const seats = workspace.users.length;
-            const seatsLabel = `${seats} member${seats > 1 ? 's' : ''}`;
-
-            const isAdmin =
-              workspace.userAuthorizations.find(team => team.userId === userId)
-                .authorization === TeamMemberAuthorization.Admin;
-            const isPro = [
-              SubscriptionType.TeamPro,
-              SubscriptionType.PersonalPro,
-            ].includes(workspace.subscription?.type);
-            const disabled = !isAdmin || isPro;
-
-            return (
-              <MenuItem
-                onSelect={() => setActiveTeam(workspace)}
-                key={workspace.id}
-                disabled={disabled}
-                style={{ opacity: disabled ? 0.5 : 1, padding: 0 }}
-              >
-                <Stack css={{ padding: '12px 24px' }} align="center">
-                  <TeamAvatar
-                    size="small"
-                    avatar={workspace.avatarUrl}
-                    name={workspace.name}
-                  />
-
-                  <Stack direction="vertical" css={{ flex: 1, marginLeft: 19 }}>
-                    <Stack gap={1}>
-                      <Text size={4}>
-                        {workspace.id === personalWorkspaceId
-                          ? 'Personal'
-                          : workspace.name}
-                      </Text>
-                    </Stack>
-
-                    <Text size={3}>
-                      {workspace.id !== personalWorkspaceId ? seatsLabel : null}
-                    </Text>
-                  </Stack>
-
-                  {!isPro && <Badge variant="trial">Free</Badge>}
-                </Stack>
-              </MenuItem>
-            );
-          })}
-
-          <MenuItem onSelect={openCreateTeamModal}>
-            <Stack
-              css={{
-                marginTop: 12,
-                padding: 24,
-                borderTop: '1px solid rgba(255, 255, 255, 0.2)',
-              }}
-              align="center"
-            >
-              <Stack
-                justify="center"
-                align="center"
-                css={{
-                  width: 24,
-                  height: 24,
-                  backgroundColor: '#252525',
-                }}
-              >
-                <Icon name="plus" size={16} />
-              </Stack>
-
-              <Text size={4} css={{ marginLeft: 19 }}>
-                Create a new team
-              </Text>
-            </Stack>
-          </MenuItem>
-        </Element>
-      </MenuList>
-    </Menu>
-  );
-};
-
-const Dialog = styled.div`
-  position: absolute;
-  top: 40px;
-  height: 148px;
-
-  background: #373737;
-
-  border: 1px solid #2a2a2a;
-  box-sizing: border-box;
-  box-shadow: 0px 2px 7px rgba(0, 0, 0, 0.15);
-
-  display: none;
-
-  &:before {
-    content: '';
-    height: 10px;
-    width: 100%;
-    position: absolute;
-    top: -10px;
-    display: block;
-  }
-`;
-
-const WorkspaceName = styled.p`
-  font-family: 'Inter', sans-serif;
-  font-style: normal;
-  font-size: 28px;
-  font-weight: 500;
-  line-height: 36px;
-  letter-spacing: -0.01em;
-
-  margin: 0;
-  text-align: left;
-  color: #e5e5e5;
-
-  & span::-moz-selection {
-    -webkit-text-stroke: 1px #e5e5e5;
-    color: transparent;
-    background: transparent;
-  }
-
-  & span::selection {
-    -webkit-text-stroke: 1px #e5e5e5;
-    color: transparent;
-    background: transparent;
-  }
-`;
-
-const WorkspaceType = styled.p`
-  font-style: normal;
-  font-weight: normal;
-  font-size: 16px;
-  line-height: 24px;
-  margin: 0;
-  text-align: left;
-
-  color: #808080;
-`;
-
-const WorkspaceSeats = styled.div`
-  background: #373737;
-  border: 1 solid #2a2a2a;
-  width: 32px;
-  height: 32px;
-  border-radius: 32px;
-  display: flex;
-  margin-left: 4px;
-  position: relative;
-
-  &:hover ${Dialog} {
-    display: block;
-  }
-
-  span {
-    margin: auto;
-    font-size: 12px;
-  }
-`;
-
-const MenuList = styled(Menu.List)`
-  &[data-reach-menu-list][data-component='MenuList'] {
-    background-color: #373737;
-    margin-top: 10px;
-    margin-left: -2px;
-    border: 0;
-    border-radius: 0;
-    overflow: visible;
-  }
-`;
-
-const MenuItem = styled(Menu.Item)`
-  &[data-reach-menu-item][data-component='MenuItem'] {
-    padding: 0;
-
-    &[data-selected] {
-      background-color: #484848;
-    }
-  }
-`;
