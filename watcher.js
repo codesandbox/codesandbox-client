c7c83715c2b1946d223b5d63da2213f2e4f54e27
Jaya Krishna Namburu <namburu1995@gmail.com>
2022-11-28 10:13:33 +0000

    Update 2 files

--- watcher.js
+++ watcher.js
@@ -2,10 +2,17 @@ const { watch } = require('fs');
 
 const watcher = watch(`${__dirname}/.git`);
 
+const packagesWatcher = watcher(`${__dirname}/packages`);
+
 watcher.on('change', (_, file) => {
   if (file === 'index.lock') {
     return;
   }
+  /* eslint-disable */
+  console.log(file);
+});
+
+packagesWatcher.on('change', (_, file) => {
   /* eslint-disable */
   console.log(file);
 });
