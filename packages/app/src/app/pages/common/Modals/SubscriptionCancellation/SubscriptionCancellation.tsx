7d235e5b5471a9c722f363e531b8d85b32042fa2
Maria Clara <claarasantana@gmail.com>
2022-11-28 21:34:06 +0000

    display limitations

--- packages/app/src/app/pages/common/Modals/SubscriptionCancellation/SubscriptionCancellation.tsx
+++ packages/app/src/app/pages/common/Modals/SubscriptionCancellation/SubscriptionCancellation.tsx
@@ -1,9 +1,23 @@
 import track from '@codesandbox/common/lib/utils/analytics';
-import { Button, Stack, IconButton, Text } from '@codesandbox/components';
+import { Button, IconButton, Stack, Text } from '@codesandbox/components';
 import { useCreateCustomerPortal } from 'app/hooks/useCreateCustomerPortal';
 import { useWorkspaceAuthorization } from 'app/hooks/useWorkspaceAuthorization';
 import { useActions, useAppState } from 'app/overmind';
 import React from 'react';
+import styled from 'styled-components';
+
+// const LIMITS = [
+//   '-3 editor seats',
+//   '25 sandboxes will be restricted',
+//   '7 repositories will be restricted',
+// ];
+
+const StyledListItem = styled('li')`
+  font-weight: 500;
+  font-size: 16px;
+  line-height: 1;
+  color: #ef7a7a;
+`;
 
 export const SubscriptionCancellationModal: React.FC = () => {
   const { activeTeam } = useAppState();
@@ -38,6 +52,25 @@ export const SubscriptionCancellationModal: React.FC = () => {
           You&apos;ll lose access to all Pro features if you decide to cancel
         </Text>
         <IconButton name="cross" title="Close" />
+      </Stack>
+      <Stack
+        as="ul"
+        css={{
+          listStyle: 'none',
+          margin: '24px 0 0',
+          padding: 0,
+        }}
+        direction="vertical"
+        gap={6}
+      >
+        <StyledListItem>Limited to 5 editors seats</StyledListItem>
+        <StyledListItem>Limited to 20 public sandboxes</StyledListItem>
+        <StyledListItem>Limited to 3 public repositories</StyledListItem>
+        <StyledListItem>Only public NPM packages</StyledListItem>
+        <StyledListItem>Only basic privacy settings</StyledListItem>
+        <StyledListItem>-67% GB RAM</StyledListItem>
+        <StyledListItem>-33% Disk space</StyledListItem>
+        <StyledListItem>-2 vCPUs</StyledListItem>
       </Stack>
       <Stack
         css={{
