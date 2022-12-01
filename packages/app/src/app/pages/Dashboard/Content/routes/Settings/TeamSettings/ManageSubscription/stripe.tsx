e04ac3d33e33092b3a037244c770747fe6cb680b
Maria Clara <claarasantana@gmail.com>
2022-12-01 03:33:21 +0000

    udpate UI if cancel at

--- packages/app/src/app/pages/Dashboard/Content/routes/Settings/TeamSettings/ManageSubscription/stripe.tsx
+++ packages/app/src/app/pages/Dashboard/Content/routes/Settings/TeamSettings/ManageSubscription/stripe.tsx
@@ -2,43 +2,88 @@ import React from 'react';
 import { format } from 'date-fns';
 import { Stack, Text, Link } from '@codesandbox/components';
 import track from '@codesandbox/common/lib/utils/analytics';
-import { useActions } from 'app/overmind';
+import { useActions, useAppState } from 'app/overmind';
 import { useWorkspaceSubscription } from 'app/hooks/useWorkspaceSubscription';
+import { useCreateCustomerPortal } from 'app/hooks/useCreateCustomerPortal';
 
 export const Stripe: React.FC = () => {
   const { openCancelSubscriptionModal } = useActions();
+  const { activeTeam } = useAppState();
   const { subscription, hasActiveTeamTrial } = useWorkspaceSubscription();
+  const [loading, createCustomerPortal] = useCreateCustomerPortal({
+    team_id: activeTeam,
+  });
+
+  if (!subscription) {
+    return null;
+  }
+
+  if (subscription.cancelAt) {
+    return (
+      <Stack
+        css={{ height: '100%' }}
+        direction="vertical"
+        justify="space-between"
+      >
+        <Text size={3} css={{ color: '#F7CC66' }}>
+          Your access to Pro features will expire on on{' '}
+          {format(new Date(subscription.cancelAt), 'PP')}. After this period,
+          your team will be automatically migrated to the Free plan.
+        </Text>
+
+        <Link
+          onClick={() => {
+            track('Team Settings - Renew subscription', {
+              codesandbox: 'V1',
+              event_source: 'UI',
+            });
+
+            createCustomerPortal();
+          }}
+          size={3}
+          variant="active"
+        >
+          {loading ? 'Loading...' : 'Upgrade to Pro'}
+        </Link>
+      </Stack>
+    );
+  }
 
   return (
     <Stack direction="vertical" gap={2}>
       <Link
         onClick={() => {
-          if (hasActiveTeamTrial) {
-            track('Team Settings - Cancel trial', {
-              codesandbox: 'V1',
-              event_source: 'UI',
-            });
-          } else {
-            track('Team Settings - Manage Subscription', {
-              codesandbox: 'V1',
-              event_source: 'UI',
-            });
-          }
+          track('Team Settings - Update payment details', {
+            codesandbox: 'V1',
+            event_source: 'UI',
+          });
 
-          openCancelSubscriptionModal();
+          createCustomerPortal();
         }}
         size={3}
         variant="active"
       >
-        {hasActiveTeamTrial ? 'Cancel trial' : 'Manage subscription'}
+        {loading ? 'Loading...' : 'Update payment details'}
       </Link>
+      <Link
+        color="#EE8269"
+        onClick={() => {
+          track(
+            hasActiveTeamTrial
+              ? 'Team Settings - Cancel trial'
+              : 'Team Settings: Cancel subscription',
+            {
+              codesandbox: 'V1',
+              event_source: 'UI',
+            }
+          );
 
-      {subscription?.cancelAt ? (
-        <Text size={3} css={{ color: '#F7CC66' }}>
-          Your subscription expires on{' '}
-          {format(new Date(subscription.cancelAt), 'PP')}.{' '}
-        </Text>
-      ) : null}
+          openCancelSubscriptionModal();
+        }}
+        size={3}
+      >
+        {hasActiveTeamTrial ? 'Cancel trial' : 'Cancel subscription'}
+      </Link>
     </Stack>
   );
 };
