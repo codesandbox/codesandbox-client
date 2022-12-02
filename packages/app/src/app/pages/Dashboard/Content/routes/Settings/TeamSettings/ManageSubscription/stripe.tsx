5e842cb16d9e6beaaf764125d29ff34cf78c2caf
Maria Clara <claarasantana@gmail.com>
2022-12-02 13:50:38 -0300

    feat(subscription/polish): cancel subscription modal (#7195)
    * open modal from stripe in team settings
    
    * wip
    
    * fixup
    
    * add title to modal
    
    * display limitations
    
    * create TEAM_FREE_LIMITS
    
    * show limitations
    
    * fixup
    
    * fixup
    
    * udpate UI if cancel at
    
    * fix
    
    * wip
    
    * fixup
    
    * remove redundant isteamadmin check
    
    * paddle when subscription has been cancelled
    
    * polish paddle xp
    
    * adds warning variant to badge component
    
    * update UI
    
    * update events and remove inline calculations
    
    * track modal close
    
    * fixup
    
    * polishing

--- packages/app/src/app/pages/Dashboard/Content/routes/Settings/TeamSettings/ManageSubscription/stripe.tsx
+++ packages/app/src/app/pages/Dashboard/Content/routes/Settings/TeamSettings/ManageSubscription/stripe.tsx
@@ -2,48 +2,88 @@ import React from 'react';
 import { format } from 'date-fns';
 import { Stack, Text, Link } from '@codesandbox/components';
 import track from '@codesandbox/common/lib/utils/analytics';
-import { useAppState } from 'app/overmind';
+import { useActions, useAppState } from 'app/overmind';
+import { useWorkspaceSubscription } from 'app/hooks/useWorkspaceSubscription';
 import { useCreateCustomerPortal } from 'app/hooks/useCreateCustomerPortal';
 
-export const Stripe: React.FC<{ hasActiveTrial: boolean }> = ({
-  hasActiveTrial,
-}) => {
-  const { activeTeam, activeTeamInfo: team } = useAppState();
+export const Stripe: React.FC = () => {
+  const { openCancelSubscriptionModal } = useActions();
+  const { activeTeam } = useAppState();
+  const { subscription, hasActiveTeamTrial } = useWorkspaceSubscription();
   const [loading, createCustomerPortal] = useCreateCustomerPortal({
     team_id: activeTeam,
   });
 
-  const ctaText = hasActiveTrial ? 'Cancel trial' : 'Manage subscription';
+  if (!subscription) {
+    return null;
+  }
+
+  if (subscription.cancelAt) {
+    return (
+      <Stack
+        css={{ height: '100%' }}
+        direction="vertical"
+        justify="space-between"
+      >
+        <Text size={3} css={{ color: '#F7CC66' }}>
+          Your access to Pro features will expire on on{' '}
+          {format(new Date(subscription.cancelAt), 'PP')}. After this period,
+          your team will be automatically migrated to the Free plan.
+        </Text>
+
+        <Link
+          onClick={() => {
+            track('Team Settings - Renew subscription', {
+              codesandbox: 'V1',
+              event_source: 'UI',
+            });
+
+            createCustomerPortal();
+          }}
+          size={3}
+          variant="active"
+        >
+          {loading ? 'Loading...' : 'Upgrade to Pro'}
+        </Link>
+      </Stack>
+    );
+  }
 
   return (
     <Stack direction="vertical" gap={2}>
       <Link
         onClick={() => {
-          if (hasActiveTrial) {
-            track('Team Settings - Cancel trial', {
-              codesandbox: 'V1',
-              event_source: 'UI',
-            });
-          } else {
-            track('Team Settings - Manage Subscription', {
-              codesandbox: 'V1',
-              event_source: 'UI',
-            });
-          }
+          track('Team Settings - Update payment details', {
+            codesandbox: 'V1',
+            event_source: 'UI',
+          });
+
           createCustomerPortal();
         }}
         size={3}
         variant="active"
       >
-        {loading ? 'Loading...' : ctaText}
+        {loading ? 'Loading...' : 'Update payment details'}
       </Link>
+      <Link
+        color="#EE8269"
+        onClick={() => {
+          track(
+            hasActiveTeamTrial
+              ? 'Team Settings: open cancel trial modal'
+              : 'Team Settings: open cancel subscription modal',
+            {
+              codesandbox: 'V1',
+              event_source: 'UI',
+            }
+          );
 
-      {!loading && team.subscription.cancelAt && (
-        <Text size={3} css={{ color: '#F7CC66' }}>
-          Your subscription expires on{' '}
-          {format(new Date(team.subscription.cancelAt), 'PP')}.{' '}
-        </Text>
-      )}
+          openCancelSubscriptionModal();
+        }}
+        size={3}
+      >
+        {hasActiveTeamTrial ? 'Cancel trial' : 'Cancel subscription'}
+      </Link>
     </Stack>
   );
 };
