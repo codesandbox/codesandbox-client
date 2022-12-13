4a5eed75f1bb10f1b13ec820feae51333199cbcc
Maria Clara <claarasantana@gmail.com>
2022-12-13 00:07:38 +0000

    move shared elements to a different folder

--- packages/app/src/app/pages/Dashboard/Content/routes/Repositories/EmptyState.tsx
+++ packages/app/src/app/pages/Dashboard/Content/routes/Repositories/EmptyState.tsx
@@ -1,25 +1,8 @@
-import {
-  ArticleCard,
-  CreateCard,
-  Element,
-  Stack,
-  Text,
-} from '@codesandbox/components';
+import { ArticleCard, CreateCard } from '@codesandbox/components';
 import track from '@codesandbox/common/lib/utils/analytics';
-import {
-  GRID_MAX_WIDTH,
-  GUTTER,
-} from 'app/pages/Dashboard/Components/VariableGrid';
 import React from 'react';
-import styled from 'styled-components';
 import { useActions } from 'app/overmind';
-
-const StyledEmptyDescription = styled(Text)`
-  margin: 0;
-  font-size: 16px;
-  line-height: 1.5;
-  color: #999999;
-`;
+import { EmptyPage } from '../../../Components/EmptyPage';
 
 const DESCRIPTION =
   'Save hours every week by shortening the review cycle and empowering everyone to contribute.<br />Every branch in Repositories is connected to git and has its own sandbox running in a fast microVM.';
@@ -28,32 +11,12 @@ export const EmptyState: React.FC = () => {
   const actions = useActions();
 
   return (
-    <Stack
-      css={{
-        width: `calc(100% - ${2 * GUTTER}px)`,
-        maxWidth: GRID_MAX_WIDTH - 2 * GUTTER,
-        margin: '24px auto 0',
-      }}
-      direction="vertical"
-      gap={10}
-    >
-      <StyledEmptyDescription
+    <EmptyPage.StyledWrapper>
+      <EmptyPage.StyledDescription
         as="p"
         dangerouslySetInnerHTML={{ __html: DESCRIPTION }}
       />
-      <Element
-        css={{
-          margin: 0,
-          padding: 0,
-          position: 'relative',
-          overflow: 'hidden',
-          display: 'grid',
-          listStyle: 'none',
-          gap: '16px',
-          gridTemplateColumns: 'repeat(auto-fill, minmax(268px, 1fr))',
-          gridAutoRows: 'minmax(156px, 1fr)',
-        }}
-      >
+      <EmptyPage.StyledGrid>
         <CreateCard
           icon="github"
           label="Import from GitHub"
@@ -71,7 +34,7 @@ export const EmptyState: React.FC = () => {
           url="https://codesandbox.io/docs/learn/repositories/overview"
           onClick={() => track('Repositories - open docs from empty state')}
         />
-      </Element>
-    </Stack>
+      </EmptyPage.StyledGrid>
+    </EmptyPage.StyledWrapper>
   );
 };
