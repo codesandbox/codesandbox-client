11f2d4048aa11999b2651e78de220755cc9535b0
Maria Clara <claarasantana@gmail.com>
2022-12-14 02:18:04 +0000

    fixup

---
+++ packages/app/src/app/pages/Dashboard/Content/routes/Drafts/EmptyState.tsx
@@ -0,0 +1,49 @@
+import { Stack } from '@codesandbox/components';
+import { useWorkspaceAuthorization } from 'app/hooks/useWorkspaceAuthorization';
+import React from 'react';
+import { TemplatesRow } from 'app/pages/Dashboard/Components/TemplatesRow';
+import { EmptyPage } from '../../../Components/EmptyPage';
+
+const TEMPLATE_IDS = [
+  'k8dsq1', // NodeJS (cloud)
+  'vanilla', // Vanilla JS (browser)
+  'uo1h0', // NextJS (cloud)
+  '9qputt', // React + Vite (browser)
+];
+
+const DESCRIPTIONS = {
+  TEAM:
+    'Sandboxes are a great way to prototype your ideas with zero startup costs and with everything you need: a code editor, previews, dev servers, unit tests, Storybook and many other devtools.<br /><br />Sandboxes in this folder are visible to all team members.',
+  PERSONAL:
+    'Sandboxes are a great way to prototype your ideas with zero startup costs and with everything you need: a code editor, previews, dev servers, unit tests, Storybook and many other devtools.',
+};
+
+export const EmptyState: React.FC = () => {
+  const { isPersonalSpace } = useWorkspaceAuthorization();
+
+  return (
+    <EmptyPage.StyledWrapper>
+      <Stack
+        css={{
+          width: '100%',
+
+          '@media screen and (min-width: 768px)': {
+            width: isPersonalSpace ? '100%' : '560px',
+          },
+        }}
+      >
+        <EmptyPage.StyledDescription
+          as="p"
+          dangerouslySetInnerHTML={{
+            __html: isPersonalSpace ? DESCRIPTIONS.PERSONAL : DESCRIPTIONS.TEAM,
+          }}
+        />
+      </Stack>
+      <TemplatesRow
+        page="drafts"
+        title="Start from a template"
+        templateIds={TEMPLATE_IDS}
+      />
+    </EmptyPage.StyledWrapper>
+  );
+};
