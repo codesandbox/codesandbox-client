08069e52c3a6f40151f15d8013e592b2ddf4a796
Maria Clara <claarasantana@gmail.com>
2022-12-16 09:55:21 -0300

    feat(dashboard/recent): update recent page (#7240)

--- packages/app/src/app/pages/Dashboard/Content/routes/Recent/index.tsx
+++ packages/app/src/app/pages/Dashboard/Content/routes/Recent/index.tsx
@@ -3,17 +3,13 @@ import { useAppState, useActions } from 'app/overmind';
 import { sandboxesTypes } from 'app/overmind/namespaces/dashboard/types';
 import { Header } from 'app/pages/Dashboard/Components/Header';
 import { VariableGrid } from 'app/pages/Dashboard/Components/VariableGrid';
-import {
-  GRID_MAX_WIDTH,
-  GUTTER,
-} from 'app/pages/Dashboard/Components/VariableGrid/constants';
 import { SelectionProvider } from 'app/pages/Dashboard/Components/Selection';
 import { Helmet } from 'react-helmet';
 import { DashboardGridItem, PageTypes } from 'app/pages/Dashboard/types';
-import { Element } from '@codesandbox/components';
 import { UpgradeBanner } from 'app/pages/Dashboard/Components/UpgradeBanner';
 import { useWorkspaceAuthorization } from 'app/hooks/useWorkspaceAuthorization';
 import { useWorkspaceSubscription } from 'app/hooks/useWorkspaceSubscription';
+import { EmptyRecent } from './EmptyRecent';
 
 export const Recent = () => {
   const {
@@ -80,24 +76,19 @@ export const Recent = () => {
       <Helmet>
         <title>Recent - CodeSandbox</title>
       </Helmet>
-      {isFree && isTeamSpace && (
-        <Element
-          css={{
-            width: `calc(100% - ${2 * GUTTER}px)`,
-            maxWidth: GRID_MAX_WIDTH - 2 * GUTTER,
-            margin: '0 auto 48px',
-          }}
-        >
-          <UpgradeBanner teamId={activeTeamId} />
-        </Element>
-      )}
+      {isFree && isTeamSpace && <UpgradeBanner teamId={activeTeamId} />}
       <Header
         title={isEmpty ? "Let's start building" : 'Recent'}
         activeTeam={activeTeamId}
         loading={dataIsLoading}
         showViewOptions
       />
-      <VariableGrid page={pageType} items={items} />
+
+      {items.length > 0 ? (
+        <VariableGrid page={pageType} items={items} />
+      ) : (
+        <EmptyRecent />
+      )}
     </SelectionProvider>
   );
 };
