9ee8404ba7d5063ac85909d3a751abdb44c91219
Ives van Hoorne <ives.v.h@gmail.com>
2022-12-19 16:23:34 +0000

    Update TemplateCard.tsx and getTemplateIcon.ts

--- packages/common/src/utils/getTemplateIcon.ts
+++ packages/common/src/utils/getTemplateIcon.ts
@@ -2,10 +2,13 @@ import React from 'react';
 import { ColorIcons as Icons } from 'template-icons';
 import getColorIcons from '../templates/icons';
 
-export const getTemplateIcon = (iconUrl, environment) => {
-  const UserIcon: React.FunctionComponent =
+export const getTemplateIcon = (iconUrl: string | undefined, environment) => {
+  const UserIcon: React.FunctionComponent<{ width?: number; height?: number }> =
     iconUrl && Icons[iconUrl] ? Icons[iconUrl] : getColorIcons(environment);
-  const OfficialIcon: React.FunctionComponent = getColorIcons(environment);
+  const OfficialIcon: React.FunctionComponent<{
+    width?: number;
+    height?: number;
+  }> = getColorIcons(environment);
 
   return {
     UserIcon,
