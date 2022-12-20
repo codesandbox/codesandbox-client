cbdbb05d07b917fca49f399b74f5da6dcf830d62
Maria Clara <claarasantana@gmail.com>
2022-12-20 20:41:44 +0000

    update DocumentationRow

--- packages/app/src/app/pages/Dashboard/Content/routes/Recent/DocumentationRow.tsx
+++ packages/app/src/app/pages/Dashboard/Content/routes/Recent/DocumentationRow.tsx
@@ -17,33 +17,57 @@ export const appendOnboardingTracking = (url: string): string => {
 
 const DOCS: DocsItem[] = [
   {
+    label: 'video_one-click-pr',
+    title: 'One-click PR',
+    duration: '0:23',
+    durationLabel: '23 seconds',
+    url:
+      'https://www.youtube.com/watch?v=YpY8oznHq2I&list=PLdX6VQdTP7GbG1Poi8JN3AJsHAsSM2IlW&index=6',
+    thumbnail: '/static/img/thumbnails/video_1-click-pr.png',
+  },
+  {
     label: 'video_contribution-branches',
-    title: 'Contribute easily with Contribution branches',
+    title: 'Easy open source with Contribution branches',
     duration: '5:33',
     durationLabel: '5 minutes, 33 seconds',
     url: 'https://www.youtube.com/watch?v=HCs49B6VVl8',
-    thumbnail: '/static/img/thumbnails/docs_contribution-branches.png',
+    thumbnail: '/static/img/thumbnails/video_contribution-branches.png',
+  },
+
+  {
+    label: 'video_command-palette',
+    title: 'Using the Command Palette',
+    duration: '0:48',
+    durationLabel: '48 seconds',
+    url:
+      'https://www.youtube.com/watch?v=OUBYFp0Yz2A&list=PLdX6VQdTP7GbG1Poi8JN3AJsHAsSM2IlW&index=7',
+    thumbnail: '/static/img/thumbnails/video_command-palette.png',
+  },
+  {
+    label: 'video_auto-deps-install',
+    title: 'Automatic Dependency Management',
+    duration: '1:18',
+    durationLabel: '1 minute, 18 seconds',
+    url: 'https://www.youtube.com/watch?v=ZJ1sNiTZw5M',
+    thumbnail: '/static/img/thumbnails/video_auto-deps.png',
   },
   {
     label: 'blog_github-app',
     title: 'Review code faster with our GitHub App',
     url: 'https://codesandbox.io/post/introducing-the-codesandbox-github-app',
-    thumbnail: '/static/img/thumbnails/docs_github-app.png',
+    thumbnail: '/static/img/thumbnails/blog_github-app.png',
   },
   {
-    label: 'video_vs-code',
-    title: 'Working locally with the VSCode Extension',
-    duration: '3:02',
-    durationLabel: '3 minutes, 2 seconds',
-    url: 'https://www.youtube.com/watch?v=ZJ1sNiTZw5M',
-    thumbnail: '/static/img/thumbnails/docs_vscode-extension.png',
+    label: 'cta_youtube',
+    title: 'More videos and tutorials',
+    url: 'https://www.youtube.com/@codesandbox',
+    thumbnail: '/static/img/thumbnails/youtube.png',
   },
   {
-    label: 'blog-iOS',
-    title: 'Code with our native iOS App',
-    url:
-      'https://codesandbox.io/post/how-to-code-your-app-using-the-codesandbox-ipad-ide',
-    thumbnail: '/static/img/thumbnails/docs_ipad.png',
+    label: 'cta_blog',
+    title: 'Latest news and releases',
+    url: 'https://codesandbox.io/blog',
+    thumbnail: '/static/img/thumbnails/blog.png',
   },
 ];
 
