d8b8e05de67c0a8cfd4eca175f0cb2c2bfc296c0
Maria Clara <claarasantana@gmail.com>
2022-12-21 11:05:13 -0300

    feat(recent): update "optimize your workflow" section (#7258)
    * assets
    
    * update DocumentationRow
    
    * hide contribution branches item if team space
    
    * update destructure

--- packages/app/src/app/pages/Dashboard/Content/routes/Recent/DocumentationRow.tsx
+++ packages/app/src/app/pages/Dashboard/Content/routes/Recent/DocumentationRow.tsx
@@ -1,5 +1,6 @@
 import track from '@codesandbox/common/lib/utils/analytics';
 import { ArticleCard, VideoCard, Stack } from '@codesandbox/components';
+import { useWorkspaceAuthorization } from 'app/hooks/useWorkspaceAuthorization';
 import { EmptyPage } from 'app/pages/Dashboard/Components/EmptyPage';
 import { appendOnboardingTracking } from 'app/pages/Dashboard/Content/utils';
 import React from 'react';
@@ -7,41 +8,71 @@ import React from 'react';
 type ArticleProps = React.ComponentProps<typeof ArticleCard>;
 type VideoProps = React.ComponentProps<typeof VideoCard>;
 
-type DocsItem = { label: string } & (VideoProps | ArticleProps);
+type DocsItem = { label: string; workspaceType?: 'personal' | 'team' } & (
+  | VideoProps
+  | ArticleProps
+);
 
 const DOCS: DocsItem[] = [
   {
+    label: 'video_one-click-pr',
+    title: 'One-click PR',
+    duration: '0:23',
+    durationLabel: '23 seconds',
+    url:
+      'https://www.youtube.com/watch?v=YpY8oznHq2I&list=PLdX6VQdTP7GbG1Poi8JN3AJsHAsSM2IlW&index=6',
+    thumbnail: '/static/img/thumbnails/video_1-click-pr.png',
+  },
+  {
     label: 'video_contribution-branches',
-    title: 'Contribute easily with Contribution branches',
+    title: 'Easy open source with Contribution branches',
     duration: '5:33',
     durationLabel: '5 minutes, 33 seconds',
     url: 'https://www.youtube.com/watch?v=HCs49B6VVl8',
-    thumbnail: '/static/img/thumbnails/docs_contribution-branches.png',
+    thumbnail: '/static/img/thumbnails/video_contribution-branches.png',
+    workspaceType: 'personal',
+  },
+
+  {
+    label: 'video_command-palette',
+    title: 'Using the Command Palette',
+    duration: '0:48',
+    durationLabel: '48 seconds',
+    url:
+      'https://www.youtube.com/watch?v=OUBYFp0Yz2A&list=PLdX6VQdTP7GbG1Poi8JN3AJsHAsSM2IlW&index=7',
+    thumbnail: '/static/img/thumbnails/video_command-palette.png',
+  },
+  {
+    label: 'video_auto-deps-install',
+    title: 'Automatic Dependency Management',
+    duration: '1:18',
+    durationLabel: '1 minute, 18 seconds',
+    url: 'https://www.youtube.com/watch?v=ZJ1sNiTZw5M',
+    thumbnail: '/static/img/thumbnails/video_auto-deps.png',
   },
   {
     label: 'blog_github-app',
     title: 'Review code faster with our GitHub App',
     url: 'https://codesandbox.io/post/introducing-the-codesandbox-github-app',
-    thumbnail: '/static/img/thumbnails/docs_github-app.png',
+    thumbnail: '/static/img/thumbnails/blog_github-app.png',
   },
   {
-    label: 'video_vs-code',
-    title: 'Working locally with the VSCode Extension',
-    duration: '3:02',
-    durationLabel: '3 minutes, 2 seconds',
-    url: 'https://www.youtube.com/watch?v=ZJ1sNiTZw5M',
-    thumbnail: '/static/img/thumbnails/docs_vscode-extension.png',
+    label: 'cta_youtube',
+    title: 'More videos and tutorials',
+    url: 'https://www.youtube.com/@codesandbox',
+    thumbnail: '/static/img/thumbnails/youtube.png',
   },
   {
-    label: 'blog-iOS',
-    title: 'Code with our native iOS App',
-    url:
-      'https://codesandbox.io/post/how-to-code-your-app-using-the-codesandbox-ipad-ide',
-    thumbnail: '/static/img/thumbnails/docs_ipad.png',
+    label: 'cta_blog',
+    title: 'Latest news and releases',
+    url: 'https://codesandbox.io/blog',
+    thumbnail: '/static/img/thumbnails/blog.png',
   },
 ];
 
 export const DocumentationRow: React.FC = () => {
+  const { isPersonalSpace, isTeamSpace } = useWorkspaceAuthorization();
+
   const handleTrack = (label: string) => {
     track('Empty State Card - Content card', {
       codesandbox: 'V1',
@@ -56,8 +87,15 @@ export const DocumentationRow: React.FC = () => {
         Optimize your workflow
       </EmptyPage.StyledGridTitle>
       <EmptyPage.StyledGrid as="ul">
-        {DOCS.map(({ url, ...item }) => {
+        {DOCS.map(({ url, workspaceType, ...item }) => {
           const urlWithTracking = appendOnboardingTracking(url);
+
+          if (
+            (workspaceType === 'personal' && !isPersonalSpace) ||
+            (workspaceType === 'team' && !isTeamSpace)
+          ) {
+            return null;
+          }
 
           return (
             <Stack as="li" key={item.label}>
