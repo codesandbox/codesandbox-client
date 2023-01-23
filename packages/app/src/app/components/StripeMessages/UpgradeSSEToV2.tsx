6e15b591cb10d00151dc6dcf03914d0eacae89a5
David Hollins <33638789+DavidHollins6@users.noreply.github.com>
2023-01-23 09:44:02 +0000

    feat(cloud sandboxes): Banner for upgrading to V2 (#7326)
    * feat(cloud sandboxes): Add message stripe for converting a sandbox to v2
    
    * feat(cloud sandbox): Adding a forking patch for when a user does not have permission.
    
    * feat(cloud sandboxes): Add loading animation and fix wording.
    
    * fix(cloud sandboxes): Ensure upgrade stripe does not appear if you aren't logged in

---
+++ packages/app/src/app/components/StripeMessages/UpgradeSSEToV2.tsx
@@ -0,0 +1,63 @@
+import React, { useState } from 'react';
+import { useEffects, useAppState } from 'app/overmind';
+import { MessageStripe } from '@codesandbox/components';
+import { sandboxUrl } from '@codesandbox/common/lib/utils/url-generator';
+import { hasPermission } from '@codesandbox/common/lib/utils/permission';
+
+export const UpgradeSSEToV2Stripe = () => {
+  const state = useAppState();
+  const effects = useEffects();
+  const [isLoading, setIsLoading] = useState(false);
+  const updateSandbox = useEffects().api.updateSandbox;
+  const canConvert = hasPermission(
+    state.editor.currentSandbox?.authorization,
+    'write_code'
+  );
+
+  return (
+    <MessageStripe variant="primary">
+      <span>
+        This sandbox runs much faster in our new editor. Do you want to{' '}
+        {canConvert ? 'convert' : 'fork'} it to a Cloud Sandbox?
+      </span>
+      <MessageStripe.Action
+        loading={isLoading}
+        onClick={async () => {
+          setIsLoading(true);
+          const sandboxId = state.editor.currentSandbox.id;
+
+          if (canConvert) {
+            const alias = state.editor.currentSandbox.alias;
+
+            await updateSandbox(sandboxId, {
+              v2: true,
+            });
+
+            const sandboxV2Url = sandboxUrl({
+              id: sandboxId,
+              alias,
+              isV2: true,
+            });
+
+            window.location.href = sandboxV2Url;
+          } else {
+            const forkedSandbox = await effects.api.forkSandbox(sandboxId, {
+              isV2: true,
+              teamId: state.activeTeam,
+            });
+
+            const sandboxV2Url = sandboxUrl({
+              id: forkedSandbox.id,
+              alias: forkedSandbox.alias,
+              isV2: true,
+            });
+
+            window.location.href = sandboxV2Url;
+          }
+        }}
+      >
+        Yes, Convert
+      </MessageStripe.Action>
+    </MessageStripe>
+  );
+};
