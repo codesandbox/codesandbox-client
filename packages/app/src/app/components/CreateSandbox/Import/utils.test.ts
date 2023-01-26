35ce5bd6feff345b0c25b8110a0787c25983df61
Maria Clara <claarasantana@gmail.com>
2023-01-26 02:44:48 +0000

    feat: improve GH import input

---
+++ packages/app/src/app/components/CreateSandbox/Import/utils.test.ts
@@ -0,0 +1,57 @@
+import { getOwnerAndRepoFromInput, checkRepoInput } from './utils';
+
+const VALID_REPOS = [
+  'github.com/owner/repo',
+  'http://github.com/owner/repo',
+  'http://www.github.com/owner/repo',
+  'https://github.com/owner/repo',
+  'https://www.github.com/owner/repo',
+  'www.github.com/owner/repo',
+  'https://github.com/owner/repo.git',
+  'git@github.com:owner/repo.git',
+  'owner/repo',
+];
+
+const INVALID_REPOS = [
+  'github.com/',
+  'github.com/user/',
+  'http://github.com/',
+  'http://github.com/user/',
+  'http://www.github.com/',
+  'http://www.github.com/user/',
+  'https://github.com/user/',
+  'https://www.github.com/user/',
+  'www.github.com/',
+  'www.github.com/user/',
+];
+
+describe('checkRepoInput', () => {
+  VALID_REPOS.forEach(possibleRepo => {
+    test(`${possibleRepo} is a valid input`, () => {
+      expect(checkRepoInput(possibleRepo)).toBe(true);
+    });
+  });
+
+  INVALID_REPOS.forEach(possibleRepo => {
+    test(`${possibleRepo} is an invalid input`, () => {
+      expect(checkRepoInput(possibleRepo)).toBe(false);
+    });
+  });
+});
+
+describe('getOwnerAndRepoFromInput', () => {
+  VALID_REPOS.forEach(possibleRepo => {
+    test(`${possibleRepo} returns an owner and repo`, () => {
+      expect(getOwnerAndRepoFromInput(possibleRepo)).toEqual({
+        owner: 'owner',
+        repo: 'repo',
+      });
+    });
+  });
+
+  INVALID_REPOS.forEach(possibleRepo => {
+    test(`${possibleRepo} returns null`, () => {
+      expect(getOwnerAndRepoFromInput(possibleRepo)).toBeNull();
+    });
+  });
+});
--- packages/app/src/app/components/CreateSandbox/Import/utils.ts
+++ packages/app/src/app/components/CreateSandbox/Import/utils.ts
@@ -1,23 +1,34 @@
 import { findBestMatch } from 'string-similarity';
 import { State } from './useGithubOrganizations';
 
-export const getOwnerAndNameFromInput = (githubUrl: string) => {
-  // Will match the following:
-  // git@github.com:{owner}/{name}
-  // git@github.com:{owner}/{name}.git
-  // https://gitbub.com/{owner}/{name}
-  const match = githubUrl.match(
-    // eslint-disable-next-line no-useless-escape
-    /^(https|git)(:\/\/|@)([^\/:]+)[\/:]([^\/:]+)\/(.+)$/
+// Will match: git@github.com:owner/repository.git
+const REGEX_SSH = /git@github\.com:(?<owner>[\w-]+)\/(?<repo>[\w-]+)\.git$/i;
+// Will match: http(s)://github.com/owner/repo(.git)
+const REGEX_HTTPS = /http(s?):\/\/github\.com\/(?<owner>[\w-]+)\/(?<repo>[\w-]+)(\.git)?$/i;
+// Will match: www.github.com/owner/repo and github.com/owner/repo
+const REGEX_PROTOCOLLESS = /(www\.)?github\.com\/(?<owner>[\w-]+)\/(?<repo>[\w-]+)$/i;
+// Will match: owner/repo
+const REGEX_PLAIN = /(?<owner>[\w-]+)\/(?<repo>[\w-]+)$/i;
+
+export const checkRepoInput = (input: string) =>
+  [REGEX_SSH, REGEX_HTTPS, REGEX_PROTOCOLLESS, REGEX_PLAIN].some(exp =>
+    exp.test(input)
   );
 
-  const owner = match?.[4];
-  const name = match?.[5];
+export const getOwnerAndRepoFromInput = (input: string) => {
+  const match =
+    REGEX_SSH.exec(input) ||
+    REGEX_HTTPS.exec(input) ||
+    REGEX_PROTOCOLLESS.exec(input) ||
+    REGEX_PLAIN.exec(input);
 
-  // make sure we only take the repo name until the first slash
-  const nameParts = name?.split('/') || [];
+  if (!match) {
+    return null;
+  }
 
-  return { owner, name: nameParts[0] };
+  const { owner, repo } = match.groups as { owner: string; repo: string };
+
+  return { owner, repo };
 };
 
 export const getGihubOrgMatchingCsbTeam = (
