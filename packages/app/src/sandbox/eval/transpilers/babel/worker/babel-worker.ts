cb3b6754a2e10475d602ca06da31eb3375dc2bbd
Jasper De Moor <jasperdemoor@gmail.com>
2023-01-30 11:18:07 +0100

    remove unused static js files

--- packages/app/src/sandbox/eval/transpilers/babel/worker/babel-worker.ts
+++ packages/app/src/sandbox/eval/transpilers/babel/worker/babel-worker.ts
@@ -632,9 +632,7 @@ async function initBabel(opts) {
     loadCustomTranspiler(babelUrl, babelEnvUrl);
   } else if (version !== 7) {
     loadCustomTranspiler(
-      process.env.NODE_ENV === 'development'
-        ? `${process.env.CODESANDBOX_HOST || ''}/static/js/babel.6.26.js`
-        : `${process.env.CODESANDBOX_HOST || ''}/static/js/babel.6.26.min.js`
+      `${process.env.CODESANDBOX_HOST || ''}/static/js/babel.6.26.min.js`
     );
   }
 
