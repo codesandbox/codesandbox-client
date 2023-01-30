cad3886d1a2072135c5143de78b6593b51323bdd
David Hollins <davidhollins06@gmail.com>
2023-01-30 11:21:38 +0000

    feat(upgrade): Add welcome query to github upgrade path

--- packages/common/src/utils/url-generator.ts
+++ packages/common/src/utils/url-generator.ts
@@ -14,15 +14,19 @@ const sandboxHost = {
 export const CSBProjectGitHubRepository = ({
   owner,
   repo,
+  welcome,
 }: {
   owner: string;
-  repo;
+  repo: string;
+  welcome: boolean;
 }) => {
   const origin = process.env.STAGING_API
     ? 'https://codesandbox.stream'
     : 'https://codesandbox.io';
 
-  return `${origin}/p/github/${owner}/${repo}?create=true`;
+  return `${origin}/p/github/${owner}/${repo}?create=true${
+    welcome ? '&welcome=true' : ''
+  }`;
 };
 
 const buildEncodedUri = (
