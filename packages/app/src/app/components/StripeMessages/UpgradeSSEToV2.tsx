590bc82155c87511a1bf22e683f10073a429a85e
Zeh Fernandes <ozehfernandes@gmail.com>
2023-02-06 16:31:40 +0000

    fix convert and fork issue

--- packages/app/src/app/components/StripeMessages/UpgradeSSEToV2.tsx
+++ packages/app/src/app/components/StripeMessages/UpgradeSSEToV2.tsx
@@ -12,7 +12,7 @@ export const UpgradeSSEToV2Stripe = () => {
         {canConvert ? 'convert' : 'fork'} it to a Cloud Sandbox?
       </span>
       <MessageStripe.Action loading={loading} onClick={perform}>
-        Yes, convert
+        {canConvert ? 'Yes, convert' : 'Yes, fork'}
       </MessageStripe.Action>
     </MessageStripe>
   );
