03220363b17936e194e69e6d33e17b52555d55d5
Maria Clara <claarasantana@gmail.com>
2023-02-08 03:37:14 +0000

    create live session confirm

---
+++ packages/app/src/app/pages/common/Modals/LiveSessionConfirm/index.tsx
@@ -0,0 +1,73 @@
+import React from 'react';
+import { Button, Stack, Text } from '@codesandbox/components';
+import { sandboxUrl } from '@codesandbox/common/lib/utils/url-generator';
+
+import { useWorkspaceAuthorization } from 'app/hooks/useWorkspaceAuthorization';
+import { useCreateCheckout } from 'app/hooks';
+import { useWorkspaceSubscription } from 'app/hooks/useWorkspaceSubscription';
+import { useActions, useAppState } from 'app/overmind';
+import { Alert } from '../Common/Alert';
+
+export const LiveSessionConfirm: React.FC = () => {
+  const {
+    activeTeam,
+    editor: {
+      currentSandbox: { id },
+    },
+  } = useAppState();
+  const { modalClosed, live } = useActions();
+  const [checkout, createCheckout] = useCreateCheckout();
+  const { isAdmin } = useWorkspaceAuthorization();
+  const { isEligibleForTrial } = useWorkspaceSubscription();
+
+  return (
+    <Alert title="After February 20, Live Sessions will become a Pro feature.">
+      {checkout.status === 'error' && (
+        <Text marginBottom={8} variant="danger" size={12}>
+          An error ocurred while trying to load the checkout. Please try again.
+        </Text>
+      )}
+      <Stack gap={2} align="center" justify="flex-end">
+        <Button
+          onClick={() => {
+            live.createLiveClicked(id);
+            modalClosed();
+          }}
+          variant="link"
+          autoWidth
+        >
+          Go Live now
+        </Button>
+        {isAdmin ? (
+          <Button
+            loading={checkout.status === 'loading'}
+            variant="primary"
+            onClick={() =>
+              createCheckout({
+                team_id: activeTeam,
+                recurring_interval: 'month',
+                success_path: sandboxUrl({ id }),
+                cancel_path: sandboxUrl({ id }),
+              })
+            }
+            autoWidth
+          >
+            <Text>
+              {isEligibleForTrial ? 'Start free trial' : 'Upgrade to Pro'}
+            </Text>
+          </Button>
+        ) : (
+          <Button
+            as="a"
+            href="/pricing"
+            target="_blank"
+            variant="secondary"
+            autoWidth
+          >
+            Learn more
+          </Button>
+        )}
+      </Stack>
+    </Alert>
+  );
+};
