9a979e44d85f1df5ce26a53cf0bff25c316a230c
Maria Clara <claarasantana@gmail.com>
2023-02-08 18:30:52 +0000

    create re-usable elements

--- packages/app/src/app/pages/Dashboard/Components/shared/ReadOnlyRepoDisclaimer.tsx
+++ packages/app/src/app/pages/Dashboard/Components/shared/ReadOnlyRepoDisclaimer.tsx
@@ -1,7 +1,12 @@
-import { Button, Element, Icon, Text } from '@codesandbox/components';
+import { Icon } from '@codesandbox/components';
 import { useGitHuPermissions } from 'app/hooks/useGitHubPermissions';
 import { useActions } from 'app/overmind';
 import React from 'react';
+import {
+  StyledDisclaimerButton,
+  StyledDisclaimerText,
+  StyledDisclaimerWrapper,
+} from './elements';
 
 export const ReadOnlyRepoDisclaimer: React.FC<{ insideGrid?: boolean }> = ({
   insideGrid = false,
@@ -14,47 +19,12 @@ export const ReadOnlyRepoDisclaimer: React.FC<{ insideGrid?: boolean }> = ({
   }
 
   return (
-    <Element
-      css={{
-        display: 'flex',
-        flexDirection: insideGrid ? 'column' : 'row',
-        alignItems: insideGrid ? 'flex-start' : 'center',
-        gap: 0,
-      }}
-    >
-      <Text
-        css={{
-          fontWeight: 400,
-          fontSize: '12px',
-          lineHeight: '16px',
-          letterSpacing: '0.005em',
-          color: '#999999',
-        }}
-      >
+    <StyledDisclaimerWrapper insideGrid={insideGrid}>
+      <StyledDisclaimerText>
         Review your GitHub permissions to modify this repository.
-      </Text>
-      <Button
-        css={{
-          display: 'flex',
-          gap: '8px',
-          cursor: 'pointer',
-          fontWeight: 500,
-          fontSize: '12px',
-          lineHeight: '20px',
-          letterSpacing: '-0.02em',
-          color: '#EBEBEB',
-          transition: 'color ease-in .3s',
-          padding: `4px ${insideGrid ? 0 : '8px'}`,
-
-          '&:hover:not(:disabled), &:focus:not(:disabled)': {
-            color: '#E0E0E0',
-          },
-
-          '&:focus-visible': {
-            outline: '#ac9cff solid 2px',
-            outlineOffset: '-2px',
-          },
-        }}
+      </StyledDisclaimerText>
+      <StyledDisclaimerButton
+        insideGrid={insideGrid}
         onClick={() =>
           actions.modalOpened({
             modal: 'preferences',
@@ -66,7 +36,7 @@ export const ReadOnlyRepoDisclaimer: React.FC<{ insideGrid?: boolean }> = ({
       >
         Review your GitHub permissions
         <Icon name="external" size={12} />
-      </Button>
-    </Element>
+      </StyledDisclaimerButton>
+    </StyledDisclaimerWrapper>
   );
 };
--- packages/app/src/app/pages/Dashboard/Components/shared/RestrictedImportDisclaimer.tsx
+++ packages/app/src/app/pages/Dashboard/Components/shared/RestrictedImportDisclaimer.tsx
@@ -1,8 +1,13 @@
-import { Button, Element, Icon, Text } from '@codesandbox/components';
+import { Icon } from '@codesandbox/components';
 import { useDismissible } from 'app/hooks';
 import { useGitHuPermissions } from 'app/hooks/useGitHubPermissions';
 import { useActions } from 'app/overmind';
 import React from 'react';
+import {
+  StyledDisclaimerButton,
+  StyledDisclaimerText,
+  StyledDisclaimerWrapper,
+} from './elements';
 
 export const RestrictedImportDisclaimer: React.FC<{ insideGrid?: boolean }> = ({
   insideGrid = false,
@@ -19,47 +24,12 @@ export const RestrictedImportDisclaimer: React.FC<{ insideGrid?: boolean }> = ({
 
   if (dismissedBannerButRestrictsPublicRepos || onlyAllowsPublicRepos) {
     return (
-      <Element
-        css={{
-          display: 'flex',
-          flexDirection: insideGrid ? 'column' : 'row',
-          alignItems: insideGrid ? 'flex-start' : 'center',
-          gap: 0,
-        }}
-      >
-        <Text
-          css={{
-            fontWeight: 400,
-            fontSize: '12px',
-            lineHeight: '16px',
-            letterSpacing: '0.005em',
-            color: '#999999',
-          }}
-        >
+      <StyledDisclaimerWrapper insideGrid={insideGrid}>
+        <StyledDisclaimerText>
           Don&apos;t see all your repositories?
-        </Text>
-        <Button
-          css={{
-            display: 'flex',
-            gap: '8px',
-            cursor: 'pointer',
-            fontWeight: 500,
-            fontSize: '12px',
-            lineHeight: '20px',
-            letterSpacing: '-0.02em',
-            color: '#EBEBEB',
-            transition: 'color ease-in .3s',
-            padding: `4px ${insideGrid ? 0 : '8px'}`,
-
-            '&:hover:not(:disabled), &:focus:not(:disabled)': {
-              color: '#E0E0E0',
-            },
-
-            '&:focus-visible': {
-              outline: '#ac9cff solid 2px',
-              outlineOffset: '-2px',
-            },
-          }}
+        </StyledDisclaimerText>
+        <StyledDisclaimerButton
+          insideGrid={insideGrid}
           onClick={() =>
             actions.modalOpened({
               modal: 'preferences',
@@ -71,8 +41,8 @@ export const RestrictedImportDisclaimer: React.FC<{ insideGrid?: boolean }> = ({
         >
           Review your GitHub permissions
           <Icon name="external" size={12} />
-        </Button>
-      </Element>
+        </StyledDisclaimerButton>
+      </StyledDisclaimerWrapper>
     );
   }
 
