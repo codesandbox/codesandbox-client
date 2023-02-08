e039beffac7bd7040c4887cb5b089474e857e93d
Maria Clara <claarasantana@gmail.com>
2023-02-08 16:35:14 +0000

    update auth utils

--- packages/app/src/app/overmind/utils/auth.ts
+++ packages/app/src/app/overmind/utils/auth.ts
@@ -1,11 +1,22 @@
+// Minimal scope asked when signing in
 export const GH_BASE_SCOPE = 'user:email';
-const SCOPE_OPTIONS = ['private_repos', 'public_repos'] as const;
 
-export type GHScopeOption = typeof SCOPE_OPTIONS[number];
+// Working scope that needs to be granted so users
+// can commit and list their organizations when
+// forking.
+const GH_WORKING_SCOPES = ['workflow', 'read:org'].toString();
+
+// Possible repo scopes, users that want to work
+// in public repos only (eg: open source maintainers)
+// don't need to grant access to their private repos
+// if they don't want to.
+const GH_REPO_SCOPE_OPTIONS = ['private_repos', 'public_repos'] as const;
+
+export type GHScopeOption = typeof GH_REPO_SCOPE_OPTIONS[number];
 
 export const MAP_GH_SCOPE_OPTIONS: Record<GHScopeOption, string> = {
-  public_repos: 'public_repo,workflow',
-  private_repos: 'repo,workflow',
+  public_repos: GH_WORKING_SCOPES + ',public_repo',
+  private_repos: GH_WORKING_SCOPES + ',repo',
 };
 
 export type AuthOptions =
