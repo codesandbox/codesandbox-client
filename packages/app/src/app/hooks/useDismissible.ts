5255561144159f847ec5a1cb07859b199f42ddad
Maria Clara <claarasantana@gmail.com>
2023-02-16 21:51:16 +0000

    update useDismissible

--- packages/app/src/app/hooks/useDismissible.ts
+++ packages/app/src/app/hooks/useDismissible.ts
@@ -4,16 +4,21 @@ import { useEffects } from 'app/overmind';
 /**
  * Localstorage keys used for dismissible modals and banners.
  */
-type DismissibleKeys =
+type DismissibleKeys<T extends string> =
+  | T
   | 'DASHBOARD_RECENT_UPGRADE'
   | 'DASHBOARD_REPOSITORIES_PERMISSIONS_BANNER';
 
-type Dismissibles = Partial<Record<DismissibleKeys, true>>;
+type Dismissibles<T extends string> = Partial<
+  Record<DismissibleKeys<T>, true | string>
+>;
 
-export const useDismissible = (key: DismissibleKeys): [boolean, () => void] => {
+export const useDismissible = <T extends string>(
+  key: DismissibleKeys<T>
+): [boolean, () => void] => {
   const { browser } = useEffects();
   const [isDismissed, setIsDismissed] = useState(() => {
-    const dismissibles = browser.storage.get<Dismissibles>('DISMISSIBLES');
+    const dismissibles = browser.storage.get<Dismissibles<T>>('DISMISSIBLES');
     return dismissibles?.[key] === true;
   });
 
@@ -21,7 +26,7 @@ export const useDismissible = (key: DismissibleKeys): [boolean, () => void] => {
     // Getting the dismissibles again to make sure other instances aren't
     // overwritten after the hook initialised.
     const prevDismissibles =
-      browser.storage.get<Dismissibles>('DISMISSIBLES') || {};
+      browser.storage.get<Dismissibles<T>>('DISMISSIBLES') || {};
 
     browser.storage.set('DISMISSIBLES', {
       ...prevDismissibles,
