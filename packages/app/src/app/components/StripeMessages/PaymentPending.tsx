d29c9d3d502a0fa7f164506ca40878954fa1e994
Maria Clara <claarasantana@gmail.com>
2023-02-16 21:27:47 +0000

    update PaymentPending copy

--- packages/app/src/app/components/StripeMessages/PaymentPending.tsx
+++ packages/app/src/app/components/StripeMessages/PaymentPending.tsx
@@ -5,34 +5,44 @@ import { useCreateCustomerPortal } from 'app/hooks/useCreateCustomerPortal';
 import { useAppState } from 'app/overmind';
 import { useLocation } from 'react-router-dom';
 import { useWorkspaceAuthorization } from 'app/hooks/useWorkspaceAuthorization';
+import { useWorkspaceSubscription } from 'app/hooks/useWorkspaceSubscription';
 
 export const PaymentPending: React.FC = () => {
   const { activeTeam } = useAppState();
   const { pathname } = useLocation();
   const { isTeamAdmin } = useWorkspaceAuthorization();
+  const { hasExpiredTeamTrial } = useWorkspaceSubscription();
   const [
     loadingCustomerPortal,
     createCustomerPortal,
   ] = useCreateCustomerPortal({ team_id: activeTeam, return_path: pathname });
 
-  if (isTeamAdmin) {
-    return (
-      <MessageStripe variant="warning">
-        There are some issues with your payment. Please update your payment
-        details.
+  const buildCopy = () => {
+    const description = {
+      trial: 'Your free trial has expired',
+      default: 'There are some issues with your payment',
+    };
+    const action = {
+      admin: 'Please update your payment details',
+      default: 'Please contact your team admin to update the payment details',
+    };
+
+    return `${hasExpiredTeamTrial ? description.trial : description.default}. ${
+      isTeamAdmin ? action.admin : action.default
+    }.`;
+  };
+
+  return (
+    <MessageStripe variant="warning">
+      {buildCopy()}
+      {isTeamAdmin ? (
         <MessageStripe.Action
           loading={loadingCustomerPortal}
           onClick={createCustomerPortal}
         >
           Update payment
         </MessageStripe.Action>
-      </MessageStripe>
-    );
-  }
-  return (
-    <MessageStripe variant="warning">
-      There are some issues with your payment. Please contact your team admin to
-      update your payment details.
+      ) : null}{' '}
     </MessageStripe>
   );
 };
