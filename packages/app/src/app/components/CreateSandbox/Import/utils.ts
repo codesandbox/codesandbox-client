2107c6e1b409a5490a27aa5f955e49ecf3ac62c4
Maria Clara <claarasantana@gmail.com>
2023-02-20 08:49:11 -0300

    fix getOwnerAndRepoFromInput (#7438)

--- packages/app/src/app/components/CreateSandbox/Import/utils.ts
+++ packages/app/src/app/components/CreateSandbox/Import/utils.ts
@@ -1,29 +1,62 @@
 import { findBestMatch } from 'string-similarity';
 import { State } from './useGithubOrganizations';
 
-// Will match: git@github.com:owner/repository.git
-const REGEX_SSH = /git@github\.com:(?<owner>[\w-]+)\/(?<repo>[\w-]+)\.git$/i;
-// Will match: http(s)://github.com/owner/repo(.git)
-const REGEX_HTTPS = /http(s?):\/\/github\.com\/(?<owner>[\w-]+)\/(?<repo>[\w-]+)(\.git)?$/i;
-// Will match: www.github.com/owner/repo and github.com/owner/repo
-const REGEX_PROTOCOLLESS = /(www\.)?github\.com\/(?<owner>[\w-]+)\/(?<repo>[\w-]+)$/i;
-// Will match: owner/repo
-const REGEX_PLAIN = /(?<owner>[\w-]+)\/(?<repo>[\w-]+)$/i;
+const REGEX_PLAIN = /(?<owner>^\w+)(?<slash>\/)(?<repo>[\w.-]+$)/g;
 
 export const getOwnerAndRepoFromInput = (input: string) => {
-  const match =
-    REGEX_SSH.exec(input) ||
-    REGEX_HTTPS.exec(input) ||
-    REGEX_PROTOCOLLESS.exec(input) ||
-    REGEX_PLAIN.exec(input);
+  let sanitizedInput = input.replace(/\s/, '');
 
-  if (!match) {
+  // Invalidate if input is empty.
+  if (!sanitizedInput) {
     return null;
   }
 
-  const { owner, repo } = match.groups as { owner: string; repo: string };
+  // If it starts with "www.", "github.com" or "http(s?)://", it
+  // should be an url.
+  if (sanitizedInput.match(/(^www.)|(^github.com)|(^http(s?):\/\/)/g)) {
+    try {
+      // If the input doesn't start with "http(s?)://", add it so
+      // the new URL constructor.
+      const parsedUrl = new URL(
+        /^(?!https?:\/\/)/g.test(sanitizedInput)
+          ? `https://${sanitizedInput}`
+          : sanitizedInput
+      );
 
-  return { owner, repo };
+      // Remove slashes from the beginning and end of the
+      // pathname before splitting it, also remove ".git".
+      const pathnameParts = parsedUrl.pathname
+        .replace(/^\/|(\/$)|(\.git$)/g, '')
+        .split('/');
+
+      // If the pathname is correctly formed (owner/repo),
+      // the length should equal 2.
+      if (pathnameParts.length !== 2) {
+        throw new Error('Invalid pathanme.');
+      }
+
+      return { owner: pathnameParts[0], repo: pathnameParts[1] };
+    } catch (error) {
+      return null;
+    }
+  }
+
+  // Check if the input is a SSH clone url.
+  if (sanitizedInput.startsWith('git@')) {
+    // If it's the case, extract the owner and repo
+    // to be returned in the next step.
+    sanitizedInput = sanitizedInput.replace(/^git@github.com:|(\.git$)/g, '');
+  }
+
+  // Check if the input matches "owner/repo".
+  if (sanitizedInput.match(REGEX_PLAIN)) {
+    const matches = REGEX_PLAIN.exec(sanitizedInput);
+    return matches?.groups
+      ? { owner: matches.groups.owner, repo: matches.groups.repo }
+      : null;
+  }
+
+  return null;
 };
 
 export const getGihubOrgMatchingCsbTeam = (
