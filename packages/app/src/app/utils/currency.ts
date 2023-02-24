f04204ff402a589f215ad23b24875e3225452134
Tristan <tristan.dubbeld@gmail.com>
2023-02-24 14:41:25 +0000

    Update currency.ts

--- packages/app/src/app/utils/currency.ts
+++ packages/app/src/app/utils/currency.ts
@@ -1,11 +1,16 @@
 type Price = { currency: string; amount: number };
 
 export const formatCurrency = ({ currency, amount }: Price) => {
+  if (typeof amount === 'undefined') {
+    return null;
+  }
+
   const formatter = new Intl.NumberFormat('en-US', {
     style: 'currency',
     minimumFractionDigits: 0,
     maximumFractionDigits: 0,
     currency,
   });
+
   return formatter.format(amount / 100);
 };
