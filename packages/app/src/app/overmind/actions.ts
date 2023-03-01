91c349fbbd971a7a68afabed731db7d86bba367d
Tristan <tristan.dubbeld@gmail.com>
2023-03-01 08:59:02 +0000

    Add modal name to type

--- packages/app/src/app/overmind/actions.ts
+++ packages/app/src/app/overmind/actions.ts
@@ -258,7 +258,8 @@ type ModalName =
   | 'sandboxPicker'
   | 'minimumPrivacy'
   | 'addMemberToWorkspace'
-  | 'legacyPayment';
+  | 'legacyPayment'
+  | 'selectWorkspaceToUpgrade';
 
 export const modalOpened = (
   { state, effects }: Context,
