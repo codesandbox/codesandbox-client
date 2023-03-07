9d8e6f548151f50dcee58f7efbd85c4706ad4f46
Maria Clara <claarasantana@gmail.com>
2023-03-07 00:36:04 +0000

    adds collaboration sticker to UpsellTeamProCard

--- packages/app/src/app/pages/Pro/components/UpsellTeamProCard.tsx
+++ packages/app/src/app/pages/Pro/components/UpsellTeamProCard.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useAppState, useActions } from 'app/overmind';
-import { Stack, Text } from '@codesandbox/components';
+import { ExperimentValues, useExperimentResult } from '@codesandbox/ab';
+import { Element, Stack, Text } from '@codesandbox/components';
 import track from '@codesandbox/common/lib/utils/analytics';
 import { TEAM_PRO_FEATURES_WITH_PILLS } from 'app/constants';
 import { formatCurrency } from 'app/utils/currency';
@@ -13,8 +14,10 @@ import { StyledPricingDetailsText } from './elements';
 export const UpsellTeamProCard: React.FC<{ trackingLocation: string }> = ({
   trackingLocation,
 }) => {
+  const experimentPromise = useExperimentResult('pro-page-sticker');
   const { dashboard, pro, personalWorkspaceId, user } = useAppState();
   const { modalOpened, openCreateTeamModal } = useActions();
+  const [showSticker, setShowSticker] = React.useState(false);
 
   const upgradeableTeams = dashboard.teams.filter(team => {
     if (
@@ -33,13 +36,23 @@ export const UpsellTeamProCard: React.FC<{ trackingLocation: string }> = ({
     return teamAdmins.includes(user?.id);
   });
 
+  const buildEventName = (event: string) => {
+    const name = `${trackingLocation} - ${event}`;
+
+    if (showSticker) {
+      return `${name} with collaboration sticker`;
+    }
+
+    return name;
+  };
+
   const upsellTeamProCta: CTA =
     upgradeableTeams.length > 0
       ? {
           text: 'Upgrade',
           variant: 'highlight',
           onClick: () => {
-            track(`${trackingLocation} - upsell team pro upgrade clicked`, {
+            track(buildEventName('upsell team pro upgrade clicked'), {
               codesandbox: 'V1',
               event_source: 'UI',
             });
@@ -50,7 +63,7 @@ export const UpsellTeamProCard: React.FC<{ trackingLocation: string }> = ({
           text: 'Create team and Upgrade',
           variant: 'highlight',
           onClick: () => {
-            track(`${trackingLocation} - upsell team pro create team clicked`, {
+            track(buildEventName('upsell team pro create team clicked'), {
               codesandbox: 'V1',
               event_source: 'UI',
             });
@@ -58,31 +71,53 @@ export const UpsellTeamProCard: React.FC<{ trackingLocation: string }> = ({
           },
         };
 
+  React.useEffect(() => {
+    experimentPromise.then(experiment => {
+      setShowSticker(experiment === ExperimentValues.B);
+    });
+  }, [experimentPromise]);
+
   return (
-    <SubscriptionCard
-      title="Team Pro"
-      subTitle="Up to 20 editors"
-      features={TEAM_PRO_FEATURES_WITH_PILLS}
-      isHighlighted
-      cta={upsellTeamProCta}
-    >
-      <Stack gap={1} direction="vertical">
-        <Text size={32} weight="500">
-          {formatCurrency({
-            currency: 'USD',
-            amount: pro?.prices?.team.year.usd / 12,
-          })}
-        </Text>
-        <StyledPricingDetailsText>
-          per editor per month,
-          <br /> billed anually, or{' '}
-          {formatCurrency({
-            currency: 'USD',
-            amount: pro?.prices?.team.month.usd,
-          })}{' '}
-          per month.
-        </StyledPricingDetailsText>
-      </Stack>
-    </SubscriptionCard>
+    <Element css={{ position: 'relative' }}>
+      {showSticker && (
+        <Element
+          as="img"
+          alt="Optimized for collaboration!"
+          css={{
+            position: 'absolute',
+            right: 0,
+            transform: 'translate(50%, -30%)',
+            width: '137px',
+            height: '149px',
+          }}
+          src="/static/img/collaboration-sticker.png"
+        />
+      )}
+      <SubscriptionCard
+        title="Team Pro"
+        subTitle="Up to 20 editors"
+        features={TEAM_PRO_FEATURES_WITH_PILLS}
+        isHighlighted
+        cta={upsellTeamProCta}
+      >
+        <Stack gap={1} direction="vertical">
+          <Text size={32} weight="500">
+            {formatCurrency({
+              currency: 'USD',
+              amount: pro?.prices?.team.year.usd / 12,
+            })}
+          </Text>
+          <StyledPricingDetailsText>
+            per editor per month,
+            <br /> billed anually, or{' '}
+            {formatCurrency({
+              currency: 'USD',
+              amount: pro?.prices?.team.month.usd,
+            })}{' '}
+            per month.
+          </StyledPricingDetailsText>
+        </Stack>
+      </SubscriptionCard>
+    </Element>
   );
 };
