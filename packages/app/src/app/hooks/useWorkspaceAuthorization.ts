ec36a3ed8cbe9f7acc40b78d133303d57dcd80ba
Maria Clara <claarasantana@gmail.com>
2023-03-16 20:55:36 +0000

    update useWorkspaceAuthorization

--- packages/app/src/app/hooks/useWorkspaceAuthorization.ts
+++ packages/app/src/app/hooks/useWorkspaceAuthorization.ts
@@ -2,24 +2,23 @@ import { TeamMemberAuthorization } from 'app/graphql/types';
 import { useAppState } from 'app/overmind';
 
 export const useWorkspaceAuthorization = () => {
-  const {
-    activeTeam,
-    personalWorkspaceId,
-    activeWorkspaceAuthorization,
-  } = useAppState();
+  const { activeTeamInfo, personalWorkspaceId, user } = useAppState();
+
+  const { authorization, teamManager } =
+    activeTeamInfo?.userAuthorizations.find(auth => auth.userId === user?.id) ??
+    {};
 
   /**
    * Personal states
    */
 
-  const isPersonalSpace = activeTeam === personalWorkspaceId;
+  const isPersonalSpace = activeTeamInfo?.id === personalWorkspaceId;
 
   /**
    * User states
    */
 
-  const isAdmin =
-    activeWorkspaceAuthorization === TeamMemberAuthorization.Admin;
+  const isAdmin = authorization === TeamMemberAuthorization.Admin;
 
   /**
    * Team states
@@ -30,20 +29,19 @@ export const useWorkspaceAuthorization = () => {
   const isTeamAdmin = isTeamSpace && isAdmin;
 
   const isTeamEditor =
-    isTeamSpace &&
-    activeWorkspaceAuthorization === TeamMemberAuthorization.Write;
+    isTeamSpace && authorization === TeamMemberAuthorization.Write;
 
   const isTeamViewer =
-    isTeamSpace &&
-    activeWorkspaceAuthorization === TeamMemberAuthorization.Read;
+    isTeamSpace && authorization === TeamMemberAuthorization.Read;
 
   return {
     isPersonalSpace,
     isTeamSpace,
     isTeamAdmin,
     isTeamEditor,
+    isBillingManager: Boolean(teamManager),
     isTeamViewer,
     isAdmin,
-    userRole: activeWorkspaceAuthorization,
+    userRole: authorization ?? null,
   };
 };
