f6c6a269daadde72ef6e23ce65b479fc8e7bb5b1
Maria Clara <claarasantana@gmail.com>
2023-03-15 23:28:51 -0300

    wip

--- packages/app/src/app/pages/Dashboard/Content/routes/Settings/components/MemberList.tsx
+++ packages/app/src/app/pages/Dashboard/Content/routes/Settings/components/MemberList.tsx
@@ -1,188 +1,23 @@
-// import React from 'react';
-// import { css } from '@styled-system/css';
-// import {
-//   List,
-//   ListAction,
-//   Grid,
-//   Column,
-//   Stack,
-//   Avatar,
-//   Menu,
-//   Text,
-//   Icon,
-//   Badge,
-// } from '@codesandbox/components';
-// import { TeamMemberAuthorization } from 'app/graphql/types';
-
-// const permissionMap = {
-//   ADMIN: 'Admin',
-//   WRITE: 'Editor',
-//   READ: 'Viewer',
-//   PENDING: 'Pending...',
-// };
-
-// export type User = {
-//   id: string;
-//   avatarUrl: string;
-//   username: string;
-// };
-
-// type ActionType = {
-//   label: string;
-//   onSelect: () => void;
-// };
-
-// type getActionsType = (user: User) => Array<ActionType>;
-
-// interface MemberListProps {
-//   getPermission: (user: User) => string;
-//   getPermissionOptions: getActionsType;
-//   getActions: getActionsType;
-//   users: User[];
-//   currentUserId?: string;
-// }
-
-// export const MemberList: React.FC<MemberListProps> = ({
-//   getPermission,
-//   getPermissionOptions,
-//   getActions,
-//   users,
-//   currentUserId,
-// }) => (
-//   <List>
-//     {users.map(user => {
-//       const actions = getActions(user);
-//       const userPermission = getPermission(user);
-//       const permissionActions = getPermissionOptions(user);
-
-//       return (
-//         <ListAction
-//           key={user.username}
-//           align="center"
-//           justify="space-between"
-//           css={css({
-//             height: 64,
-//             borderBottom: '1px solid',
-//             borderColor: 'grays.600',
-//           })}
-//         >
-//           <Grid css={{ width: '100%' }}>
-//             <Column span={6}>
-//               <Stack
-//                 gap={4}
-//                 align="center"
-//                 css={{ height: '100%', width: '100%' }}
-//               >
-//                 <Avatar user={user} />
-//                 <Stack align="center" gap={2}>
-//                   <Text size={3}>{user.username}</Text>
-//                   {user.id === currentUserId ? (
-//                     <Text size={3} variant="muted">
-//                       (You)
-//                     </Text>
-//                   ) : null}
-//                   {userPermission === TeamMemberAuthorization.Admin ? (
-//                     <Badge>Admin</Badge>
-//                   ) : null}
-//                 </Stack>
-//               </Stack>
-//             </Column>
-//             <Column span={6}>
-//               <Stack
-//                 justify="space-between"
-//                 align="center"
-//                 css={{ height: '100%' }}
-//               >
-//                 {permissionActions.length ? (
-//                   <Menu>
-//                     <Menu.Button
-//                       style={{ paddingLeft: 0, paddingRight: 0 }}
-//                       css={css({ fontSize: 3, fontWeight: 'normal' })}
-//                     >
-//                       <Text variant="muted">
-//                         {permissionMap[userPermission]}
-//                       </Text>
-//                       <Icon name="caret" size={8} marginLeft={1} />
-//                     </Menu.Button>
-//                     <Menu.List>
-//                       {permissionActions.map(action => (
-//                         <Menu.Item
-//                           key={action.label}
-//                           onSelect={action.onSelect}
-//                           style={{ display: 'flex', alignItems: 'center' }}
-//                         >
-//                           <Text style={{ width: '100%' }}>{action.label}</Text>
-//                           {action.label === permissionMap[userPermission] && (
-//                             <Icon
-//                               style={{}}
-//                               name="simpleCheck"
-//                               size={12}
-//                               marginLeft={1}
-//                             />
-//                           )}
-//                         </Menu.Item>
-//                       ))}
-//                     </Menu.List>
-//                   </Menu>
-//                 ) : (
-//                   <Text variant="muted" size={3} css={css({ width: '100%' })}>
-//                     {permissionMap[getPermission(user)]}
-//                   </Text>
-//                 )}
-//                 {actions.length > 0 ? (
-//                   <Menu>
-//                     <Menu.IconButton
-//                       name="more"
-//                       size={9}
-//                       title="Member options"
-//                     />
-//                     <Menu.List>
-//                       {actions.map(action => (
-//                         <Menu.Item
-//                           key={action.label}
-//                           onSelect={action.onSelect}
-//                         >
-//                           {action.label}
-//                         </Menu.Item>
-//                       ))}
-//                     </Menu.List>
-//                   </Menu>
-//                 ) : null}
-//               </Stack>
-//             </Column>
-//           </Grid>
-//         </ListAction>
-//       );
-//     })}
-//   </List>
-// );
-
 import React from 'react';
-
 import {
   Avatar,
   Badge,
   Column,
   Grid,
-  // Icon,
+  Icon,
   List,
   ListAction,
   Menu,
   Stack,
   Text,
 } from '@codesandbox/components';
-import {
-  useActions,
-  // useActions,
-  useAppState,
-} from 'app/overmind';
+import { useActions, useAppState } from 'app/overmind';
 import {
   CurrentTeamInfoFragmentFragment,
   TeamMemberAuthorization,
 } from 'app/graphql/types';
 import { sortBy } from 'lodash-es';
 import { useWorkspaceAuthorization } from 'app/hooks/useWorkspaceAuthorization';
-// import { useWorkspaceAuthorization } from 'app/hooks/useWorkspaceAuthorization';
 
 type User = CurrentTeamInfoFragmentFragment['users'][number];
 
@@ -244,6 +79,43 @@ export const MembersList: React.FC<MemberListProps> = ({
 
     return acc;
   }, {} as AuthorizationsMap);
+
+  // Returns either options for the role dropdown or a string
+  // to render if the user is not allowed to change the role.
+  const buildRoleOptions = (member: User): Action[] | string => {
+    const memberAuth = authorizationsMap[member.id];
+    if (memberAuth.teamAdmin) {
+      return 'Editor';
+    }
+
+    if (!isTeamAdmin) {
+      return ROLE_MAP[memberAuth.role];
+    }
+
+    return [
+      {
+        name: 'Editor',
+        onSelect: () => {
+          changeAuthorization({
+            userId: member.id,
+            authorization: TeamMemberAuthorization.Write,
+            teamManager: memberAuth.teamManager,
+            confirm: shouldConfirmRoleChange,
+          });
+        },
+      },
+      {
+        name: 'Viewer',
+        onSelect: () => {
+          changeAuthorization({
+            userId: member.id,
+            authorization: TeamMemberAuthorization.Read,
+            teamManager: memberAuth.teamManager,
+          });
+        },
+      },
+    ];
+  };
 
   // Used in the "more" menu at the end of the row.
   const buildMemberActions = (member: User): Action[] => {
@@ -328,6 +200,7 @@ export const MembersList: React.FC<MemberListProps> = ({
       {sortBy(users, u => u.id !== currentUserId).map(user => {
         const authorization = authorizationsMap[user.id];
         const actions = buildMemberActions(user);
+        const roleOptions = buildRoleOptions(user);
 
         return (
           <ListAction
@@ -356,7 +229,40 @@ export const MembersList: React.FC<MemberListProps> = ({
                 </Stack>
               </Column>
               <Column span={5}>
-                <Text size={3}>{ROLE_MAP[authorization.role]}</Text>
+                {typeof roleOptions === 'string' ? (
+                  <Text size={3}>{ROLE_MAP[authorization.role]}</Text>
+                ) : (
+                  <Menu>
+                    <Menu.Button
+                      style={{ paddingLeft: 0, paddingRight: 0 }}
+                      css={{ fontSize: '13px', fontWeight: 'normal' }}
+                    >
+                      <Text variant="muted">
+                        {ROLE_MAP[authorization.role]}
+                      </Text>
+                      <Icon name="chevronDown" size={8} marginLeft={1} />
+                    </Menu.Button>
+                    <Menu.List>
+                      {roleOptions.map(action => (
+                        <Menu.Item
+                          key={action.name}
+                          onSelect={action.onSelect}
+                          style={{ display: 'flex', alignItems: 'center' }}
+                        >
+                          <Text style={{ width: '100%' }}>{action.name}</Text>
+                          {action.name === ROLE_MAP[authorization.role] && (
+                            <Icon
+                              style={{}}
+                              name="simpleCheck"
+                              size={12}
+                              marginLeft={1}
+                            />
+                          )}
+                        </Menu.Item>
+                      ))}
+                    </Menu.List>
+                  </Menu>
+                )}
               </Column>
               <Column span={1}>
                 {actions.length > 0 ? (
