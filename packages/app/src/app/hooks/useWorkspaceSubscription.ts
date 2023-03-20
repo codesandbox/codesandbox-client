cb68533d22179f15b511ee2aea373495695dbe25
Maria Clara <claarasantana@gmail.com>
2023-03-20 11:51:57 -0300

    fix: leva setup (#7515)
    * feat: leva setup (#7503)
    
    * install leva
    
    * wip: Debug component
    
    * update positioning
    
    * fixup
    
    * hook leva controls to useWorkspaceSubscription
    
    * fixup
    
    * linting
    
    * fixup
    
    * fix leva panel
    
    * move Debug to routes root

--- packages/app/src/app/hooks/useWorkspaceSubscription.ts
+++ packages/app/src/app/hooks/useWorkspaceSubscription.ts
@@ -7,17 +7,44 @@ import {
 } from 'app/graphql/types';
 import { useAppState } from 'app/overmind';
 import { isBefore, startOfToday } from 'date-fns';
+import { useControls } from 'leva';
 import { useWorkspaceAuthorization } from './useWorkspaceAuthorization';
+
+enum SubscriptionDebugStatus {
+  'DEFAULT' = 'Default (use API data)',
+  'NO_SUBSCRIPTION' = 'Free (without prior subscription)',
+}
 
 export const useWorkspaceSubscription = (): WorkspaceSubscriptionReturn => {
   const { activeTeamInfo } = useAppState();
   const { isTeamSpace } = useWorkspaceAuthorization();
 
+  const options: SubscriptionDebugStatus[] = [SubscriptionDebugStatus.DEFAULT];
+
+  if (activeTeamInfo) {
+    options.push(SubscriptionDebugStatus.NO_SUBSCRIPTION);
+  }
+
+  const { debugStatus } = useControls(
+    'Subscription',
+    {
+      debugStatus: {
+        label: 'Status',
+        value: SubscriptionDebugStatus.DEFAULT,
+        options,
+      },
+    },
+    [options]
+  );
+
   if (!activeTeamInfo) {
     return NO_WORKSPACE;
   }
 
-  const subscription = activeTeamInfo.subscription;
+  const subscription =
+    debugStatus === SubscriptionDebugStatus.NO_SUBSCRIPTION
+      ? null
+      : activeTeamInfo.subscription;
 
   if (!subscription) {
     return {
