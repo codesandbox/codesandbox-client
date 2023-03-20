afba1fac8824198417c08cd054dae04b3e48f87b
Maria Clara <claarasantana@gmail.com>
2023-03-20 12:57:00 +0000

    move Debug to routes root

--- packages/app/src/app/pages/Dashboard/index.tsx
+++ packages/app/src/app/pages/Dashboard/index.tsx
@@ -23,7 +23,6 @@ import { useShowBanner } from 'app/components/StripeMessages/TrialWithoutPayment
 import { useWorkspaceSubscription } from 'app/hooks/useWorkspaceSubscription';
 import { useDashboardVisit } from 'app/hooks/useDashboardVisit';
 import { SubscriptionStatus } from 'app/graphql/types';
-import { Debug } from 'app/components/Debug';
 import { Header } from './Header';
 import { Sidebar } from './Sidebar';
 import { SIDEBAR_WIDTH } from './Sidebar/constants';
@@ -210,7 +209,6 @@ export const Dashboard: FunctionComponent = () => {
         </Stack>
       </DndProvider>
       <NewTeamModal />
-      <Debug />
     </ThemeProvider>
   );
 };
