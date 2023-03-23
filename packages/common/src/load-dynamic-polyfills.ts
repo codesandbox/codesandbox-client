7724683aeb269b48c6658657b4004c37c5ab9719
Jasper De Moor <jasperdemoor@gmail.com>
2023-03-23 11:42:41 +0100

    more bundle cleanup

--- packages/common/src/load-dynamic-polyfills.ts
+++ packages/common/src/load-dynamic-polyfills.ts
@@ -1,12 +1,5 @@
-import detectOldBrowser from './detect-old-browser';
-
 export default function requirePolyfills() {
   const promises = [];
-  if (detectOldBrowser() || typeof Object.entries === 'undefined') {
-    promises.push(
-      import(/* webpackChunkName: 'polyfills' */ '@babel/polyfill')
-    );
-  }
 
   if (typeof Error.captureStackTrace === 'undefined') {
     promises.push(
