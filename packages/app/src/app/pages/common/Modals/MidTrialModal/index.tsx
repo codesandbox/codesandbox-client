4e6c8cd654af7b6f9413ab6d1ae52d942bb0a5bb
Tristan <tristandubbeld@users.noreply.github.com>
2023-03-23 17:01:38 +0100

    fix(trial): Create customer portal instead of checkout url and combine components (#7527)
    * Update styles on mid trial modal
    
    * Combine lost features on subscription end in one component
    
    * Fix bug where checkout url is always null because active trial is not eligible
    
    * Fix comment to reference the right hook

--- packages/app/src/app/pages/common/Modals/MidTrialModal/index.tsx
+++ packages/app/src/app/pages/common/Modals/MidTrialModal/index.tsx
@@ -14,13 +14,15 @@ import {
 import track from '@codesandbox/common/lib/utils/analytics';
 import { ExperimentValues, useExperimentResult } from '@codesandbox/ab';
 
-import { useDismissible, useGetCheckoutURL } from 'app/hooks';
-import { useActions, useAppState } from 'app/overmind';
+import { useDismissible } from 'app/hooks';
+import { useActions, useAppState, useEffects } from 'app/overmind';
 import { useWorkspaceSubscription } from 'app/hooks/useWorkspaceSubscription';
 import { pluralize } from 'app/utils/pluralize';
+import { LoseFeatures } from 'app/components/LoseFeatures/LoseFeatures';
 
 export const MidTrialModal = () => {
   const { modalClosed } = useActions();
+  const { api } = useEffects();
   const { activeTeam } = useAppState();
   const { pathname } = useLocation();
   const [
@@ -32,9 +34,10 @@ export const MidTrialModal = () => {
     `DASHBOARD_MID_TRIAL_REMINDER_${activeTeam}`
   );
   const { subscription } = useWorkspaceSubscription();
+  const [loadingPortal, setLoadingPortal] = useState(false);
 
   const today = startOfToday();
-  const trialEndDate = new Date(subscription.trialEnd);
+  const trialEndDate = new Date(subscription?.trialEnd);
   const remainingTrialDays = differenceInDays(trialEndDate, today);
 
   useEffect(() => {
@@ -42,11 +45,6 @@ export const MidTrialModal = () => {
       setExperimentValue(experiment);
     });
   }, [experimentPromise]);
-
-  const checkoutUrl = useGetCheckoutURL({
-    success_path: pathname,
-    cancel_path: pathname,
-  });
 
   const handleClose = () => {
     track('Mid trial reminder: close modal', {
@@ -58,48 +56,70 @@ export const MidTrialModal = () => {
     modalClosed();
   };
 
-  const handleUpgrade = () => {
+  const handleUpgrade = async () => {
     track('Mid trial reminder: upgrade to pro', {
       codesandbox: 'V1',
       event_source: 'UI',
     });
 
-    dismissMidTrialReminder();
+    // Custom implementation based on useCreateCustomerPortal hook because the modal
+    // needs to be dismissed before redirecting.
+    try {
+      setLoadingPortal(true);
+      const payload = await api.stripeCustomerPortal(activeTeam, pathname);
+
+      if (payload.stripeCustomerPortalUrl) {
+        dismissMidTrialReminder();
+        window.location.href = payload.stripeCustomerPortalUrl;
+      }
+    } catch (err) {
+      setLoadingPortal(false);
+      console.error(err);
+    }
   };
 
   return (
     <Stack css={{ padding: '24px' }} direction="vertical" gap={12}>
-      <Stack justify="flex-end">
-        <IconButton name="cross" title="Close" onClick={handleClose} />
-      </Stack>
       <Stack direction="vertical" gap={6} css={{ textAlign: 'center' }}>
-        <Stack direction="vertical" gap={1}>
-          <Text
-            as="h2"
-            size={32}
-            color="#FFFFFF"
-            align="center"
-            css={{ marginTop: 0 }}
-            fontFamily="everett"
-          >
-            {remainingTrialDays === 7
-              ? 'One week'
-              : `${remainingTrialDays} ${pluralize({
-                  word: 'day',
-                  count: remainingTrialDays,
-                })}`}{' '}
-            left on your trial.
-          </Text>
-          <Text as="p" size={19} color="#C2C2C2" align="center">
-            {!experimentValue ? <SkeletonText /> : null}
+        <Stack direction="vertical" gap={4}>
+          <Stack justify="flex-end">
+            <IconButton name="cross" title="Close" onClick={handleClose} />
+          </Stack>
+          <Stack direction="vertical" gap={1}>
+            <Text
+              as="h2"
+              size={32}
+              color="#FFFFFF"
+              align="center"
+              css={{ marginTop: 0 }}
+              fontFamily="everett"
+            >
+              {remainingTrialDays === 7
+                ? 'One week'
+                : `${remainingTrialDays} ${pluralize({
+                    word: 'day',
+                    count: remainingTrialDays,
+                  })}`}{' '}
+              left on your trial.
+            </Text>
+            <Text
+              as="p"
+              size={19}
+              color="#C2C2C2"
+              align="center"
+              lineHeight="28px"
+              css={{ margin: 0 }}
+            >
+              {!experimentValue ? <SkeletonText /> : null}
 
-            {experimentValue === ExperimentValues.A
-              ? 'You will lose access to all Pro features when your trial expires.'
-              : null}
-            {experimentValue === ExperimentValues.B
-              ? 'You will need a Team Pro subscription to continue using Pro features.'
-              : null}
-          </Text>
+              {experimentValue === ExperimentValues.A
+                ? 'You will lose access to all Pro features when your trial expires.'
+                : null}
+              {experimentValue === ExperimentValues.B
+                ? 'You will need a Team Pro subscription to continue using Pro features.'
+                : null}
+            </Text>
+          </Stack>
         </Stack>
 
         {!experimentValue ? (
@@ -114,7 +134,7 @@ export const MidTrialModal = () => {
           </Stack>
         ) : null}
 
-        {experimentValue === ExperimentValues.A ? <LoseAfterTrial /> : null}
+        {experimentValue === ExperimentValues.A ? <LoseFeatures /> : null}
         {experimentValue === ExperimentValues.B ? <KeepWithTrial /> : null}
       </Stack>
       <Stack gap={4} justify="space-between">
@@ -125,114 +145,14 @@ export const MidTrialModal = () => {
         </Element>
         <Element css={{ flexGrow: 1 }}>
           <Button
-            as="a"
-            href={checkoutUrl}
             onClick={handleUpgrade}
             variant="primary"
+            loading={loadingPortal}
           >
             Upgrade to pro
           </Button>
         </Element>
       </Stack>
-    </Stack>
-  );
-};
-
-// Based on FEATURES in SubscriptionCancellation.tsx, they're just in a different
-// order.
-const LOSE_DEFAULT = [
-  {
-    key: 'editors',
-    pro: 'Up to 20 editors',
-    free: 'Limited to 5 editors',
-  },
-  {
-    key: 'sandboxes',
-    pro: 'Unlimited private sandboxes',
-    free: 'Limited to 20 public sandboxes',
-  },
-  {
-    key: 'repos',
-    pro: 'Unlimited private repositories',
-    free: 'Limited to 3 public repositories',
-  },
-  {
-    key: 'npm',
-    pro: 'Private NPM packages',
-    free: 'Limited to public NPM packages',
-  },
-  {
-    key: 'live',
-    pro: 'Live sessions',
-    free: 'No ability to go live',
-  },
-  {
-    key: 'ram',
-    pro: '6GB RAM',
-    free: '2GB RAM',
-    pill: '-66% capacity',
-  },
-  {
-    key: 'cpu',
-    pro: '4 vCPUs',
-    free: '2 vCPUs',
-    pill: '-2 vCPUs',
-  },
-  {
-    key: 'disk',
-    pro: '12GB Disk',
-    free: '6GB Disk',
-    pill: '-50% storage',
-  },
-];
-
-const LoseAfterTrial = () => {
-  const { activeTeamInfo } = useAppState();
-  const usage = activeTeamInfo?.usage;
-
-  return (
-    <Stack direction="vertical" gap={4}>
-      {LOSE_DEFAULT.map(feature => {
-        let lostFeature = feature.pro;
-        let pillValue = feature.pill;
-
-        if (feature.key === 'editors' && usage?.editorsQuantity > 5) {
-          const lostAmount = usage?.editorsQuantity - 5;
-
-          lostFeature = `${usage?.editorsQuantity} editors`;
-          pillValue = `-${lostAmount} editor seat${lostAmount > 1 ? 's' : ''}`;
-        }
-
-        if (
-          feature.key === 'sandboxes' &&
-          usage?.privateSandboxesQuantity > 0
-        ) {
-          pillValue = `${usage?.privateSandboxesQuantity} restricted`;
-        }
-
-        if (feature.key === 'repos' && usage?.privateProjectsQuantity > 3) {
-          pillValue = `${usage?.privateProjectsQuantity} restricted`;
-        }
-
-        return (
-          <div key={feature.key}>
-            <Text
-              size={16}
-              color="#999999"
-              lineHeight="24px"
-              css={{ textDecoration: 'line-through' }}
-            >
-              {lostFeature}
-            </Text>
-            <Stack align="center" gap={3} justify="center">
-              <Text size={16} color="#FFFFFF" lineHeight="24px">
-                {feature.free}
-              </Text>
-              {pillValue ? <Badge variant="error">{pillValue}</Badge> : null}
-            </Stack>
-          </div>
-        );
-      })}
     </Stack>
   );
 };
--- packages/app/src/app/pages/common/Modals/SubscriptionCancellation/SubscriptionCancellation.tsx
+++ packages/app/src/app/pages/common/Modals/SubscriptionCancellation/SubscriptionCancellation.tsx
@@ -1,71 +1,16 @@
 import track from '@codesandbox/common/lib/utils/analytics';
 import {
-  Badge,
+  Element,
   Button,
   IconButton,
   Stack,
   Text,
 } from '@codesandbox/components';
-import { TEAM_FREE_LIMITS } from 'app/constants';
+import { LoseFeatures } from 'app/components/LoseFeatures/LoseFeatures';
 import { useCreateCustomerPortal } from 'app/hooks/useCreateCustomerPortal';
 import { useWorkspaceSubscription } from 'app/hooks/useWorkspaceSubscription';
 import { useActions, useAppState } from 'app/overmind';
-import { pluralize } from 'app/utils/pluralize';
 import React from 'react';
-
-type FeatureComp = {
-  key: string;
-  free: string;
-  pro: string;
-  pill?: string;
-};
-const FEATURES: FeatureComp[] = [
-  {
-    key: 'sandboxes',
-    pro: 'Unlimited private sandboxes',
-    free: 'Limited to 20 public sandboxes',
-  },
-  {
-    key: 'repos',
-    pro: 'Unlimited private repositories',
-    free: 'Limited to 3 public repositories',
-  },
-  {
-    key: 'editors',
-    pro: 'Up to 20 editors',
-    free: 'Limited to 5 editors',
-  },
-  {
-    key: 'npm',
-    pro: 'Private NPM packages',
-    free: 'Limited to public NPM packages',
-  },
-  {
-    key: 'live',
-    pro: 'Live sessions',
-    free: 'No ability to go live',
-  },
-  {
-    key: 'ram',
-    pro: '6GB RAM',
-    free: '2GB RAM',
-    pill: '-66% capacity',
-  },
-  {
-    key: 'cpu',
-    pro: '4 vCPUs',
-    free: '2 vCPUs',
-    pill: '-2 vCPUs',
-  },
-  {
-    key: 'disk',
-    pro: '12GB Disk',
-    free: '6GB Disk',
-    pill: '-50% storage',
-  },
-];
-
-const FEATURES_WITH_DYNAMIC_PILLS = ['editors', 'sandboxes', 'repos'];
 
 export const SubscriptionCancellationModal: React.FC = () => {
   const { activeTeamInfo } = useAppState();
@@ -87,154 +32,67 @@ export const SubscriptionCancellationModal: React.FC = () => {
     modalClosed();
   };
 
-  // TO DO: if we need this info more often, extract
-  // to a custom useWorkspaceUsage similar to
-  // useWorkspaceLimits.
-  const teamUsage = activeTeamInfo?.usage;
-
-  const getEditorsLabel = () => {
-    if (teamUsage && teamUsage.editorsQuantity > TEAM_FREE_LIMITS.editors) {
-      const lostSeats = teamUsage.editorsQuantity - TEAM_FREE_LIMITS.editors;
-      return `-${lostSeats} editor ${pluralize({
-        word: 'seat',
-        count: lostSeats,
-      })}`;
-    }
-
-    return undefined;
-  };
-
-  const getSandboxesLabel = () => {
-    const restrictedSandboxesCount =
-      (teamUsage?.privateProjectsQuantity ?? 0) -
-      TEAM_FREE_LIMITS.private_repos;
-    if (restrictedSandboxesCount > 0) {
-      return `${restrictedSandboxesCount} restricted`;
-    }
-
-    return undefined;
-  };
-
-  const getReposLabel = () => {
-    const restrictedRepositoriesCount =
-      (teamUsage?.privateProjectsQuantity ?? 0) -
-      TEAM_FREE_LIMITS.private_repos;
-    if (restrictedRepositoriesCount > 0) {
-      return `${restrictedRepositoriesCount} restricted`;
-    }
-
-    return undefined;
-  };
-
   return (
-    <Stack
-      css={{
-        position: 'relative',
-        overflow: 'hidden',
-        padding: '24px 32px 32px',
-      }}
-      direction="vertical"
-    >
-      <Stack justify="space-between">
-        <Text
-          as="h1"
-          css={{
-            margin: 0,
-            lineHeight: '28px',
-            letterSpacing: '-0.019em',
-          }}
-          size={19}
-          weight="normal"
-        >
-          You&apos;ll lose access to all Pro features
-        </Text>
-        <IconButton name="cross" title="Close" onClick={handleCloseModal} />
-      </Stack>
-      <Stack
-        as="ul"
-        css={{
-          listStyle: 'none',
-          margin: '24px 0 0',
-          padding: 0,
-        }}
-        direction="vertical"
-        gap={6}
-      >
-        {FEATURES.map(f => {
-          let badge = f.pill;
-          if (FEATURES_WITH_DYNAMIC_PILLS.includes(f.key)) {
-            badge = {
-              editors: getEditorsLabel(),
-              sandboxes: getSandboxesLabel(),
-              repos: getReposLabel(),
-            }[f.key];
-          }
-
-          return (
-            <Stack direction="vertical" key={f.key} gap={1}>
-              <Text
-                css={{
-                  textDecoration: 'line-through',
-                  color: '#999999',
-                }}
-                size={13}
-              >
-                {f.pro}
-              </Text>
-              <Stack align="baseline" gap={3}>
-                <Text size={13}>{f.free}</Text>
-                {badge ? <Badge variant="warning">{badge}</Badge> : null}
-              </Stack>
-            </Stack>
-          );
-        })}
-      </Stack>
-      <Stack
-        css={{
-          flexWrap: 'wrap',
-          marginLeft: 'auto',
-          marginRight: 0,
-          marginTop: '32px',
-        }}
-        gap={6}
-      >
-        <Button onClick={handleCloseModal} variant="link" autoWidth>
-          {hasActiveTeamTrial ? 'Continue trial' : 'Continue subscription'}
-        </Button>
-        {isPaddle ? (
-          <Button
-            css={{ padding: '0 32px' }}
-            loading={paddleLoading}
-            onClick={() => {
-              setPaddeLoading(true);
-              track('Team Settings - confirm paddle cancel subscription', {
-                codesandbox: 'V1',
-                event_source: 'UI',
-              });
-
-              pro.cancelWorkspaceSubscription();
-            }}
-            autoWidth
+    <Stack css={{ padding: '24px' }} direction="vertical" gap={12}>
+      <Stack direction="vertical" gap={6} css={{ textAlign: 'center' }}>
+        <Stack direction="vertical" gap={4}>
+          <Stack justify="flex-end">
+            <IconButton name="cross" title="Close" onClick={handleCloseModal} />
+          </Stack>
+          <Text
+            as="h2"
+            size={32}
+            color="#FFFFFF"
+            align="center"
+            css={{ marginTop: 0 }}
+            fontFamily="everett"
           >
-            I understand
-          </Button>
-        ) : (
-          <Button
-            css={{ padding: '0 32px' }}
-            loading={loadingCustomerPortal}
-            onClick={() => {
-              track('Team Settings: confirm cancellation from modal', {
-                codesandbox: 'V1',
-                event_source: 'UI',
-              });
+            You&apos;ll lose access to all Pro features
+          </Text>
+        </Stack>
+        <LoseFeatures />
+      </Stack>
 
-              createCustomerPortal();
-            }}
-            autoWidth
-          >
-            OK
+      <Stack gap={4} justify="space-between">
+        <Element css={{ flexGrow: 1 }}>
+          <Button onClick={handleCloseModal} variant="secondary">
+            Continue {hasActiveTeamTrial ? 'trial' : 'subscription'}
           </Button>
-        )}
+        </Element>
+        <Element css={{ flexGrow: 1 }}>
+          {isPaddle ? (
+            <Button
+              loading={paddleLoading}
+              onClick={() => {
+                setPaddeLoading(true);
+                track('Team Settings - confirm paddle cancel subscription', {
+                  codesandbox: 'V1',
+                  event_source: 'UI',
+                });
+
+                pro.cancelWorkspaceSubscription();
+              }}
+              variant="primary"
+            >
+              I understand
+            </Button>
+          ) : (
+            <Button
+              loading={loadingCustomerPortal}
+              onClick={() => {
+                track('Team Settings: confirm cancellation from modal', {
+                  codesandbox: 'V1',
+                  event_source: 'UI',
+                });
+
+                createCustomerPortal();
+              }}
+              variant="primary"
+            >
+              Cancel {hasActiveTeamTrial ? 'trial' : 'subscription'}
+            </Button>
+          )}
+        </Element>
       </Stack>
     </Stack>
   );
