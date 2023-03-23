cd0276d62128a2a04009f02c3b66a66f15d89531
Maria Clara <claarasantana@gmail.com>
2023-03-23 19:24:31 +0000

    debug workspace limits

--- packages/app/src/app/hooks/useWorkspaceLimits.ts
+++ packages/app/src/app/hooks/useWorkspaceLimits.ts
@@ -1,12 +1,40 @@
 import { TeamMemberAuthorization } from 'app/graphql/types';
 import { useAppState } from 'app/overmind';
+import { folder, useControls } from 'leva';
 import { useWorkspaceAuthorization } from './useWorkspaceAuthorization';
-import { useWorkspaceSubscription } from './useWorkspaceSubscription';
+import {
+  SubscriptionDebugStatus,
+  useWorkspaceSubscription,
+} from './useWorkspaceSubscription';
 
 export const useWorkspaceLimits = (): WorkspaceLimitsReturn => {
   const { activeTeamInfo } = useAppState();
   const { isTeamSpace } = useWorkspaceAuthorization();
   const { isFree } = useWorkspaceSubscription();
+  const debugLimits = useControls('Limits', {
+    debugLimits: folder(
+      {
+        restrictEditors: { label: 'Restrict editors', value: false },
+        hasMaxPublicRepositories: {
+          label: 'Restrict public repos',
+          value: false,
+        },
+        hasMaxPrivateRepositories: {
+          label: 'Restrict private repos',
+          value: false,
+        },
+        hasMaxPublicSandboxes: {
+          label: 'Restrict public sandboxes',
+          value: false,
+        },
+      },
+      {
+        render: get =>
+          get('Subscription.debugStatus') ===
+          SubscriptionDebugStatus.NO_SUBSCRIPTION,
+      }
+    ),
+  });
 
   if (!activeTeamInfo) {
     return {
@@ -26,41 +54,47 @@ export const useWorkspaceLimits = (): WorkspaceLimitsReturn => {
       authorization === TeamMemberAuthorization.Admin ||
       authorization === TeamMemberAuthorization.Write
   );
+
   const numberOfEditors = isTeamSpace
     ? editorOrAdminAuthorizations?.length || 1
     : 1; // Personal
 
   const hasMaxNumberOfEditors =
-    isFree === true && numberOfEditors === limits.maxEditors;
+    debugLimits?.restrictEditors ||
+    (isFree === true && numberOfEditors === limits.maxEditors);
 
   const numberOfEditorsIsOverTheLimit =
-    isFree === true &&
-    limits.maxEditors !== null &&
-    numberOfEditors > limits.maxEditors;
+    debugLimits?.restrictEditors ||
+    (isFree === true &&
+      limits.maxEditors !== null &&
+      numberOfEditors > limits.maxEditors);
 
   const publicProjectsQuantity = usage.publicProjectsQuantity;
   const maxPublicProjects = limits.maxPublicProjects;
 
   const hasMaxPublicRepositories =
-    isFree === true &&
-    maxPublicProjects !== null &&
-    publicProjectsQuantity >= maxPublicProjects;
+    debugLimits?.hasMaxPublicRepositories ||
+    (isFree === true &&
+      maxPublicProjects !== null &&
+      publicProjectsQuantity >= maxPublicProjects);
 
   const privateRepositoriesQuantity = usage.privateProjectsQuantity;
   const maxPrivateRepositories = limits.maxPrivateProjects;
 
   const hasMaxPrivateRepositories =
-    isFree === true &&
-    maxPrivateRepositories !== null &&
-    privateRepositoriesQuantity > maxPrivateRepositories;
+    debugLimits?.hasMaxPrivateRepositories ||
+    (isFree === true &&
+      maxPrivateRepositories !== null &&
+      privateRepositoriesQuantity > maxPrivateRepositories);
 
   const publicSandboxesQuantity = usage.publicSandboxesQuantity;
   const maxPublicSandboxes = limits.maxPublicSandboxes;
 
   const hasMaxPublicSandboxes =
-    isFree === true &&
-    maxPublicSandboxes !== null &&
-    publicSandboxesQuantity >= maxPublicSandboxes;
+    debugLimits?.hasMaxPrivateRepositories ||
+    (isFree === true &&
+      maxPublicSandboxes !== null &&
+      publicSandboxesQuantity >= maxPublicSandboxes);
 
   return {
     numberOfEditors,
