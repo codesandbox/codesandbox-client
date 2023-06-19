648491bd0fac03d422071c6c5ae73210a7d5a292
Christian Alfoni <christianalfoni@gmail.com>
2023-06-19 08:53:14 +0000

    add CLI postMessage for token

---
+++ packages/app/src/app/pages/CLI/PostToken.tsx
@@ -0,0 +1,21 @@
+import React, { FunctionComponent, useEffect } from 'react';
+import { Title } from 'app/components/Title';
+
+export const PostToken: FunctionComponent<{ authToken: string }> = ({
+  authToken,
+}) => {
+  useEffect(() => {
+    window.top.postMessage(
+      {
+        type: 'CLI_TOKEN',
+        token: authToken,
+      },
+      // We do not need to care about the origin here as this only happens when the popup
+      // was triggered by user interaction and the token itself needs to be exchanged
+      // in the API
+      '*'
+    );
+  }, []);
+
+  return <Title>Authorization key fetched!</Title>;
+};
--- packages/app/src/app/pages/CLI/index.tsx
+++ packages/app/src/app/pages/CLI/index.tsx
@@ -3,16 +3,91 @@ import css from '@styled-system/css';
 import { withTheme } from 'styled-components';
 import { Element, ThemeProvider } from '@codesandbox/components';
 
-import { useActions } from 'app/overmind';
-import { Container } from './elements';
+import { useActions, useAppState } from 'app/overmind';
+import { Container, Buttons, ContentContainer } from './elements';
+import { PostToken } from './PostToken';
 import { Prompt } from './Prompt';
+import { Button } from '@codesandbox/components';
+
+import { SubTitle } from 'app/components/SubTitle';
+import { Title } from 'app/components/Title';
+
+import { SignIn } from 'app/pages/SignIn/SignIn';
+import { LogoFull } from '@codesandbox/common/lib/components/Logo';
 
 export const CLI: FunctionComponent = withTheme(({ theme }) => {
-  const { cliMounted } = useActions();
+  const {
+    authToken,
+    isLoadingAuthToken,
+    error,
+    isLoadingCLI,
+    user,
+    isLoggedIn,
+  } = useAppState();
+  const actions = useActions();
+  const postTokenToParent = window.location.search.includes('post=true');
 
   useEffect(() => {
-    cliMounted();
-  }, [cliMounted]);
+    if (isLoggedIn && !authToken && !isLoadingAuthToken) {
+      actions.internal.authorize();
+    }
+  }, [isLoggedIn]);
+
+  const render = () => {
+    if (error) {
+      return (
+        <>
+          <LogoFull style={{ paddingBottom: 32 }} />
+
+          <Title>An error occurred:</Title>
+          <SubTitle>{error}</SubTitle>
+
+          <Buttons>
+            <Button
+              autoWidth
+              href="/?from-app=1"
+              style={{
+                fontSize: 16,
+                height: 40,
+                width: '100%',
+                marginTop: '1rem',
+              }}
+            >
+              Go to homepage
+            </Button>
+          </Buttons>
+        </>
+      );
+    }
+
+    if (!user?.username) {
+      return (
+        <>
+          <LogoFull style={{ paddingBottom: 32 }} />
+          <Title>
+            Welcome to <br />
+            CodeSandbox!
+          </Title>
+
+          <SubTitle style={{ paddingBottom: 16 }}>
+            You need to sign in to use the CLI.
+          </SubTitle>
+
+          <SignIn />
+        </>
+      );
+    }
+
+    if (isLoadingCLI) {
+      return <Title>Fetching authorization key...</Title>;
+    }
+
+    return postTokenToParent ? (
+      <PostToken authToken={authToken} />
+    ) : (
+      <Prompt authToken={authToken} />
+    );
+  };
 
   return (
     <ThemeProvider theme={theme.vsCode}>
@@ -24,7 +99,7 @@ export const CLI: FunctionComponent = withTheme(({ theme }) => {
         })}
       >
         <Container>
-          <Prompt />
+          <ContentContainer>{render()}</ContentContainer>
         </Container>
       </Element>
     </ThemeProvider>
