54ab4a09cc473555baae602b0c710fcf0d6907da
Christian Alfoni <christianalfoni@gmail.com>
2023-06-20 17:22:21 +0200

    put initial auth into own component (#7690)

--- packages/app/src/app/pages/CLI/index.tsx
+++ packages/app/src/app/pages/CLI/index.tsx
@@ -18,10 +18,79 @@ import { Container, Buttons, ContentContainer } from './elements';
  * This component renders the CLI token page. By giving a query of "post=true" it will post
  * the JWT token back to the window.opener, as opposed to showing it in the UI to "copy/paste"
  */
-export const CLI: FunctionComponent = withTheme(({ theme }) => {
+const AuthorizedCLI: FunctionComponent = () => {
   const { authToken, error, isLoadingAuthToken, isLoggedIn } = useAppState();
   const actions = useActions();
   const postTokenToParent = window.location.search.includes('post=true');
+
+  useEffect(() => {
+    // We want to reauthorize when we change logged in state
+    if (isLoggedIn) {
+      actions.internal.authorize();
+    }
+  }, [isLoggedIn]);
+
+  if (error) {
+    return (
+      <>
+        <LogoFull style={{ paddingBottom: 32 }} />
+
+        <Title>An error occurred:</Title>
+        <SubTitle>{error}</SubTitle>
+
+        <Buttons>
+          <Button
+            autoWidth
+            href="/?from-app=1"
+            style={{
+              fontSize: 16,
+              height: 40,
+              width: '100%',
+              marginTop: '1rem',
+            }}
+          >
+            Go to homepage
+          </Button>
+        </Buttons>
+      </>
+    );
+  }
+
+  if (isLoadingAuthToken) {
+    return <Title>Fetching authorization key...</Title>;
+  }
+
+  if (authToken) {
+    return postTokenToParent ? (
+      <PostToken authToken={authToken} />
+    ) : (
+      <Prompt authToken={authToken} />
+    );
+  }
+
+  return (
+    <>
+      <LogoFull style={{ paddingBottom: 32 }} />
+      <Title>
+        Welcome to <br />
+        CodeSandbox!
+      </Title>
+
+      <SubTitle style={{ paddingBottom: 16 }}>
+        You need to sign in to use the CLI.
+      </SubTitle>
+
+      <SignIn />
+    </>
+  );
+};
+
+/**
+ * This component does the initial authorization of the user, which leads to being logged in or not
+ */
+export const CLI: FunctionComponent = withTheme(({ theme }) => {
+  const actions = useActions();
+
   // Because we need to run `authorize` (given we are logged in) before evaluating what to render,
   // we have to wait until the effect has run before showing any UI
   const [hasRunInitialAuthorization, setHasRunInitialAuthorization] = useState(
@@ -29,73 +98,10 @@ export const CLI: FunctionComponent = withTheme(({ theme }) => {
   );
 
   useEffect(() => {
-    // We want to reauthorize when we change logged in state
-    if (isLoggedIn) {
-      actions.internal.authorize();
-    }
-
-    setHasRunInitialAuthorization(true);
-  }, [isLoggedIn]);
-
-  const render = () => {
-    if (error) {
-      return (
-        <>
-          <LogoFull style={{ paddingBottom: 32 }} />
-
-          <Title>An error occurred:</Title>
-          <SubTitle>{error}</SubTitle>
-
-          <Buttons>
-            <Button
-              autoWidth
-              href="/?from-app=1"
-              style={{
-                fontSize: 16,
-                height: 40,
-                width: '100%',
-                marginTop: '1rem',
-              }}
-            >
-              Go to homepage
-            </Button>
-          </Buttons>
-        </>
-      );
-    }
-
-    if (isLoadingAuthToken) {
-      return <Title>Fetching authorization key...</Title>;
-    }
-
-    if (authToken) {
-      return postTokenToParent ? (
-        <PostToken authToken={authToken} />
-      ) : (
-        <Prompt authToken={authToken} />
-      );
-    }
-
-    return (
-      <>
-        <LogoFull style={{ paddingBottom: 32 }} />
-        <Title>
-          Welcome to <br />
-          CodeSandbox!
-        </Title>
-
-        <SubTitle style={{ paddingBottom: 16 }}>
-          You need to sign in to use the CLI.
-        </SubTitle>
-
-        <SignIn />
-      </>
-    );
-  };
-
-  if (!hasRunInitialAuthorization) {
-    return null;
-  }
+    actions.cliMounted().then(() => {
+      setHasRunInitialAuthorization(true);
+    });
+  }, []);
 
   return (
     <ThemeProvider theme={theme.vsCode}>
@@ -107,7 +113,13 @@ export const CLI: FunctionComponent = withTheme(({ theme }) => {
         })}
       >
         <Container>
-          <ContentContainer>{render()}</ContentContainer>
+          <ContentContainer>
+            {hasRunInitialAuthorization ? (
+              <AuthorizedCLI />
+            ) : (
+              <Title>Authorizing...</Title>
+            )}
+          </ContentContainer>
         </Container>
       </Element>
     </ThemeProvider>
