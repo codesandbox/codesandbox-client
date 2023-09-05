a2376cd632191329bb9b6fdb6a25667a6ef25f7e
Ives van Hoorne <Ives.v.h@gmail.com>
2023-09-05 13:15:14 +0000

    fix: remove git requirement

--- packages/app/src/app/components/CreateSandbox/SearchResults/SearchResults.tsx
+++ packages/app/src/app/components/CreateSandbox/SearchResults/SearchResults.tsx
@@ -62,7 +62,7 @@ export const SearchResults = ({
         <Configure
           query={search}
           hitsPerPage={50}
-          facetFilters={['is_template: true', 'is_git: false']}
+          facetFilters={[['is_template: true']]}
         />
 
         <Stack css={{ height: '100%' }} direction="vertical" gap={4}>
