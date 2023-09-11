8129b619481993f60563d4588b7693df3c6b20ea
Ives van Hoorne <ives@codesandbox.io>
2023-09-11 11:52:00 +0200

    chore: update typescript to v5.2.2 (#8072)

--- packages/app/src/app/overmind/effects/vscode/extensionHostWorker/common/fs.ts
+++ packages/app/src/app/overmind/effects/vscode/extensionHostWorker/common/fs.ts
@@ -92,7 +92,7 @@ export async function initializeBrowserFS({
           fs: 'JSDelivrRequest',
           options: {
             dependency: 'typescript',
-            version: '4.9.3',
+            version: '5.2.2',
           },
         };
       }
