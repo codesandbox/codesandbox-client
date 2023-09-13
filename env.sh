126abbf7efaf2f63192d6cf7c34388036d093c45
Utwo <mihai.legat@gmail.com>
2023-09-13 13:37:51 +0300

    Update env.sh to support sh

--- env.sh
+++ env.sh
@@ -9,23 +9,19 @@ touch $envConfigFilePath
 # Add assignment 
 echo "window._env_ = {" >> $envConfigFilePath
 
-# Read each line in .env file
-# Each line represents key=value pairs
-while read -r line || [[ -n "$line" ]];
-do
-  # Split env variables by character `=`
-  if printf '%s\n' "$line" | grep -q -e '='; then
-    varname=$(printf '%s\n' "$line" | sed -e 's/=.*//')
-    varvalue=$(printf '%s\n' "$line" | sed -e 's/^[^=]*=//')
+
+while IFS='=' read -r varname varvalue || [ -n "$varname" ]; do
+  value=""
+  if [ -n "$varname" ]; then
+    eval "value=\"\${$varname}\""
   fi
 
-  # Read value of current variable if exists as Environment variable
-  value=$(printf '%s\n' "${!varname}")
   # Otherwise use value from .env file
-  [[ -z $value ]] && value=${varvalue}
+  if [ -z "$value" ]; then
+    value="$varvalue"
+  fi
   
-  # Append configuration property to JS file
-  echo "  $varname: \"$value\"," >> $envConfigFilePath
+  echo "  $varname: \"$value\"," >> "$envConfigFilePath"
 done < ".env.$NODE_ENV"
 
-echo "}" >> $envConfigFilePath
+echo "}" >> "$envConfigFilePath"
