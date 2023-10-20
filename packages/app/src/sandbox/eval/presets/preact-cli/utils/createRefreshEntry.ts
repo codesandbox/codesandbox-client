39df9ca535ba6ee714e4fb795fffe0688efeeb59
lcch <381582176@qq.com>
2023-10-20 22:52:01 +0800

    chore: remove comments

--- packages/app/src/sandbox/eval/presets/preact-cli/utils/createRefreshEntry.ts
+++ packages/app/src/sandbox/eval/presets/preact-cli/utils/createRefreshEntry.ts
@@ -1,9 +1,5 @@
 import { Manager } from 'sandpack-core';
 
-/**
- * When using React Refresh we need to evaluate some code before 'react-dom' is initialized
- * (https://github.com/facebook/react/issues/16604#issuecomment-528663101) this is the code.
- */
 export async function createRefreshEntry(manager: Manager) {
   const entryModule = {
     path: '/node_modules/__csb/prefresh-entrypoint.js',
