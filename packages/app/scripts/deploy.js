df6d2c53cbfddff5546abd387c1efa4f3a9ccd5e
Utwo <mihai.legat@gmail.com>
2023-11-14 13:19:27 +0000

    feat: add backstage deploy api

--- packages/app/scripts/deploy.js
+++ packages/app/scripts/deploy.js
@@ -23,10 +23,48 @@ fetch('https://deploy-api.ops.csb.dev/image', {
 
     return x.json();
   })
-  .then(res => {
+  .then(deployRes => {
     // eslint-disable-next-line
-    console.log(res);
-    process.exit(0);
+    console.log(deployRes);
+
+    fetch(
+      `https://backstage.csbops.io/api/deploy/gitops/codesandbox/codesandbox-gitops/codesandbox-core/codesandbox/${process.env.ENVIRONMENT}/helm-chart-values/values.yaml`,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'CF-Access-Client-Id': process.env.CF_ZERO_TRUST_BACKSTAGE_DEPLOY_ID,
+          'CF-Access-Client-Secret':
+            process.env.CF_ZERO_TRUST_BACKSTAGE_DEPLOY_TOKEN,
+        },
+        body: JSON.stringify({
+          key: 'client.image.tag',
+          commitMessage: `Deploy Client V1 to ${
+            process.env.ENVIRONMENT
+          } with ${process.env.CIRCLE_SHA1.substr(0, 7)}`,
+          value: process.env.CIRCLE_SHA1.substr(0, 7),
+        }),
+      }
+    )
+      .then(x => {
+        if (!x.ok) {
+          return x.json().then(resss => {
+            console.error(resss);
+            throw new Error('Request failed');
+          });
+        }
+
+        return x.json();
+      })
+      .then(res => {
+        // eslint-disable-next-line
+        console.log(res);
+        process.exit(0);
+      })
+      .catch(e => {
+        console.error(e);
+        process.exit(1);
+      });
   })
   .catch(e => {
     console.error(e);
