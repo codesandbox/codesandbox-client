ab2e67971d16981a177d734217a10c0a30d00aff
Christian Alfoni <christianalfoni@gmail.com>
2023-11-17 14:56:33 +0000

    feat: Add support for .eslintrc.json file
    Add .eslintrc.json to the HIDDEN_FILES array in files.ts

--- packages/common/src/templates/constants/files.ts
+++ packages/common/src/templates/constants/files.ts
@@ -1,2 +1,2 @@
 export const HIDDEN_DIRECTORIES = ['.codesandbox', '.devcontainer'];
-export const HIDDEN_FILES = ['.eslintrc.cjs'];
+export const HIDDEN_FILES = ['.eslintrc.cjs', '.eslintrc.json'];
