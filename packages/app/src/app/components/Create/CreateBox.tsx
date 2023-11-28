64a02214d8e3a2b1243316009723202a0e135ed8
Alex Moldovan <alex.n.moldovan@gmail.com>
2023-11-28 17:32:23 +0000

    feat: create modal second step

--- packages/app/src/app/components/Create/CreateBox.tsx
+++ packages/app/src/app/components/Create/CreateBox.tsx
@@ -51,7 +51,7 @@ type CreateBoxProps = ModalContentProps & {
 export const CreateBox: React.FC<CreateBoxProps> = ({
   collectionId,
   type = 'devbox',
-  hasSecondStep = false,
+  hasSecondStep = true,
   closeModal,
   isModal,
 }) => {
