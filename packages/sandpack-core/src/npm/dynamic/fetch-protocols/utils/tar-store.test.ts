62c9a89f5d092b063a09bc4fbb93b9c417349dde
Sergey Belyanin <Ku3mi41@narod.ru>
2023-11-29 15:04:14 +0700

    chore: strict types

--- packages/sandpack-core/src/npm/dynamic/fetch-protocols/utils/tar-store.test.ts
+++ packages/sandpack-core/src/npm/dynamic/fetch-protocols/utils/tar-store.test.ts
@@ -5,25 +5,21 @@ jest.mock('../utils', () => ({ fetchWithRetries: jest.fn() }));
 jest.mock('isomorphic-untar-gzip', () => (x: any) => x);
 
 describe('tar-store', () => {
-  let tarStore;
+  let tarStore = new TarStore();
   const content = 'package.json content';
 
-  const getTar = (name) => ({
+  const getTar = (name: string) => ({
     arrayBuffer: () => [{ name, buffer: Buffer.from(content) }],
   });
 
-  beforeEach(() => {
-    tarStore = new TarStore();
-  });
-
   it('Should be able to get "/package.json" from standard structured tar', async () => {
-    fetchWithRetries.mockResolvedValueOnce(getTar('package/package.json'));
+    (fetchWithRetries as jest.Mock).mockResolvedValueOnce(getTar('package/package.json'));
     const file = await tarStore.file('typescript', 'http://localhost:4000/typescript/-/typescript-4.0.2.tgz', '/package.json');
     expect(file).toEqual(content);
   });
 
   it('Should be able to get "/package.json" from non-standard structured tar', async () => {
-    fetchWithRetries.mockResolvedValueOnce(getTar('parse-json/package.json'));
+    (fetchWithRetries as jest.Mock).mockResolvedValueOnce(getTar('parse-json/package.json'));
     const file = await tarStore.file('parse-json', 'http://localhost:4000/@types/parse-json/-/parse-json-4.0.2.tgz', '/package.json');
     expect(file).toEqual(content);
   });
