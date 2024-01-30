84573a28e9dd703761b9384e6becd486993765d8
Ives van Hoorne <ives@codesandbox.io>
2024-01-30 09:54:53 +0100

    feat: add a "Go to Dashboard" button to /s (#8301)

--- packages/app/src/app/pages/Sandbox/Editor/index.tsx
+++ packages/app/src/app/pages/Sandbox/Editor/index.tsx
@@ -276,6 +276,7 @@ export const Editor = ({ showNewSandboxModal }: EditorTypes) => {
                           width: 950,
                           position: 'relative',
                           marginTop: '-200px',
+                          borderRadius: '8px',
 
                           '@media screen and (max-width: 950)': {
                             width: 'auto',
