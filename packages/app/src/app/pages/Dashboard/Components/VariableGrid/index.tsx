c5078c6e927b44dda047c48e52fc8c50f9a24125
Alex Moldovan <alex.n.moldovan@gmail.com>
2024-04-08 08:14:18 +0000

    fix: lint

--- packages/app/src/app/pages/Dashboard/Components/VariableGrid/index.tsx
+++ packages/app/src/app/pages/Dashboard/Components/VariableGrid/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link as RouterLink, useLocation } from 'react-router-dom';
+import { Link as RouterLink } from 'react-router-dom';
 import { useAppState } from 'app/overmind';
 import { Element, Stack, Text, Link } from '@codesandbox/components';
 import css from '@styled-system/css';
