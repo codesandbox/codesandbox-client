99410ed0d97a06e68ecb93a928d8ed24ccf3bb3e
Alex Moldovan <alex.n.moldovan@gmail.com>
2024-04-15 13:00:21 +0000

    fix typecheck

--- packages/app/src/app/overmind/namespaces/sidebar/actions.ts
+++ packages/app/src/app/overmind/namespaces/sidebar/actions.ts
@@ -15,7 +15,7 @@ export const getSidebarData = async (
     const result = await queries.getTeamSidebarData({ id: teamId });
 
     const syncedSandboxes = result.me?.team?.syncedSandboxes || null;
-    const sandboxes = result.me?.team?.sandboxes || null;
+    const sandboxes = result.me?.team?.sandboxes || [];
     const templates = result.me?.team?.templates || null;
     const repositories =
       result.me?.team?.projects?.map(p => ({
