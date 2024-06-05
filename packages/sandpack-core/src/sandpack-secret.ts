a273bac84f7732619ecf87f6164bd93de252fbde
Ives van Hoorne <Ives.v.h@gmail.com>
2024-06-05 13:55:28 +0000

    fix: add partitioned to the removal

--- packages/sandpack-core/src/sandpack-secret.ts
+++ packages/sandpack-core/src/sandpack-secret.ts
@@ -9,7 +9,7 @@ export const getSandpackSecret = () =>
   );
 
 export const removeSandpackSecret = () => {
-  document.cookie = `${SANDPACK_SECRET_COOKIE_NAME}=; expires=Thu, 01 Jan 1970 00:00:01 GMT;samesite=none;secure;`;
+  document.cookie = `${SANDPACK_SECRET_COOKIE_NAME}=; expires=Thu, 01 Jan 1970 00:00:01 GMT;samesite=none;secure;partitioned`;
 };
 
 export const setSandpackSecret = (secret: string) => {
