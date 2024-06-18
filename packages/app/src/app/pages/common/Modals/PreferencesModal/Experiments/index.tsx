c3f80fa89d7b43dbb86b3e9ead97394d27e4a94c
Alex Moldovan <alex.n.moldovan@gmail.com>
2024-06-18 16:20:20 +0300

    feat: deprecate legacy editor usage (#8467)

--- packages/app/src/app/pages/common/Modals/PreferencesModal/Experiments/index.tsx
+++ packages/app/src/app/pages/common/Modals/PreferencesModal/Experiments/index.tsx
@@ -3,8 +3,6 @@ import React, { FunctionComponent } from 'react';
 
 import { SubContainer } from '../elements';
 
-import { BetaSandboxEditor } from './BetaSandboxEditor';
-
 export const Experiments: FunctionComponent = () => (
   <>
     <Text block marginBottom={6} size={4} weight="regular">
@@ -13,7 +11,9 @@ export const Experiments: FunctionComponent = () => (
 
     <SubContainer>
       <Element paddingTop={2}>
-        <BetaSandboxEditor />
+        <Text size={13} variant="muted">
+          No experiements available at the moment
+        </Text>
       </Element>
     </SubContainer>
   </>
