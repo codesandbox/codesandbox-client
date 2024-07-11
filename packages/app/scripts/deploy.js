8a0760cbc3dfca85a57dc3de72dc0b5914bb39b2
Joji Augustine <jojimail@gmail.com>
2024-07-11 13:46:23 +0200

    chore: move deployment to deployer apis

--- packages/app/scripts/deploy.js
+++ packages/app/scripts/deploy.js
@@ -1,20 +1,18 @@
 const fetch = require('cross-fetch');
 
 fetch(
-  `https://backstage.csbops.io/api/deploy/gitops/codesandbox/codesandbox-gitops/codesandbox-core/codesandbox/${process.env.ENVIRONMENT}/helm-chart-values/values.yaml`,
+  `https://deployer.csbops.io/apps/codesandbox-editor-v1`,
   {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
-      'CF-Access-Client-Id': process.env.CF_ZERO_TRUST_BACKSTAGE_DEPLOY_ID,
+      'CF-Access-Client-Id': process.env.CF_ZERO_TRUST_DEPLOYER_CLIENT_ID,
       'CF-Access-Client-Secret':
-        process.env.CF_ZERO_TRUST_BACKSTAGE_DEPLOY_TOKEN,
+        process.env.CF_ZERO_TRUST_DEPLOYER_CLIENT_SECRET,
+      'Authorization': `Bearer${process.env.DEPLOYER_API_TOKEN }`
     },
     body: JSON.stringify({
-      key: 'client.image.tag',
-      commitMessage: `Client V1: deploy to ${
-        process.env.ENVIRONMENT
-      } with ${process.env.CIRCLE_SHA1.substr(0, 7)}`,
+      environment: process.env.ENVIRONMENT
       value: process.env.CIRCLE_SHA1.substr(0, 7),
     }),
   }
