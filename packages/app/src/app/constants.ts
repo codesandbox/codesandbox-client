9a333703b1fa9d08340919f322f4bba1c5259e6a
Filipe Lima <45261568+filipelima18@users.noreply.github.com>
2024-11-08 10:18:31 +0000

    chore: update Pipeform URL on 'Enterprise' CTA of Upgrade page (#8654)

--- packages/app/src/app/constants.ts
+++ packages/app/src/app/constants.ts
@@ -2,7 +2,7 @@ export const DIALOG_TRANSITION_DURATION = 0.25;
 export const DIALOG_WIDTH = 420;
 export const REPLY_TRANSITION_DELAY = 0.5;
 export const ORGANIZATION_CONTACT_LINK =
-  'https://webforms.pipedrive.com/f/ckvSiEQynHckLBoq0wghZhoy9cQGYZrzaCwP7suEln3tMu5zgFKxY6sSZjTYLRC16X';
+  'https://webforms.pipedrive.com/f/72gS7iXoP8qjL8Ku9HZQcN5UKpUpZkgKRCjhbqREjCOYyBgzrCKCr7Mys5AyczOHBN';
 
 export const CSB_FRIENDS_LINK =
   'https://codesandbox.io/docs/learn/plans/codesandbox-friends';
