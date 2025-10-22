503017bced3c71307910a73adc67c769610036ed
tristandubbeld <tristan.dubbeld@gmail.com>
2025-10-22 16:47:56 +0200

    feat(sdk): udpate getting started section for SDK users

--- packages/app/src/app/pages/Dashboard/Content/routes/GetStarted/index.tsx
+++ packages/app/src/app/pages/Dashboard/Content/routes/GetStarted/index.tsx
@@ -2,20 +2,32 @@ import React from 'react';
 import { Helmet } from 'react-helmet';
 import { StyledContentWrapper } from 'app/pages/Dashboard/Components/shared/elements';
 import { Element } from '@codesandbox/components';
+import { useActiveTeamInfo } from 'app/hooks/useActiveTeamInfo';
 import { InstructionsRow } from './InstructionsRow';
 import { DocumentationRow } from './DocumentationRow';
+import { SDKRow } from './SDKRow';
 
 export const GetStarted = () => {
+  const { sdkWorkspace } = useActiveTeamInfo();
+
   return (
     <StyledContentWrapper>
       <Helmet>
         <title>Get started - CodeSandbox</title>
       </Helmet>
 
-      <Element css={{ paddingTop: '8px' }}>
-        <InstructionsRow />
-      </Element>
-      <DocumentationRow />
+      {sdkWorkspace ? (
+        <Element css={{ paddingTop: '8px' }}>
+          <SDKRow />
+        </Element>
+      ) : (
+        <>
+          <Element css={{ paddingTop: '8px' }}>
+            <InstructionsRow />
+          </Element>
+          <DocumentationRow />
+        </>
+      )}
     </StyledContentWrapper>
   );
 };
