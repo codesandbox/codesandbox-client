6417916226c77b0f6241520d0d07be83a077ab12
Tristan <tristandubbeld@users.noreply.github.com>
2026-03-02 12:15:28 +0100

    fix(auth): validate signin redirect URL to prevent open redirect (CSB-1109) (#8868)

--- packages/app/src/app/pages/SignIn/index.tsx
+++ packages/app/src/app/pages/SignIn/index.tsx
@@ -3,7 +3,10 @@ import { Redirect } from 'react-router-dom';
 import { css } from '@styled-system/css';
 
 import { useAppState, useActions } from 'app/overmind';
-import { dashboardUrl } from '@codesandbox/common/lib/utils/url-generator';
+import {
+  dashboardUrl,
+  isSafeRedirectPath,
+} from '@codesandbox/common/lib/utils/url-generator';
 import { Element, Stack, ThemeProvider } from '@codesandbox/components';
 
 import { createWorkspaceUrl } from '@codesandbox/common/lib/utils/url-generator/dashboard';
@@ -12,26 +15,21 @@ import { SignIn } from './SignIn';
 export const SignInPage = () => {
   const state = useAppState();
   const { genericPageMounted } = useActions();
+  const continueParam = new URL(location.href).searchParams.get('continue');
   const redirectAfterSignIn = state.newUserFirstWorkspaceId
     ? createWorkspaceUrl({
         workspaceId: state.newUserFirstWorkspaceId,
       })
-    : new URL(location.href).searchParams.get('continue');
+    : continueParam && isSafeRedirectPath(continueParam)
+      ? continueParam
+      : null;
 
   useEffect(() => {
     genericPageMounted();
   }, [genericPageMounted]);
 
   if (state.hasLogIn) {
-    // All post-sigin redirects are handled here
     if (redirectAfterSignIn) {
-      // Full redirect if https://
-      if (redirectAfterSignIn.startsWith('https')) {
-        window.location.replace(redirectAfterSignIn);
-
-        return null;
-      }
-
       return <Redirect to={redirectAfterSignIn} />;
     }
 
